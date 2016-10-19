28ec4dd8cbdd253fb73eeb9dd91c3ef553ccc5be
TomasVotruba <tomas.vot@gmail.com>
2016-10-19 23:32:30 +0200

    improve build scripts

--- build/symplify-split.sh
+++ build/symplify-split.sh
@@ -2,5 +2,6 @@
 git subsplit init git@github.com:symplify/symplify.git
 git subsplit publish --heads="master" --no-tags src/AutoServiceRegistration:git@github.com:Symplify/AutoServiceRegistration.git
 git subsplit publish --heads="master" --no-tags src/ModularDoctrineFilters:git@github.com:Symplify/ModularDoctrineFilters.git
+rm -rf .subsplit/
 
 # inspired by laravel: https://github.com/laravel/framework/blob/master/build/illuminate-split.sh
