627dfa57968c808f8a1bbca20e2ab929b467256e
TomasVotruba <tomas.vot@gmail.com>
2016-10-23 22:39:06 +0200

    [PHP7_Sculpin] move Console\Appliaction to ConsoleApplication

--- src/PHP7_Sculpin/tests/Console/ConsoleApplicationTest.php
+++ src/PHP7_Sculpin/tests/Console/ConsoleApplicationTest.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace Symplify\PHP7_Sculpin\Tests\Console;
 
 use PHPUnit\Framework\TestCase;
-use Symplify\PHP7_Sculpin\Console\Application;
+use Symplify\PHP7_Sculpin\Console\ConsoleApplication;
 
 final class ApplicationTest extends TestCase
 {
     public function test()
     {
-        $application = new Application();
+        $application = new ConsoleApplication();
         $this->assertSame('<info>Sculpin - Static Site Generator</info>', $application->getLongVersion());
     }
 }
