33cfa9b819537d1763aa357d919a23d18415023f
TomasVotruba <tomas.vot@gmail.com>
2016-11-09 23:52:48 +0100

    build: fix packages paths

--- build/symplify-split.sh
+++ build/symplify-split.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 git subsplit init git@github.com:symplify/symplify.git
 
-git subsplit publish --heads="master" --no-tags src/ActionAutowire:git@github.com:Symplify/ActionAutowire.git
-git subsplit publish --heads="master" --no-tags src/AutoServiceRegistration:git@github.com:Symplify/AutoServiceRegistration.git
-git subsplit publish --heads="master" --no-tags src/CodingStandard:git@github.com:Symplify/CodingStandard.git
-git subsplit publish --heads="master" --no-tags src/ControllerAutowire:git@github.com:Symplify/ControllerAutowire.git
-git subsplit publish --heads="master" --no-tags src/DefaultAutowire:git@github.com:Symplify/DefaultAutowire.git
-git subsplit publish --heads="master" --no-tags src/ModularDoctrineFilters:git@github.com:Symplify/ModularDoctrineFilters.git
-git subsplit publish --heads="master" --no-tags src/ModularRouting:git@github.com:Symplify/ModularRouting.git
-git subsplit publish --heads="master" --no-tags src/NetteAdapterForSymfonyBundles:git@github.com:Symplify/NetteAdapterForSymfonyBundles.git
-git subsplit publish --heads="master" --no-tags src/Statie:git@github.com:Symplify/Statie.git
+git subsplit publish --heads="master" --no-tags packages/ActionAutowire:git@github.com:Symplify/ActionAutowire.git
+git subsplit publish --heads="master" --no-tags packages/AutoServiceRegistration:git@github.com:Symplify/AutoServiceRegistration.git
+git subsplit publish --heads="master" --no-tags packages/CodingStandard:git@github.com:Symplify/CodingStandard.git
+git subsplit publish --heads="master" --no-tags packages/ControllerAutowire:git@github.com:Symplify/ControllerAutowire.git
+git subsplit publish --heads="master" --no-tags packages/DefaultAutowire:git@github.com:Symplify/DefaultAutowire.git
+git subsplit publish --heads="master" --no-tags packages/ModularDoctrineFilters:git@github.com:Symplify/ModularDoctrineFilters.git
+git subsplit publish --heads="master" --no-tags packages/ModularRouting:git@github.com:Symplify/ModularRouting.git
+git subsplit publish --heads="master" --no-tags packages/NetteAdapterForSymfonyBundles:git@github.com:Symplify/NetteAdapterForSymfonyBundles.git
+git subsplit publish --heads="master" --no-tags packages/Statie:git@github.com:Symplify/Statie.git
 
 rm -rf .subsplit/
 
