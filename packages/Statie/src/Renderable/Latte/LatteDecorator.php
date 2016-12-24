fcda0342283d925ef80f128e245ba4260fa4d68e
TomasVotruba <tomas.vot@gmail.com>
2016-12-24 18:43:21 +0100

    [Statie] fix layout removal

--- packages/Statie/src/Renderable/Latte/LatteDecorator.php
+++ packages/Statie/src/Renderable/Latte/LatteDecorator.php
@@ -68,7 +68,7 @@ final class LatteDecorator implements DecoratorInterface
 
         // 3. trim left-over {layout tag}, probably bug-fix
         if ($file instanceof PostFile) {
-            $htmlContent = preg_replace('/{layout [^)]+}/', '', $htmlContent);
+            $htmlContent = preg_replace('/{layout "[a-z]+"}/', '', $htmlContent);
         }
 
         $file->changeContent($htmlContent);
