6e57bd003eee8b53602806f731581d523037a904
TomasVotruba <tomas.vot@gmail.com>
2017-01-23 00:58:23 +0100

    [CodingStandard] tests - decouple bit of SniffRunner

--- packages/CodingStandard/tests/Sniffs/AbstractSniffTestCase.php
+++ packages/CodingStandard/tests/Sniffs/AbstractSniffTestCase.php
@@ -37,22 +37,18 @@ abstract class AbstractSniffTestCase extends TestCase
         foreach ($this->findFilesInDirectory($directory) as $file) {
             $this->errorDataCollector = $this->createErrorDataCollector();
             if (Strings::startsWith($file->getFilename(), 'correct')) {
-                $this->runSniffTestForCorrectFile($file);
+                $this->runSniffTestForCorrectFile($sniffClass, $file);
             }
 
             if (Strings::startsWith($file->getFilename(), 'wrong')) {
-                $this->runSniffTestForWrongFile($file);
+                $this->runSniffTestForWrongFile($sniffClass, $file);
             }
         }
     }
 
-    private function runSniffTestForCorrectFile(SplFileInfo $fileInfo) : void
+    private function runSniffTestForCorrectFile(string $sniffClass, SplFileInfo $fileInfo) : void
     {
-        $file = $this->createFileFromFilePath($fileInfo->getPathname());
-        $this->processFile($file);
-
-        $errorCount = $this->errorDataCollector->getErrorCount();
-
+        $errorCount = SniffRunner::getErrorCountForSniffInFile($sniffClass, $fileInfo);
         $this->assertSame(0, $errorCount, sprintf(
             'File "%s" should have at 0 errors. %s found.',
             $fileInfo->getPathname(),
@@ -60,20 +56,26 @@ abstract class AbstractSniffTestCase extends TestCase
         ));
     }
 
-    private function runSniffTestForWrongFile(SplFileInfo $fileInfo) : void
+    private function runSniffTestForWrongFile(string $sniffClass, SplFileInfo $fileInfo) : void
     {
-        $file = $this->createFileFromFilePath($fileInfo->getPathname());
-        $this->processFile($file);
+        $errorCount = SniffRunner::getErrorCountForSniffInFile($sniffClass, $fileInfo);
+        $this->assertSame(1, $errorCount, sprintf(
+            'File "%s" should have at least 1 error.',
+            $fileInfo->getPathname()
+        ));
 
-        $errorCount = $this->errorDataCollector->getErrorCount();
+        $fixedFileName = $this->getFixedFileName($fileInfo);
+        if (! file_exists($fixedFileName)) {
+            return;
+        }
 
-        $this->assertSame(
-            1,
-            $errorCount,
-            sprintf('File "%s" should have at least 1 error.', $fileInfo->getPathname())
-        );
-
-        $this->runSniffFixerTestIfPresent($fileInfo, $file);
+        $fixedContent = SniffRunner::getFixedContentForSniffInFile($sniffClass, $fileInfo);
+        $this->assertSame(file_get_contents($fixedFileName), $fixedContent, sprintf(
+            'File "%s" was not fixed properly. "%s" expected, "%s" given.',
+            $fileInfo->getPathname(),
+            $fixedContent,
+            file_get_contents($fixedFileName)
+        ));
     }
 
     /**
@@ -89,43 +91,17 @@ abstract class AbstractSniffTestCase extends TestCase
         return iterator_to_array($iterator);
     }
 
-    private function runSniffFixerTestIfPresent(SplFileInfo $file, File $processedFile) : void
+    private function getFixedFileName(SplFileInfo $fileInfo) : string
     {
-        $fixedFileName = $this->getFixedFileName($file);
-
-        if (! file_exists($fixedFileName)) {
-            return;
-        }
-
-        $fixedContent = $processedFile->fixer->getContents();
-        $this->assertSame(
-            file_get_contents($fixedFileName),
-            $fixedContent,
-            sprintf('File "%s" was not fixed properly.', $file->getPathname())
-        );
+        return dirname($fileInfo->getPathname()) . '/' . $fileInfo->getBasename('.php.inc') . '-fixed.php.inc';
     }
 
-    private function getFixedFileName(SplFileInfo $file) : string
+    private function createSniffDispatcherWithSniff(string $sniffClass) : SniffDispatcher
     {
-        return dirname($file->getPathname()) . '/' . $file->getBasename('.php.inc') . '-fixed.php.inc';
-    }
-
-    private function createSniffDispatcherWithSniff(string $sniffName) : SniffDispatcher
-    {
-        // legacy required
-        if (!defined('PHP_CODESNIFFER_VERBOSITY')) {
-            define('PHP_CODESNIFFER_VERBOSITY', 0);
-        }
-        // legacy required
-        (new Tokens);
-
-        if (class_exists($sniffName)) {
-            /** @var Sniff $sniff */
-            $sniff = new $sniffName;
-        }
+        $this->setupLegacy();
 
         $sniffDispatcher = new SniffDispatcher(new CurrentListenerSniffCodeProvider());
-        $sniffDispatcher->addSniffListeners([$sniffName => $sniff]);
+        $sniffDispatcher->addSniffListeners([new $sniffClass]);
 
         return $sniffDispatcher;
     }
@@ -150,13 +126,23 @@ abstract class AbstractSniffTestCase extends TestCase
         return $file;
     }
 
-    private function processFile(File $file) : void
+//    private function processFile(File $file) : void
+//    {
+//        foreach ($file->getTokens() as $stackPointer => $token) {
+//            $this->sniffDispatcher->dispatch(
+//                $token['code'],
+//                new CheckFileTokenEvent($file, $stackPointer)
+//            );
+//        }
+//    }
+
+    private function setupLegacy(): void
     {
-        foreach ($file->getTokens() as $stackPointer => $token) {
-            $this->sniffDispatcher->dispatch(
-                $token['code'],
-                new CheckFileTokenEvent($file, $stackPointer)
-            );
+// legacy required
+        if (!defined('PHP_CODESNIFFER_VERBOSITY')) {
+            define('PHP_CODESNIFFER_VERBOSITY', 0);
         }
+        // legacy required
+        (new Tokens);
     }
 }
