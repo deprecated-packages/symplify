437ddf9fd51ab241d1c9435158153a71d7f95adc
TomasVotruba <tomas.vot@gmail.com>
2017-01-30 23:48:24 +0100

    [PHP7_CodeSniffer] cleanup legacy, fix test directories

--- packages/CodingStandard/tests/Sniffs/NetteRefactoring/InjectToConstructorInjection/InjectToConstructorInjectionSniffTest.php
+++ packages/CodingStandard/tests/Sniffs/NetteRefactoring/InjectToConstructorInjection/InjectToConstructorInjectionSniffTest.php
@@ -2,10 +2,7 @@
 
 namespace Symplify\CodingStandard\Tests\Sniffs\NetteRefactoring\InjectToConstructorInjection;
 
-use PHPUnit\Framework\TestCase;
-use Symplify\CodingStandard\Tests\CodeSnifferRunner;
 use Symplify\CodingStandard\Tests\Sniffs\AbstractSniffTestCase;
-use Symplify\CodingStandard\Tests\Sniffs\SniffRunner;
 use SymplifyCodingStandard\Sniffs\NetteRefactoring\InjectToConstructorInjectionSniff;
 
 /**
@@ -18,25 +15,4 @@ final class InjectToConstructorInjectionSniffTest extends AbstractSniffTestCase
     {
         $this->runSniffTestForDirectory(InjectToConstructorInjectionSniff::class, __DIR__);
     }
-
-
-//    public function testDetection()
-//    {
-//        $this->codeSnifferRunner = new SniffRunner(InjectToConstructorInjectionSniff::class);
-
-        //        $this->assertSame(0, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct.php.inc'));
-//        $this->assertSame(0, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct2.php.inc'));
-//        $this->assertSame(0, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct3.php.inc'));
-//        $this->assertSame(1, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/wrong.php.inc'));
-//        $this->assertSame(1, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/wrong2.php.inc'));
-//    }
-
-//    public function testFixing()
-//    {
-//        $fixedContent = $this->codeSnifferRunner->getFixedContent(__DIR__ . '/wrong.php.inc');
-//        $this->assertSame(file_get_contents(__DIR__ . '/wrong-fixed.php.inc'), $fixedContent);
-
-//        $fixedContent = $this->codeSnifferRunner->getFixedContent(__DIR__ . '/wrong2.php.inc');
-//        $this->assertSame(file_get_contents(__DIR__ . '/wrong-fixed.php.inc'), $fixedContent);
-//    }
 }
