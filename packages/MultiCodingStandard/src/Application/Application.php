ba4faf4667ee03b1671122222bfd4e1f8424debb
TomasVotruba <tomas.vot@gmail.com>
2017-02-02 15:39:31 +0100

    [CodingStandard, MultiCodingStandard] update API

--- packages/MultiCodingStandard/src/Application/Application.php
+++ packages/MultiCodingStandard/src/Application/Application.php
@@ -6,15 +6,15 @@ use Symplify\MultiCodingStandard\Application\Command\RunApplicationCommand;
 use Symplify\MultiCodingStandard\PhpCsFixer\Application\Application as PhpCsFixerApplication;
 use Symplify\MultiCodingStandard\PhpCsFixer\Application\Command\RunApplicationCommand
     as PhpCsFixerRunApplicationCommand;
-use Symplify\PHP7_CodeSniffer\Application\Application as Php7CodeSnifferApplication;
-use Symplify\PHP7_CodeSniffer\Application\Command\RunApplicationCommand as Php7CodeSnifferRunApplicationCommand;
+use Symplify\SniffRunner\Application\Application as SniffRunnerApplication;
+use Symplify\SniffRunner\Application\Command\RunApplicationCommand as Php7CodeSnifferRunApplicationCommand;
 
 final class Application
 {
     /**
-     * @var Php7CodeSnifferApplication
+     * @var SniffRunnerApplication
      */
-    private $php7CodeSnifferApplication;
+    private $sniffRunnerApplication;
 
     /**
      * @var PhpCsFixerApplication
@@ -22,16 +22,16 @@ final class Application
     private $phpCsFixerApplication;
 
     public function __construct(
-        Php7CodeSnifferApplication $php7CodeSnifferApplication,
+        SniffRunnerApplication $php7CodeSnifferApplication,
         PhpCsFixerApplication $phpCsFixerApplication
     ) {
-        $this->php7CodeSnifferApplication = $php7CodeSnifferApplication;
+        $this->sniffRunnerApplication = $php7CodeSnifferApplication;
         $this->phpCsFixerApplication = $phpCsFixerApplication;
     }
 
     public function runCommand(RunApplicationCommand $command)
     {
-        $this->php7CodeSnifferApplication->runCommand(
+        $this->sniffRunnerApplication->runCommand(
             $this->createPhp7CodeSnifferRunApplicationCommand($command)
         );
 
@@ -40,11 +40,9 @@ final class Application
         );
     }
 
-    /**
-     * @return Php7CodeSnifferRunApplicationCommand
-     */
-    private function createPhp7CodeSnifferRunApplicationCommand(RunApplicationCommand $command)
-    {
+    private function createPhp7CodeSnifferRunApplicationCommand(
+        RunApplicationCommand $command
+    ): Php7CodeSnifferRunApplicationCommand {
         return new Php7CodeSnifferRunApplicationCommand(
             $command->getSource(),
             $command->getJsonConfiguration()['standards'] ?? [],
@@ -54,11 +52,9 @@ final class Application
         );
     }
 
-    /**
-     * @return PhpCsFixerRunApplicationCommand
-     */
-    private function createPhpCsFixerRunApplicationCommand(RunApplicationCommand $command)
-    {
+    private function createPhpCsFixerRunApplicationCommand(
+        RunApplicationCommand $command
+    ): PhpCsFixerRunApplicationCommand {
         return new PhpCsFixerRunApplicationCommand(
             $command->getSource(),
             $command->getJsonConfiguration()['fixer-levels'] ?? [],
