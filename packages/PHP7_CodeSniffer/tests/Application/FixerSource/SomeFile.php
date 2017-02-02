1f3bea348cda02048a547fce685d41838b6616e7
TomasVotruba <tomas.vot@gmail.com>
2017-02-02 14:35:08 +0100

    rename PHP7_CodeSniffer to SniffRunner

--- packages/PHP7_CodeSniffer/tests/Application/FixerSource/SomeFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Symplify\PHP7_CodeSniffer\Tests\Application\FixerSource;
-
-final class SomeFile
-{
-
-}
--- packages/PHP7_CodeSniffer/tests/File/Finder/SourceFinderSource/SomeFiles/SomeSource.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Symplify\PHP7_CodeSniffer\Tests\File\Finder\SourceFinderSource\SomeFiles;
-
-final class SomeSource
-{
-
-}
