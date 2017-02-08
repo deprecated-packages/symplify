0822151a81e74cbe1791416def6ca9b14fada460
TomasVotruba <tomas.vot@gmail.com>
2017-02-08 16:19:37 +0100

    [MultiCodingStandard] use php-cs-fixer 2.0

--- packages/MultiCodingStandard/bin/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-if (file_exists($autoload = __DIR__.'/../../../autoload.php')) {
-	require_once $autoload;
-} else {
-	require_once __DIR__.'/../vendor/autoload.php';
-}
