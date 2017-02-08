aa442b57876c7ea44285943916304d718f9225a6
TomasVotruba <tomas.vot@gmail.com>
2017-02-08 17:26:21 +0100

    [MultiCodingStandard] cleanup DiffDataCollector

--- packages/MultiCodingStandard/src/Console/Output/InfoMessagePrinter.php
+++ packages/MultiCodingStandard/src/Console/Output/InfoMessagePrinter.php
@@ -4,7 +4,6 @@ namespace Symplify\MultiCodingStandard\Console\Output;
 
 use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Output\Output;
-use Symplify\MultiCodingStandard\PhpCsFixer\Report\DiffDataCollector;
 use Symplify\SniffRunner\Report\ErrorDataCollector;
 
 final class InfoMessagePrinter
@@ -15,32 +14,21 @@ final class InfoMessagePrinter
     private $errorDataCollector;
 
     /**
-     * @var DiffDataCollector
-     */
-    private $diffDataCollector;
-
-    /**
      * @var Output
      */
     private $output;
 
     public function __construct(
         ErrorDataCollector $errorDataCollector,
-        DiffDataCollector $diffDataCollector,
         Output $output
     ) {
         $this->errorDataCollector = $errorDataCollector;
-        $this->diffDataCollector = $diffDataCollector;
         $this->output = $output;
     }
 
     public function hasSomeErrorMessages() : bool
     {
         if ($this->errorDataCollector->getErrorCount()) {
-            return true;
-        }
-
-        if (count($this->diffDataCollector->getDiffs())) {
             return true;
         }
 
@@ -57,48 +45,5 @@ final class InfoMessagePrinter
         // php-cs-fixer
 //        $diffs = $this->diffDataCollector->getDiffs();
 //        $this->printDiffs($diffs);
-    }
-
-    /**
-     * Used original code from PHP-CS-FIXER/CS/Console/FixCommand.php
-     * https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/185c1758aadc942956e84accf1b24be9e2609718/Symfony/CS/Console/Command/FixCommand.php#L431-L493.
-     */
-    private function printDiffs(array $diffs)
-    {
-        $fixerDetailLine = ' (<comment>%s</comment>)';
-
-        $i = 0;
-        foreach ($diffs as $file => $fixResult) {
-            $this->output->write(sprintf('%4d) %s', $i++, $file));
-
-            if ($fixerDetailLine) {
-                $this->output->write(sprintf($fixerDetailLine, implode(', ', $fixResult['appliedFixers'])));
-            }
-
-            $this->output->writeln('');
-            $this->output->writeln('<comment>      ---------- begin diff ----------</comment>');
-
-            $diff = implode(
-                PHP_EOL,
-                array_map(
-                    function ($string) {
-                        $string = preg_replace('/^(\+){3}/', '<info>+++</info>', $string);
-                        $string = preg_replace('/^(\+){1}/', '<info>+</info>', $string);
-                        $string = preg_replace('/^(\-){3}/', '<error>---</error>', $string);
-                        $string = preg_replace('/^(\-){1}/', '<error>-</error>', $string);
-                        $string = str_repeat(' ', 6).$string;
-
-                        return $string;
-                    },
-                    explode(PHP_EOL, OutputFormatter::escape($fixResult['diff']))
-                )
-            );
-
-            $this->output->writeln($diff);
-
-            $this->output->writeln('<comment>      ---------- end diff ----------</comment>');
-        }
-
-        $this->output->writeln('');
     }
 }
