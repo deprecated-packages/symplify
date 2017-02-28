e395093cc116aeeb13b5882a0c9661c95d701399
TomasVotruba <tomas.vot@gmail.com>
2017-02-28 18:14:34 +0100

    build: fix paths

--- build/subtree-split-master-and-last-tag.sh
+++ build/subtree-split-master-and-last-tag.sh
@@ -9,18 +9,19 @@ git subsplit publish --heads="master" --tags=$LAST_TAG packages/CodingStandard:g
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/ControllerAutowire:git@github.com:Symplify/ControllerAutowire.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/DefaultAutowire:git@github.com:Symplify/DefaultAutowire.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/ModularDoctrineFilters:git@github.com:Symplify/ModularDoctrineFilters.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/EasyCodingStandard:git@github.com:Symplify/EasyCodingStandard.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/ModularRouting:git@github.com:Symplify/ModularRouting.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/ServiceDefinitionDecorator:git@github.com:Symplify/ServiceDefinitionDecorator.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/Statie:git@github.com:Symplify/Statie.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/SymfonyEventDispatcher:git@github.com:Symplify/SymfonyEventDispatcher.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/SymfonySecurity:git@github.com:Symplify/SymfonySecurity.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/SymfonySecurityVoters:git@github.com:Symplify/SymfonySecurityVoters.git
 
 # Zenify
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/DoctrineBehaviors:git@github.com:Zenify/DoctrineBehaviors.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/DoctrineExtensionsTree:git@github.com:Zenify/DoctrineExtensionsTree.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/DoctrineFixtures:git@github.com:Zenify/DoctrineFixtures.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/DoctrineMigrations:git@github.com:Zenify/DoctrineMigrations.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/ModularLatteFilters:git@github.com:Zenify/ModularLatteFilters.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/DoctrineBehaviors:git@github.com:Symplify/DoctrineBehaviors.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/DoctrineExtensionsTree:git@github.com:Symplify/DoctrineExtensionsTree.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/DoctrineFixtures:git@github.com:Symplify/DoctrineFixtures.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/DoctrineMigrations:git@github.com:Symplify/DoctrineMigrations.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/ModularLatteFilters:git@github.com:Symplify/ModularLatteFilters.git
 
 rm -rf .subsplit/
 
