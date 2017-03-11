31a29e57f5feebce7c2231bb00a464f93ad0afee
TomasVotruba <tomas.vot@gmail.com>
2017-03-11 20:45:48 +0100

    [Statie] similar post init

---
+++ packages/Statie/src/Latte/Filter/SimilarPostsFilter.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace Symplify\Statie\Latte\Filter;
+
+final class SimilarPostsFilter
+{
+    // arguments post
+    // insert posts ocnfiguration
+    // find similar posts => x
+    // http://php.net/manual/en/function.similar-text.php
+}
