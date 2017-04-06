8bd5f4db52c8636dedd955ba1224fe1b39529f05
TomasVotruba <tomas.vot@gmail.com>
2017-04-06 13:32:37 +0200

    conflics resolution

--- packages/PackageBuilder/tests/Adapter/Nette/DI/DefinitionFinderTest.php
+++ packages/PackageBuilder/tests/Adapter/Nette/DI/DefinitionFinderTest.php
@@ -3,6 +3,7 @@
 namespace Symplify\PackageBuilder\Tests\Adapter\Nette\DI;
 
 use Nette\DI\ContainerBuilder;
+use Nette\DI\ServiceDefinition;
 use PHPUnit\Framework\TestCase;
 use stdClass;
 use Symplify\PackageBuilder\Adapter\Nette\DI\DefinitionFinder;
@@ -21,32 +22,21 @@ final class DefinitionFinderTest extends TestCase
 
     public function testAutowired(): void
     {
-<<<<<<< f3c277da3188a28d9e939b2b6dee9d677bc7fd8e
-        $definition = $this->containerBuilder->addDefinition('some')
-            ->setClass(stdClass::class);
-=======
         $definition = new ServiceDefinition;
         $definition->setClass(stdClass::class);
 
         $this->containerBuilder->addDefinition('some', $definition);
->>>>>>> cs fixes
 
         $this->assertSame($definition, DefinitionFinder::getByType($this->containerBuilder, stdClass::class));
     }
 
     public function testNonAutowired(): void
     {
-<<<<<<< f3c277da3188a28d9e939b2b6dee9d677bc7fd8e
-        $definition = $this->containerBuilder->addDefinition('some')
-            ->setClass(stdClass::class)
-            ->setAutowired(false);
-=======
         $definition = new ServiceDefinition;
         $definition->setClass(stdClass::class);
         $definition->setAutowired(false);
 
         $this->containerBuilder->addDefinition('some', $definition);
->>>>>>> cs fixes
 
         $this->assertSame($definition, DefinitionFinder::getByType($this->containerBuilder, stdClass::class));
     }
