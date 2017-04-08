c01dffc3c87d5bbf185f60b90605e2872000e673
TomasVotruba <tomas.vot@gmail.com>
2017-04-08 22:52:56 +0200

    test fixes, phpstan fixes, cs fixes

--- packages/SymbioticController/src/Application/PresenterFactory.php
+++ packages/SymbioticController/src/Application/PresenterFactory.php
@@ -2,29 +2,15 @@
 
 namespace Symplify\SymbioticController\Application;
 
-use Nette;
-use Nette\Application\InvalidPresenterException;
 use Nette\Application\IPresenter;
 use Nette\Application\IPresenterFactory;
 use Nette\Application\UI\Presenter;
 use Nette\DI\Container;
-use ReflectionClass;
+use Symplify\SymbioticController\Application\Routing\PresenterMapper;
+use Symplify\SymbioticController\Application\Validator\PresenterGuardian;
 
 final class PresenterFactory implements IPresenterFactory
 {
-    /**
-     * @var string
-     */
-    private const PRESENTER_NAME_PATTERN = '#^[a-zA-Z\x7f-\xff][a-zA-Z0-9\x7f-\xff:]*\z#';
-
-    /**
-     * @var string[][] of module => splited mask
-     */
-    private $mapping = [
-        '*' => ['', '*Module\\', '*Presenter'],
-        'Nette' => ['NetteModule\\', '*\\', '*Presenter'],
-    ];
-
     /**
      * @var string[]
      */
@@ -35,9 +21,24 @@ final class PresenterFactory implements IPresenterFactory
      */
     private $container;
 
-    public function __construct(Container $container)
-    {
+    /**
+     * @var PresenterMapper
+     */
+    private $presenterMapper;
+
+    /**
+     * @var PresenterGuardian
+     */
+    private $presenterGuardian;
+
+    public function __construct(
+        Container $container,
+        PresenterMapper $presenterMapper,
+        PresenterGuardian $presenterGuardian
+    ) {
         $this->container = $container;
+        $this->presenterMapper = $presenterMapper;
+        $this->presenterGuardian = $presenterGuardian;
     }
 
     /**
@@ -57,27 +58,21 @@ final class PresenterFactory implements IPresenterFactory
     }
 
     /**
-     * Generates and checks presenter class name.
-     * @param string $name presenter name
+     * @param string $presenterName
      */
-    public function getPresenterClass(&$name): string
+    public function getPresenterClass(&$presenterName): string
     {
-        if (isset($this->cache[$name])) {
-            return $this->cache[$name];
+        if (isset($this->cache[$presenterName])) {
+            return $this->cache[$presenterName];
         }
 
-        $this->ensurePresenterNameIsValid($name);
+        $this->presenterGuardian->ensurePresenterNameIsValid($presenterName);
 
-        $class = $this->formatPresenterClass($name);
-        if (! class_exists($class)) {
-            throw new InvalidPresenterException("Cannot load presenter '$name', class '$class' was not found.");
-        }
+        $presenterClass = $this->presenterMapper->detectPresenterClassFromPresenterName($presenterName);
 
-        $this->ensurePresenterClassIsNotAbstract($name, $class);
+        $this->ensurePresenterClassIsValid($presenterName, $presenterClass);
 
-        $this->cache[$name] = $class;
-
-        return $class;
+        return $this->cache[$presenterName] = $presenterClass;
     }
 
     /**
@@ -85,63 +80,12 @@ final class PresenterFactory implements IPresenterFactory
      */
     public function setMapping(array $mapping): void
     {
-        foreach ($mapping as $module => $mask) {
-            if (is_string($mask)) {
-                if (! preg_match('#^\\\\?([\w\\\\]*\\\\)?(\w*\*\w*?\\\\)?([\w\\\\]*\*\w*)\z#', $mask, $m)) {
-                    throw new Nette\InvalidStateException(sprintf(
-                        'Invalid mapping mask "%s".',
-                        $mask
-                    ));
-                }
-                $this->mapping[$module] = [$m[1], $m[2] ?: '*Module\\', $m[3]];
-            } elseif (is_array($mask) && count($mask) === 3) {
-                $this->mapping[$module] = [$mask[0] ? $mask[0] . '\\' : '', $mask[1] . '\\', $mask[2]];
-            } else {
-                throw new Nette\InvalidStateException(sprintf(
-                    'Invalid mapping mask "%s".',
-                    $mask
-                ));
-            }
-        }
+        $this->presenterMapper->setMapping($mapping);
     }
 
-    /**
-     * Formats presenter class name from its name.
-     */
-    public function formatPresenterClass(string $presenter): string
+    private function ensurePresenterClassIsValid(string $presenterName, string $presenterClass): void
     {
-        $parts = explode(':', $presenter);
-        $mapping = isset($parts[1], $this->mapping[$parts[0]])
-            ? $this->mapping[array_shift($parts)]
-            : $this->mapping['*'];
-
-        while ($part = array_shift($parts)) {
-            $mapping[0] .= str_replace('*', $part, $mapping[$parts ? 1 : 2]);
-        }
-
-        return $mapping[0];
-    }
-
-    private function ensurePresenterNameIsValid(string $name): void
-    {
-        if (!is_string($name) || !Nette\Utils\Strings::match($name, self::PRESENTER_NAME_PATTERN)) {
-            throw new InvalidPresenterException(sprintf(
-                'Presenter name must be alphanumeric string, "%s" is invalid.',
-                $name
-            ));
-        }
-    }
-
-    private function ensurePresenterClassIsNotAbstract(string $name, string $class): void
-    {
-        $reflection = new ReflectionClass($class);
-
-        if ($reflection->isAbstract()) {
-            throw new InvalidPresenterException(sprintf(
-                'Cannot load presenter "%s", class "%s" is abstract.',
-                $name,
-                $class
-            ));
-        }
+        $this->presenterGuardian->ensurePresenterClassExists($presenterName, $presenterClass);
+        $this->presenterGuardian->ensurePresenterClassIsNotAbstract($presenterName, $presenterClass);
     }
 }
