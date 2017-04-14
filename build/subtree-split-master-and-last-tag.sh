b9697e5c7ec862059239307b061cbafec60d5ceb
TomasVotruba <tomas.vot@gmail.com>
2017-04-14 22:23:30 +0200

    [subsplit] fix last tag, improve reference to inspiration

--- build/subtree-split-master-and-last-tag.sh
+++ build/subtree-split-master-and-last-tag.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git subsplit init git@github.com:symplify/symplify.git
 
-LAST_TAG=$(git tag -l | tail -n1);
+LAST_TAG=$(git tag -l  --sort=committerdate | tail -n1);
 
 # Symplify
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/AutoServiceRegistration:git@github.com:Symplify/AutoServiceRegistration.git
@@ -20,4 +20,5 @@ git subsplit publish --heads="master" --tags=$LAST_TAG packages/SymfonySecurityV
 
 rm -rf .subsplit/
 
-# inspired by laravel: https://github.com/laravel/framework/blob/master/build/illuminate-split-full.sh
+# inspired by laravel: https://github.com/laravel/framework/blob/5.4/build/illuminate-split-full.sh
+# they use SensioLabs now though: https://github.com/laravel/framework/pull/17048#issuecomment-269915319
