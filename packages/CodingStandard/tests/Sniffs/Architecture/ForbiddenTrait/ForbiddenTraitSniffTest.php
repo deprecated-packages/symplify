c0167bfe3ffc7f564c8ed5b5bc43c2ec4be2cd7c
TomasVotruba <tomas.vot@gmail.com>
2017-04-15 13:11:35 +0200

    [CodingStandard] add ForbiddenTraitSniff [ref #59]

---
+++ packages/CodingStandard/tests/Sniffs/Architecture/ForbiddenTrait/ForbiddenTraitSniffTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Sniffs\Classes\ForbiddenTrait;
+
+use Symplify\CodingStandard\Sniffs\Architecture\ForbiddenTraitSniff;
+use Symplify\CodingStandard\Tests\Sniffs\AbstractSniffTestCase;
+
+final class ForbiddenTraitSniffTest extends AbstractSniffTestCase
+{
+    public function test(): void
+    {
+        $this->runSniffTestForDirectory(ForbiddenTraitSniff::class, __DIR__);
+    }
+}
