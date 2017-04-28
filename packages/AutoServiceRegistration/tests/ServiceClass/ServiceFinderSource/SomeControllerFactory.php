b910657b9be0fd1247f0bc8a70c69e34ae5ae592
TomasVotruba <tomas.vot@gmail.com>
2017-04-28 18:12:10 +0200

    [AutoServiceRegistration] deprecated in favor of DunglasActionBundle and Symfony 3.3

--- packages/AutoServiceRegistration/tests/ServiceClass/ServiceFinderSource/SomeControllerFactory.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Symplify\AutoServiceRegistration\Tests\ServiceClass\ServiceFinderSource;
-
-class SomeControllerFactory
-{
-}
--- packages/AutoServiceRegistration/tests/ServiceClass/ServiceFinderSource/Tests/SomeController.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Symplify\AutoServiceRegistration\Tests\ServiceClass\ServiceFinderSource\Tests;
-
-class SomeController
-{
-}
