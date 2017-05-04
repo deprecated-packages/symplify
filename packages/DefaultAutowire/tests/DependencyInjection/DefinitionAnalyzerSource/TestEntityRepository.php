7669119e4e65f97883f14ced177649cb0c544b9d
TomasVotruba <tomas.vot@gmail.com>
2017-05-05 00:36:43 +0200

    [DefaultAutowire] drop for _dfeaults

--- packages/DefaultAutowire/tests/DependencyInjection/DefinitionAnalyzerSource/TestEntityRepository.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Symplify\DefaultAutowire\Tests\DependencyInjection\DefinitionAnalyzerSource;
-
-use Doctrine\ORM\EntityRepository;
-
-class TestEntityRepository extends EntityRepository
-{
-}
