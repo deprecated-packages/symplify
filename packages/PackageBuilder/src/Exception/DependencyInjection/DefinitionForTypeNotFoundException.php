3d1641bde6f0e34a48b914572e7529822e0738d6
TomasVotruba <tomas.vot@gmail.com>
2017-06-27 12:19:02 +0200

    cleanup dead code

--- packages/PackageBuilder/src/Exception/DependencyInjection/DefinitionForTypeNotFoundException.php
+++ packages/PackageBuilder/src/Exception/DependencyInjection/DefinitionForTypeNotFoundException.php
@@ -3,22 +3,7 @@
 namespace Symplify\PackageBuilder\Exception\DependencyInjection;
 
 use Exception;
-use Symfony\Component\Config\Loader\DelegatingLoader;
-use Symfony\Component\Config\Loader\LoaderResolver;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symplify\EasyCodingStandard\Configuration\Loader\NeonLoader;
 
 final class DefinitionForTypeNotFoundException extends Exception
 {
-    protected function getContainerLoader(ContainerInterface $container): DelegatingLoader
-    {
-        /** @var DelegatingLoader $delegationLoader */
-        $delegationLoader = parent::getContainerLoader($container);
-
-        /** @var LoaderResolver $resolver */
-        $resolver = $delegationLoader->getResolver();
-        $resolver->addLoader(new NeonLoader($container));
-
-        return $delegationLoader;
-    }
 }
