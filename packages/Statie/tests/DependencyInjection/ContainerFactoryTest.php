560bda021286ad53bd803bc51e19fe2e3bcef349
TomasVotruba <tomas.vot@gmail.com>
2017-07-04 13:06:47 +0200

    [Statie] add check for valid sections

--- packages/Statie/tests/DependencyInjection/ContainerFactoryTest.php
+++ packages/Statie/tests/DependencyInjection/ContainerFactoryTest.php
@@ -5,6 +5,7 @@ namespace Symplify\Statie\Tests\DependencyInjection;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\DependencyInjection\Container;
+use Symplify\PackageBuilder\Exception\Neon\InvalidSectionException;
 use Symplify\Statie\DependencyInjection\ContainerFactory;
 
 final class ContainerFactoryTest extends TestCase
@@ -17,5 +18,16 @@ final class ContainerFactoryTest extends TestCase
         $this->assertInstanceOf(Container::class, $container);
 
         $this->assertTrue($container->isCompiled());
+    }
+
+    public function testCreateWithConfigWithInvalidSections(): void
+    {
+        $this->expectException(InvalidSectionException::class);
+        $this->expectExceptionMessage(
+            'Invalid sections found: "invalid". Only "parameters", "includes", "services" are allowed.'
+        );
+
+        $containerFactory = new ContainerFactory;
+        $containerFactory->createWithConfig(__DIR__ . '/ContainerFactorySource/config-with-invalid-sections.neon');
     }
 }
