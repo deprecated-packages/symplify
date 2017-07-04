af3fd9e47f50b4dcbb9d5c3f216557c0d7860aee
Tomáš Votruba <info@tomasvotruba.cz>
2017-07-04 12:23:29 +0200

    [PackageBuilder] NeonLoader - validate allowed sections feature added (#223)
    * [PackageBuilder] add NeonLoader with section limits
    
    * fix phpstan

---
+++ packages/PackageBuilder/src/Exception/Neon/InvalidSectionException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Exception\Neon;
+
+use Exception;
+
+final class InvalidSectionException extends Exception
+{
+}
--- packages/PackageBuilder/tests/Neon/Loader/NeonLoaderTest.php
+++ packages/PackageBuilder/tests/Neon/Loader/NeonLoaderTest.php
@@ -4,6 +4,7 @@ namespace Symplify\PackageBuilder\Tests\Neon\Loader;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symplify\PackageBuilder\Exception\Neon\InvalidSectionException;
 use Symplify\PackageBuilder\Neon\Loader\NeonLoader;
 
 final class NeonLoaderTest extends TestCase
@@ -28,5 +29,24 @@ final class NeonLoaderTest extends TestCase
     {
         $this->neonLoader->load(__DIR__ . '/NeonLoaderSource/someConfig.neon');
         $this->assertSame('value', $this->containerBuilder->getParameter('key'));
+    }
+
+    public function testValidSections(): void
+    {
+        $this->neonLoader->load(
+            __DIR__ . '/NeonLoaderSource/configWithAllowedSections.neon',
+            ['parameters', 'includes', 'services', 'checkers']
+        );
+        $this->assertSame('value', $this->containerBuilder->getParameter('key'));
+    }
+
+    public function testInvalidSections(): void
+    {
+        $this->expectException(InvalidSectionException::class);
+        $this->expectExceptionMessage('Invalid sections found: "exclude". Only "parameters", "services" are allowed.');
+        $this->neonLoader->load(
+            __DIR__ . '/NeonLoaderSource/configWithUnknownSections.neon',
+            ['parameters', 'services']
+        );
     }
 }
