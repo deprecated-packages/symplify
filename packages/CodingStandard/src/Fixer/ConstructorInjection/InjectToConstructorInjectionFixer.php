53f6945b896502aef8c6ba376fdcb17c45dc3722
TomasVotruba <tomas.vot@gmail.com>
2017-07-09 00:42:27 +0200

    [CodingStandard] InjectToConstructorInjection constructor and @inject

--- packages/CodingStandard/src/Fixer/ConstructorInjection/InjectToConstructorInjectionFixer.php
+++ packages/CodingStandard/src/Fixer/ConstructorInjection/InjectToConstructorInjectionFixer.php
@@ -2,6 +2,8 @@
 
 namespace Symplify\CodingStandard\Fixer\ConstructorInjection;
 
+use Nette\PhpGenerator\Method;
+use Nette\Utils\Strings;
 use PhpCsFixer\DocBlock\DocBlock;
 use PhpCsFixer\Fixer\DefinedFixerInterface;
 use PhpCsFixer\FixerDefinition\CodeSample;
@@ -10,6 +12,7 @@ use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
 use PhpCsFixer\Tokenizer\Tokens;
 use PhpCsFixer\Tokenizer\TokensAnalyzer;
 use SplFileInfo;
+use Twig\Token;
 
 final class InjectToConstructorInjectionFixer implements DefinedFixerInterface
 {
@@ -57,7 +60,7 @@ public function injectValue(stdClass $stdClass)
         // 1. find annotation @inject
         // array_reverse($elements, true)
         foreach ($tokens as $index => $token) {
-            if (!$token->isGivenKind(T_DOC_COMMENT)) {
+            if (! $token->isGivenKind(T_DOC_COMMENT)) {
                 continue;
             }
 
@@ -80,10 +83,32 @@ public function injectValue(stdClass $stdClass)
                     break;
                 }
             }
+
+            // 3. add dependency to constructor
+            $propertyName = '';
+            for ($i = $index; ; ++$i) {
+                $token = $tokens[$i];
+                if ($token->isGivenKind(T_VARIABLE)) {
+                    $propertyName = ltrim($token->getContent(), '$');
+                    break;
+                }
+            }
+            $varAnnotation = $doc->getAnnotationsOfType('var')[0];
+            $propertyType = $varAnnotation->getTypes()[0];
+
+            $this->addConstructorMethod($tokens, $propertyType, $propertyName);
         }
 
         // 2. find method starting with inject*()
-//        dump($tokens);
+//        foreach ($tokens as $index => $token) {
+//            if (! $token->isGivenKind(T_FUNCTION)) {
+//                continue;
+//            }
+//
+//
+//            dump($token);
+//            die;
+//        }
     }
 
     public function isRisky(): bool
@@ -104,5 +129,49 @@ public function injectValue(stdClass $stdClass)
     public function supports(SplFileInfo $file): bool
     {
         return true;
+    }
+
+    private function addConstructorMethod(Tokens $tokens, string $propertyType, string $propertyName): void
+    {
+        $method = new Method('__construct');
+        $method->setVisibility('public');
+
+        $parameter = $method->addParameter($propertyName);
+        $parameter->setTypeHint($propertyType);
+        $method->setBody('$this->? = $?;', [$propertyName, $propertyName]);
+
+        // indent method code with 4 spaces
+        $methodCode = Strings::indent((string) $method, 1, '    ');
+
+        $constructorPosition = $this->getConstructorPosition($tokens);
+
+        $constructorTokens = Tokens::fromCode(sprintf('<?php class SomeClass { %s }', $methodCode));
+
+        $constructorTokens->clearRange(0, 5); // drop initial code, like php tag opening
+        $constructorTokens->clearRange(30, 31); // drop initial code, like php tag opening
+        $constructorTokens->clearEmptyTokens();
+
+        $tokens->insertAt($constructorPosition, $constructorTokens);
+    }
+
+    private function getConstructorPosition(Tokens $tokens): int
+    {
+        // 1. after last property
+        for ($index = count($tokens) - 1; $index > 1; --$index) {
+            $token = $tokens[$index];
+            if ($token->isGivenKind(T_VARIABLE)) {
+                $propertyEndSemicolonPosition = $tokens->getNextTokenOfKind($index, [';']);
+                return $propertyEndSemicolonPosition + 1;
+            }
+        }
+
+        // 2. before first method
+        foreach ($tokens as $index => $token) {
+            if ($token->isGivenKind(T_FUNCTION)) {
+                $methodStartPosition = $tokens->getPrevTokenOfKind($index, [T_PUBLIC, T_PRIVATE, T_PROTECTED, T_DOC_COMMENT]);
+                // @todo test
+                return $methodStartPosition - 1;
+            }
+        }
     }
 }
