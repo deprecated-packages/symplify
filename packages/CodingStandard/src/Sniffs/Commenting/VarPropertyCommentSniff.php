c07bd43b0d3f3694b636b7227f55c9955a117343
TomasVotruba <tomas.vot@gmail.com>
2017-07-10 16:38:55 +0200

    [CodingStandard] gradually deprecated VarPropertyCommentSniff

--- packages/CodingStandard/src/Sniffs/Commenting/VarPropertyCommentSniff.php
+++ packages/CodingStandard/src/Sniffs/Commenting/VarPropertyCommentSniff.php
@@ -6,7 +6,12 @@ use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use SlevomatCodingStandard\Helpers\AnnotationHelper;
 use SlevomatCodingStandard\Helpers\PropertyHelper;
+use SlevomatCodingStandard\Sniffs\TypeHints\TypeHintDeclarationSniff;
 
+/**
+ * @deprecated Will be removed in 3.0.
+ * Use @see \SlevomatCodingStandard\Sniffs\TypeHints\TypeHintDeclarationSniff instead.
+ */
 final class VarPropertyCommentSniff implements Sniff
 {
     /**
@@ -18,6 +23,15 @@ final class VarPropertyCommentSniff implements Sniff
      * @var string
      */
     private const ERROR_MESSAGE = 'Property should have docblock comment.';
+
+    public function __construct()
+    {
+        trigger_error(sprintf(
+            'Class "%s" was deprecated in favor of "%s" that performs the same check. Use it instead.',
+            self::class,
+            TypeHintDeclarationSniff::class
+        ), E_USER_DEPRECATED);
+    }
 
     /**
      * @return int[]
