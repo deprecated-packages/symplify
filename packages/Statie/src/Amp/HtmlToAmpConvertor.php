a4576ddf4b01603d1b9be7eeb5bfc8e46f4c3e7f
TomasVotruba <tomas.vot@gmail.com>
2017-07-11 23:58:18 +0200

    [Statie] fix relative paths for canonical urls

--- packages/Statie/src/Amp/HtmlToAmpConvertor.php
+++ packages/Statie/src/Amp/HtmlToAmpConvertor.php
@@ -40,7 +40,7 @@ final class HtmlToAmpConvertor
 
         $options = [
             'scope' => Scope::HTML_SCOPE,
-            'canonical_path' => $originalUrl,
+            'canonical_path' => $this->makeSurePathStartsWithSlash($originalUrl),
         ];
 
         $this->amp->loadHtml($html, $options);
@@ -60,5 +60,10 @@ final class HtmlToAmpConvertor
                 $url
             ));
         }
+    }
+
+    private function makeSurePathStartsWithSlash(string $originalUrl): string
+    {
+        return Strings::startsWith($originalUrl, '/') ? $originalUrl : '/' . $originalUrl;
     }
 }
