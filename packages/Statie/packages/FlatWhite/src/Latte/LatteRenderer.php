cc14971cfa6f98e742daed1e41735840feff10d4
TomasVotruba <tomas.vot@gmail.com>
2017-07-12 11:40:09 +0200

    [Statie] naming details

--- packages/Statie/packages/FlatWhite/src/Latte/LatteRenderer.php
+++ packages/Statie/packages/FlatWhite/src/Latte/LatteRenderer.php
@@ -36,32 +36,32 @@ final class LatteRenderer
     /**
      * @param mixed[] $parameters
      */
-    public function renderExcludingHighlightBlocks(string $templateFileContent, array $parameters): string
+    public function renderExcludingHighlightBlocks(string $content, array $parameters): string
     {
         $i = 0;
-        $blocksWithHighlightedContents = [];
+        $highlightedCodeBlocks = [];
 
-        $templateWithPlaceholders = Strings::replace(
-            $templateFileContent,
+        $contentWithPlaceholders = Strings::replace(
+            $content,
             self::MATCH_CODE_BLOCKS,
-            function (array $match) use (&$i, &$blocksWithHighlightedContents) {
-                $highlightedContents = $match[0];
+            function (array $match) use (&$i, &$highlightedCodeBlocks) {
+                $highlightedCodeBlock = $match[0];
                 $placeholder = self::PLACEHOLDER_PREFIX . ++$i;
-                $blocksWithHighlightedContents[$placeholder] = $highlightedContents;
+                $highlightedCodeBlocks[$placeholder] = $highlightedCodeBlock;
 
                 return $placeholder;
             }
         );
 
-        $parseTemplateWithPlaceholders = $this->latte->renderToString($templateWithPlaceholders, $parameters);
+        $renderedContentWithPlaceholders = $this->latte->renderToString($contentWithPlaceholders, $parameters);
 
         return Strings::replace(
-            $parseTemplateWithPlaceholders,
+            $renderedContentWithPlaceholders,
             self::MATCH_PLACEHOLDERS,
-            function (array $match) use ($blocksWithHighlightedContents) {
+            function (array $match) use ($highlightedCodeBlocks) {
                 $placeholder = $match[0];
 
-                return $blocksWithHighlightedContents[$placeholder];
+                return $highlightedCodeBlocks[$placeholder];
             }
         );
     }
