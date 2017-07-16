ebffd36efb46510a8100c0326df9a259bbaf8cea
TomasVotruba <tomas.vot@gmail.com>
2017-07-16 13:44:01 +0200

    fix phpstan

--- packages/CodingStandard/src/Sniffs/Namespaces/ClassNamesWithoutPreSlashSniff.php
+++ packages/CodingStandard/src/Sniffs/Namespaces/ClassNamesWithoutPreSlashSniff.php
@@ -5,6 +5,7 @@ namespace Symplify\CodingStandard\Sniffs\Namespaces;
 use DateTime;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
+use SlevomatCodingStandard\Sniffs\Namespaces\ReferenceUsedNamesOnlySniff;
 use SplFileInfo;
 use stdClass;
 use Throwable;
@@ -22,14 +23,6 @@ final class ClassNamesWithoutPreSlashSniff implements Sniff
         DateTime::class, stdClass::class, SplFileInfo::class, Throwable::class,
     ];
 
-    /**
-     * @return int[]
-     */
-    public function register(): array
-    {
-        return [T_NEW, T_INSTANCEOF];
-    }
-
     public function __construct()
     {
         trigger_error(sprintf(
@@ -37,6 +30,14 @@ final class ClassNamesWithoutPreSlashSniff implements Sniff
             self::class,
             ReferenceUsedNamesOnlySniff::class
         ), E_USER_DEPRECATED);
+    }
+
+    /**
+     * @return int[]
+     */
+    public function register(): array
+    {
+        return [T_NEW, T_INSTANCEOF];
     }
 
     /**
