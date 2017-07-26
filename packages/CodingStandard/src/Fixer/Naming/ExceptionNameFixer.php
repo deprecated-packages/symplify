8f399fcbcf35dca6273c2de0c7d888262f22839a
TomasVotruba <tomas.vot@gmail.com>
2017-07-26 16:39:53 +0200

    [EasyCodingStandard] add configuration to SniffSetExtractor

--- packages/CodingStandard/src/Fixer/Naming/ExceptionNameFixer.php
+++ packages/CodingStandard/src/Fixer/Naming/ExceptionNameFixer.php
@@ -65,7 +65,7 @@ class SomeClass extends Exception
                 continue;
             }
 
-            $classNamePosition = $tokens->getPrevMeaningfulToken($index);
+            $classNamePosition = (int) $tokens->getPrevMeaningfulToken($index);
             $classNameToken = $tokens[$classNamePosition];
             if (Strings::endsWith($classNameToken->getContent(), 'Exception')) {
                 continue;
