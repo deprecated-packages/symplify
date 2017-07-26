9150618f49f244a7815125f33fc034730fc030dd
TomasVotruba <tomas.vot@gmail.com>
2017-07-26 11:54:03 +0200

    [CodingStandard] init ExceptionNameFixer (mirror to Sniff one)

---
+++ packages/CodingStandard/src/Fixer/Naming/ExceptionNameFixer.php
@@ -0,0 +1,95 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Fixer\Naming;
+
+use Nette\Utils\Strings;
+use PhpCsFixer\Fixer\DefinedFixerInterface;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+use SplFileInfo;
+
+final class ExceptionNameFixer implements DefinedFixerInterface
+{
+    public function getName(): string
+    {
+        return self::class;
+    }
+
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition('Exception classes should have suffix "Exception".', [
+            new CodeSample(
+                '<?php
+class SomeClass extends Exception
+{
+}'),
+        ]);
+    }
+
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isAllTokenKindsFound([T_CLASS, T_EXTENDS, T_STRING]);
+    }
+
+    public function isRisky(): bool
+    {
+        return true;
+    }
+
+    public function supports(SplFileInfo $file): bool
+    {
+        return true;
+    }
+
+    /**
+     * Run before PSR4 fixer to fix-up file names if needed.
+     * @see Psr4Fixer
+     */
+    public function getPriority(): int
+    {
+        return -5;
+    }
+
+    public function fix(SplFileInfo $file, Tokens $tokens): void
+    {
+        foreach ($tokens as $index => $token) {
+            if (! $token->isGivenKind(T_EXTENDS)) {
+                continue;
+            }
+
+            if (! $this->isException($tokens, $index)) {
+                continue;
+            }
+
+            $classNamePosition = $tokens->getPrevMeaningfulToken($index);
+            $classNameToken = $tokens[$classNamePosition];
+            if (Strings::endsWith($classNameToken->getContent(), 'Exception')) {
+                continue;
+            }
+
+            $this->fixClassName($tokens, $classNamePosition, $classNameToken->getContent());
+        }
+    }
+
+    private function isException(Tokens $tokens, int $index): bool
+    {
+        $parentClassName = $this->getParentClassName($tokens, $index);
+        return Strings::endsWith($parentClassName, 'Exception');
+    }
+
+    private function getParentClassName(Tokens $tokens, int $index): string
+    {
+        $parentClassNamePosition = $tokens->getNextMeaningfulToken($index);
+        $parentClassNameToken = $tokens[$parentClassNamePosition];
+
+        return $parentClassNameToken->getContent();
+    }
+
+    private function fixClassName(Tokens $tokens, int $classNamePosition, string $oldClassName): void
+    {
+        $tokens[$classNamePosition] = new Token([T_STRING, $oldClassName . 'Exception']);
+    }
+}
---
+++ packages/CodingStandard/tests/Fixer/Naming/ExceptionNameFixer/Test.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Fixer\Naming\ExceptionNameFixer;
+
+use PhpCsFixer\Fixer\FixerInterface;
+use PhpCsFixer\Test\AbstractFixerTestCase;
+use Symplify\CodingStandard\Fixer\Naming\ExceptionNameFixer;
+
+final class Test extends AbstractFixerTestCase
+{
+    /**
+     * @dataProvider provideFixCases()
+     */
+    public function testFix(string $expected, string $input): void
+    {
+        $this->doTest($expected, $input);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function provideFixCases(): array
+    {
+        return [
+            [
+                file_get_contents(__DIR__ . '/fixed/fixed.php.inc'),
+                file_get_contents(__DIR__ . '/wrong/wrong.php.inc'),
+            ],
+        ];
+    }
+
+    protected function createFixer(): FixerInterface
+    {
+        return new ExceptionNameFixer;
+    }
+}
