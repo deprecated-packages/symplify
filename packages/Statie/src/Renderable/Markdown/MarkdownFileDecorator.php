deed253027fdfcd6d89dac17c241e5dd94fa6a1c
TomasVotruba <tomas.vot@gmail.com>
2017-08-02 22:52:47 +0200

    [Statie] drop Spatie/regex, not needed [closes #296]

--- packages/Statie/src/Renderable/Markdown/MarkdownFileDecorator.php
+++ packages/Statie/src/Renderable/Markdown/MarkdownFileDecorator.php
@@ -4,8 +4,6 @@ namespace Symplify\Statie\Renderable\Markdown;
 
 use Nette\Utils\Strings;
 use ParsedownExtra;
-use Spatie\Regex\MatchResult;
-use Spatie\Regex\Regex;
 use Symplify\Statie\Configuration\Configuration;
 use Symplify\Statie\Contract\Renderable\FileDecoratorInterface;
 use Symplify\Statie\Renderable\File\AbstractFile;
@@ -65,20 +63,20 @@ final class MarkdownFileDecorator implements FileDecoratorInterface
 
     private function decorateHeadlinesWithTocAnchors(string $htmlContent): string
     {
-        return Regex::replace('/<h([1-6])>(.*?)<\/h([1-6])>/', function (MatchResult $result) {
-            $headline = $result->group(2);
-            $headlineId = Strings::webalize($result->group(2));
+        return Strings::replace($htmlContent,'#<h([1-6])>(.*?)<\/h([1-6])>#', function ($result) {
+            $headline = $result[2];
+            $headlineId = Strings::webalize($result[2]);
             $iconLink = '<a class="anchor" href="#' . $headlineId . '" ' .
                 'aria-hidden="true"><span class="anchor-icon">#</span></a>';
 
             return sprintf(
                 '<h%s id="%s">' .
                 $iconLink . '%s</h%s>',
-                $result->group(1),
+                $result[1],
                 $headlineId,
                 $headline,
-                $result->group(1)
+                $result[1]
             );
-        }, $htmlContent)->result();
+        });
     }
 }
