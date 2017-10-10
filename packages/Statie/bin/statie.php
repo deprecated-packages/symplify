9bf0f54963e1a33731cdaab998776d92e3744439
TomasVotruba <tomas.vot@gmail.com>
2017-10-10 08:13:42 +0200

    CLI entry points improvements

--- packages/Statie/bin/statie.php
+++ packages/Statie/bin/statie.php
@@ -3,6 +3,7 @@
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symplify\PackageBuilder\Configuration\ConfigFilePathHelper;
+use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
 use Symplify\Statie\DependencyInjection\ContainerFactory;
 
 // performance boost
@@ -22,19 +23,27 @@ foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
     }
 }
 
-// 1. Detect configuration
-ConfigFilePathHelper::detectFromInput('statie', new ArgvInput);
 
-// 2. Build DI container
-$containerFactory = new ContainerFactory;
-$configFile = ConfigFilePathHelper::provide('statie', 'statie.neon');
-if ($configFile) {
-    $container = $containerFactory->createWithConfig($configFile);
-} else {
-    $container = $containerFactory->create();
+try {
+    // 1. Detect configuration
+    ConfigFilePathHelper::detectFromInput('statie', new ArgvInput);
+
+    // 2. Build DI container
+    $containerFactory = new ContainerFactory;
+    $configFile = ConfigFilePathHelper::provide('statie', 'statie.neon');
+
+    if ($configFile) {
+        $container = $containerFactory->createWithConfig($configFile);
+    } else {
+        $container = $containerFactory->create();
+    }
+
+    // 3. Run Console Application
+    /** @var Application $application */
+    $application = $container->get(Application::class);
+    exit($application->run());
+} catch (Throwable $throwable) {
+    $symfonyStyle = SymfonyStyleFactory::create();
+    $symfonyStyle->error($throwable->getMessage());
+    exit(1);
 }
-
-// 3. Run Console Application
-/** @var Application $application */
-$application = $container->get(Application::class);
-$application->run();
