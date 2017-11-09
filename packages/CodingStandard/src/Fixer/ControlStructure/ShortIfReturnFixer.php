f520bc5b3a3812781111026a692c516367238666
Martin Patera <mzstic@gmail.com>
2017-11-10 00:51:31 +0100

    SortIfReturnFixer - implement PR suggestions
    I'll squash commits before final PR

--- packages/CodingStandard/src/Fixer/ControlStructure/ShortIfReturnFixer.php
+++ packages/CodingStandard/src/Fixer/ControlStructure/ShortIfReturnFixer.php
@@ -1,216 +1,232 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace Symplify\CodingStandard\Fixer\ControlStructure;
 
 use PhpCsFixer\Fixer\DefinedFixerInterface;
 use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
+use SplFileInfo;
+use Symplify\CodingStandard\Exception\UnexpectedTokenException;
+use UnexpectedValueException;
 
 final class ShortIfReturnFixer implements DefinedFixerInterface
 {
-
-	public function getDefinition(): FixerDefinitionInterface
-	{
-		return new FixerDefinition(
-			'Short if return should be used.',
-			[
-				new CodeSample('<?php
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition(
+            'Short if return should be used.',
+            [
+                new CodeSample('<?php
 if ($value === true) {
     return true;
 }
-return false;'
-				),
-			]
-		);
-	}
+return false;'),
+            ]
+        );
+    }
 
-	/**
-	 * Check if the fixer is a candidate for given Tokens collection.
-	 *
-	 * Fixer is a candidate when the collection contains tokens that may be fixed
-	 * during fixer work. This could be considered as some kind of bloom filter.
-	 * When this method returns true then to the Tokens collection may or may not
-	 * need a fixing, but when this method returns false then the Tokens collection
-	 * need no fixing for sure.
-	 *
-	 * @param Tokens $tokens
-	 *
-	 * @return bool
-	 */
-	public function isCandidate(Tokens $tokens)
-	{
-		return $tokens->isAllTokenKindsFound([T_IF, T_RETURN, T_IS_IDENTICAL]);
-	}
+    /**
+     * Check if the fixer is a candidate for given Tokens collection.
+     *
+     * Fixer is a candidate when the collection contains tokens that may be fixed
+     * during fixer work. This could be considered as some kind of bloom filter.
+     * When this method returns true then to the Tokens collection may or may not
+     * need a fixing, but when this method returns false then the Tokens collection
+     * need no fixing for sure.
+     */
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isAllTokenKindsFound([T_IF, T_RETURN, T_IS_IDENTICAL]);
+    }
 
-	/**
-	 * Check if fixer is risky or not.
-	 *
-	 * Risky fixer could change code behavior!
-	 *
-	 * @return bool
-	 */
-	public function isRisky()
-	{
-		return false;
-	}
+    /**
+     * Check if fixer is risky or not.
+     *
+     * Risky fixer could change code behavior!
+     */
+    public function isRisky(): bool
+    {
+        return false;
+    }
 
-	/**
-	 * Fixes a file.
-	 *
-	 * @param \SplFileInfo $file A \SplFileInfo instance
-	 * @param Tokens $tokens Tokens collection
-	 */
-	public function fix(\SplFileInfo $file, Tokens $tokens)
-	{
-		foreach ($tokens as $index => $token) {
-			if (! $token->isGivenKind(T_IF)) {
-				continue;
-			}
-			$nextTokenId = $tokens->getNextNonWhitespace($index);
-			$nextToken = $tokens[$nextTokenId];
+    /**
+     * Fixes a file.
+     *
+     * @param \SplFileInfo $file A \SplFileInfo instance
+     * @param Tokens $tokens Tokens collection
+     */
+    public function fix(SplFileInfo $file, Tokens $tokens): void
+    {
+        foreach ($tokens as $index => $token) {
+            if (! $token->isGivenKind(T_IF)) {
+                continue;
+            }
 
-			// Try to look for condition surrounded by braces, mark its start and end position
-			if ($nextToken->getContent() !== '(') {
-				continue;
-			}
-			$conditionStart = $nextTokenId;
-			$conditionEnd = $this->getConditionEnd($tokens, $conditionStart);
+            $nextTokenId = $tokens->getNextNonWhitespace($index);
+            $nextToken = $tokens[$nextTokenId];
 
-			// Check condition body - it must consist only of "return true;"
-			$tokenId = $tokens->getNextNonWhitespace($conditionEnd);
-			$tokenId = $tokens->getNextNonWhitespace($tokenId);
-			$tokenId = $this->checkConditionBody($tokens, $tokenId);
-			if ($tokenId < 0) {
-				continue;
-			}
+            // Try to look for condition surrounded by braces, mark its start and end position
+            if ($nextToken->getContent() !== '(') {
+                continue;
+            }
 
-			// Check statement after condition - must be "return false;"
-			$tokenId = $this->checkStatementAfterCondition($tokens, $tokenId);
-			if ($tokenId < 0) {
-				continue;
-			}
-			$lastTokenId = $tokenId;
-			$shortReturnTokens = $this->createShortReturnTokens($tokens, $conditionStart, $conditionEnd);
-			$tokenId = $index;
-			while ($tokenId !== $lastTokenId) {
-				$tokens->clearAt($tokenId);
-				$tokenId++;
-			}
-			$tokens->clearAt($tokenId);
-			$tokens->clearEmptyTokens();
-			$tokens->insertAt($index, $shortReturnTokens);
+            $conditionStart = $nextTokenId;
 
-		}
-	}
+            try {
+                $conditionEnd = $this->getConditionEnd($tokens, $conditionStart);
+            } catch (UnexpectedValueException $e) {
+                continue;
+            }
 
-	/**
-	 * Returns the name of the fixer.
-	 *
-	 * The name must be all lowercase and without any spaces.
-	 *
-	 * @return string The name of the fixer
-	 */
-	public function getName()
-	{
-		return self::class;
-	}
+            // Get condition body end - it must consist only of "return true;"
+            $tokenId = $tokens->getNextNonWhitespace($conditionEnd);
 
-	/**
-	 * Returns the priority of the fixer.
-	 *
-	 * The default priority is 0 and higher priorities are executed first.
-	 *
-	 * @return int
-	 */
-	public function getPriority()
-	{
-		return 0;
-	}
+            try {
+                $tokenId = $this->getConditionBodyEnd($tokens, $tokenId);
+            } catch (UnexpectedTokenException $e) {
+                continue;
+            }
 
-	/**
-	 * Returns true if the file is supported by this fixer.
-	 *
-	 * @param \SplFileInfo $file
-	 *
-	 * @return bool true if the file is supported by this fixer, false otherwise
-	 */
-	public function supports(\SplFileInfo $file)
-	{
-		return true;
-	}
+            // Get statement after condition end - must be "return false;"
+            try {
+                $tokenId = $this->getStatementAfterConditionEnd($tokens, $tokenId);
+            } catch (UnexpectedTokenException $e) {
+                continue;
+            }
 
-	private function getConditionEnd(Tokens $tokens, $conditionStart)
-	{
-		$nextTokenId = $conditionStart;
-		$startingBracesCount = 1;
-		while ($startingBracesCount > 0) {
-			$nextTokenId = $tokens->getNextMeaningfulToken($nextTokenId);
-			$nextToken = $tokens[$nextTokenId];
-			if ($nextToken->getContent() === '(') {
-				$startingBracesCount++;
-			} elseif ($nextToken->getContent() === ')') {
-				$startingBracesCount--;
-			}
-		}
-		$conditionEnd = $tokens->getPrevMeaningfulToken($nextTokenId);
-		return $conditionEnd;
-	}
+            $lastTokenId = $tokenId;
+            $shortReturnTokens = $this->createShortReturnTokens($tokens, $conditionStart, $conditionEnd);
+            $tokenId = $index;
+            while ($tokenId !== $lastTokenId) {
+                $tokens->clearAt($tokenId);
+                ++$tokenId;
+            }
 
-	private function checkConditionBody(Tokens $tokens, $tokenId)
-	{
-		if ($tokens[$tokenId]->getContent() !== '{') {
-			return -1;
-		}
-		$tokenId = $tokens->getNextNonWhitespace($tokenId);
+            $tokens->clearAt($tokenId);
+            $tokens->clearEmptyTokens();
+            $tokens->insertAt($index, $shortReturnTokens);
+        }
+    }
 
-		if (!$tokens[$tokenId]->isGivenKind(T_RETURN)) {
-			return -1;
-		}
-		$tokenId = $tokens->getNextNonWhitespace($tokenId);
-		if (strtolower($tokens[$tokenId]->getContent()) !== 'true') {
-			return -1;
-		}
-		$tokenId = $tokens->getNextNonWhitespace($tokenId);
-		if ($tokens[$tokenId]->getContent() !== ";") {
-			return -1;
-		}
-		$tokenId = $tokens->getNextNonWhitespace($tokenId);
-		if ($tokens[$tokenId]->getContent() !== '}') {
-			return -1;
-		}
-		return $tokenId;
-	}
+    /**
+     * Returns the name of the fixer.
+     *
+     * The name must be all lowercase and without any spaces.
+     *
+     * @return string The name of the fixer
+     */
+    public function getName(): string
+    {
+        return self::class;
+    }
 
-	private function checkStatementAfterCondition(Tokens $tokens, $tokenId)
-	{
-		$tokenId = $tokens->getNextNonWhitespace($tokenId);
-		if (!$tokens[$tokenId]->isGivenKind(T_RETURN)) {
-			return -1;
-		}
-		$tokenId = $tokens->getNextNonWhitespace($tokenId);
-		if (strtolower($tokens[$tokenId]->getContent()) !== 'false') {
-			return -1;
-		}
-		$tokenId = $tokens->getNextNonWhitespace($tokenId);
-		if ($tokens[$tokenId]->getContent() !== ";") {
-			return -1;
-		}
-		return $tokenId;
-	}
+    /**
+     * Returns the priority of the fixer.
+     *
+     * The default priority is 0 and higher priorities are executed first.
+     */
+    public function getPriority(): int
+    {
+        return 0;
+    }
 
-	private function createShortReturnTokens(Tokens $tokens, $conditionStart, $conditionEnd)
-	{
-		$shortReturnTokens = [
-			new Token([T_RETURN, 'return']),
-			new Token([T_WHITESPACE, ' ']),
-		];
-		$tokenId = $tokens->getNonEmptySibling($conditionStart, 1);
-		while ($tokenId !== $conditionEnd) {
-			$shortReturnTokens[] = $tokens[$tokenId];
-			$tokenId = $tokens->getNonEmptySibling($tokenId, 1);
-		}
-		$shortReturnTokens[] = $tokens[$tokenId];
-		$shortReturnTokens[] = new Token(";");
-		return $shortReturnTokens;
-	}
+    /**
+     * Returns true if the file is supported by this fixer.
+     *
+     *
+     * @return bool true if the file is supported by this fixer, false otherwise
+     */
+    public function supports(SplFileInfo $file): bool
+    {
+        return true;
+    }
+
+    private function getConditionEnd(Tokens $tokens, int $conditionStart): int
+    {
+        return $tokens->findBlockEnd($tokens::BLOCK_TYPE_PARENTHESIS_BRACE, $conditionStart);
+    }
+
+    private function getConditionBodyEnd(Tokens $tokens, int $tokenId): int
+    {
+        if ($tokens[$tokenId]->getContent() !== '{') {
+            throw new UnexpectedTokenException;
+        }
+
+        $tokenId = $tokens->getNextNonWhitespace($tokenId);
+
+        if (! $tokens[$tokenId]->isGivenKind(T_RETURN)) {
+            throw new UnexpectedTokenException;
+        }
+
+        $tokenId = $tokens->getNextNonWhitespace($tokenId);
+        if (strtolower($tokens[$tokenId]->getContent()) !== 'true') {
+            throw new UnexpectedTokenException;
+        }
+
+        $tokenId = $tokens->getNextNonWhitespace($tokenId);
+        if ($tokens[$tokenId]->getContent() !== ';') {
+            throw new UnexpectedTokenException;
+        }
+
+        $tokenId = $tokens->getNextNonWhitespace($tokenId);
+        if ($tokens[$tokenId]->getContent() !== '}') {
+            throw new UnexpectedTokenException;
+        }
+
+        return $tokenId;
+    }
+
+    private function getStatementAfterConditionEnd(Tokens $tokens, int $tokenId): int
+    {
+        $tokenId = $tokens->getNextNonWhitespace($tokenId);
+        if (! $tokens[$tokenId]->isGivenKind(T_RETURN)) {
+            throw new UnexpectedTokenException;
+        }
+
+        $tokenId = $tokens->getNextNonWhitespace($tokenId);
+        if (strtolower($tokens[$tokenId]->getContent()) !== 'false') {
+            throw new UnexpectedTokenException;
+        }
+
+        $tokenId = $tokens->getNextNonWhitespace($tokenId);
+        if ($tokens[$tokenId]->getContent() !== ';') {
+            throw new UnexpectedTokenException;
+        }
+
+        return $tokenId;
+    }
+
+    /**
+     * @return Token[]
+     */
+    private function createShortReturnTokens(Tokens $tokens, int $conditionStart, int $conditionEnd): array
+    {
+        $shortReturnTokens = [
+            new Token([T_RETURN, 'return']),
+            new Token([T_WHITESPACE, ' ']),
+        ];
+        $shortReturnTokens = array_merge(
+            $shortReturnTokens,
+            $this->getConditionNonEmptyTokens($tokens, $conditionStart, $conditionEnd)
+        );
+        $shortReturnTokens[] = new Token(';');
+
+        return $shortReturnTokens;
+    }
+
+    /**
+     * @return Token[]
+     */
+    private function getConditionNonEmptyTokens(Tokens $tokens, int $conditionStart, int $conditionEnd): array
+    {
+        $conditionTokens = [];
+        $tokenId = $tokens->getNonEmptySibling($conditionStart, 1);
+        while ($tokenId !== $conditionEnd) {
+            $conditionTokens[] = $tokens[$tokenId];
+            $tokenId = $tokens->getNonEmptySibling($tokenId, 1);
+        }
+
+        return $conditionTokens;
+    }
 }
--- packages/CodingStandard/tests/Fixer/ControlStructure/ShortIfReturnFixer/Test.php
+++ packages/CodingStandard/tests/Fixer/ControlStructure/ShortIfReturnFixer/Test.php
@@ -8,29 +8,37 @@ use Symplify\CodingStandard\Fixer\ControlStructure\ShortIfReturnFixer;
 
 final class Test extends AbstractFixerTestCase
 {
-	/**
-	 * @dataProvider provideFixCases()
-	 */
-	public function testFix(string $expected, string $input): void
-	{
-		$this->doTest($expected, $input);
-	}
+    /**
+     * @dataProvider provideFixCases()
+     */
+    public function testFix(string $expected, string $input): void
+    {
+        $this->doTest($expected, $input);
+    }
 
-	/**
-	 * @return string[][]
-	 */
-	public function provideFixCases(): array
-	{
-		return [
-			[
-				file_get_contents(__DIR__ . '/fixed/fixed.php.inc'),
-				file_get_contents(__DIR__ . '/wrong/wrong.php.inc'),
-			],
-		];
-	}
+    /**
+     * @return string[][]
+     */
+    public function provideFixCases(): array
+    {
+        return [
+            [
+                file_get_contents(__DIR__ . '/fixed/fixed.php.inc'),
+                file_get_contents(__DIR__ . '/wrong/wrong.php.inc'),
+            ],
+            [
+                file_get_contents(__DIR__ . '/fixed/fixed2.php.inc'),
+                file_get_contents(__DIR__ . '/wrong/wrong2.php.inc'),
+            ],
+            [
+                file_get_contents(__DIR__ . '/fixed/fixed3.php.inc'),
+                file_get_contents(__DIR__ . '/wrong/wrong3.php.inc'),
+            ],
+        ];
+    }
 
-	protected function createFixer(): FixerInterface
-	{
-		return new ShortIfReturnFixer;
-	}
+    protected function createFixer(): FixerInterface
+    {
+        return new ShortIfReturnFixer;
+    }
 }
