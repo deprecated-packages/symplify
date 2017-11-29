40db781466a23afdc438a2b958d2f49ef77cd99d
TomasVotruba <tomas.vot@gmail.com>
2017-11-29 02:01:51 +0100

    init test

---
+++ packages/TokenRunner/tests/DocBlock/DocBlockSerializerFactoryTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\TokenRunner\Tests\DocBlock;
+
+use PHPUnit\Framework\TestCase;
+
+final class DocBlockSerializerFactoryTest extends TestCase
+{
+
+}
