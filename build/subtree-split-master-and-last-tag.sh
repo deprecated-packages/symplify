c69a5ce0d56a556f46b1189e9d77280d3c38f767
Jáchym Toušek <enumag@gmail.com>
2017-12-04 08:38:56 +0100

    Subsplit for the 2.5 branch

--- build/subtree-split-master-and-last-tag.sh
+++ build/subtree-split-master-and-last-tag.sh
@@ -4,10 +4,10 @@ git subsplit init git@github.com:symplify/symplify.git
 LAST_TAG=$(git tag -l  --sort=committerdate | tail -n1);
 
 # Symplify
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/CodingStandard:git@github.com:Symplify/CodingStandard.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/EasyCodingStandard:git@github.com:Symplify/EasyCodingStandard.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/PackageBuilder:git@github.com:Symplify/PackageBuilder.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/Statie:git@github.com:Symplify/Statie.git
+git subsplit publish --heads="2.5" --tags=$LAST_TAG packages/CodingStandard:git@github.com:Symplify/CodingStandard.git
+git subsplit publish --heads="2.5" --tags=$LAST_TAG packages/EasyCodingStandard:git@github.com:Symplify/EasyCodingStandard.git
+git subsplit publish --heads="2.5" --tags=$LAST_TAG packages/PackageBuilder:git@github.com:Symplify/PackageBuilder.git
+git subsplit publish --heads="2.5" --tags=$LAST_TAG packages/Statie:git@github.com:Symplify/Statie.git
 
 rm -rf .subsplit/
 
