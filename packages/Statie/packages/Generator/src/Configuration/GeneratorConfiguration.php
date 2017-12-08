0f22aaec89ae67f790bdc9c2502599922a05978b
TomasVotruba <tomas.vot@gmail.com>
2017-12-08 18:20:42 +0100

    GeneratorConfiguration: fix missing posts defaults

--- packages/Statie/packages/Generator/src/Configuration/GeneratorConfiguration.php
+++ packages/Statie/packages/Generator/src/Configuration/GeneratorConfiguration.php
@@ -4,6 +4,7 @@ namespace Symplify\Statie\Generator\Configuration;
 
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
 use Symplify\Statie\Configuration\Configuration;
+use Symplify\Statie\Renderable\File\PostFile;
 
 final class GeneratorConfiguration
 {
@@ -43,6 +44,7 @@ final class GeneratorConfiguration
         }
 
         $generators = (array) $this->parameterProvider->provideParameter(self::CONFIG_KEY);
+        $generators = $this->addPostDefaults($generators);
 
         $generatorElements = [];
 
@@ -67,5 +69,33 @@ final class GeneratorConfiguration
             $generatorConfiguration['path'];
 
         return $generatorConfiguration;
+    }
+
+    /**
+     * @param mixed[] $generators
+     * @return mixed[]
+     */
+    private function addPostDefaults(array $generators): array
+    {
+        if (isset($generators['posts'])) {
+            return $generators;
+        }
+
+        $generators['posts'] = [
+            # name of variable inside one elg, elenent (e,post)
+            'variable' => 'post',
+            # used global variable with all found items
+            'variable_global' => 'posts',
+            # directory, where to look for them
+            'path' => '_posts',
+            # which layout to use, a file from "_layouts/<name>.latte"
+            'layout' => 'post',
+            # and url prefix, e.g. /blog/some-post.md
+            'route_prefix' => 'blog',
+            # an object that will wrap it's logic, you can add helper methods into it and use it in templates
+            'object' => PostFile::class,
+        ];
+
+        return $generators;
     }
 }
