301228f1d3163dbce0cc2b9ac32608c2a238a5c7
TomasVotruba <tomas.vot@gmail.com>
2017-12-12 01:09:15 +0100

    FileFactory: drop Post

--- packages/Statie/src/Renderable/File/FileFactory.php
+++ packages/Statie/src/Renderable/File/FileFactory.php
@@ -2,7 +2,6 @@
 
 namespace Symplify\Statie\Renderable\File;
 
-use Nette\Utils\Strings;
 use SplFileInfo;
 use Symplify\Statie\Configuration\Configuration;
 
@@ -44,10 +43,6 @@ final class FileFactory
     {
         $relativeSource = substr($file->getPathname(), strlen($this->configuration->getSourceDirectory()));
         $relativeSource = ltrim($relativeSource, DIRECTORY_SEPARATOR);
-
-        if (Strings::contains($file->getPath(), DIRECTORY_SEPARATOR . '_posts')) {
-            return new PostFile($file, $relativeSource, $file->getPathname());
-        }
 
         return new File($file, $relativeSource, $file->getPathname());
     }
