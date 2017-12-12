71ffaea4ca1ff718a1cb4a6efc23a4deb3c89f7c
TomasVotruba <tomas.vot@gmail.com>
2017-12-12 01:08:25 +0100

    Configuration: deprecated and validate broken OPTION_GITHUB_REPOSITORY_SLUG

--- packages/Statie/src/Configuration/Configuration.php
+++ packages/Statie/src/Configuration/Configuration.php
@@ -2,7 +2,10 @@
 
 namespace Symplify\Statie\Configuration;
 
+use Nette\Utils\Strings;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use Symplify\Statie\Exception\Configuration\DeprecatedConfigException;
+use Symplify\Statie\Exception\Configuration\InvalidGithubRepositorySourceDirectoryException;
 use Symplify\Statie\Exception\Configuration\MissingGithubRepositorySlugException;
 use Symplify\Statie\FileSystem\FileSystemGuard;
 
@@ -11,12 +14,18 @@ final class Configuration
     /**
      * @var string
      */
-    public const OPTION_GITHUB_REPOSITORY_SLUG = 'github_repository_slug';
+    private const OPTION_MARKDOWN_HEADLINE_ANCHORS = 'markdown_headline_anchors';
 
     /**
      * @var string
      */
-    public const OPTION_MARKDOWN_HEADLINE_ANCHORS = 'markdown_headline_anchors';
+    private const OPTION_GITHUB_REPOSITORY_SOURCE_DIRECTORY = 'github_repository_source_directory';
+
+    /**
+     * @var string
+     * @deprecated use self::OPTION_GITHUB_REPOSITORY_SOURCE_DIRECTORY instead
+     */
+    private const OPTION_GITHUB_REPOSITORY_SLUG = 'github_repository_slug';
 
     /**
      * @var array
@@ -75,17 +84,24 @@ final class Configuration
         return getcwd() . DIRECTORY_SEPARATOR . 'source';
     }
 
-    public function getGithubRepositorySlug(): string
+    public function getGithubRepositorySourceDirectory(): string
     {
+        // BC compatibility
         if (isset($this->options[self::OPTION_GITHUB_REPOSITORY_SLUG])) {
-            return $this->options[self::OPTION_GITHUB_REPOSITORY_SLUG];
+            $githubRepositorySlug = $this->options[self::OPTION_GITHUB_REPOSITORY_SLUG];
+            $this->ensureStartsWithGithub($githubRepositorySlug);
+            return $githubRepositorySlug;
+        }
+
+        if (isset($this->options[self::OPTION_GITHUB_REPOSITORY_SOURCE_DIRECTORY])) {
+            return $this->options[self::OPTION_GITHUB_REPOSITORY_SOURCE_DIRECTORY];
         }
 
         throw new MissingGithubRepositorySlugException(sprintf(
             'Settings of "%s" is required for "{$post|githubEditPostUrl}" Latte filter. '
             . 'Add it to "statie.yml" under "parameters" section, e.g.: "%s".',
-            self::OPTION_GITHUB_REPOSITORY_SLUG,
-            'TomasVotruba/tomasvotruba.cz'
+            self::OPTION_GITHUB_REPOSITORY_SOURCE_DIRECTORY,
+            'https://github.com/TomasVotruba/tomasvotruba.cz/tree/master/source'
         ));
     }
 
@@ -136,5 +152,20 @@ final class Configuration
     public function isDryRun(): bool
     {
         return $this->isDryRun;
+    }
+
+    private function ensureStartsWithGithub(string $githubRepositorySlug): void
+    {
+        if (Strings::startsWith($githubRepositorySlug, 'https://github.com/')) {
+            return;
+        }
+
+        throw new InvalidGithubRepositorySourceDirectoryException(sprintf(
+            'Option "parameters > %s" should be in "%s" format, where <source> is directory where Statie content is.%s"%s" was given.',
+            self::OPTION_GITHUB_REPOSITORY_SLUG,
+            'https://github.com/<user>/<repository>/tree/master/<source>',
+            PHP_EOL . PHP_EOL,
+            $githubRepositorySlug
+        ));
     }
 }
