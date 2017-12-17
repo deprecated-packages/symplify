edbfdaf703cf9aa8ed4ba149d66a007f3e9fac61
TomasVotruba <tomas.vot@gmail.com>
2017-12-17 03:05:38 +0100

    fix namespace

--- packages/ChangelogLinker/tests/Worker/BracketsAroundReferencesWorker/BracketsAroundReferencesWorkerTest.php
+++ packages/ChangelogLinker/tests/Worker/BracketsAroundReferencesWorker/BracketsAroundReferencesWorkerTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symplify\ChangelogLinker\Tests\Worker\DiffLinksToVersionsWorker;
+namespace Symplify\ChangelogLinker\Tests\Worker\BracketsAroundReferencesWorker;
 
 use PHPUnit\Framework\TestCase;
 use Symplify\ChangelogLinker\ChangelogApplication;
