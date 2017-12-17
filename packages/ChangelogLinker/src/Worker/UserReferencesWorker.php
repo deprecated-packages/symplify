719d7c3d726d90d0e9aca96b2ec1084842d6dc22
TomasVotruba <tomas.vot@gmail.com>
2017-12-17 17:30:14 +0100

    add UserReferencesWorker

--- packages/ChangelogLinker/src/Worker/UserReferencesWorker.php
+++ packages/ChangelogLinker/src/Worker/UserReferencesWorker.php
@@ -2,6 +2,7 @@
 
 namespace Symplify\ChangelogLinker\Worker;
 
+use Nette\Utils\Strings;
 use Symplify\ChangelogLinker\Contract\Worker\WorkerInterface;
 
 /**
@@ -9,10 +10,37 @@ use Symplify\ChangelogLinker\Contract\Worker\WorkerInterface;
  */
 final class UserReferencesWorker implements WorkerInterface
 {
+    /**
+     * @var string[]
+     */
+    private $linksToPrepend = [];
+
     public function processContent(string $content, string $repositoryLink): string
     {
-        dump($content);
-        die;
-        // @todo
+        $linksToAppend = [];
+
+        $matches = Strings::matchAll($content, '#\[@(?<name>[a-z]+)\]#');
+        foreach ($matches as $match) {
+            if (isset($this->linksToPrepend[$match['name']])) {
+                continue;
+            }
+
+            $markdownUserLink = sprintf(
+                '[@%s]: https://github.com/%s',
+                $match['name'],
+                $match['name']
+            );
+
+            $linksToAppend[$match['name']] = $markdownUserLink;
+        }
+
+        if (! count($linksToAppend)) {
+            return $content;
+        }
+
+        rsort($linksToAppend);
+
+        // append new links to the file
+        return $content . implode(PHP_EOL, $linksToAppend) . PHP_EOL;
     }
 }
