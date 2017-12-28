549f0f75488a3097b543b35429def76fa045c3e9
TomasVotruba <tomas.vot@gmail.com>
2017-12-28 11:19:47 +0100

    phpstan fixes

--- packages/BetterReflectionDocBlock/tests/DocBlockSerializerFactoryTest.php
+++ packages/BetterReflectionDocBlock/tests/DocBlockSerializerFactoryTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symplify\TokenRunner\Tests\DocBlock;
+namespace Symplify\BetterReflectionDocBlock\Tests;
 
 use PhpCsFixer\WhitespacesFixerConfig;
 use phpDocumentor\Reflection\DocBlock;
