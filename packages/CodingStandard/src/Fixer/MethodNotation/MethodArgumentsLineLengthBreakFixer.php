4cd13b2dda68f05f02768e09e71294ae37bf56c5
TomasVotruba <tomas.vot@gmail.com>
2017-12-28 18:29:14 +0100

    [CodingStandard] MethodArgumentsLineLenthBreakFixer idea

---
+++ packages/CodingStandard/src/Fixer/MethodNotation/MethodArgumentsLineLengthBreakFixer.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Fixer\MethodNotation;
+
+use PhpCsFixer\Fixer\DefinedFixerInterface;
+use PhpCsFixer\Fixer\WhitespacesAwareFixerInterface;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+use PhpCsFixer\WhitespacesFixerConfig;
+use SplFileInfo;
+
+final class MethodArgumentsLineLengthBreakFixer implements DefinedFixerInterface, WhitespacesAwareFixerInterface
+{
+    /**
+     * @var int
+     */
+    private const LINE_LENGTH = 120;
+
+    /**
+     * @var WhitespacesFixerConfig
+     */
+    private $whitespacesFixerConfig;
+
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition('Arguments should be on the same/standalone line to fit line length.', [
+            new CodeSample(
+                    '<?php
+class SomeClass
+{
+    public function someMethod(SuperLongArguments $superLongArguments, AnotherSuperLongArguments $anotherSuperLongArguments)
+    {
+    }
+}'
+                ),
+        ]);
+    }
+
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isAllTokenKindsFound([T_FUNCTION, '?,']);
+    }
+
+    public function fix(SplFileInfo $file, Tokens $tokens): void
+    {
+        /** @var Token[] $reversedTokens */
+        $reversedTokens = array_reverse($tokens->toArray(), true);
+
+        foreach ($reversedTokens as $position => $token) {
+            if (! $token->isGivenKind(T_FUNCTION)) {
+                continue;
+            }
+
+            dump($token);
+            die;
+        }
+    }
+
+    public function isRisky(): bool
+    {
+        return false;
+    }
+
+    public function getName(): string
+    {
+        return self::class;
+    }
+
+    /**
+     * Maybe include indent fixer and run before
+     * it to delegate spaces indentation
+     */
+    public function getPriority(): int
+    {
+        return 0;
+    }
+
+    public function supports(SplFileInfo $file): bool
+    {
+        return true;
+    }
+
+    public function setWhitespacesConfig(WhitespacesFixerConfig $whitespacesFixerConfig): void
+    {
+        $this->whitespacesFixerConfig = $whitespacesFixerConfig;
+    }
+
+    private function fixMethod(Tokens $tokens): void
+    {
+    }
+}
---
+++ packages/CodingStandard/tests/Fixer/MethodNotation/MethodArgumentsLineLengthBreakFixer/MethodArgumentsLineLengthBreakFixerTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Fixer\ClassNotation\MethodArgumentsLineLengthBreakFixer;
+
+use PhpCsFixer\Fixer\FixerInterface;
+use Symplify\CodingStandard\Fixer\MethodNotation\MethodArgumentsLineLengthBreakFixer;
+use Symplify\TokenRunner\Testing\AbstractSimpleFixerTestCase;
+
+final class MethodArgumentsLineLengthBreakFixerTest extends AbstractSimpleFixerTestCase
+{
+    /**
+     * @dataProvider wrongToFixedCases()
+     */
+    public function test(string $wrongFile, string $fixedFile): void
+    {
+        $this->doTestWrongToFixedFile($wrongFile, $fixedFile);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function wrongToFixedCases(): array
+    {
+        return [
+            [__DIR__ . '/wrong/wrong.php.inc', __DIR__ . '/fixed/fixed.php.inc'],
+        ];
+    }
+
+    protected function createFixer(): FixerInterface
+    {
+        return new MethodArgumentsLineLengthBreakFixer();
+    }
+}
