2deaa426971c86b0c94f7e83dd7dc7977e1055e8
TomasVotruba <tomas.vot@gmail.com>
2018-01-06 00:20:24 +0100

    [Statie] deprecated PushToGithubCommand

--- packages/Statie/src/Console/Command/PushToGithubCommand.php
+++ packages/Statie/src/Console/Command/PushToGithubCommand.php
@@ -7,10 +7,14 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\Statie\Exception\Console\Command\GithubRepositoryNotFoundException;
 use Symplify\Statie\Exception\Console\Command\MissingGithubTokenException;
 use Symplify\Statie\Github\GithubPublishingProcess;
 
+/**
+ * @deprecated
+ */
 final class PushToGithubCommand extends Command
 {
     /**
@@ -21,14 +25,13 @@ final class PushToGithubCommand extends Command
     public function __construct(GithubPublishingProcess $githubPublishingProcess)
     {
         $this->githubPublishingProcess = $githubPublishingProcess;
-
         parent::__construct();
     }
 
     protected function configure(): void
     {
         $this->setName('push-to-github');
-        $this->setDescription('Push generated site to Github pages.');
+        $this->setDescription('[deprecated] Push generated site to Github pages.');
         $this->addArgument(
             'repository-slug',
             InputArgument::REQUIRED,
@@ -47,6 +50,12 @@ final class PushToGithubCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $output->writeln(
+            '<fg=white;bg=red>This command is deprecated and will be removed. Use approach described in docs ' .
+            'https://www.statie.org/docs/github-pages/ instead.</>'
+        );
+        sleep(3);
+
         $this->ensureInputIsValid($input);
 
         $githubRepository = $this->createGithubRepositoryUrlWithToken(
