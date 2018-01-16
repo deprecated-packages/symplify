5986fb5a59e00100ef50530c038fd142464acaef
TomasVotruba <tomas.vot@gmail.com>
2018-01-16 19:17:59 +0100

    [CodingStandard] init BreakMethodCallsFixer

---
+++ packages/TokenRunner/src/Wrapper/FixerWrapper/MethodCallWrapper.php
@@ -0,0 +1,155 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\TokenRunner\Wrapper\FixerWrapper;
+
+use Nette\Utils\Strings;
+use PhpCsFixer\Tokenizer\Tokens;
+use Symplify\TokenRunner\Guard\TokenTypeGuard;
+
+/**
+ * Wrapper around "$this->_someCall_()" token
+ */
+final class MethodCallWrapper
+{
+    /**
+     * @var Tokens
+     */
+    private $tokens;
+
+    /**
+     * @var int
+     */
+    private $index;
+
+    /**
+     * @var int
+     */
+    private $argumentsBracketStart;
+
+    /**
+     * @var int
+     */
+    private $argumentsBracketEnd;
+
+    private function __construct(Tokens $tokens, int $index)
+    {
+        TokenTypeGuard::ensureIsTokenType($tokens[$index], [T_STRING], __METHOD__);
+
+        $this->tokens = $tokens;
+        $this->index = $index;
+
+        $this->argumentsBracketStart = $this->tokens->getNextTokenOfKind($this->index, ['(']);
+        $this->argumentsBracketEnd = $this->tokens->findBlockEnd(
+            Tokens::BLOCK_TYPE_PARENTHESIS_BRACE,
+            $this->argumentsBracketStart
+        );
+    }
+
+    public static function createFromTokensAndPosition(Tokens $tokens, int $position): self
+    {
+        return new self($tokens, $position);
+    }
+
+    /**
+     * @return ArgumentWrapper[]
+     */
+    public function getArguments(): array
+    {
+        $argumentsBracketStart = $this->tokens->getNextTokenOfKind($this->index, ['(']);
+        $argumentsBracketEnd = $this->tokens->findBlockEnd(
+            Tokens::BLOCK_TYPE_PARENTHESIS_BRACE,
+            $argumentsBracketStart
+        );
+
+        if ($argumentsBracketStart === ($argumentsBracketEnd + 1)) {
+            return [];
+        }
+
+        $arguments = [];
+        for ($i = $argumentsBracketStart + 1; $i < $argumentsBracketEnd; ++$i) {
+            $token = $this->tokens[$i];
+
+            if ($token->isGivenKind(T_VARIABLE) === false) {
+                continue;
+            }
+
+            $arguments[] = ArgumentWrapper::createFromTokensAndPosition($this->tokens, $i);
+        }
+
+        return $arguments;
+    }
+
+    public function getFirstLineLength(): int
+    {
+        $lineLength = 0;
+
+        // compute from here to start of line
+        $currentPosition = $this->index;
+        while (! Strings::startsWith($this->tokens[$currentPosition]->getContent(), PHP_EOL)) {
+            $lineLength += strlen($this->tokens[$currentPosition]->getContent());
+            --$currentPosition;
+        }
+
+        $currentToken = $this->tokens[$currentPosition];
+
+        // includes indent in the beggining
+        // -1 = do not count PHP_EOL as character
+        $lineLength += strlen($currentToken->getContent()) - 2;
+
+        // compute from here to end of line
+        $currentPosition = $this->index + 1;
+        while (! Strings::startsWith($this->tokens[$currentPosition]->getContent(), PHP_EOL)) {
+            $lineLength += strlen($this->tokens[$currentPosition]->getContent());
+            ++$currentPosition;
+        }
+
+        return $lineLength;
+    }
+
+    public function getArgumentsBracketStart(): int
+    {
+        return $this->argumentsBracketStart;
+    }
+
+    public function getArgumentsBracketEnd(): int
+    {
+        return $this->argumentsBracketEnd;
+    }
+
+    public function getLineLengthToEndOfArguments(): int
+    {
+        $lineLength = 0;
+
+        // compute from function to start of line
+        $currentPosition = $this->index;
+        while (! Strings::startsWith($this->tokens[$currentPosition]->getContent(), PHP_EOL)) {
+            $lineLength += strlen($this->tokens[$currentPosition]->getContent());
+            --$currentPosition;
+        }
+
+        // get length from start of function till end of arguments - with spaces as one
+        $currentPosition = $this->index;
+        while ($currentPosition < $this->argumentsBracketEnd) {
+            $currentToken = $this->tokens[$currentPosition];
+            if ($currentToken->isGivenKind(T_WHITESPACE)) {
+                $lineLength += 1;
+                ++$currentPosition;
+                continue;
+            }
+
+            $lineLength += strlen($this->tokens[$currentPosition]->getContent());
+            ++$currentPosition;
+        }
+
+        // get length from end or arguments to first line break
+        $currentPosition = $this->argumentsBracketEnd;
+        while (! Strings::startsWith($this->tokens[$currentPosition]->getContent(), PHP_EOL)) {
+            $currentToken = $this->tokens[$currentPosition];
+
+            $lineLength += strlen($currentToken->getContent());
+            ++$currentPosition;
+        }
+
+        return $lineLength;
+    }
+}
