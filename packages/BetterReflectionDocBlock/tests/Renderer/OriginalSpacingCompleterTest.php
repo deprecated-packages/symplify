1f24a4ae635b3038127ec1aa7638cebad8d63f4d
TomasVotruba <tomas.vot@gmail.com>
2018-01-20 17:28:05 +0100

    add test case

---
+++ packages/BetterReflectionDocBlock/tests/Renderer/OriginalSpacingCompleterTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\BetterReflectionDocBlock\Tests\Renderer;
+
+use PHPUnit\Framework\TestCase;
+use Symplify\BetterReflectionDocBlock\Renderer\OriginalSpacingCompleter;
+
+final class OriginalSpacingCompleterTest extends TestCase
+{
+    /**
+     * @var OriginalSpacingCompleter
+     */
+    private $originalSpacingCompleter;
+
+    protected function setUp(): void
+    {
+        $this->originalSpacingCompleter = new OriginalSpacingCompleter();
+    }
+
+    /**
+     * @dataProvider provideNewAndOriginalContents()
+     */
+    public function test(string $originalInFile, string $messedInFile): void
+    {
+        $original = file_get_contents($originalInFile);
+        $messed = file_get_contents($messedInFile);
+
+        $fixed = $this->originalSpacingCompleter->completeTagSpaces($messed, $original);
+        $this->assertSame($original, trim($fixed));
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function provideNewAndOriginalContents(): array
+    {
+        return [
+            [
+                __DIR__ . '/OriginalSpacingCompleterSource/original/original1.txt',
+                __DIR__ . '/OriginalSpacingCompleterSource/messed/messed1.txt'
+            ],
+            [
+                __DIR__ . '/OriginalSpacingCompleterSource/original/original2.txt',
+                __DIR__ . '/OriginalSpacingCompleterSource/messed/messed2.txt'
+            ]
+        ];
+    }
+}
