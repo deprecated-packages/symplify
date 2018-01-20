0be5e6638cfdbcc4625c8b0355854b0c958c359c
TomasVotruba <tomas.vot@gmail.com>
2018-01-20 17:48:04 +0100

    build: order subsplit to push dependencies for other packages first

--- build/subtree-split-master-and-last-tag.sh
+++ build/subtree-split-master-and-last-tag.sh
@@ -3,14 +3,13 @@ git subsplit init git@github.com:symplify/symplify.git
 
 LAST_TAG=$(git tag -l  --sort=committerdate | tail -n1);
 
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/CodingStandard:git@github.com:Symplify/CodingStandard.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/EasyCodingStandard:git@github.com:Symplify/EasyCodingStandard.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/PackageBuilder:git@github.com:Symplify/PackageBuilder.git
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/Statie:git@github.com:Symplify/Statie.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/TokenRunner:git@github.com:Symplify/TokenRunner.git
-# 3.0
-git subsplit publish --heads="master" --tags=$LAST_TAG packages/ChangelogLinker:git@github.com:Symplify/ChangelogLinker.git
 git subsplit publish --heads="master" --tags=$LAST_TAG packages/BetterReflectionDocBlock:git@github.com:Symplify/BetterReflectionDocBlock.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/EasyCodingStandard:git@github.com:Symplify/EasyCodingStandard.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/CodingStandard:git@github.com:Symplify/CodingStandard.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/Statie:git@github.com:Symplify/Statie.git
+git subsplit publish --heads="master" --tags=$LAST_TAG packages/ChangelogLinker:git@github.com:Symplify/ChangelogLinker.git
 
 rm -rf .subsplit/
 
