7e5860d6efae224a21fa11f0da6eebcdbbde186d
TomasVotruba <tomas.vot@gmail.com>
2018-01-31 15:21:35 +0100

    make test fail

--- packages/Monorepo/tests/RepositoryToPackageMergerTest.php
+++ packages/Monorepo/tests/RepositoryToPackageMergerTest.php
@@ -2,15 +2,18 @@
 
 namespace Symplify\Monorepo\Tests;
 
-use GitWrapper\GitWorkingCopy;
+use GitWrapper\GitWrapper;
+use Nette\Utils\FileSystem;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symplify\Monorepo\RepositoryToPackageMerger;
 
 final class RepositoryToPackageMergerTest extends AbstractContainerAwareTestCase
 {
+    const TEMP_MONOREPO_DIRECTORY = __DIR__ . '/RepositoryToPackageMergerSource/TempRepository';
     /**
-     * @var GitWorkingCopy
+     * @var GitWrapper
      */
-    private $gitWorkingCopy;
+    private $gitWrapper;
 
     /**
      * @var RepositoryToPackageMerger
@@ -19,19 +22,30 @@ final class RepositoryToPackageMergerTest extends AbstractContainerAwareTestCase
 
     protected function setUp(): void
     {
-        $this->gitWorkingCopy = $this->container->get(GitWorkingCopy::class);
+        $this->gitWrapper = $this->container->get(GitWrapper::class);
         $this->repositoryToPackageMerger = $this->container->get(RepositoryToPackageMerger::class);
+
+        /** @var OutputInterface $output */
+        $output = $this->container->get(OutputInterface::class);
+        $output->setVerbosity(OutputInterface::VERBOSITY_QUIET);
     }
 
+    /**
+     * @doesNotPerformAssertions
+     */
     public function test(): void
     {
-        $monorepositoryDirectory = __DIR__ . '/RepositoryToPackageMergerSource';
-
-        $this->gitWorkingCopy->($monorepositoryDirectory);
+        $this->gitWrapper->init(self::TEMP_MONOREPO_DIRECTORY);
 
         $this->repositoryToPackageMerger->mergeRepositoryToPackage(
-
+            'git@github.com:shopsys/product-feed-zbozi.git',
+            self::TEMP_MONOREPO_DIRECTORY,
+            'packages/ProductFeed'
         );
     }
 
+    protected function tearDown(): void
+    {
+        FileSystem::delete(self::TEMP_MONOREPO_DIRECTORY);
+    }
 }
