f8ee455da9bf5b33cf24911c1cd9277f31b253b7
TomasVotruba <tomas.vot@gmail.com>
2018-02-02 10:01:21 +0100

    Monorepo: add Miro test case

--- packages/Monorepo/tests/RepositoryToPackageMergerTest.php
+++ packages/Monorepo/tests/RepositoryToPackageMergerTest.php
@@ -47,4 +47,30 @@ final class RepositoryToPackageMergerTest extends AbstractContainerAwareTestCase
         $this->assertDirectoryNotExists(self::TEMP_MONOREPO_DIRECTORY . '/src');
         $this->assertDirectoryExists(self::TEMP_MONOREPO_DIRECTORY . '/packages/Monorepo/src');
     }
+
+    /**
+     * Covers https://github.com/Symplify/Symplify/issues/617
+     */
+    public function testRemote(): void
+    {
+        $this->gitWrapper->init(self::TEMP_MONOREPO_DIRECTORY);
+
+        $this->repositoryToPackageMerger->mergeRepositoryToPackage(
+            'https://github.com/shopsys/product-feed-zbozi-copy.git',
+            self::TEMP_MONOREPO_DIRECTORY,
+            'packages/product-feed-zbozi-copy'
+        );
+
+        $this->assertDirectoryNotExists(self::TEMP_MONOREPO_DIRECTORY . '/src');
+        $this->assertDirectoryExists(self::TEMP_MONOREPO_DIRECTORY . '/packages/product-feed-zbozi-copy/src');
+
+        $this->repositoryToPackageMerger->mergeRepositoryToPackage(
+            'https://github.com/shopsys/product-feed-google-copy.git',
+            self::TEMP_MONOREPO_DIRECTORY,
+            'packages/product-feed-google-copy'
+        );
+
+        $this->assertDirectoryNotExists(self::TEMP_MONOREPO_DIRECTORY . '/src');
+        $this->assertDirectoryExists(self::TEMP_MONOREPO_DIRECTORY . '/packages/product-feed-google-copy/src');
+    }
 }
