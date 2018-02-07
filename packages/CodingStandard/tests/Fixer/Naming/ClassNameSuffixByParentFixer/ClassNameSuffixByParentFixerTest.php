0ae88e00fa6c9929a80b89f295c416b37d4bbda6
TomasVotruba <tomas.vot@gmail.com>
2018-02-07 15:08:51 +0100

    add test case

--- packages/CodingStandard/tests/Fixer/Naming/ClassNameSuffixByParentFixer/ClassNameSuffixByParentFixerTest.php
+++ packages/CodingStandard/tests/Fixer/Naming/ClassNameSuffixByParentFixer/ClassNameSuffixByParentFixerTest.php
@@ -24,14 +24,12 @@ final class ClassNameSuffixByParentFixerTest extends AbstractSimpleFixerTestCase
         return [
             [__DIR__ . '/wrong/wrong.php.inc', __DIR__ . '/fixed/fixed.php.inc'],
             [__DIR__ . '/wrong/wrong2.php.inc', __DIR__ . '/fixed/fixed2.php.inc'],
+            [__DIR__ . '/wrong/wrong3.php.inc', __DIR__ . '/fixed/fixed3.php.inc'],
         ];
     }
 
     protected function createFixer(): FixerInterface
     {
-        $fixer = new ClassNameSuffixByParentFixer();
-        $fixer->configure([]);
-
-        return $fixer;
+        return new ClassNameSuffixByParentFixer();
     }
 }
