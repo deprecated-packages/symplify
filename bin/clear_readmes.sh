7d1bdec5c64b7106e11556801d306ca7c7e4e690
TomasVotruba <tomas.vot@gmail.com>
2018-02-26 00:15:47 +0100

    bin/clear_readmes: apply on any *.md

--- bin/clear_readmes.sh
+++ bin/clear_readmes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # trailing whitespaces
-sed -i -E 's/\s+$//g' packages/*/README.md
+sed -i -E 's/\s+$//g' packages/*/README.md *.md
 
 # remove extra lines
-sed -i -E ':a;$!N;s/^\s*\n\s*$//;ta;P;D' packages/*/README.md
+sed -i -E ':a;$!N;s/^\s*\n\s*$//;ta;P;D' packages/*/README.md *.md
