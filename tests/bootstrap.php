54a26b225ec2581da365abd9560d43303df86e8a
TomasVotruba <tomas.vot@gmail.com>
2018-03-09 19:24:34 +0100

    tests: fix setup for tests with PHP_CodeSniffer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,12 @@
 <?php declare(strict_types=1);
 
+use PHP_CodeSniffer\Util\Tokens;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
+
+// required for PHP_CodeSniffer in packages/EasyCodingStandard/tests/*
+if (! defined('PHP_CODESNIFFER_VERBOSITY')) {
+    define('PHP_CODESNIFFER_VERBOSITY', 0);
+    new Tokens();
+}
