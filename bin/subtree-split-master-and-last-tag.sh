fd8b41c8fa8537ec3cd8ad6227ffb60aa75381dd
TomasVotruba <tomas.vot@gmail.com>
2018-03-12 13:53:56 +0100

    split: move CodingStandard up to allow ECS install it

--- bin/subtree-split-master-and-last-tag.sh
+++ bin/subtree-split-master-and-last-tag.sh
@@ -7,8 +7,8 @@ HEADS="$(git branch | grep \* | cut -d ' ' -f2)"
 git subsplit publish --heads=$HEADS --tags=$LAST_TAG packages/PackageBuilder:git@github.com:Symplify/PackageBuilder.git
 git subsplit publish --heads=$HEADS --tags=$LAST_TAG packages/TokenRunner:git@github.com:Symplify/TokenRunner.git
 git subsplit publish --heads=$HEADS --tags=$LAST_TAG packages/BetterReflectionDocBlock:git@github.com:Symplify/BetterReflectionDocBlock.git
-git subsplit publish --heads=$HEADS --tags=$LAST_TAG packages/EasyCodingStandard:git@github.com:Symplify/EasyCodingStandard.git
 git subsplit publish --heads=$HEADS --tags=$LAST_TAG packages/CodingStandard:git@github.com:Symplify/CodingStandard.git
+git subsplit publish --heads=$HEADS --tags=$LAST_TAG packages/EasyCodingStandard:git@github.com:Symplify/EasyCodingStandard.git
 git subsplit publish --heads=$HEADS --tags=$LAST_TAG packages/Statie:git@github.com:Symplify/Statie.git
 git subsplit publish --heads=$HEADS --tags=$LAST_TAG packages/ChangelogLinker:git@github.com:Symplify/ChangelogLinker.git
 
