d1a1b234fe9f0063d5fb00850191a69f157e9814
TomasVotruba <tomas.vot@gmail.com>
2018-03-15 21:16:07 +0100

    [phpunit] add FixedSameStringsConstraint temporary

--- packages/CodingStandard/tests/bootstrap.php
+++ packages/CodingStandard/tests/bootstrap.php
@@ -1,4 +1,10 @@
 <?php declare(strict_types=1);
 
+use PhpCsFixer\Tests\Test\Constraint\SameStringsConstraint;
+use Symplify\CodingStandard\Tests\PHPUnit\Constraint\FixedSameStringsConstraint;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
+
+// fix from https://github.com/FriendsOfPHP/PHP-CS-Fixer/pull/3592
+class_alias(FixedSameStringsConstraint::class, SameStringsConstraint::class);
