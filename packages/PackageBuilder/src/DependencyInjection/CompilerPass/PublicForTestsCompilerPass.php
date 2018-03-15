617d81efa622f71e2ac6427170c8aea4c3310e23
TomasVotruba <tomas.vot@gmail.com>
2018-03-15 13:26:52 +0100

    [PackageBuilder] add PublicForTestsCompilerPass

---
+++ packages/PackageBuilder/src/DependencyInjection/CompilerPass/PublicForTestsCompilerPass.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Make services public for tests only.
+ */
+final class PublicForTestsCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $containerBuilder): void
+    {
+        if (! $this->isPHPUnit()) {
+            return;
+        }
+
+        foreach ($containerBuilder->getDefinitions() as $definition) {
+            $definition->setPublic(true);
+        }
+    }
+
+    private function isPHPUnit(): bool
+    {
+        // defined by PHPUnit
+        return defined('PHPUNIT_COMPOSER_INSTALL') || defined('__PHPUNIT_PHAR__');
+    }
+}
