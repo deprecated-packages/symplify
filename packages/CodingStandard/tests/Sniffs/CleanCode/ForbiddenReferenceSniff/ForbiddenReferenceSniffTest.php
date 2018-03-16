43aa1f6bfb11462613a73c34361c09f1ee207795
TomasVotruba <tomas.vot@gmail.com>
2018-03-16 02:45:39 +0100

    fix namespace

--- packages/CodingStandard/tests/Sniffs/CleanCode/ForbiddenReferenceSniff/ForbiddenReferenceSniffTest.php
+++ packages/CodingStandard/tests/Sniffs/CleanCode/ForbiddenReferenceSniff/ForbiddenReferenceSniffTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symplify\CodingStandard\Tests\Sniffs\Commenting\ForbiddenReferenceSniff;
+namespace Symplify\CodingStandard\Tests\Sniffs\CleanCode\ForbiddenReferenceSniff;
 
 use PHP_CodeSniffer\Sniffs\Sniff;
 use Symplify\CodingStandard\Sniffs\CleanCode\ForbiddenReferenceSniff;
