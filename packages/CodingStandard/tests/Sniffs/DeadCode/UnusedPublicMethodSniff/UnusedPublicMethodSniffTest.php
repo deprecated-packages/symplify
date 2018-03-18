4ea09d875d28901463069c5a5a87d28732f4fcdb
TomasVotruba <tomas.vot@gmail.com>
2018-03-18 14:55:11 +0100

    [CodingStandard] fix UnusedPublicMethodSniff for static methods

--- packages/CodingStandard/tests/Sniffs/DeadCode/UnusedPublicMethodSniff/UnusedPublicMethodSniffTest.php
+++ packages/CodingStandard/tests/Sniffs/DeadCode/UnusedPublicMethodSniff/UnusedPublicMethodSniffTest.php
@@ -2,15 +2,30 @@
 
 namespace Symplify\CodingStandard\Tests\Sniffs\DeadCode\UnusedPublicMethodSniff;
 
+use Iterator;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use Symplify\CodingStandard\Sniffs\DeadCode\UnusedPublicMethodSniff;
 use Symplify\CodingStandard\Tests\Sniffs\AbstractSniffTestCase;
 
 final class UnusedPublicMethodSniffTest extends AbstractSniffTestCase
 {
-    public function testWrong(): void
+    public function testCorrect(): void
     {
-        $this->doTestWrongFile(__DIR__ . '/wrong/wrong.php.inc');
+        $this->doTestCorrectFile(__DIR__ . '/correct/correct.php.inc');
+    }
+
+    /**
+     * @dataProvider provideWrongCases()
+     */
+    public function testWrong(string $file): void
+    {
+        $this->doTestWrongFile($file);
+    }
+
+    public function provideWrongCases(): Iterator
+    {
+        yield [__DIR__ . '/wrong/wrong.php.inc'];
+        yield [__DIR__ . '/wrong/wrong2.php.inc'];
     }
 
     protected function createSniff(): Sniff
