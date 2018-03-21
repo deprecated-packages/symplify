7141e4bfd1cfd101efa0e059a7aa710454138bcd
Tomas Votruba <tomas.vot@gmail.com>
2018-03-21 16:50:11 +0100

    drop unused FixedSameStringsConstraint

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types=1);
 
 use PHP_CodeSniffer\Util\Tokens;
-use PhpCsFixer\Tests\Test\Constraint\SameStringsConstraint;
-use Symplify\CodingStandard\Tests\PHPUnit\Constraint\FixedSameStringsConstraint;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
@@ -13,6 +11,3 @@ if (! defined('PHP_CODESNIFFER_VERBOSITY')) {
     // initialize custom T_* token constants used by PHP_CodeSniffer parser
     new Tokens();
 }
-
-// fix from https://github.com/FriendsOfPHP/PHP-CS-Fixer/pull/3592
-class_alias(FixedSameStringsConstraint::class, SameStringsConstraint::class);
