b9ad4fedd129a3be1f945cfbdb9c378295f4919f
Tomas Votruba <tomas.vot@gmail.com>
2018-03-22 03:02:00 +0100

    ctor over static

--- packages/TokenRunner/src/Wrapper/FixerWrapper/ClassWrapperFactory.php
+++ packages/TokenRunner/src/Wrapper/FixerWrapper/ClassWrapperFactory.php
@@ -3,6 +3,7 @@
 namespace Symplify\TokenRunner\Wrapper\FixerWrapper;
 
 use PhpCsFixer\Tokenizer\Tokens;
+use Symplify\TokenRunner\Analyzer\FixerAnalyzer\DocBlockFinder;
 use Symplify\TokenRunner\Guard\TokenTypeGuard;
 
 final class ClassWrapperFactory
@@ -17,18 +18,31 @@ final class ClassWrapperFactory
      */
     private $methodWrapperFactory;
 
+    /**
+     * @var DocBlockFinder
+     */
+    private $docBlockFinder;
+
     public function __construct(
         PropertyWrapperFactory $propertyWrapperFactory,
-        MethodWrapperFactory $methodWrapperFactory
+        MethodWrapperFactory $methodWrapperFactory,
+        DocBlockFinder $docBlockFinder
     ) {
         $this->propertyWrapperFactory = $propertyWrapperFactory;
         $this->methodWrapperFactory = $methodWrapperFactory;
+        $this->docBlockFinder = $docBlockFinder;
     }
 
     public function createFromTokensArrayStartPosition(Tokens $tokens, int $startIndex): ClassWrapper
     {
         TokenTypeGuard::ensureIsTokenType($tokens[$startIndex], [T_CLASS, T_INTERFACE, T_TRAIT], self::class);
 
-        return new ClassWrapper($tokens, $startIndex, $this->propertyWrapperFactory, $this->methodWrapperFactory);
+        return new ClassWrapper(
+            $tokens,
+            $startIndex,
+            $this->propertyWrapperFactory,
+            $this->methodWrapperFactory,
+            $this->docBlockFinder
+        );
     }
 }
