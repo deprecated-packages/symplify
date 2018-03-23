a010e5bf13606b7ef2c9774ed98b5203af0b6033
Tomas Votruba <tomas.vot@gmail.com>
2018-03-23 20:42:51 +0100

    Console: move Output and Input to service style

--- packages/Statie/src/Console/Application.php
+++ packages/Statie/src/Console/Application.php
@@ -5,15 +5,32 @@ namespace Symplify\Statie\Console;
 use Jean85\PrettyVersions;
 use Symfony\Component\Console\Application as SymfonyApplication;
 use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class Application extends SymfonyApplication
 {
-    public function __construct()
-    {
-        $version = PrettyVersions::getVersion('symplify/statie');
+    /**
+     * @var InputInterface
+     */
+    private $input;
+    /**
+     * @var OutputInterface
+     */
+    private $output;
 
-        parent::__construct('Statie', $version->getPrettyVersion());
+    public function __construct(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+
+        parent::__construct('Statie', $this->getPrettyVersion());
+    }
+
+    public function run(InputInterface $input = null, OutputInterface $output = null): int
+    {
+        return parent::run($this->input, $this->output);
     }
 
     protected function getDefaultInputDefinition(): InputDefinition
@@ -33,5 +50,12 @@ final class Application extends SymfonyApplication
             'Path to config file.',
             'statie.(yml|yaml)'
         ));
+    }
+
+    private function getPrettyVersion(): string
+    {
+        $version = PrettyVersions::getVersion('symplify/statie');
+
+        return $version->getPrettyVersion();
     }
 }
