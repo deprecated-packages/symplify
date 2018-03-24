554b7947f5a2d9d4689c08cbf9a842e9dcbebd03
Tomas Votruba <tomas.vot@gmail.com>
2018-03-24 11:39:23 +0100

    add Naming as service

--- packages/TokenRunner/src/Wrapper/SnifferWrapper/ClassWrapperFactory.php
+++ packages/TokenRunner/src/Wrapper/SnifferWrapper/ClassWrapperFactory.php
@@ -3,10 +3,21 @@
 namespace Symplify\TokenRunner\Wrapper\SnifferWrapper;
 
 use PHP_CodeSniffer\Files\File;
+use Symplify\TokenRunner\Analyzer\SnifferAnalyzer\Naming;
 use Symplify\TokenRunner\Guard\TokenTypeGuard;
 
 final class ClassWrapperFactory
 {
+    /**
+     * @var Naming
+     */
+    private $naming;
+
+    public function __construct(Naming $naming)
+    {
+        $this->naming = $naming;
+    }
+
     public function createFromFirstClassInFile(File $file): ?ClassWrapper
     {
         $possibleClassPosition = $file->findNext(T_CLASS, 0);
@@ -20,6 +31,6 @@ final class ClassWrapperFactory
             T_INTERFACE,
         ], __METHOD__);
 
-        return new ClassWrapper($file, $possibleClassPosition);
+        return new ClassWrapper($file, $possibleClassPosition, $this->naming);
     }
 }
