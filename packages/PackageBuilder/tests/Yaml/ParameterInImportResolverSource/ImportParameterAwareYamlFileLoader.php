ccd79a94debf591ea6aa93d05eb92b99273f84f5
Tomas Votruba <tomas.vot@gmail.com>
2018-03-28 13:56:14 +0200

    move ParameterInImportResolver to PackageBuilder, so more packages can use it

---
+++ packages/PackageBuilder/tests/Yaml/ParameterInImportResolverSource/ImportParameterAwareYamlFileLoader.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Tests\Yaml;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symplify\EasyCodingStandard\DependencyInjection\DelegatingLoaderFactory;
+use Symplify\PackageBuilder\Yaml\ParameterInImportResolver;
+
+/**
+ * @see ParameterInImportResolver
+ */
+final class ParameterInImportResolverTest extends TestCase
+{
+    public function test(): void
+    {
+        $containerBuilder = new ContainerBuilder();
+
+        $delegatingLoader = (new DelegatingLoaderFactory())->createFromContainerBuilderAndDirectory(
+            $containerBuilder,
+            __DIR__ . '/CheckerTolerantYamlFileLoader'
+        );
+
+        $delegatingLoader->load($this->provideConfig());
+
+        $this->assertTrue($containerBuilder->getParameter('it_works'));
+    }
+
+    private function provideConfig(): string
+    {
+        if (defined('SYMPLIFY_MONOREPO')) {
+            return __DIR__ . '/ParameterInImportResolverSource/config-with-import-param-monorepo.yml';
+        }
+
+        return __DIR__ . '/ParameterInImportResolverSource/config-with-import-param-split.yml';
+    }
+}
