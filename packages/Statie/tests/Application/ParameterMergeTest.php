9b5dc8ce4c32156ecaad9574008b410e1bde1729
Tomas Votruba <tomas.vot@gmail.com>
2018-04-01 12:26:16 +0200

    [Statie] add simple merge test

---
+++ packages/Statie/tests/Application/ParameterMergeTest.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\Statie\Tests\Application;
+
+use Nette\Utils\FileSystem;
+use PHPUnit\Framework\TestCase;
+use Symplify\Statie\Application\StatieApplication;
+use Symplify\Statie\DependencyInjection\ContainerFactory;
+use Symplify\Statie\Exception\Utils\MissingDirectoryException;
+use Symplify\Statie\FlatWhite\Latte\DynamicStringLoader;
+
+final class ParameterMergeTest extends TestCase
+{
+    /**
+     * @var StatieApplication
+     */
+    private $statieApplication;
+
+    /**
+     * @var DynamicStringLoader
+     */
+    private $dynamicStringLoader;
+
+    protected function setUp(): void
+    {
+        $container = (new ContainerFactory())->createWithConfig(__DIR__ . '/StatieApplicationSource/statie.yml');
+        $this->statieApplication = $container->get(StatieApplication::class);
+        $this->dynamicStringLoader = $container->get(DynamicStringLoader::class);
+    }
+
+    protected function tearDown(): void
+    {
+        FileSystem::delete(__DIR__ . '/StatieApplicationSource/output');
+    }
+
+    public function testRun(): void
+    {
+        $this->statieApplication->run(
+            __DIR__ . '/StatieApplicationSource/source',
+            __DIR__ . '/StatieApplicationSource/output'
+        );
+
+        $this->assertFileExists(__DIR__ . '/StatieApplicationSource/output/index.html');
+        $this->assertFileEquals(
+            __DIR__ . '/StatieApplicationSource/expected-index.html',
+            __DIR__ . '/StatieApplicationSource/output/index.html'
+        );
+
+        $this->assertFileExists(__DIR__ . '/StatieApplicationSource/output/feed.xml');
+        $this->assertFileExists(__DIR__ . '/StatieApplicationSource/output/atom.rss');
+
+        $this->assertNotEmpty($this->dynamicStringLoader->getContent('default'));
+    }
+
+    public function testRunForMissingSource(): void
+    {
+        $this->expectException(MissingDirectoryException::class);
+        $this->statieApplication->run('missing', 'random');
+    }
+}
