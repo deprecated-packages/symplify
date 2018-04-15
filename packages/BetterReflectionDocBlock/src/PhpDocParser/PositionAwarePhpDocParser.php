6af0882f952fe65281293ed8a6cf52870995b808
Tomas Votruba <tomas.vot@gmail.com>
2018-04-15 15:05:01 +0200

    detect and store positions for nodes

--- packages/BetterReflectionDocBlock/src/PhpDocParser/PositionAwarePhpDocParser.php
+++ packages/BetterReflectionDocBlock/src/PhpDocParser/PositionAwarePhpDocParser.php
@@ -2,12 +2,14 @@
 
 namespace Symplify\BetterReflectionDocBlock\PhpDocParser;
 
+use PHPStan\PhpDocParser\Ast\Node;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
 use PHPStan\PhpDocParser\Lexer\Lexer;
 use PHPStan\PhpDocParser\Parser\ConstExprParser;
 use PHPStan\PhpDocParser\Parser\PhpDocParser;
 use PHPStan\PhpDocParser\Parser\TokenIterator;
 use PHPStan\PhpDocParser\Parser\TypeParser;
+use SplObjectStorage;
 use Symplify\PackageBuilder\Reflection\PrivatesCaller;
 
 final class PositionAwarePhpDocParser extends PhpDocParser
@@ -17,9 +19,15 @@ final class PositionAwarePhpDocParser extends PhpDocParser
      */
     private $privatesCaller;
 
+    /**
+     * @var mixed[]
+     */
+    private $childrenWithPositions = [];
+
     public function __construct(TypeParser $typeParser, ConstExprParser $constantExprParser)
     {
         $this->privatesCaller = new PrivatesCaller();
+        $this->childrenWithPositions = new SplObjectStorage();
 
         parent::__construct($typeParser, $constantExprParser);
     }
@@ -32,9 +40,9 @@ final class PositionAwarePhpDocParser extends PhpDocParser
         $children = [];
 
         if (!$tokens->isCurrentTokenType(Lexer::TOKEN_CLOSE_PHPDOC)) {
-            $children[] = $this->privatesCaller->callPrivateMethod($this, 'parseChild', $tokens);
+            $children[] = $this->parseChildAndStoreItsPositions($tokens);
             while ($tokens->tryConsumeTokenType(Lexer::TOKEN_PHPDOC_EOL) && !$tokens->isCurrentTokenType(Lexer::TOKEN_CLOSE_PHPDOC)) {
-                $children[] = $this->privatesCaller->callPrivateMethod($this, 'parseChild', $tokens);;
+                $children[] = $this->parseChildAndStoreItsPositions($tokens);;
             }
 
             // @todo store position metadata here for each $child here
@@ -43,5 +51,21 @@ final class PositionAwarePhpDocParser extends PhpDocParser
         $tokens->consumeTokenType(Lexer::TOKEN_CLOSE_PHPDOC);
 
         return new PhpDocNode(array_values($children));
+    }
+
+    private function parseChildAndStoreItsPositions(TokenIterator $tokens): Node
+    {
+        $tokenStart = $tokens->currentTokenOffset();
+
+        $node = $this->privatesCaller->callPrivateMethod($this, 'parseChild', $tokens);
+
+        $tokenEnd = $tokens->currentTokenOffset();
+
+        $this->childrenWithPositions[$node] = [
+            'tokenStart' => $tokenStart,
+            'tokenEnd' => $tokenEnd,
+        ];
+
+        return $node;
     }
 }
