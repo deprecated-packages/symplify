6357d409f8bd3711422103ab2ab07c1752ada9c9
Tomas Votruba <tomas.vot@gmail.com>
2018-05-03 12:08:56 +0200

    remove unused deps

--- packages/TokenRunner/src/Wrapper/FixerWrapper/DocBlockWrapperFactory.php
+++ packages/TokenRunner/src/Wrapper/FixerWrapper/DocBlockWrapperFactory.php
@@ -4,7 +4,6 @@ namespace Symplify\TokenRunner\Wrapper\FixerWrapper;
 
 use PhpCsFixer\Tokenizer\Tokens;
 use Symplify\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
-use Symplify\BetterPhpDocParser\PhpDocParser\TypeNodeToStringsConvertor;
 use Symplify\BetterPhpDocParser\Printer\PhpDocInfoPrinter;
 
 final class DocBlockWrapperFactory
@@ -19,19 +18,10 @@ final class DocBlockWrapperFactory
      */
     private $phpDocInfoPrinter;
 
-    /**
-     * @var TypeNodeToStringsConvertor
-     */
-    private $typeNodeToStringsConvertor;
-
-    public function __construct(
-        PhpDocInfoFactory $phpDocInfoFactory,
-        PhpDocInfoPrinter $phpDocInfoPrinter,
-        TypeNodeToStringsConvertor $typeNodeToStringsConvertor
-    ) {
+    public function __construct(PhpDocInfoFactory $phpDocInfoFactory, PhpDocInfoPrinter $phpDocInfoPrinter)
+    {
         $this->phpDocInfoFactory = $phpDocInfoFactory;
         $this->phpDocInfoPrinter = $phpDocInfoPrinter;
-        $this->typeNodeToStringsConvertor = $typeNodeToStringsConvertor;
     }
 
     public function create(Tokens $tokens, int $position, string $content): DocBlockWrapper
@@ -40,8 +30,7 @@ final class DocBlockWrapperFactory
             $tokens,
             $position,
             $this->phpDocInfoFactory->createFrom($content),
-            $this->phpDocInfoPrinter,
-            $this->typeNodeToStringsConvertor
+            $this->phpDocInfoPrinter
         );
     }
 }
