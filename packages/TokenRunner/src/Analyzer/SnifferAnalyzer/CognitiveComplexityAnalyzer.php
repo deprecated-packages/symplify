d4d15d3d598f221e4c6dac28583666698036f7d3
Tomas Votruba <tomas.vot@gmail.com>
2018-05-18 18:58:47 +0200

    move namespace up, no need to nest

--- packages/TokenRunner/src/Analyzer/SnifferAnalyzer/CognitiveComplexityAnalyzer.php
+++ packages/TokenRunner/src/Analyzer/SnifferAnalyzer/CognitiveComplexityAnalyzer.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symplify\TokenRunner\Analyzer\SnifferAnalyzer\CognitiveComplexity;
+namespace Symplify\TokenRunner\Analyzer\SnifferAnalyzer;
 
 /**
  * Inspired by https://www.sonarsource.com/docs/CognitiveComplexity.pdf
