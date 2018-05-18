9a7a7bc0f14cc46076e3dc2e01a57bf28fd5664c
Tomas Votruba <tomas.vot@gmail.com>
2018-05-18 19:28:44 +0200

    fix namespace

--- packages/TokenRunner/tests/Analyzer/SniffAnalyzer/CognitiveComplexityAnalyzer/CognitiveComplextyAnalyzerTest.php
+++ packages/TokenRunner/tests/Analyzer/SniffAnalyzer/CognitiveComplexityAnalyzer/CognitiveComplextyAnalyzerTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symplify\TokenRunner\Tests\DocBlock;
+namespace Symplify\TokenRunner\Tests\Analyzer\SniffAnalyzer\CognitiveComplexityAnalyzer;
 
 use Iterator;
 use PHP_CodeSniffer\Config;
