135bfa2682f3a4ed183740d8ff75e9c4d61119a5
Tomas Votruba <tomas.vot@gmail.com>
2018-05-22 14:25:29 +0200

    Revert "source: make use of Configuration object, remove paramter"
    This reverts commit 7541cf5fea99eaa0bd266d9737fecf906a853332.

--- packages/ChangelogLinker/src/Worker/DiffLinksToVersionsWorker.php
+++ packages/ChangelogLinker/src/Worker/DiffLinksToVersionsWorker.php
@@ -3,7 +3,6 @@
 namespace Symplify\ChangelogLinker\Worker;
 
 use Nette\Utils\Strings;
-use Symplify\ChangelogLinker\Configuration\ChangelogLinkerConfiguration;
 use Symplify\ChangelogLinker\Contract\Worker\WorkerInterface;
 use Symplify\ChangelogLinker\Regex\RegexPattern;
 
@@ -20,13 +19,13 @@ final class DiffLinksToVersionsWorker implements WorkerInterface
     private $versions = [];
 
     /**
-     * @var ChangelogLinkerConfiguration
+     * @var string
      */
-    private $changelogLinkerConfiguration;
+    private $repositoryLink;
 
-    public function __construct(ChangelogLinkerConfiguration $changelogLinkerConfiguration)
+    public function __construct(string $repositoryLink)
     {
-        $this->changelogLinkerConfiguration = $changelogLinkerConfiguration;
+        $this->repositoryLink = $repositoryLink;
     }
 
     public function processContent(string $content): string
@@ -43,7 +42,7 @@ final class DiffLinksToVersionsWorker implements WorkerInterface
             $linksToAppend[] = sprintf(
                 '[%s]: %s/compare/%s...%s',
                 $version,
-                $this->changelogLinkerConfiguration->getRepositoryLink(),
+                $this->repositoryLink,
                 $this->versions[$index + 1],
                 $version
             );
