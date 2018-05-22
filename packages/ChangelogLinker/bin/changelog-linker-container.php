1b9ad5e597c7963ab0a7eb6995505a2f6d872247
Tomas Votruba <tomas.vot@gmail.com>
2018-05-22 14:38:01 +0200

    decopule changelog-linker-container

---
+++ packages/ChangelogLinker/bin/changelog-linker-container.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+use Symplify\ChangelogLinker\DependencyInjection\ContainerFactory;
+
+$config = null;
+$argvInput = new Symfony\Component\Console\Input\ArgvInput;
+if ($argvInput->hasParameterOption('--config')) {
+    $config = $argvInput->getParameterOption('--config');
+}
+
+$containerFactory = new ContainerFactory();
+if ($config) {
+    return $containerFactory->createWithConfig($config);
+}
+
+return $containerFactory->create();
