c3713fadf1307f38b43d960e3836641ea09df53e
Tomas Votruba <tomas.vot@gmail.com>
2018-06-02 20:58:27 +0200

    decopule priority test to own class

--- packages/CodingStandard/tests/Fixer/Commenting/RemoveEmptyDocBlockFixer/OtherFixerPrioritiesTest.php
+++ packages/CodingStandard/tests/Fixer/Commenting/RemoveEmptyDocBlockFixer/OtherFixerPrioritiesTest.php
@@ -8,7 +8,7 @@ use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
 /**
  * @see \Symplify\CodingStandard\Fixer\Commenting\RemoveEmptyDocBlockFixer
  */
-final class RemoveEmptyDocBlockFixerTest extends AbstractCheckerTestCase
+final class OtherFixerPrioritiesTest extends AbstractCheckerTestCase
 {
     /**
      * @dataProvider provideWrongToFixedCases()
@@ -20,13 +20,13 @@ final class RemoveEmptyDocBlockFixerTest extends AbstractCheckerTestCase
 
     public function provideWrongToFixedCases(): Iterator
     {
-        yield [__DIR__ . '/wrong/wrong.php.inc', __DIR__ . '/fixed/fixed.php.inc'];
-        yield [__DIR__ . '/wrong/wrong2.php.inc', __DIR__ . '/fixed/fixed2.php.inc'];
-        yield [__DIR__ . '/wrong/wrong3.php.inc', __DIR__ . '/fixed/fixed3.php.inc'];
+        yield [__DIR__ . '/wrong/wrong4.php.inc', __DIR__ . '/fixed/fixed4.php.inc'];
+        yield [__DIR__ . '/wrong/wrong5.php.inc', __DIR__ . '/fixed/fixed5.php.inc'];
+        yield [__DIR__ . '/wrong/wrong6.php.inc', __DIR__ . '/fixed/fixed6.php.inc'];
     }
 
     protected function provideConfig(): string
     {
-        return __DIR__ . '/config.yml';
+        return __DIR__ . '/priorities-config.yml';
     }
 }
