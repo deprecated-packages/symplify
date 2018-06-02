016c2e44cdec4c4252f83d153d4938f50e133e62
Tomas Votruba <tomas.vot@gmail.com>
2018-06-03 01:03:08 +0200

    ChangeFilesDetector - move to FileInfo

--- packages/PackageBuilder/src/FileSystem/FileGuard.php
+++ packages/PackageBuilder/src/FileSystem/FileGuard.php
@@ -2,9 +2,7 @@
 
 namespace Symplify\PackageBuilder\FileSystem;
 
-use Nette\Utils\FileSystem;
 use Symplify\PackageBuilder\Exception\Configuration\FileNotFoundException;
-use Symplify\PackageBuilder\Exception\FilePathNotAbsoluteException;
 
 final class FileGuard
 {
@@ -15,18 +13,5 @@ final class FileGuard
         }
 
         throw new FileNotFoundException(sprintf('File "%s" not found in "%s".', $file, $location));
-    }
-
-    public function ensureIsAbsolutePath(string $file, string $location): void
-    {
-        if (FileSystem::isAbsolute($file)) {
-            return;
-        }
-
-        throw new FilePathNotAbsoluteException(sprintf(
-            'File path "%s" is not absolute in "%s".',
-            $file,
-            $location
-        ));
     }
 }
