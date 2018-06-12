667d3193ee525d7fba3f1e8cab8ffe323b20e891
Tomas Votruba <tomas.vot@gmail.com>
2018-06-12 17:22:52 +0200

    Add Console\Application, that supports config loading

---
+++ packages/MonorepoBuilder/src/Console/Application.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Console;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+
+final class Application extends SymfonyApplication
+{
+    protected function getDefaultInputDefinition(): InputDefinition
+    {
+        $inputDefinition = parent::getDefaultInputDefinition();
+
+        // adds "--config" | "-c" option
+        $inputDefinition->addOption(new InputOption('config', 'c', InputOption::VALUE_REQUIRED, 'Config file.'));
+
+        return $inputDefinition;
+    }
+}
