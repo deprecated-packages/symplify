a15d7fc61c0274bc034889ad2ef9d3da799b7fa3
Tomas Votruba <tomas.vot@gmail.com>
2018-06-25 20:07:25 +0200

    remove unused property

--- packages/Statie/packages/Twig/tests/TwigRendererTest.php
+++ packages/Statie/packages/Twig/tests/TwigRendererTest.php
@@ -6,7 +6,6 @@ use Symfony\Component\Finder\SplFileInfo;
 use Symplify\Statie\Renderable\File\FileFactory;
 use Symplify\Statie\Tests\AbstractContainerAwareTestCase;
 use Symplify\Statie\Twig\TwigRenderer;
-use Twig\Loader\ArrayLoader;
 
 final class TwigRendererTest extends AbstractContainerAwareTestCase
 {
@@ -20,16 +19,10 @@ final class TwigRendererTest extends AbstractContainerAwareTestCase
      */
     private $fileFactory;
 
-    /**
-     * @var ArrayLoader
-     */
-    private $twigArrayLoader;
-
     protected function setUp(): void
     {
         $this->twigRenderer = $this->container->get(TwigRenderer::class);
         $this->fileFactory = $this->container->get(FileFactory::class);
-        $this->twigArrayLoader = $this->container->get(ArrayLoader::class);
     }
 
     public function test(): void
