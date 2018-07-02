81b9ea3f213634c61b23d88fd38c21d6c94d9eb5
Tomas Votruba <tomas.vot@gmail.com>
2018-07-02 12:14:24 +0200

    improve readme of command

--- packages/MonorepoBuilder/packages/Split/bash/subsplit.sh
+++ packages/MonorepoBuilder/packages/Split/bash/subsplit.sh
@@ -23,14 +23,17 @@ fi
 
 OPTS_SPEC="\
 subsplit.sh --splits=<splits> --branches=<branches> --tags=<tags>
+
+For example:
+subsplit.sh --splits=packages/MonorepoBuilder:git@github.com:Symplify/MonorepoBuilder.git --branches=master --tags=v5.0
 --
 h,help        show the help
 debug         show plenty of debug output
 dry-run       do everything except actually send the updates
 work-dir=     directory that contains the subsplit working directory
-splits=       listed repositories <directory:repository>
-branches=     publish for listed branches
-tags=         publish for listed tags
+splits=       listed repositories <directory:repository>, e.g. '--splits=packages/MonorepoBuilder:git@github.com:Symplify/MonorepoBuilder.git'
+branches=     publish for listed branches, e.g '--branches=master', '--branches=master dev',
+tags=         publish for listed tags, e.g. '--tags=v5.0', '--tags=v5.0 v5.5'
 "
 eval "$(echo "$OPTS_SPEC" | git rev-parse --parseopt -- "$@" || echo exit $?)"
 
