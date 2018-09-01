56c8813b18ada916cc97a7cb9b580a950a931272
Tomas Votruba <tomas.vot@gmail.com>
2018-09-01 22:37:18 +0200

    [ChanlogLinker] Add cleanup command

---
+++ packages/ChangelogLinker/tests/Analyzer/LinksAnalyzer/Source/SomeFileWithDeadlinks.md
@@ -0,0 +1,7 @@
+### Added
+
+- [#10] A link
+
+[#5]: Link
+[#10]: Deadlink
+[#15]: Deadlink
---
+++ packages/ChangelogLinker/tests/ChangelogCleaner/ChangelogCleanerTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\ChangelogCleaner\Tests\ChangelogCleaner;
+
+use Iterator;
+use Symplify\ChangelogLinker\ChangelogCleaner;
+use Symplify\ChangelogLinker\Tests\AbstractContainerAwareTestCase;
+
+final class ChangelogCleanerTest extends AbstractContainerAwareTestCase
+{
+    /**
+     * @var ChangelogCleaner
+     */
+    private $changelogCleaner;
+
+    protected function setUp(): void
+    {
+        $this->changelogCleaner = $this->container->get(ChangelogCleaner::class);
+    }
+
+    /**
+     * @dataProvider dataProvider()
+     */
+    public function test(string $originalFile, string $expectedFile): void
+    {
+        $processedFile = $this->changelogCleaner->processContent(file_get_contents($originalFile));
+
+        $this->assertStringEqualsFile($expectedFile, $processedFile);
+    }
+
+    public function dataProvider(): Iterator
+    {
+        yield [__DIR__ . '/Source/before/01.md', __DIR__ . '/Source/after/01.md'];
+        yield [__DIR__ . '/Source/before/02.md', __DIR__ . '/Source/after/02.md'];
+    }
+}
