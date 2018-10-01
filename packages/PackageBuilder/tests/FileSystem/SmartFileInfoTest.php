6e8593e992bdf349aa9a6ff49593727db8f52bc3
Tomas Votruba <tomas.vot@gmail.com>
2018-10-01 18:26:34 +0800

    [PackageBuilder] Fix SmartFileInfo invalid dir argument

--- packages/PackageBuilder/tests/FileSystem/SmartFileInfoTest.php
+++ packages/PackageBuilder/tests/FileSystem/SmartFileInfoTest.php
@@ -3,6 +3,7 @@
 namespace Symplify\PackageBuilder\Tests\FileSystem;
 
 use PHPUnit\Framework\TestCase;
+use Symplify\PackageBuilder\Exception\FileSystem\DirectoryNotFoundException;
 use Symplify\PackageBuilder\Exception\FileSystem\FileNotFoundException;
 use Symplify\PackageBuilder\FileSystem\SmartFileInfo;
 
@@ -22,5 +23,23 @@ final class SmartFileInfoTest extends TestCase
 
         $this->assertStringEndsWith($smartFileInfo->getRelativePath(), __DIR__);
         $this->assertStringEndsWith($smartFileInfo->getRelativePathname(), __FILE__);
+    }
+
+    public function testRelativeToDir(): void
+    {
+        $smartFileInfo = new SmartFileInfo(__DIR__ . '/SmartFileInfoSource/AnotherFile.txt');
+
+        $this->assertSame(
+            'SmartFileInfoSource/AnotherFile.txt',
+            $smartFileInfo->getRelativeFilePathFromDirectory(__DIR__)
+        );
+    }
+
+    public function testRelativeToDirExceptoin(): void
+    {
+        $smartFileInfo = new SmartFileInfo(__FILE__);
+
+        $this->expectException(DirectoryNotFoundException::class);
+        $smartFileInfo->getRelativeFilePathFromDirectory('tests/FileSystem');
     }
 }
