e75956c0f4dd7ab1e89cb66db16df66f4e34c35b
Tomas Votruba <tomas.vot@gmail.com>
2018-11-20 17:53:04 +0100

    [MonorepoBuilder] Fix merging of scalar values in composer.json

--- packages/PackageBuilder/src/Yaml/ParametersMerger.php
+++ packages/PackageBuilder/src/Yaml/ParametersMerger.php
@@ -34,4 +34,38 @@ final class ParametersMerger
 
         return $left;
     }
+
+    /**
+     * The same as above, just with the case if both values being non-array, it will combined them to array:
+     *
+     * $this->mergeWithCombine(1, 2); // [1, 2]
+     *
+     * @param mixed $left
+     * @param mixed $right
+     * @return mixed[]|string
+     */
+    public function mergeWithCombine($left, $right)
+    {
+        if (is_array($left) && is_array($right)) {
+            foreach ($left as $key => $val) {
+                if (is_int($key)) {
+                    $right[] = $val;
+                } else {
+                    if (isset($right[$key])) {
+                        $val = $this->mergeWithCombine($val, $right[$key]);
+                    }
+                    $right[$key] = $val;
+                }
+            }
+            return $right;
+        } elseif ($left === null && is_array($right)) {
+            return $right;
+        }
+
+        if (! empty($right) && (array) $left !== (array) $right) {
+            return $this->mergeWithCombine((array) $right, (array) $left);
+        }
+
+        return $left;
+    }
 }
