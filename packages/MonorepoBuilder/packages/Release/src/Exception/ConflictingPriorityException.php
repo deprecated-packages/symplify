06c8074430f43283126f3e7cf626b1cdc3974670
Tomas Votruba <tomas.vot@gmail.com>
2018-11-21 19:58:44 +0100

    typo

--- packages/MonorepoBuilder/packages/Release/src/Exception/ConflictingPriorityException.php
+++ packages/MonorepoBuilder/packages/Release/src/Exception/ConflictingPriorityException.php
@@ -11,7 +11,7 @@ final class ConflictingPriorityException extends Exception
     public function __construct(ReleaseWorkerInterface $firstReleaseWorker, ReleaseWorkerInterface $secondReleaseWorker)
     {
         $message = sprintf(
-            'There 2 workers with %d priority:%s-%s%s-%s.%sChange value in "getPriority()" method in one of them',
+            'There are 2 workers with %d priority:%s-%s%s-%s.%sChange value in "getPriority()" method in one of them to different value',
             $firstReleaseWorker->getPriority(),
             PHP_EOL,
             get_class($firstReleaseWorker),
