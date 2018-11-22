0e48587d558e63d661157c8b80a08d84b46ac877
Tomas Votruba <tomas.vot@gmail.com>
2018-11-22 23:29:06 +0100

    Add Message

---
+++ packages/MonorepoBuilder/packages/Release/src/Message.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Release;
+
+final class Message
+{
+    /**
+     * @var string
+     */
+    public const SUCCESS = 'All good!';
+}
