ad4c7a87abe2202084dbf0d377a89397e55d593f
Tomas Votruba <tomas.vot@gmail.com>
2018-11-26 17:19:36 +0100

    [MB] Add reporting to ProcessRunner

--- packages/MonorepoBuilder/packages/Release/src/Process/ProcessRunner.php
+++ packages/MonorepoBuilder/packages/Release/src/Process/ProcessRunner.php
@@ -2,18 +2,43 @@
 
 namespace Symplify\MonorepoBuilder\Release\Process;
 
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 final class ProcessRunner
 {
-    public function run(string $commandLine): string
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
+
+    public function __construct(SymfonyStyle $symfonyStyle)
     {
-        $process = new Process($commandLine);
+        $this->symfonyStyle = $symfonyStyle;
+    }
+
+    public function run(string $commandLine, bool $shouldDisplayOutput = false): void
+    {
+        if ($this->symfonyStyle->isVerbose()) {
+            $this->symfonyStyle->note('Running process: ' . $commandLine);
+        }
+
+        // set reasonable timeout to report hang off, 10 minutes
+        $process = new Process($commandLine, null, null, null, 10 * 60.0);
         $process->run();
 
+        $this->reportResult($shouldDisplayOutput, $process);
+    }
+
+    private function reportResult(bool $shouldDisplayOutput, Process $process): void
+    {
         if ($process->isSuccessful()) {
-            return trim($process->getOutput());
+            if ($shouldDisplayOutput) {
+                $this->symfonyStyle->writeln(trim($process->getOutput()));
+            }
+
+            return;
         }
 
         throw new ProcessFailedException($process);
