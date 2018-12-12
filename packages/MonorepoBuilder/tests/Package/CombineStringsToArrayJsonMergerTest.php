ab64794c570216b30c898aa125eef571f6d655ff
Anton Zdanov <azdanov@pm.me>
2018-12-12 09:18:50 +0200

    test(ECS): revert to master

--- packages/MonorepoBuilder/tests/Package/CombineStringsToArrayJsonMergerTest.php
+++ packages/MonorepoBuilder/tests/Package/CombineStringsToArrayJsonMergerTest.php
@@ -35,8 +35,8 @@ final class CombineStringsToArrayJsonMergerTest extends AbstractContainerAwareTe
         $this->assertSame([
             'autoload' => [
                 'psr-4' => [
-                    'ACME\Another\\' => 'packages/A',
                     'ACME\Model\Core\\' => ['packages/A', 'packages/B'],
+                    'ACME\Another\\' => 'packages/A',
                     'ACME\\YetAnother\\' => ['packages/A'],
                     'ACME\\YetYetAnother\\' => 'packages/A',
                 ],
