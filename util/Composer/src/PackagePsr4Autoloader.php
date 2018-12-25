4d9ed30520d05e1feaf180a0bfcd89b8e80a1559
Tomas Votruba <tomas.vot@gmail.com>
2018-12-26 00:14:03 +0100

    symplify autoload

--- util/Composer/src/PackagePsr4Autoloader.php
+++ util/Composer/src/PackagePsr4Autoloader.php
@@ -2,54 +2,51 @@
 
 namespace Symplify\Util\Composer;
 
+use Composer\Package\RootPackageInterface;
 use Composer\Script\Event;
 use Nette\Utils\Strings;
 
 final class PackagePsr4Autoloader
 {
     /**
+     * @var string
+     */
+    private const MAIN_NAMESPACE = 'Symplify';
+
+    /**
+     * @var string
+     */
+    private const PACKAGES = 'packages';
+
+    /**
      * @see https://www.drupal.org/files/issues/vendor-classmap-2468499-14.patch
      */
     public static function autoload(Event $event): void
     {
-        // input
-        $packagesDirectory = 'packages';
-        $mainNamespace = 'Symplify';
-        $absolutePackagesDirectory = getcwd() . '/' . $packagesDirectory . '/*';
+        $absolutePackagesDirectory = getcwd() . '/' . self::PACKAGES . '/*';
         $package = $event->getComposer()->getPackage();
 
-        // src
-        $srcDirectories = self::getDirectoriesInPath($absolutePackagesDirectory, 'src');
-        $namespaceToDirectory = self::createNamespaceToDirectory($srcDirectories, $mainNamespace, $packagesDirectory);
-        $autoload = $package->getAutoload();
-        $autoload['psr-4'] = array_merge($autoload['psr-4'], $namespaceToDirectory);
-        $package->setAutoload($autoload);
-
-        // tests
-        $testDirectories = self::getDirectoriesInPath($absolutePackagesDirectory, 'tests');
-        $namespaceToDirectory = self::createNamespaceToDirectory(
-            $testDirectories,
-            $mainNamespace,
-            $packagesDirectory,
-            'Tests'
-        );
-        $devAutoload = $package->getDevAutoload();
-        $devAutoload['psr-4'] = array_merge($devAutoload['psr-4'], $namespaceToDirectory);
-        $package->setDevAutoload($devAutoload);
+        self::autoloadSrc($absolutePackagesDirectory, $package);
+        self::autoloadTests($absolutePackagesDirectory, $package);
     }
+
     /**
      * @return string[]
      */
     private static function getDirectoriesInPath(string $directory, string $name): array
     {
         $globResult = self::globRecursive($directory, GLOB_ONLYDIR);
+
         $directories = array_filter($globResult, function ($path) use ($name) {
             // keep only dirs ending with "$name"
             return Strings::match($path, '#\/' . preg_quote($name) . '$#') && ! Strings::contains($path, 'templates');
         });
+
         $directories = self::absolutizePaths($directories);
+
         return self::relativizeToCwd($directories);
     }
+
     /**
      * @see https://stackoverflow.com/a/12109100/1348344
      * @return string[]
@@ -60,8 +57,10 @@ final class PackagePsr4Autoloader
         foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir) {
             $files = array_merge($files, self::globRecursive($dir . '/' . basename($pattern), $flags));
         }
+
         return $files;
     }
+
     /**
      * @param string[] $directories
      * @return string[]
@@ -71,8 +70,10 @@ final class PackagePsr4Autoloader
         foreach ($directories as $key => $directory) {
             $directories[$key] = Strings::substring($directory, strlen(getcwd()) + 1);
         }
+
         return $directories;
     }
+
     /**
      * @param string[] $paths
      * @return string[]
@@ -85,8 +86,10 @@ final class PackagePsr4Autoloader
                 $paths[$key] = $realpath;
             }
         }
+
         return $paths;
     }
+
     /**
      * From:
      * "packages/SomePath/src"
@@ -99,22 +102,56 @@ final class PackagePsr4Autoloader
      */
     private static function createNamespaceToDirectory(
         array $directories,
-        string $mainNamespace,
         string $packagesDirectory,
         string $namespaceSuffix = ''
     ): array {
         $namespaceToDirectory = [];
         foreach ($directories as $directory) {
-            $match = Strings::match($directory, '#' . $packagesDirectory . '/(?<namespacePart>.*?)/#');
+            $pattern = '#' . $packagesDirectory . '/(?<namespacePart>.*?)/(src|tests|packages)(/(?<secondNamespacePart>.*?)/)?#';
+
+            $match = Strings::match($directory, $pattern);
             if (! isset($match['namespacePart'])) {
                 continue;
             }
-            $namespace = $mainNamespace . '\\' . $match['namespacePart'] . '\\';
+
+            $namespace = self::MAIN_NAMESPACE . '\\' . $match['namespacePart'] . '\\';
+
+            // subpackage
+            if (isset($match['secondNamespacePart'])) {
+                $namespace .= $match['secondNamespacePart'] . '\\';
+            }
+
             if ($namespaceSuffix) {
                 $namespace .= $namespaceSuffix . '\\';
             }
+
             $namespaceToDirectory[$namespace] = $directory;
         }
+
         return $namespaceToDirectory;
+    }
+
+    private static function autoloadSrc(string $absolutePackagesDirectory, RootPackageInterface $package): void
+    {
+        $srcDirectories = self::getDirectoriesInPath($absolutePackagesDirectory, 'src');
+        $namespaceToDirectory = self::createNamespaceToDirectory($srcDirectories, self::PACKAGES);
+
+        $autoload = $package->getAutoload();
+        $autoload['psr-4'] = array_merge($autoload['psr-4'] ?? [], $namespaceToDirectory);
+        $package->setAutoload($autoload);
+    }
+
+    private static function autoloadTests(string $absolutePackagesDirectory, RootPackageInterface $package): void
+    {
+        $testDirectories = self::getDirectoriesInPath($absolutePackagesDirectory, 'tests');
+        $namespaceToDirectory = self::createNamespaceToDirectory(
+            $testDirectories,
+            self::PACKAGES,
+            'Tests'
+        );
+
+        $devAutoload = $package->getDevAutoload();
+        $devAutoload['psr-4'] = array_merge($devAutoload['psr-4'] ?? [], $namespaceToDirectory);
+        $package->setDevAutoload($devAutoload);
     }
 }
