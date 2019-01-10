c850206bf370a9993468682b13c2a7d669c88230
Rostislav Vítek <rostislav.vitek@shopsys.com>
2019-01-10 12:25:32 +0100

    ReleaseCommand: stage is taken into account in success message
    - the message "Version XY is now released!" is confusing when using stages
    - we have the stage "release-candidate" that ends with sending the release-candidate branch for code-review and someone might get a fright when he sees the message telling him that the version is released

--- packages/MonorepoBuilder/packages/Release/src/Command/ReleaseCommand.php
+++ packages/MonorepoBuilder/packages/Release/src/Command/ReleaseCommand.php
@@ -96,8 +96,10 @@ final class ReleaseCommand extends Command
 
         if ($isDryRun) {
             $this->symfonyStyle->note('Running in dry mode, nothing is changed');
-        } else {
+        } elseif ($stage === null) {
             $this->symfonyStyle->success(sprintf('Version "%s" is now released!', $version->getVersionString()));
+        } else {
+            $this->symfonyStyle->success(sprintf('Stage "%s" is now finished!', $stage));
         }
 
         return ShellCode::SUCCESS;
