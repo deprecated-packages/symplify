0874fc230b52410296b4fdb9eeadb196a41f4143
Tomas Votruba <tomas.vot@gmail.com>
2019-01-12 19:31:18 +0100

    add spaceless

---
+++ packages/LatteToTwigConverter/src/CaseConverter/RenameMacroCaseConverter.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\LatteToTwigConverter\CaseConverter;
+
+use Nette\Utils\Strings;
+use Symplify\LatteToTwigConverter\Contract\CaseConverter\CaseConverterInterface;
+
+final class RenameMacroCaseConverter implements CaseConverterInterface
+{
+    public function convertContent(string $content): string
+    {
+        return Strings::replace($content, '#{spaceless}(.*?){/spaceless}#ms', '{% spaceless %}$1{% endspaceless %}');
+    }
+}
