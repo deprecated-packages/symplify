eeba32f3d424caca00ea8d92a1fa12bd5d46d2cc
Tomas Votruba <tomas.vot@gmail.com>
2019-01-12 19:26:21 +0100

    move latte/twig files to own dirs

--- packages/LatteToTwigConverter/tests/LatteToTwigConverterTest.php
+++ packages/LatteToTwigConverter/tests/LatteToTwigConverterTest.php
@@ -28,22 +28,25 @@ final class LatteToTwigConverterTest extends AbstractContainerAwareTestCase
 
     public function provideData(): Iterator
     {
-        yield [__DIR__ . '/Source/variables.latte', __DIR__ . '/Source/expected-variables.twig'];
-        yield [__DIR__ . '/Source/block-file.latte', __DIR__ . '/Source/expected-block-file.twig'];
-        yield [__DIR__ . '/Source/filter.latte', __DIR__ . '/Source/expected-filter.twig'];
-        yield [__DIR__ . '/Source/loops.latte', __DIR__ . '/Source/expected-loops.twig'];
-        yield [__DIR__ . '/Source/conditions.latte', __DIR__ . '/Source/expected-conditions.twig'];
-        yield [__DIR__ . '/Source/comment.latte', __DIR__ . '/Source/expected-comment.twig'];
-        yield [__DIR__ . '/Source/capture.latte', __DIR__ . '/Source/expected-capture.twig'];
-        yield [__DIR__ . '/Source/javascript.latte', __DIR__ . '/Source/expected-javascript.twig'];
+        yield [__DIR__ . '/Source/latte/variables.latte', __DIR__ . '/Source/twig/variables.twig'];
+        yield [__DIR__ . '/Source/latte/block-file.latte', __DIR__ . '/Source/twig/block-file.twig'];
+        yield [__DIR__ . '/Source/latte/filter.latte', __DIR__ . '/Source/twig/filter.twig'];
+        yield [__DIR__ . '/Source/latte/loops.latte', __DIR__ . '/Source/twig/loops.twig'];
+        yield [__DIR__ . '/Source/latte/conditions.latte', __DIR__ . '/Source/twig/conditions.twig'];
+        yield [__DIR__ . '/Source/latte/comment.latte', __DIR__ . '/Source/twig/comment.twig'];
+        yield [__DIR__ . '/Source/latte/capture.latte', __DIR__ . '/Source/twig/capture.twig'];
+        yield [__DIR__ . '/Source/latte/javascript.latte', __DIR__ . '/Source/twig/javascript.twig'];
 
-        // new testing
         yield [__DIR__ . '/Source/latte/extends.latte', __DIR__ . '/Source/twig/extends.twig'];
         yield [__DIR__ . '/Source/latte/default.latte', __DIR__ . '/Source/twig/default.twig'];
         yield [__DIR__ . '/Source/latte/nested_variable.latte', __DIR__ . '/Source/twig/nested_variable.twig'];
         yield [__DIR__ . '/Source/latte/first_last.latte', __DIR__ . '/Source/twig/first_last.twig'];
-        yield [__DIR__ . '/Source/latte/filter.latte', __DIR__ . '/Source/twig/filter.twig'];
+        yield [
+            __DIR__ . '/Source/latte/filter_with_arguments.latte',
+            __DIR__ . '/Source/twig/filter_with_arguments.twig',
+        ];
         yield [__DIR__ . '/Source/latte/include.latte', __DIR__ . '/Source/twig/include.twig'];
+        yield [__DIR__ . '/Source/latte/spaceless.latte', __DIR__ . '/Source/twig/spaceless.twig'];
 
         // complex
         yield [
