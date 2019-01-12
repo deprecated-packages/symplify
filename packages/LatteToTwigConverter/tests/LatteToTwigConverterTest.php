b527896b53d3532a209575a0dc12e1f41a6bc6da
Tomas Votruba <tomas.vot@gmail.com>
2019-01-12 19:35:03 +0100

    split filter and number test

--- packages/LatteToTwigConverter/tests/LatteToTwigConverterTest.php
+++ packages/LatteToTwigConverter/tests/LatteToTwigConverterTest.php
@@ -30,7 +30,6 @@ final class LatteToTwigConverterTest extends AbstractContainerAwareTestCase
     {
         yield [__DIR__ . '/Source/latte/variables.latte', __DIR__ . '/Source/twig/variables.twig'];
         yield [__DIR__ . '/Source/latte/block-file.latte', __DIR__ . '/Source/twig/block-file.twig'];
-        yield [__DIR__ . '/Source/latte/filter.latte', __DIR__ . '/Source/twig/filter.twig'];
         yield [__DIR__ . '/Source/latte/loops.latte', __DIR__ . '/Source/twig/loops.twig'];
         yield [__DIR__ . '/Source/latte/conditions.latte', __DIR__ . '/Source/twig/conditions.twig'];
         yield [__DIR__ . '/Source/latte/comment.latte', __DIR__ . '/Source/twig/comment.twig'];
@@ -41,10 +40,15 @@ final class LatteToTwigConverterTest extends AbstractContainerAwareTestCase
         yield [__DIR__ . '/Source/latte/default.latte', __DIR__ . '/Source/twig/default.twig'];
         yield [__DIR__ . '/Source/latte/nested_variable.latte', __DIR__ . '/Source/twig/nested_variable.twig'];
         yield [__DIR__ . '/Source/latte/first_last.latte', __DIR__ . '/Source/twig/first_last.twig'];
+
+        // filters
+        yield [__DIR__ . '/Source/latte/filter.latte', __DIR__ . '/Source/twig/filter.twig'];
         yield [
             __DIR__ . '/Source/latte/filter_with_arguments.latte',
             __DIR__ . '/Source/twig/filter_with_arguments.twig',
         ];
+        yield [__DIR__ . '/Source/latte/filter_with_number.latte', __DIR__ . '/Source/twig/filter_with_number.twig'];
+
         yield [__DIR__ . '/Source/latte/include.latte', __DIR__ . '/Source/twig/include.twig'];
         yield [__DIR__ . '/Source/latte/spaceless.latte', __DIR__ . '/Source/twig/spaceless.twig'];
 
