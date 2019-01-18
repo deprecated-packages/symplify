0ccfc08f2d4df564dbfecdb1e98dd47c6574f439
Tomas Votruba <tomas.vot@gmail.com>
2019-01-18 15:00:17 +0100

    [Statie] Add 'group_by_field' filter

---
+++ packages/Statie/src/Templating/ArrayUtils.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\Statie\Templating;
+
+use Symplify\Statie\Exception\Templating\InvalidSortByCriteriaException;
+use function Safe\usort;
+
+final class ArrayUtils
+{
+    /**
+     * @param mixed[] $items
+     * @return mixed[]
+     */
+    public function sortByField(array $items, string $sortBy, string $direction = 'asc'): array
+    {
+        if (count($items) < 1) {
+            return $items;
+        }
+
+        $this->ensureKeyIsValid($items, $sortBy);
+
+        usort($items, function ($firstItem, $secondItem) use ($sortBy, $direction) {
+            $inverseConstant = $this->resolveInverseConstant($direction);
+
+            return ($firstItem[$sortBy] <=> $secondItem[$sortBy]) * $inverseConstant;
+        });
+
+        return $items;
+    }
+
+    /**
+     * @param mixed[] $items
+     * @return mixed[]
+     */
+    public function groupByField(array $items, string $groupBy): array
+    {
+        if (count($items) < 1) {
+            return $items;
+        }
+
+        $this->ensureKeyIsValid($items, $groupBy);
+
+        $grouped = [];
+        foreach ($items as $item) {
+            $grouped[$item[$groupBy]][] = $item;
+        }
+
+        ksort($grouped);
+
+        return $grouped;
+    }
+
+    /**
+     * @param mixed[]|object[] $items
+     */
+    private function ensureKeyIsValid(array $items, string $field): void
+    {
+        foreach ($items as $item) {
+            if (isset($item[$field])) {
+                continue;
+            }
+
+            throw new InvalidSortByCriteriaException($field, array_keys($item));
+        }
+    }
+
+    private function resolveInverseConstant(string $direction): int
+    {
+        $direction = strtolower($direction);
+
+        return in_array($direction, ['d', 'des', 'desc'], true) ? -1 : 1;
+    }
+}
