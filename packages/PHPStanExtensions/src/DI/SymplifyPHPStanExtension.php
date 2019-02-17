e9eabacabe0dd293299adb3969d7ca8860021364
Tomas Votruba <tomas.vot@gmail.com>
2019-02-17 12:55:01 +0100

    add SymplifyPHPStanExtension with exclude_rules support

--- packages/PHPStanExtensions/src/DI/SymplifyPHPStanExtension.php
+++ packages/PHPStanExtensions/src/DI/SymplifyPHPStanExtension.php
@@ -2,6 +2,28 @@
 
 namespace Symplify\PHPStanExtensions\DI;
 
-final class SymplifyPHPStanExtension extends Extension
+use Nette\DI\CompilerExtension;
+use PHPStan\Rules\Rule;
+
+final class SymplifyPHPStanExtension extends CompilerExtension
 {
+    public function beforeCompile(): void
+    {
+        $containerBuilder = $this->getContainerBuilder();
+
+        $excludedRules = $containerBuilder->parameters['excluded_rules'] ?? [];
+        if ($excludedRules === []) {
+            return;
+        }
+
+        $ruleDefinitions = $containerBuilder->findByType(Rule::class);
+
+        foreach ($ruleDefinitions as $name => $ruleDefinition) {
+            if (! in_array($ruleDefinition->getType(), $excludedRules, true)) {
+                continue;
+            }
+
+            $containerBuilder->removeDefinition($name);
+        }
+    }
 }
