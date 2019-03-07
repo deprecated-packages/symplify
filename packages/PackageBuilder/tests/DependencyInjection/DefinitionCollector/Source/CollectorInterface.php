ee9adad4bb07f6cfe9b8c933efe9ad59f8a81fe1
Tomas Votruba <tomas.vot@gmail.com>
2019-03-07 23:07:22 +0000

    [PackageBuilder] Remove deprecated collector compiler pass-related DefinitionCollector

--- packages/PackageBuilder/tests/DependencyInjection/DefinitionCollector/Source/CollectorInterface.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Symplify\PackageBuilder\Tests\DependencyInjection\DefinitionCollector\Source;
-
-interface CollectorInterface
-{
-    public function addCollected(CollectedInterface $collected): void;
-}
