1c4e6c21fc73ca08559735b2455ae428b722d8c6
Tomas Votruba <tomas.vot@gmail.com>
2019-03-29 00:02:23 +0100

    [Statie] Add source argument to tweet-post command

--- packages/Statie/packages/Tweeter/src/Command/TweetPostCommand.php
+++ packages/Statie/packages/Tweeter/src/Command/TweetPostCommand.php
@@ -3,11 +3,13 @@
 namespace Symplify\Statie\Tweeter\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
 use Symplify\PackageBuilder\Console\ShellCode;
+use Symplify\Statie\Configuration\StatieConfiguration;
 use Symplify\Statie\Tweeter\Configuration\Keys;
 use Symplify\Statie\Tweeter\Tweet\PostTweet;
 use Symplify\Statie\Tweeter\TweetFilter\TweetsFilter;
@@ -16,6 +18,11 @@ use Symplify\Statie\Tweeter\TwitterApi\TwitterApiWrapper;
 
 final class TweetPostCommand extends Command
 {
+    /**
+     * @var string
+     */
+    private const OPTION_SOURCE = 'source';
+
     /**
      * @var int
      */
@@ -41,12 +48,18 @@ final class TweetPostCommand extends Command
      */
     private $tweetsFilter;
 
+    /**
+     * @var StatieConfiguration
+     */
+    private $statieConfiguration;
+
     public function __construct(
         int $twitterMinimalGapInDays,
         TwitterApiWrapper $twitterApiWrapper,
         PostTweetsProvider $postTweetsProvider,
         TweetsFilter $tweetsFilter,
-        SymfonyStyle $symfonyStyle
+        SymfonyStyle $symfonyStyle,
+        StatieConfiguration $statieConfiguration
     ) {
         $this->twitterMinimalGapInDays = $twitterMinimalGapInDays;
         $this->twitterApiWrapper = $twitterApiWrapper;
@@ -55,16 +68,24 @@ final class TweetPostCommand extends Command
         $this->symfonyStyle = $symfonyStyle;
 
         parent::__construct();
+        $this->statieConfiguration = $statieConfiguration;
     }
 
     protected function configure(): void
     {
         $this->setName(CommandNaming::classToName(self::class));
+        $this->addArgument(self::OPTION_SOURCE, InputArgument::OPTIONAL, 'Directory to load page from.');
         $this->setDescription(sprintf('Publish new tweet from post "%s:" config', Keys::TWEET));
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        /** @var string|null $source */
+        $source = $input->getArgument(self::OPTION_SOURCE);
+        if ($source) {
+            $this->statieConfiguration->setSourceDirectory($source);
+        }
+
         // to soon to tweet after recent tweet
         if ($this->isNewTweetAllowed() === false) {
             return $this->reportTooSoonToTweet();
