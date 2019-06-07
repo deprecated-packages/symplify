afc3547dee26c121763d4d431c0b709b36d614e7
Jan Mikeš <j.mikes@me.com>
2019-06-07 23:03:54 +0200

    Ignoring simply implemented class if service with interface name already exists

--- packages/PackageBuilder/tests/DependencyInjection/CompilerPass/AutowireSinglyImplementedCompilerPass/Source/AutowireSinglyImplementedInterfaceKernel.php
+++ packages/PackageBuilder/tests/DependencyInjection/CompilerPass/AutowireSinglyImplementedCompilerPass/Source/AutowireSinglyImplementedInterfaceKernel.php
@@ -1,11 +1,10 @@
-<?php declare (strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Symplify\PackageBuilder\Tests\DependencyInjection\CompilerPass\AutowireSinglyImplementedCompilerPass\Source;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
-use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutoBindParametersCompilerPass;
 use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutowireSinglyImplementedCompilerPass;
 use Symplify\PackageBuilder\HttpKernel\SimpleKernelTrait;
 
--- packages/PackageBuilder/tests/DependencyInjection/CompilerPass/AutowireSinglyImplementedCompilerPass/Source/ServiceInterface.php
+++ packages/PackageBuilder/tests/DependencyInjection/CompilerPass/AutowireSinglyImplementedCompilerPass/Source/ServiceInterface.php
@@ -1,8 +1,7 @@
-<?php declare (strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Symplify\PackageBuilder\Tests\DependencyInjection\CompilerPass\AutowireSinglyImplementedCompilerPass\Source;
 
 interface ServiceInterface
 {
-
 }
