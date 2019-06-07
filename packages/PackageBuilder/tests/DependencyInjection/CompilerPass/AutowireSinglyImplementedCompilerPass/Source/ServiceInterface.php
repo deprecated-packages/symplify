b8979bb05e727db962942c35426da2d8468ce742
Jan Mikeš <j.mikes@me.com>
2019-06-07 22:47:22 +0200

    Added failing test - Singly implemented service overwrites interface if registered

---
+++ packages/PackageBuilder/tests/DependencyInjection/CompilerPass/AutowireSinglyImplementedCompilerPass/Source/ServiceInterface.php
@@ -0,0 +1,8 @@
+<?php declare (strict_types=1);
+
+namespace Symplify\PackageBuilder\Tests\DependencyInjection\CompilerPass\AutowireSinglyImplementedCompilerPass\Source;
+
+interface ServiceInterface
+{
+
+}
