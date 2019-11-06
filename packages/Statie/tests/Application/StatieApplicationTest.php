5e35ffa6ce71458afedb325a99dfe18e3292871b
Tomas Votruba <tomas.vot@gmail.com>
2019-11-06 21:53:18 +0100

    [Statie] Drop Latte support to lower the complexity

--- packages/Statie/tests/Application/StatieApplicationTest.php
+++ packages/Statie/tests/Application/StatieApplicationTest.php
@@ -9,7 +9,6 @@ use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
 use Symplify\Statie\Application\StatieApplication;
 use Symplify\Statie\Exception\Utils\MissingDirectoryException;
 use Symplify\Statie\HttpKernel\StatieKernel;
-use Symplify\Statie\Latte\Loader\ArrayLoader;
 
 /**
  * @requires PHP < 7.4
@@ -21,17 +20,11 @@ final class StatieApplicationTest extends AbstractKernelTestCase
      */
     private $statieApplication;
 
-    /**
-     * @var ArrayLoader
-     */
-    private $arrayLoader;
-
     protected function setUp(): void
     {
         $this->bootKernelWithConfigs(StatieKernel::class, [__DIR__ . '/StatieApplicationSource/statie.yml']);
 
         $this->statieApplication = self::$container->get(StatieApplication::class);
-        $this->arrayLoader = self::$container->get(ArrayLoader::class);
 
         $symfonyStyle = self::$container->get(SymfonyStyle::class);
         $symfonyStyle->setVerbosity(OutputInterface::VERBOSITY_QUIET);
@@ -55,7 +48,6 @@ final class StatieApplicationTest extends AbstractKernelTestCase
             __DIR__ . '/StatieApplicationSource/output/index.html'
         );
 
-        $this->assertFileExists(__DIR__ . '/StatieApplicationSource/output/feed.xml');
         $this->assertFileExists(__DIR__ . '/StatieApplicationSource/output/atom.rss');
 
         // markdown test
@@ -64,24 +56,11 @@ final class StatieApplicationTest extends AbstractKernelTestCase
             __DIR__ . '/StatieApplicationSource/expected-file.html',
             __DIR__ . '/StatieApplicationSource/output/file/index.html'
         );
-
-        $this->assertNotEmpty($this->arrayLoader->getContent('_layouts/default.latte'));
     }
 
     public function testRunForMissingSource(): void
     {
         $this->expectException(MissingDirectoryException::class);
         $this->statieApplication->run('missing', 'random');
-    }
-
-    public function testForSuggestedSource(): void
-    {
-        $this->statieApplication->run(
-            __DIR__ . '/StatieApplicationSource/source',
-            __DIR__ . '/StatieApplicationSource/output'
-        );
-
-        $this->expectExceptionMessageRegExp('#Did you mean "_layouts/default.latte"#');
-        $this->assertNotEmpty($this->arrayLoader->getContent('layout/default.latte'));
     }
 }
