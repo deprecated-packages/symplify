739108ef65f06c8b48170c40bc4b2b506cd97bff
jawira <dev@tugal.be>
2019-11-28 00:01:51 +0100

    ecs again :(

--- packages/ChangelogLinker/src/Github/GithubRepositoryFromRemoteResolver.php
+++ packages/ChangelogLinker/src/Github/GithubRepositoryFromRemoteResolver.php
@@ -4,21 +4,21 @@ namespace Symplify\ChangelogLinker\Github;
 
 use Nette\Utils\Strings;
 use Symplify\ChangelogLinker\Exception\Git\InvalidGitRemoteException;
-use function parse_url;
-use function pathinfo;
-use function rtrim;
-use function sprintf;
-use function str_replace;
 use const PATHINFO_DIRNAME;
 use const PATHINFO_FILENAME;
 use const PHP_URL_HOST;
 use const PHP_URL_PATH;
 use const PHP_URL_SCHEME;
+use function parse_url;
+use function pathinfo;
+use function rtrim;
+use function sprintf;
+use function str_replace;
 
 final class GithubRepositoryFromRemoteResolver
 {
     /**
-     * The scheme from web URLs
+     * @var string The scheme from web URLs
      */
     private const HTTPS_SCHEME = 'https';
 
@@ -26,10 +26,10 @@ final class GithubRepositoryFromRemoteResolver
     {
         // Normalizing "https" url
         if (Strings::startsWith($url, self::HTTPS_SCHEME)) {
-            $url_scheme    = parse_url($url, PHP_URL_SCHEME);
-            $url_host      = parse_url($url, PHP_URL_HOST);
-            $url_path      = parse_url($url, PHP_URL_PATH);
-            $path_dirname  = pathinfo($url_path, PATHINFO_DIRNAME);
+            $url_scheme = parse_url($url, PHP_URL_SCHEME);
+            $url_host = parse_url($url, PHP_URL_HOST);
+            $url_path = parse_url($url, PHP_URL_PATH);
+            $path_dirname = pathinfo($url_path, PATHINFO_DIRNAME);
             $path_filename = pathinfo($url_path, PATHINFO_FILENAME);
 
             return sprintf('%s://%s%s/%s', $url_scheme, $url_host, $path_dirname, $path_filename);
