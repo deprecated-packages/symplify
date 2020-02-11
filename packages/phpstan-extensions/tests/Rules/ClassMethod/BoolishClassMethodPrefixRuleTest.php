fba07f0d56f80ede07303be7cdbe23ac01861273
TomasVotruba <tomas.vot@gmail.com>
2020-02-11 16:45:05 +0100

    [PHPStanExtensions] Make BoolishClassMethodPrefixRule skip parent interface required methods

--- packages/phpstan-extensions/tests/Rules/ClassMethod/BoolishClassMethodPrefixRuleTest.php
+++ packages/phpstan-extensions/tests/Rules/ClassMethod/BoolishClassMethodPrefixRuleTest.php
@@ -4,27 +4,36 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanExtensions\Tests\Rules\ClassMethod;
 
+use Iterator;
 use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
 use Symplify\PHPStanExtensions\Rules\ClassMethod\BoolishClassMethodPrefixRule;
 
 final class BoolishClassMethodPrefixRuleTest extends RuleTestCase
 {
-    public function testRule(): void
+    /**
+     * @dataProvider provideData()
+     */
+    public function testRule(string $analysedFilePath, array $expectedErrorsWithLine): void
     {
-        $this->analyse(
-            [__DIR__ . '/Source/ClassWithBoolishMethods.php'],
+        $this->analyse([$analysedFilePath], $expectedErrorsWithLine);
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [
+            __DIR__ . '/Source/ClassWithBoolishMethods.php',
             [
                 ['Method "honesty()" returns bool type, so the name should start with is/has/was...', 9],
                 ['Method "thatWasGreat()" returns bool type, so the name should start with is/has/was...', 14],
-            ]
-        );
+            ],
+        ];
 
-        $this->analyse([__DIR__ . '/Source/ClassWithEmptyReturn.php'], []);
+        yield [__DIR__ . '/Source/ClassWithEmptyReturn.php', []];
 
-        $this->analyse([__DIR__ . '/Source/ClassThatImplementsInterface.php'], [
-            ['Method "nothing()" returns bool type, so the name should start with is/has/was...', 9],
-        ]);
+        yield [__DIR__ . '/Source/ClassThatImplementsInterface.php', []];
+
+        yield [__DIR__ . '/Source/SkipRequiredByInterface.php', []];
     }
 
     protected function getRule(): Rule
