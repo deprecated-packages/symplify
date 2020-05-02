c11231a71879d69bd37bbf563c47bcbf5c75b8df
Jibé Barth <barth.jib@gmail.com>
2020-05-02 16:27:23 +0200

    [SSD] Split tests

--- packages/symfony-static-dumper/tests/Application/SymfonyStaticDumperApplicationTest.php
+++ packages/symfony-static-dumper/tests/Application/SymfonyStaticDumperApplicationTest.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
 use Symplify\SymfonyStaticDumper\Application\SymfonyStaticDumperApplication;
-use Symplify\SymfonyStaticDumper\Routing\RoutesProvider;
 use Symplify\SymfonyStaticDumper\Tests\TestProject\HttpKernel\TestSymfonyStaticDumperKernel;
 
 final class SymfonyStaticDumperApplicationTest extends AbstractKernelTestCase
@@ -29,21 +28,17 @@ final class SymfonyStaticDumperApplicationTest extends AbstractKernelTestCase
      */
     private $symfonyStaticDumperApplication;
 
-    /**
-     * @var RoutesProvider
-     */
-    private $routesProvider;
-
     protected function setUp(): void
     {
         $this->bootKernel(TestSymfonyStaticDumperKernel::class);
 
         $this->symfonyStaticDumperApplication = self::$container->get(SymfonyStaticDumperApplication::class);
-        $this->routesProvider = self::$container->get(RoutesProvider::class);
 
         // disable output in tests
         $symfonyStyle = self::$container->get(SymfonyStyle::class);
         $symfonyStyle->setVerbosity(OutputInterface::VERBOSITY_QUIET);
+
+        $this->symfonyStaticDumperApplication->run(__DIR__ . '/../test_project/public', self::OUTPUT_DIRECTORY);
     }
 
     protected function tearDown(): void
@@ -51,23 +46,24 @@ final class SymfonyStaticDumperApplicationTest extends AbstractKernelTestCase
         FileSystem::delete(self::OUTPUT_DIRECTORY);
     }
 
-    public function test(): void
+    public function testCssIsDumped(): void
     {
-        $this->symfonyStaticDumperApplication->run(__DIR__ . '/../test_project/public', self::OUTPUT_DIRECTORY);
-
         // css
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/some.css');
         $this->assertFileEquals(self::EXPECTED_DIRECTORY . '/some.css', self::OUTPUT_DIRECTORY . '/some.css');
+    }
 
-        // controllers
-        $this->assertCount(6, $this->routesProvider->provide());
-
+    public function testRenderHtml(): void
+    {
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/kedlubna/index.html');
         $this->assertFileEquals(
             self::EXPECTED_DIRECTORY . '/kedlubna/index.html',
             self::OUTPUT_DIRECTORY . '/kedlubna/index.html'
         );
+    }
 
+    public function testRenderJson(): void
+    {
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/api.json');
 
         $expectedFileContent = FileSystem::read(self::EXPECTED_DIRECTORY . '/api.json');
@@ -77,8 +73,10 @@ final class SymfonyStaticDumperApplicationTest extends AbstractKernelTestCase
         $outputFileContent = trim($outputFileContent);
 
         $this->assertSame($expectedFileContent, $outputFileContent);
+    }
 
-        // static page with TemplateController
+    public function testRenderPageWithTemplateController(): void
+    {
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/static/index.html');
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/static.html');
         $this->assertFileEquals(
@@ -89,7 +87,10 @@ final class SymfonyStaticDumperApplicationTest extends AbstractKernelTestCase
             self::OUTPUT_DIRECTORY . '/static.html',
             self::OUTPUT_DIRECTORY . '/static/index.html'
         );
+    }
 
+    public function testRenderControllerWithOneArgument(): void
+    {
         // controller with 1 arg
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/one-param/1/index.html');
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/one-param/2/index.html');
@@ -102,7 +103,10 @@ final class SymfonyStaticDumperApplicationTest extends AbstractKernelTestCase
             self::EXPECTED_DIRECTORY . '/one-param/2.html',
             self::OUTPUT_DIRECTORY . '/one-param/2/index.html'
         );
+    }
 
+    public function testRenderControllerWithMultipleArguments(): void
+    {
         // Controller with 2 args
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/test/1/index.html');
         $this->assertFileExists(self::OUTPUT_DIRECTORY . '/test/2/index.html');
