c4a7f07e69150d7424486c150487c4121dcb4686
TomasVotruba <tomas.vot@gmail.com>
2020-05-16 13:18:53 +0200

    fix test case class name

--- packages/smart-file-system/tests/FileSystemFilter/FileSystemFilterTest.php
+++ packages/smart-file-system/tests/FileSystemFilter/FileSystemFilterTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\SmartFileSystem\Tests\FileSystemFilter;
 
-use PHPStan\Testing\TestCase;
+use PHPUnit\Framework\TestCase;
 use Symplify\SmartFileSystem\FileSystemFilter;
 
 final class FileSystemFilterTest extends TestCase
