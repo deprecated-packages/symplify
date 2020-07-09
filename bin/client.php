9f29384726463c3fa13a71b6aad1a3a3439d7cb5
Jan Mikeš <j.mikes@me.com>
2020-07-09 19:47:38 +0200

    Socket server + client 2-way communication

--- bin/client.php
+++ bin/client.php
@@ -7,12 +7,27 @@ use React\Socket\TcpConnector;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$loop = \React\EventLoop\Factory::create();
+if ($argc < 3) {
+    throw new RuntimeException('Port must be specified');
+}
 
+$address = (string) $argv[1];
+$id = (string) $argv[2];
+
+$loop = \React\EventLoop\Factory::create();
 $tcpConector = new TcpConnector($loop);
-$tcpConector->connect('tcp://127.0.0.1:55330')->then(function (ConnectionInterface $connection) use ($loop): void {
-    $connection->pipe(new React\Stream\WritableResourceStream(STDOUT, $loop));
-    $connection->write("Hello World!\n");
+$tcpConector->connect($address)->then(function (ConnectionInterface $connection) use ($loop, $id): void {
+
+    // $connection->pipe(new React\Stream\WritableResourceStream(STDOUT, $loop));
+    $connection->write('hello');
+
+    $connection->on('data', function($data) use ($connection, $id) {
+        if ($data === 'analyse') {
+            sleep(1);
+            $connection->write('exit');
+        }
+    });
+
 });
 
 $loop->run();
--- bin/server.php
+++ bin/server.php
@@ -2,27 +2,47 @@
 
 declare(strict_types=1);
 
+use React\ChildProcess\Process;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $loop = \React\EventLoop\Factory::create();
+
+// 0 will create local socket server with random available port
 $server = new \React\Socket\TcpServer(0, $loop);
 
-$server->on('connection', function (React\Socket\ConnectionInterface $connection) use ($loop) {
-    $connection->pipe(new React\Stream\WritableResourceStream(STDOUT, $loop));
-    $connection->write("Hello " . $connection->getRemoteAddress() . "!\n");
-    $connection->write("Welcome to this amazing server!\n");
-    $connection->write("Here's a tip: don't say anything.\n");
+$processes = [];
 
-    $connection->on('data', function ($data) use ($connection) {
-        echo $data . "\n";
-        $connection->close();
+$server->on('connection', function (React\Socket\ConnectionInterface $connection) use ($loop) {
+    $connection->on('data', function ($data) use ($connection, $loop) {
+        echo $connection->getRemoteAddress() . ': ' . $data . PHP_EOL;
+
+        if ($data === 'exit') {
+            $connection->close();
+        }
+
+        if ($data === 'hello') {
+            // $connection->pipe(new React\Stream\WritableResourceStream(STDOUT, $loop));
+            $connection->write('analyse');
+        }
     });
 });
 
-$server->on('error', function (Exception $e) {
-    echo 'Error' . $e->getMessage() . PHP_EOL;
-});
+for($i=0 ; $i<=2 ; $i++) {
+    $id = uniqid();
+    $command = 'php bin/client.php ' . $server->getAddress() . ' ' . $id;
+    $process = new Process($command, null, null, []);
+    $process->start($loop);
+    $processes[$id] = $id;
 
-echo $server->getAddress() . "\n";
+    $process->on('exit', function ($exitcode) use ($server, &$processes, $id) {
+        unset($processes[$id]);
+
+        if (count($processes) === 0) {
+            echo 'No more processes, closing server' . PHP_EOL;
+            $server->close();
+        }
+    });
+}
 
 $loop->run();
