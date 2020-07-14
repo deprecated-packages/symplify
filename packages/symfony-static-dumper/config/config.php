aed0017d237d504464b40b1761f63fccb6db731e
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 13:28:21 +0200

    [SymfonyStaticDumper] yaml config to php

---
+++ packages/symfony-static-dumper/config/config.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->public();
+
+    $services->load('Symplify\SymfonyStaticDumper\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/Exception/*']);
+
+    $services->set(FinderSanitizer::class);
+
+    $services->set(SymfonyStyleFactory::class);
+
+    $services->set(SymfonyStyle::class)
+        ->factory([ref(SymfonyStyleFactory::class), 'create']);
+};
