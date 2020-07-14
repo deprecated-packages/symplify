5657e98329afe7b17405f628f5b0faf243a61988
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 13:21:06 +0200

    [EasyHydrator] yaml config to php

--- packages/easy-hydrator/src/DependencyInjection/Extension/EasyHydratorExtension.php
+++ packages/easy-hydrator/src/DependencyInjection/Extension/EasyHydratorExtension.php
@@ -7,13 +7,13 @@ namespace Symplify\EasyHydrator\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 final class EasyHydratorExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $containerBuilder): void
     {
-        $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../../../config'));
-        $loader->load('config.yaml');
+        $loader = new PhpFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../../../config'));
+        $loader->load('config.php');
     }
 }
