cdfaa6db0c92221082592abbf32e8ad71c60fa96
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 18:41:31 +0200

    fix build

--- packages/monorepo-builder/config/config.php
+++ packages/monorepo-builder/config/config.php
@@ -9,12 +9,19 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../packages/**/config/config.yaml');
 
     $parameters = $containerConfigurator->parameters();
+
     $parameters->set('package_directories', ['packages']);
+
     $parameters->set('package_directories_excludes', []);
+
     $parameters->set('data_to_append', []);
+
     $parameters->set('data_to_remove', []);
+
     $parameters->set('package_alias_format', '<major>.<minor>-dev');
+
     $parameters->set('inline_sections', ['keywords']);
+
     $parameters->set(
         'section_order',
         [
