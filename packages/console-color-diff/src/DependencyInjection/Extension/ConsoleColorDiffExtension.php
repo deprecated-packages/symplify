7a04cc41f4c08ea708e97326395462abf27991fe
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 13:00:14 +0200

    [ConsoleColorDiff] yaml config to php

--- packages/console-color-diff/src/DependencyInjection/Extension/ConsoleColorDiffExtension.php
+++ packages/console-color-diff/src/DependencyInjection/Extension/ConsoleColorDiffExtension.php
@@ -7,14 +7,14 @@ namespace Symplify\ConsoleColorDiff\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 final class ConsoleColorDiffExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $containerBuilder): void
     {
-        $yamlFileLoader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../../../config'));
+        $yamlFileLoader = new PhpFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../../../config'));
 
-        $yamlFileLoader->load('config.yaml');
+        $yamlFileLoader->load('config.php');
     }
 }
