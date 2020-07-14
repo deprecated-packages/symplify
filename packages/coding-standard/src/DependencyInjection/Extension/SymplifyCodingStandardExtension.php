4732a0c6e8d354e9cc0ce1d213388cccdc81b4f6
TomasVotruba <tomas.vot@gmail.com>
2020-07-14 17:25:19 +0200

    [CS] Move config from YAML to PHP

--- packages/coding-standard/src/DependencyInjection/Extension/SymplifyCodingStandardExtension.php
+++ packages/coding-standard/src/DependencyInjection/Extension/SymplifyCodingStandardExtension.php
@@ -7,16 +7,16 @@ namespace Symplify\CodingStandard\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symplify\EasyCodingStandard\Yaml\FileLoader\CheckerTolerantYamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 final class SymplifyCodingStandardExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $containerBuilder): void
     {
         // needed for parameter shifting of sniff/fixer params
-        $checkerTolerantYamlFileLoader = new CheckerTolerantYamlFileLoader($containerBuilder, new FileLocator(
+        $checkerTolerantYamlFileLoader = new PhpFileLoader($containerBuilder, new FileLocator(
             __DIR__ . '/../../../config'
         ));
-        $checkerTolerantYamlFileLoader->load('config.yaml');
+        $checkerTolerantYamlFileLoader->load('config.php');
     }
 }
