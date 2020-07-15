c6b7f6e6417d81fb335b9d5eeb96208bcf0b9d33
TomasVotruba <tomas.vot@gmail.com>
2020-07-16 00:18:03 +0200

    [ECS] Rename exclude_files to exclude_paths

--- ecs.php
+++ ecs.php
@@ -6,13 +6,10 @@ use PHP_CodeSniffer\Standards\Squiz\Sniffs\Arrays\ArrayDeclarationSniff;
 use PhpCsFixer\Fixer\Operator\UnaryOperatorSpacesFixer;
 use PhpCsFixer\Fixer\PhpTag\BlankLineAfterOpeningTagFixer;
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer;
-<<<<<<< HEAD
-=======
 use SlevomatCodingStandard\Sniffs\Exceptions\ReferenceThrowableOnlySniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff;
 use Symplify\CodingStandard\Sniffs\Debug\CommentedOutCodeSniff;
 use Symplify\EasyCodingStandard\Configuration\Option;
->>>>>>> 7b3f4a4aa... make use of PHP
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
@@ -21,11 +18,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters->set(Option::PATHS, ['packages', 'tests']);
 
-<<<<<<< HEAD
-    $parameters->set('exclude_files', ['*/Fixture/*', '*/Source/*', 'packages/easy-coding-standard/compiler/build/scoper.inc.php', 'packages/easy-hydrator/tests/Fixture/TypedProperty.php', 'packages/easy-hydrator/tests/TypedPropertiesTest.php']);
-
-    $parameters->set('skip', ['PHP_CodeSniffer\Standards\Squiz\Sniffs\Arrays\ArrayDeclarationSniff' => null, BlankLineAfterOpeningTagFixer::class => null, UnaryOperatorSpacesFixer::class => null, PhpUnitStrictFixer::class => ['packages/easy-coding-standard/tests/Indentation/IndentationTest.php'], 'SlevomatCodingStandard\Sniffs\Exceptions\ReferenceThrowableOnlySniff.ReferencedGeneralException' => ['packages/coding-standard/src/Rules/NoDefaultExceptionRule.php'], 'SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff.MissingNativeTypeHint' => ['*Sniff.php', '*YamlFileLoader.php', 'packages/package-builder/src/Reflection/PrivatesCaller.php'], 'Symplify\CodingStandard\Sniffs\Debug\CommentedOutCodeSniff.Found' => ['packages/latte-to-twig-converter/src/CaseConverter/*CaseConverter.php']]);
-=======
     $parameters->set(Option::EXCLUDE_PATHS, [
         '*/Fixture/*',
         '*/Source/*',
@@ -48,5 +40,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ],
         CommentedOutCodeSniff::class => ['packages/latte-to-twig-converter/src/CaseConverter/*CaseConverter.php']
     ]);
->>>>>>> 7b3f4a4aa... make use of PHP
 };
