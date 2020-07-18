28e6269b314a5198c904fa05bf2366499952a05a
TomasVotruba <tomas.vot@gmail.com>
2020-07-18 12:45:26 +0200

    [SetConfigResolver] Prefer .php version, return instant match

--- packages/set-config-resolver/src/SetResolver.php
+++ packages/set-config-resolver/src/SetResolver.php
@@ -12,6 +12,8 @@ use Symfony\Component\Finder\SplFileInfo;
 use Symplify\SetConfigResolver\Console\Option\OptionName;
 use Symplify\SetConfigResolver\Console\OptionValueResolver;
 use Symplify\SetConfigResolver\Exception\SetNotFoundException;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class SetResolver
 {
@@ -26,12 +28,18 @@ final class SetResolver
     private $optionValueResolver;
 
     /**
+     * @var FinderSanitizer
+     */
+    private $finderSanitizer;
+
+    /**
      * @param string[] $optionNames
      */
     public function __construct(array $optionNames = OptionName::SET)
     {
         $this->optionNames = $optionNames;
         $this->optionValueResolver = new OptionValueResolver();
+        $this->finderSanitizer = new FinderSanitizer();
     }
 
     public function detectFromInputAndDirectory(InputInterface $input, string $setsDirectory): ?string
@@ -46,57 +54,53 @@ final class SetResolver
 
     public function detectFromNameAndDirectory(string $setName, string $configDirectory): string
     {
-        $nearestMatches = $this->findNearestMatchingFiles($configDirectory, $setName);
-        if (count($nearestMatches) === 0) {
+        $nearestMatch = $this->findNearestMatchingFileInfo($configDirectory, $setName);
+        if ($nearestMatch === null) {
             $this->reportSetNotFound($configDirectory, $setName);
         }
-
-        /** @var SplFileInfo $nearestMatch */
-        $nearestMatch = array_shift($nearestMatches);
 
         return $nearestMatch->getPathname();
     }
 
-    /**
-     * @return SplFileInfo[]
-     */
-    private function findNearestMatchingFiles(string $configDirectory, string $setName): array
+    private function findNearestMatchingFileInfo(string $configDirectory, string $setName): ?SmartFileInfo
     {
-        $configFiles = Finder::create()
-            ->files()
-            ->in($configDirectory)
-            ->getIterator();
+        $configFileInfos = $this->getConfigFileInfos($configDirectory);
 
         $nearestMatches = [];
-
         $setName = Strings::lower($setName);
 
         // the version must match, so 401 is not compatible with 40
         $setVersion = $this->matchVersionInTheEnd($setName);
 
-        foreach ($configFiles as $configFile) {
+        foreach ($configFileInfos as $configFileInfo) {
             // only similar configs, not too far
             // this allows to match "Symfony.40" to "symfony40" config
-            $fileNameWithoutExtension = pathinfo($configFile->getFilename(), PATHINFO_FILENAME);
+            $fileNameWithoutExtension = $configFileInfo->getBasenameWithoutSuffix();
+
             $distance = levenshtein($fileNameWithoutExtension, $setName);
             if ($distance > 2) {
                 continue;
             }
 
-            if ($setVersion) {
-                $fileVersion = $this->matchVersionInTheEnd($fileNameWithoutExtension);
-                if ($setVersion !== $fileVersion) {
-                    // not a version match
-                    continue;
-                }
+            if (! $this->isVersionMatch($setVersion, $fileNameWithoutExtension)) {
+                continue;
             }
 
-            $nearestMatches[$distance] = $configFile;
+            // exact match
+            if ($distance === 0) {
+                return $configFileInfo;
+            }
+
+            $nearestMatches[$distance] = $configFileInfo;
+        }
+
+        if (count($nearestMatches) === 0) {
+            return null;
         }
 
         ksort($nearestMatches);
 
-        return $nearestMatches;
+        return array_shift($nearestMatches);
     }
 
     private function reportSetNotFound(string $configDirectory, string $setName): void
@@ -198,5 +202,42 @@ final class SetResolver
         }
 
         return $setsListInString;
+    }
+
+    /**
+     * @return SmartFileInfo[]
+     */
+    private function getConfigFileInfos(string $configDirectory): array
+    {
+        $configFinder = Finder::create()
+            ->files()
+            ->in($configDirectory)
+            ->sort(function (SplFileInfo $firstFileInfo, SplFileInfo $secondFileInfo) {
+                // prefer PHP files first
+                $firstFileSuffix = pathinfo($firstFileInfo->getFilename(), PATHINFO_EXTENSION);
+                if ($firstFileSuffix !== 'php') {
+                    return 1;
+                }
+
+                $secondFileInfo = pathinfo($secondFileInfo->getFilename(), PATHINFO_EXTENSION);
+                if ($secondFileInfo === 'php') {
+                    return -1;
+                }
+
+                return 0;
+            });
+
+        return $this->finderSanitizer->sanitize($configFinder);
+    }
+
+    private function isVersionMatch(?string $setVersion, string $fileNameWithoutExtension): bool
+    {
+        if ($setVersion === null) {
+            return true;
+        }
+
+        $fileVersion = $this->matchVersionInTheEnd($fileNameWithoutExtension);
+
+        return $setVersion === $fileVersion;
     }
 }
