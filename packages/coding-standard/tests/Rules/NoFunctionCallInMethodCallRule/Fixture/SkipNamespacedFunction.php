c12ae6c07b40970fe8e87a7b78ca77b9abae9e41
TomasVotruba <tomas.vot@gmail.com>
2020-07-22 20:42:05 +0200

    [CS] Allow nesting func call in method call args if namespaced

---
+++ packages/coding-standard/tests/Rules/NoFunctionCallInMethodCallRule/Fixture/SkipNamespacedFunction.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Rules\NoFunctionCallInMethodCallRule\Fixture;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+
+final class SkipNamespacedFunction
+{
+    public function something()
+    {
+        $this->process(ref('reference'));
+    }
+
+    private function process(\Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator $ref)
+    {
+    }
+}
