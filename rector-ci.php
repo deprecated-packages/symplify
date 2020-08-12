6667730e11d2452bcfdc8adb616ae57bab5d8363
TomasVotruba <tomas.vot@gmail.com>
2020-08-12 22:25:40 +0200

    update dependencies

--- rector-ci.php
+++ rector-ci.php
@@ -24,10 +24,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-
     $parameters->set(Option::AUTOLOAD_PATHS, [__DIR__ . '/tests/bootstrap.php', __DIR__ . '/ecs.php']);
 
-    $parameters->set(Option::SETS, [SetList::CODE_QUALITY, SetList::DEAD_CODE, SetList::CODING_STYLE, SetList::PHP_70, SetList::PHP_71, SetList::PHP_72]);
+    $parameters->set(Option::SETS, [
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::CODING_STYLE,
+        SetList::PHP_70,
+        SetList::PHP_71,
+        SetList::PHP_72,
+    ]);
 
     $parameters->set(Option::PATHS, [__DIR__ . '/packages']);
 
@@ -40,5 +46,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         # parameter Symfony autowire hack
         __DIR__ . '/packages/changelog-linker/src/DependencyInjection/Dummy/ResolveAutowiringExceptionHelper.php',
         __DIR__ . '/packages/monorepo-builder/packages/init/templates/*',
+        __DIR__ . '/packages/easy-hydrator/tests/Fixture/TypedProperty.php',
+        __DIR__ . '/packages/easy-hydrator/tests/TypedPropertiesTest.php',
     ]);
 };
