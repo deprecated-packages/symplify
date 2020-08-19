d2b3eca27eb4a05404042d0a28185985656cee87
TomasVotruba <tomas.vot@gmail.com>
2020-08-19 16:13:41 +0200

    fix exception message

--- packages/package-builder/src/Tests/AbstractKernelTestCase.php
+++ packages/package-builder/src/Tests/AbstractKernelTestCase.php
@@ -99,7 +99,7 @@ abstract class AbstractKernelTestCase extends TestCase
 
         throw new MissingInterfaceException(sprintf(
             '"%s" is missing an "%s" interface',
-            static::class,
+            get_class(static::$kernel),
             ExtraConfigAwareKernelInterface::class
         ));
     }
