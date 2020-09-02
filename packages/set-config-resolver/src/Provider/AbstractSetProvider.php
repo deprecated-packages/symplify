cf43b0044a1ce19aab46e064bdb3358f39c365aa
Tomas Votruba <tomas.vot@gmail.com>
2020-09-02 17:01:21 +0200

    [SetConfigResolver] Fix set loading in realpath phar (#2112)

--- packages/set-config-resolver/src/Provider/AbstractSetProvider.php
+++ packages/set-config-resolver/src/Provider/AbstractSetProvider.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Symplify\SetConfigResolver\Provider;
 
+use Nette\Utils\Strings;
+use Symplify\EasyTesting\Exception\ShouldNotHappenException;
 use Symplify\SetConfigResolver\Contract\SetProviderInterface;
+use Symplify\SetConfigResolver\Exception\SetNotFoundException;
 use Symplify\SetConfigResolver\ValueObject\Set;
 
 abstract class AbstractSetProvider implements SetProviderInterface
@@ -22,11 +25,11 @@ abstract class AbstractSetProvider implements SetProviderInterface
         return $setNames;
     }
 
-    public function provideByName(string $setName): ?Set
+    public function provideByName(string $desiredSetName): ?Set
     {
         // 1. name-based approach
         foreach ($this->provide() as $set) {
-            if ($set->getName() !== $setName) {
+            if ($set->getName() !== $desiredSetName) {
                 continue;
             }
 
@@ -35,13 +38,29 @@ abstract class AbstractSetProvider implements SetProviderInterface
 
         // 2. path-based approach
         foreach ($this->provide() as $set) {
-            if (realpath($set->getSetFileInfo()->getRealPath()) !== realpath($setName)) {
+            // possible bug for PHAR files, see https://bugs.php.net/bug.php?id=52769
+            // this is very tricky to handle, see https://stackoverflow.com/questions/27838025/how-to-get-a-phar-file-real-directory-within-the-phar-file-code
+            $setUniqueId = $this->resolveSetUniquePathId($set->getSetFileInfo()->getPathname());
+            $desiredSetUniqueId = $this->resolveSetUniquePathId($desiredSetName);
+
+            if ($setUniqueId !== $desiredSetUniqueId) {
                 continue;
             }
 
             return $set;
         }
 
-        return null;
+        $message = sprintf('Set "%s" was not found', $desiredSetName);
+        throw new SetNotFoundException($message, $desiredSetName, $this->provideSetNames());
+    }
+
+    private function resolveSetUniquePathId(string $setPath): string
+    {
+        $setPath = Strings::after($setPath, DIRECTORY_SEPARATOR, -2);
+        if ($setPath === null) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $setPath;
     }
 }
