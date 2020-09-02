8dbe54746573d41a9e6fd5f3412a912e4512a369
TomasVotruba <tomas.vot@gmail.com>
2020-09-02 16:56:10 +0200

    [SetConfigResolver] Fix set loading in realpath phar

--- packages/set-config-resolver/src/Provider/AbstractSetProvider.php
+++ packages/set-config-resolver/src/Provider/AbstractSetProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\SetConfigResolver\Provider;
 
+use Nette\Utils\Strings;
 use Symplify\EasyTesting\Exception\ShouldNotHappenException;
 use Symplify\SetConfigResolver\Contract\SetProviderInterface;
 use Symplify\SetConfigResolver\Exception\SetNotFoundException;
@@ -38,7 +39,11 @@ abstract class AbstractSetProvider implements SetProviderInterface
         // 2. path-based approach
         foreach ($this->provide() as $set) {
             // possible bug for PHAR files, see https://bugs.php.net/bug.php?id=52769
-            if (realpath($set->getSetFileInfo()->getRealPath()) !== realpath($desiredSetName)) {
+            // this is very tricky to handle, see https://stackoverflow.com/questions/27838025/how-to-get-a-phar-file-real-directory-within-the-phar-file-code
+            $setUniqueId = $this->resolveSetUniquePathId($set->getSetFileInfo()->getPathname());
+            $desiredSetUniqueId = $this->resolveSetUniquePathId($desiredSetName);
+
+            if ($setUniqueId !== $desiredSetUniqueId) {
                 continue;
             }
 
@@ -47,5 +52,15 @@ abstract class AbstractSetProvider implements SetProviderInterface
 
         $message = sprintf('Set "%s" was not found', $desiredSetName);
         throw new SetNotFoundException($message, $desiredSetName, $this->provideSetNames());
+    }
+
+    private function resolveSetUniquePathId(string $setPath): string
+    {
+        $setPath = Strings::after($setPath, DIRECTORY_SEPARATOR, -2);
+        if ($setPath === null) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $setPath;
     }
 }
