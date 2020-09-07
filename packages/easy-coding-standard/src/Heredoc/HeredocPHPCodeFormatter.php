9603d35897aa34825854acefc8c1705a49208293
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-07 13:04:48 +0700

    add left right check quote

--- packages/easy-coding-standard/src/Heredoc/HeredocPHPCodeFormatter.php
+++ packages/easy-coding-standard/src/Heredoc/HeredocPHPCodeFormatter.php
@@ -20,7 +20,7 @@ final class HeredocPHPCodeFormatter
      * @see https://regex101.com/r/SZr0X5/2
      * @var string
      */
-    private const PHP_CODE_SNIPPET_IN_HEREDOC = '#(?<opening><<<[\'\"]?PHP[\'\"]?\s+)(?<content>[^PHP]+\n)(?<closing>(\s+)?PHP)#ms';
+    private const PHP_CODE_SNIPPET_IN_HEREDOC = '#(?<opening><<<(\'?PHP\'?|\"?PHP\"?)\s+)(?<content>[^PHP]+\n)(?<closing>(\s+)?PHP)#ms';
 
     /**
      * @var SmartFileSystem
