c94587ee620ad65b7da7cb3514edf9c96d3e77a1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-09 16:08:58 +0700

    Try fix heredoc now doc

--- packages/easy-coding-standard/src/HeredocNowdoc/HeredocNowdocPHPCodeFormatter.php
+++ packages/easy-coding-standard/src/HeredocNowdoc/HeredocNowdocPHPCodeFormatter.php
@@ -15,5 +15,5 @@ final class HeredocNowdocPHPCodeFormatter extends AbstractPHPFormatter
      * @see https://regex101.com/r/SZr0X5/4
      * @var string
      */
-    protected const PHP_CODE_SNIPPET = '#(?<opening><<<(\'?([A-Z]+)\'?|\"?([A-Z]+)\"?)\s+|(\'?([A-Z]+)\'?|\"?([A-Z]+)\"?)\s+)(?<content>[^\3|\4]+\n)(?<closing>(\s+)?\3|\4)#ms';
+    protected const PHP_CODE_SNIPPET = '#(?<opening><<<(\'?([A-Z]+)\'?|\"?([A-Z]+)\"?)\s+|(\'?([A-Z]+)\'?|\"?([A-Z]+)\"?)\s+)(?<content>[^\3|\4]+\n)(?<closing>(\s+)?\3|\4)#msU';
 }
