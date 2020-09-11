fc55a69aa591e6bfd782133bcfd727e98f20e86f
TomasVotruba <tomas.vot@gmail.com>
2020-09-11 16:09:58 +0200

    [PHPStanExtensions] Add argument swap check

--- packages/phpstan-extensions/src/Testing/AbstractServiceAwareRuleTestCase.php
+++ packages/phpstan-extensions/src/Testing/AbstractServiceAwareRuleTestCase.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanExtensions\Testing;
 
+use Nette\Utils\Strings;
 use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
 use Symplify\PHPStanExtensions\DependencyInjection\PHPStanContainerFactory;
+use Symplify\PHPStanExtensions\Exception\SwappedArgumentsException;
 
 abstract class AbstractServiceAwareRuleTestCase extends RuleTestCase
 {
     protected function getRuleFromConfig(string $ruleClass, string $config): Rule
     {
+        if (Strings::contains($config, '\\') && file_exists($ruleClass)) {
+            $message = sprintf('Swapped arguments in "%s()" method', __METHOD__);
+            throw new SwappedArgumentsException($message);
+        }
+
         $phpStanContainerFactory = new PHPStanContainerFactory();
         $container = $phpStanContainerFactory->createContainer([$config]);
 
