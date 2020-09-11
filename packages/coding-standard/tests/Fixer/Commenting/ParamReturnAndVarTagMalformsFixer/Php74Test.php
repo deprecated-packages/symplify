0d211bf3a26161ae9925376ab44d9eb336bab3fa
Tomas Votruba <tomas.vot@gmail.com>
2020-09-12 01:30:35 +0200

    [CS] Fix ParamReturnAndVarTagMalformsFixer for fn() (#1991)
    * Add test case for #1982
    
    * Update skip_nested_call.php.inc
    
    * [CS] Fix nearest fn discovery
    
    Co-authored-by: Jáchym Toušek <enumag@gmail.com>

---
+++ packages/coding-standard/tests/Fixer/Commenting/ParamReturnAndVarTagMalformsFixer/Php74Test.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Fixer\Commenting\ParamReturnAndVarTagMalformsFixer;
+
+use Iterator;
+use Symplify\CodingStandard\Fixer\Commenting\ParamReturnAndVarTagMalformsFixer;
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+/**
+ * @requires PHP >= 7.4
+ * @mimic https://github.com/rectorphp/rector/pull/807/files
+ */
+final class Php74Test extends AbstractCheckerTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/FixturePhp74');
+    }
+
+    protected function getCheckerClass(): string
+    {
+        return ParamReturnAndVarTagMalformsFixer::class;
+    }
+}
