6c09e59fc8a660a20e300c5ba5abb09bcfa48485
TomasVotruba <tomas.vot@gmail.com>
2020-09-14 22:28:32 +0200

    [ECS] Decouple AbstractSnippetFormatterCommand

--- packages/easy-coding-standard/packages/snippet-formatter/src/Command/CheckHeredocNowdocCommand.php
+++ packages/easy-coding-standard/packages/snippet-formatter/src/Command/CheckHeredocNowdocCommand.php
@@ -6,60 +6,11 @@ namespace Symplify\EasyCodingStandard\SnippetFormatter\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symplify\EasyCodingStandard\Configuration\Configuration;
-use Symplify\EasyCodingStandard\Console\Command\AbstractCheckCommand;
-use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
-use Symplify\EasyCodingStandard\SnippetFormatter\Formatter\SnippetFormatter;
 use Symplify\EasyCodingStandard\SnippetFormatter\ValueObject\SnippetPattern;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
-use Symplify\PackageBuilder\Console\ShellCode;
-use Symplify\SmartFileSystem\Finder\SmartFinder;
-use Symplify\SmartFileSystem\SmartFileInfo;
-use Symplify\SmartFileSystem\SmartFileSystem;
 
-final class CheckHeredocNowdocCommand extends AbstractCheckCommand
+final class CheckHeredocNowdocCommand extends AbstractSnippetFormatterCommand
 {
-    /**
-     * @var Configuration
-     */
-    protected $configuration;
-
-    /**
-     * @var SmartFileSystem
-     */
-    private $smartFileSystem;
-
-    /**
-     * @var EasyCodingStandardStyle
-     */
-    private $easyCodingStandardStyle;
-
-    /**
-     * @var SnippetFormatter
-     */
-    private $snippetFormatter;
-
-    /**
-     * @var SmartFinder
-     */
-    private $smartFinder;
-
-    public function __construct(
-        SmartFileSystem $smartFileSystem,
-        EasyCodingStandardStyle $easyCodingStandardStyle,
-        SnippetFormatter $snippetFormatter,
-        Configuration $configuration,
-        SmartFinder $smartFinder
-    ) {
-        $this->smartFileSystem = $smartFileSystem;
-        $this->easyCodingStandardStyle = $easyCodingStandardStyle;
-        $this->configuration = $configuration;
-        $this->snippetFormatter = $snippetFormatter;
-        $this->smartFinder = $smartFinder;
-
-        parent::__construct();
-    }
-
     protected function configure(): void
     {
         $this->setName(CommandNaming::classToName(self::class));
@@ -70,50 +21,10 @@ final class CheckHeredocNowdocCommand extends AbstractCheckCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->configuration->resolveFromInput($input);
-
-        $sources = $this->configuration->getSources();
-        $phpFileInfos = $this->smartFinder->find($sources, '*.php');
-
-        $fileCount = count($phpFileInfos);
-
-        if ($fileCount > 0) {
-            $this->easyCodingStandardStyle->progressStart($fileCount);
-
-            foreach ($phpFileInfos as $phpFileInfo) {
-                $this->processPHPFileInfo($phpFileInfo);
-            }
-        } else {
-            return $this->printFileWarningAndExitSuccess($sources);
-        }
-
-        return $this->reportProcessedFiles($fileCount);
-    }
-
-    private function printFileWarningAndExitSuccess(array $sources): int
-    {
-        $warningMessage = sprintf(
-            'No PHP files found in "%s" paths.%sCheck CLI arguments or "Option::PATHS" parameter in "ecs.php" config file',
-            implode('", ', $sources),
-            PHP_EOL
+        return $this->doExecuteSnippetFormatterWithFileNamesAndSnippetPattern(
+            $input,
+            '*.php',
+            SnippetPattern::HERENOWDOC_SNIPPET_PATTERN
         );
-        $this->easyCodingStandardStyle->warning($warningMessage);
-
-        return ShellCode::SUCCESS;
-    }
-
-    private function processPHPFileInfo(SmartFileInfo $phpFileInfo): void
-    {
-        $fixedContent = $this->snippetFormatter->format($phpFileInfo, SnippetPattern::HERENOWDOC_SNIPPET_PATTERN);
-        $this->easyCodingStandardStyle->progressAdvance();
-
-        if ($phpFileInfo->getContents() === $fixedContent) {
-            // nothing has changed
-            return;
-        }
-
-        if ($this->configuration->isFixer()) {
-            $this->smartFileSystem->dumpFile($phpFileInfo->getPathname(), (string) $fixedContent);
-        }
     }
 }
--- packages/easy-coding-standard/packages/snippet-formatter/src/Command/CheckMarkdownCommand.php
+++ packages/easy-coding-standard/packages/snippet-formatter/src/Command/CheckMarkdownCommand.php
@@ -6,55 +6,11 @@ namespace Symplify\EasyCodingStandard\SnippetFormatter\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symplify\EasyCodingStandard\Console\Command\AbstractCheckCommand;
-use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
-use Symplify\EasyCodingStandard\SnippetFormatter\Formatter\SnippetFormatter;
 use Symplify\EasyCodingStandard\SnippetFormatter\ValueObject\SnippetPattern;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
-use Symplify\PackageBuilder\Console\ShellCode;
-use Symplify\SmartFileSystem\Finder\SmartFinder;
-use Symplify\SmartFileSystem\SmartFileInfo;
-use Symplify\SmartFileSystem\SmartFileSystem;
 
-/**
- * @todo refactor to generic parent AbstractSnippetFormatterCommand
- */
-final class CheckMarkdownCommand extends AbstractCheckCommand
+final class CheckMarkdownCommand extends AbstractSnippetFormatterCommand
 {
-    /**
-     * @var SmartFileSystem
-     */
-    private $smartFileSystem;
-
-    /**
-     * @var EasyCodingStandardStyle
-     */
-    private $easyCodingStandardStyle;
-
-    /**
-     * @var SnippetFormatter
-     */
-    private $snippetFormatter;
-
-    /**
-     * @var SmartFinder
-     */
-    private $smartFinder;
-
-    public function __construct(
-        SmartFileSystem $smartFileSystem,
-        EasyCodingStandardStyle $easyCodingStandardStyle,
-        SnippetFormatter $snippetFormatter,
-        SmartFinder $smartFinder
-    ) {
-        $this->smartFileSystem = $smartFileSystem;
-        $this->easyCodingStandardStyle = $easyCodingStandardStyle;
-        $this->snippetFormatter = $snippetFormatter;
-        $this->smartFinder = $smartFinder;
-
-        parent::__construct();
-    }
-
     protected function configure(): void
     {
         $this->setName(CommandNaming::classToName(self::class));
@@ -65,52 +21,10 @@ final class CheckMarkdownCommand extends AbstractCheckCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->configuration->resolveFromInput($input);
-
-        $sources = $this->configuration->getSources();
-        $markdownFileInfos = $this->smartFinder->find($sources, '*.md');
-
-        $fileCount = count($markdownFileInfos);
-        if ($fileCount > 0) {
-            $this->easyCodingStandardStyle->progressStart($fileCount);
-
-            foreach ($markdownFileInfos as $markdownFileInfo) {
-                $this->processMarkdownFileInfo($markdownFileInfo);
-            }
-        } else {
-            return $this->printNoFilesWarningAndExistSuccess($sources);
-        }
-
-        return $this->reportProcessedFiles($fileCount);
-    }
-
-    private function processMarkdownFileInfo(SmartFileInfo $markdownFileInfo): void
-    {
-        $fixedContent = $this->snippetFormatter->format(
-            $markdownFileInfo,
-            SnippetPattern::MARKDOWN_PHP_SNIPPET_PATTERN
+        return $this->doExecuteSnippetFormatterWithFileNamesAndSnippetPattern(
+            $input,
+            '*.md',
+            SnippetPattern::HERENOWDOC_SNIPPET_PATTERN
         );
-        $this->easyCodingStandardStyle->progressAdvance();
-
-        if ($markdownFileInfo->getContents() === $fixedContent) {
-            // nothing has changed
-            return;
-        }
-
-        if ($this->configuration->isFixer()) {
-            $this->smartFileSystem->dumpFile($markdownFileInfo->getPathname(), (string) $fixedContent);
-        }
-    }
-
-    private function printNoFilesWarningAndExistSuccess(array $sources): int
-    {
-        $warningMessage = sprintf(
-            'No Markdown files found in "%s" paths.%sCheck CLI arguments or "Option::PATHS" parameter in "ecs.php" config file',
-            implode('", ', $sources),
-            PHP_EOL
-        );
-        $this->easyCodingStandardStyle->warning($warningMessage);
-
-        return ShellCode::SUCCESS;
     }
 }
