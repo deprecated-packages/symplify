086870c39a27218322df6f322496174a49d5292f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-16 23:51:06 +0700

    cs test

--- packages/coding-standard/tests/Rules/NoStaticPropertyRule/NoStaticPropertyRuleTest.php
+++ packages/coding-standard/tests/Rules/NoStaticPropertyRule/NoStaticPropertyRuleTest.php
@@ -22,7 +22,10 @@ final class NoStaticPropertyRuleTest extends RuleTestCase
     public function provideData(): Iterator
     {
         yield [__DIR__ . '/Fixture/SomeProperty.php', []];
-        yield [__DIR__ . '/Fixture/SomeStaticProperty.php', [[NoStaticPropertyRule::ERROR_MESSAGE, 9], [NoStaticPropertyRule::ERROR_MESSAGE, 10]]];
+        yield [
+            __DIR__ . '/Fixture/SomeStaticProperty.php',
+            [[NoStaticPropertyRule::ERROR_MESSAGE, 9], [NoStaticPropertyRule::ERROR_MESSAGE, 10]],
+        ];
         yield [__DIR__ . '/Fixture/SomeStaticPropertyWithoutModifier.php', [[NoStaticPropertyRule::ERROR_MESSAGE, 10]]];
     }
 
