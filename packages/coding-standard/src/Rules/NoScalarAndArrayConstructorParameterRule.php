7550e6b3a92a939cb25e7e7aa209994ecbc7599e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-18 21:05:32 +0700

    @var doc

--- packages/coding-standard/src/Rules/NoScalarAndArrayConstructorParameterRule.php
+++ packages/coding-standard/src/Rules/NoScalarAndArrayConstructorParameterRule.php
@@ -95,10 +95,12 @@ final class NoScalarAndArrayConstructorParameterRule extends AbstractManyNodeTyp
     private function getPossibleTypes($type): array
     {
         if ($type instanceof NullableType) {
+            /** @var Identifier|Name $type->type */
             return [$type->type];
         }
 
         if ($type instanceof UnionType) {
+            /** @var Identifier[]|Name[] $type->types */
             return $type->types;
         }
 
