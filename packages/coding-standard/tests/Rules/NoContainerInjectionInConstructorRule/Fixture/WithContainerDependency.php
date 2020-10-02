9247dc5405ad6df36cec891149dd631c267fbaa9
TomasVotruba <tomas.vot@gmail.com>
2020-10-03 01:38:28 +0200

    [CS] Add NoContainerInjectionInConstructorRule

---
+++ packages/coding-standard/tests/Rules/NoContainerInjectionInConstructorRule/Fixture/WithContainerDependency.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Rules\NoContainerInjectionInConstructorRule\Fixture;
+
+use Psr\Container\ContainerInterface;
+
+final class WithContainerDependency
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+}
