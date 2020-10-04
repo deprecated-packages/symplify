76434e193913663f0b241b6d0327b6f96c6b8005
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-05 03:26:12 +0700

    CategoryResolver regex link

--- packages/changelog-linker/src/ChangeTree/Resolver/CategoryResolver.php
+++ packages/changelog-linker/src/ChangeTree/Resolver/CategoryResolver.php
@@ -14,21 +14,25 @@ final class CategoryResolver
 {
     /**
      * @var string
+     * @see https://regex101.com/r/Tvajsb/1
      */
     private const ADDED_REGEX = '#\b(add(s|ed|ing)?)\b#i';
 
     /**
      * @var string
+     * @see https://regex101.com/r/Y9y951/1
      */
     private const FIXED_REGEX = '#\b(fix(es|ed|ing)?)\b#i';
 
     /**
      * @var string
+     * @see https://regex101.com/r/a23Pob/1
      */
     private const REMOVED_REGEX = '#\b(remov(e|es|ed|ing)|delet(e|es|ed|ing|)|drop(s|ped|ping)?)\b#i';
 
     /**
      * @var string
+     * @see https://regex101.com/r/PQ1ahG/1
      */
     private const DEPRECATED_REGEX = '#\b(deprecat(e|es|ed|ing))\b#i';
 
