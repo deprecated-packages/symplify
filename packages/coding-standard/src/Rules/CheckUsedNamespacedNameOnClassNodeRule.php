363c395ba11744b669626828a98410bf622e246f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-07 09:52:20 +0700

    cs fix

--- packages/coding-standard/src/Rules/CheckUsedNamespacedNameOnClassNodeRule.php
+++ packages/coding-standard/src/Rules/CheckUsedNamespacedNameOnClassNodeRule.php
@@ -6,7 +6,6 @@ namespace Symplify\CodingStandard\Rules;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\Identifier;
 use PhpParser\Node\Stmt\Class_;
 use PHPStan\Analyser\Scope;
 use Symplify\CodingStandard\ValueObject\PHPStanAttributeKey;
