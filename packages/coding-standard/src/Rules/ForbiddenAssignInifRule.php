be86443f1c89a38800cc15a3087b33a523e83877
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-08 17:04:22 +0700

    [cs] Fixes 2328 Add ForbiddenAssignInifRule

---
+++ packages/coding-standard/src/Rules/ForbiddenAssignInifRule.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Stmt\If_;
+use PHPStan\Analyser\Scope;
+use Symplify\CodingStandard\ValueObject\PHPStanAttributeKey;
+
+/**
+ * @see \Symplify\CodingStandard\Tests\Rules\ForbiddenAssignInifRule\ForbiddenAssignInifRuleTest
+ */
+final class ForbiddenAssignInifRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'If in assignment is not allowed. Use before if instead.';
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Assign::class];
+    }
+
+    /**
+     * @param Assign $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        if (! $this->isInsideIf($node)) {
+            return [];
+        }
+
+        return [self::ERROR_MESSAGE];
+    }
+
+    private function isInsideIf(Assign $assign): bool
+    {
+        $if = $assign->getAttribute(PHPStanAttributeKey::PARENT);
+        while ($if) {
+            if ($if instanceof If_) {
+                break;
+            }
+
+            $if = $if->getAttribute(PHPStanAttributeKey::PARENT);
+        }
+
+        return $if instanceof If_;
+    }
+}
