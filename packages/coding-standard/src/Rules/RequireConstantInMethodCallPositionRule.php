dc2f3e2c51501e6d51c7ca949e36c05e5ba29158
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-10 08:41:14 +0700

    use requiredLocalConstantInMethodCall and requiredExternalConstantInMethodCall

--- packages/coding-standard/src/Rules/RequireConstantInMethodCallPositionRule.php
+++ packages/coding-standard/src/Rules/RequireConstantInMethodCallPositionRule.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace Symplify\CodingStandard\Rules;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
 use PHPStan\Analyser\Scope;
 use Symplify\CodingStandard\PHPStan\Types\ContainsTypeAnalyser;
 
@@ -49,18 +53,83 @@ final class RequireConstantInMethodCallPositionRule extends AbstractSymplifyRule
     }
 
     /**
-     * @return string[]
-     */
-    public function getNodeTypes(): array
-    {
-        return [MethodCall::class];
-    }
-
-    /**
      * @param MethodCall $node
      * @return string[]
      */
     public function process(Node $node, Scope $scope): array
     {
+        if (! $node->name instanceof Identifier) {
+            return [];
+        }
+
+        $errorMessages = [];
+
+        $methodName = (string) $node->name;
+
+        foreach ($this->requiredLocalConstantInMethodCall as $type => $positionsByMethods) {
+            $positions = $this->matchPositions($node, $scope, $type, $positionsByMethods, $methodName);
+            if ($positions === null) {
+                continue;
+            }
+
+            foreach ($node->args as $key => $arg) {
+                if ($this->shouldSkipArg($key, $positions, $arg)) {
+                    continue;
+                }
+
+                $errorMessages[] = sprintf(self::ERROR_MESSAGE, $key);
+            }
+        }
+
+        foreach ($this->requiredExternalConstantInMethodCall as $type => $positionsByMethods) {
+            $positions = $this->matchPositions($node, $scope, $type, $positionsByMethods, $methodName);
+            if ($positions === null) {
+                continue;
+            }
+
+            foreach ($node->args as $key => $arg) {
+                if ($this->shouldSkipArg($key, $positions, $arg)) {
+                    continue;
+                }
+
+                $errorMessages[] = sprintf(self::ERROR_MESSAGE, $key);
+            }
+        }
+
+        return $errorMessages;
+    }
+
+    /**
+     * @param class-string $desiredType
+     * @return mixed|null
+     */
+    private function matchPositions(
+        MethodCall $methodCall,
+        Scope $scope,
+        string $desiredType,
+        array $positionsByMethods,
+        string $methodName
+    ) {
+        if (! $this->containsTypeAnalyser->containsExprTypes($methodCall->var, $scope, [$desiredType])) {
+            return null;
+        }
+
+        return $positionsByMethods[$methodName] ?? null;
+    }
+
+    /**
+     * @param int[] $positions
+     */
+    private function shouldSkipArg(int $key, array $positions, Arg $arg): bool
+    {
+        if (! in_array($key, $positions, true)) {
+            return true;
+        }
+
+        if ($arg->value instanceof Variable) {
+            return true;
+        }
+
+        return $arg->value instanceof ClassConstFetch;
     }
 }
