1d560ccd6a2445c8c22e5c06ff0ea791f927fd3e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-13 14:47:52 +0700

    Fixes #2342 : Add NoMethodTagInClassDocblockRule

---
+++ packages/coding-standard/src/Rules/NoMethodTagInClassDocblockRule.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Rules;
+
+use Nette\Utils\Strings;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Comment\Doc;
+use PHPStan\Analyser\Scope;
+use Symplify\CodingStandard\ValueObject\PHPStanAttributeKey;
+
+/**
+ * @see \Symplify\CodingStandard\Tests\Rules\NoMethodTagInClassDocblockRule\NoMethodTagInClassDocblockRuleTest
+ */
+final class NoMethodTagInClassDocblockRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Do not use @method tag in class docblock';
+
+    public const METHOD_TAG_REGEX = '#\*\s+@method\s+.*\n?#';
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Comment $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        $docComment = $node->getDocComment();
+        if (! $docComment instanceof Doc) {
+            return [];
+        }
+
+        if (! Strings::match($docComment->getText(), self::METHOD_TAG_REGEX)) {
+            return [];
+        }
+
+        return [self::ERROR_MESSAGE];
+    }
+}
