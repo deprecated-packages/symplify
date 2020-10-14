e7746b33aa7a38dde3a8f61bc7a296fcca0bc1a6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-14 22:52:19 +0700

    reactivate coverage report (#2370)

--- packages/gerudoc/src/ValueObject/Section.php
+++ packages/gerudoc/src/ValueObject/Section.php
@@ -2,17 +2,19 @@
 
 declare(strict_types=1);
 
-namespace Symplify\MonorepoBuilder\ValueObject;
+namespace Symplify\Gerudoc\ValueObject;
 
 final class Section
 {
     /**
      * @var string
+     * @noRector \Rector\DeadCode\Rector\ClassConst\RemoveUnusedClassConstantRector
      */
     public const REQUIRE = 'require';
 
     /**
      * @var string
+     * @noRector \Rector\DeadCode\Rector\ClassConst\RemoveUnusedClassConstantRector
      */
     public const REQUIRE_DEV = 'require-dev';
 }
