e2e82edb1c0da3243294a40f699e3f36e243a3f5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-15 13:52:33 +0700

    improve ForbiddenProtectedPropertyRule : handle injection with required and autowire

--- packages/coding-standard/src/NodeAnalyzer/DependencyNodeAnalyzer.php
+++ packages/coding-standard/src/NodeAnalyzer/DependencyNodeAnalyzer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\CodingStandard\NodeAnalyzer;
 
+use Nette\Utils\Strings;
 use PhpParser\Node;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\PropertyFetch;
@@ -23,6 +24,12 @@ final class DependencyNodeAnalyzer
      * @var NodeFinder
      */
     private $nodeFinder;
+
+    /**
+     * @var string
+     * @see https://regex101.com/r/gn2P0C/1
+     */
+    private const REQUIRED_DOCBLOCK_REGEX = '#\*\s+@required\n?#';
 
     public function __construct()
     {
@@ -87,6 +94,46 @@ final class DependencyNodeAnalyzer
         }
 
         return false;
+    }
+
+    /**
+     * @param Property|ClassConst $node
+     */
+    public function isInsideClassAndPassedAsDependencyViaAutowireMethod(Node $node): bool
+    {
+        /** @var Class_ $class */
+        $class = $this->resolveCurrentClass($node);
+        if (! $class instanceof Class_) {
+            return false;
+        }
+
+        $shortClassName = $class->name;
+        $classMethod = $class->getMethod('autowire' . (string) $shortClassName);
+        if (! $classMethod instanceof ClassMethod) {
+            return false;
+        }
+
+        $docComment = $classMethod->getDocComment();
+        if ($docComment === null) {
+            return [];
+        }
+
+        if (! Strings::match($docComment->getText(), self::REQUIRED_DOCBLOCK_REGEX)) {
+            return [];
+        }
+
+        $parameters = $classMethod->getParams();
+        if ($parameters === []) {
+            return false;
+        }
+
+        /** @var Assign[] $assigns */
+        $assigns = $this->nodeFinder->findInstanceOf($classMethod, Assign::class);
+        if ($assigns === []) {
+            return false;
+        }
+
+        return $this->isInsideAssignByParameter($parameters, $assigns);
     }
 
     private function resolveCurrentClass(Node $node): ?Class_
