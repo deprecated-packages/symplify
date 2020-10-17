d1cdda1ec0a35e17e30a138e201eaed3bc94a6e5
TomasVotruba <tomas.vot@gmail.com>
2020-10-17 18:30:08 +0200

    [CS] Make NoProtectedElementInFinalClassRule work with MicroKernelTrait methods

---
+++ packages/coding-standard/tests/Rules/NoProtectedElementInFinalClassRule/Fixture/SkipMicroKernelProtectedMethod.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Rules\NoProtectedElementInFinalClassRule\Fixture;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+final class SkipMicroKernelProtectedMethod
+{
+    use MicroKernelTrait;
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+    }
+
+    protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
+    {
+    }
+}
