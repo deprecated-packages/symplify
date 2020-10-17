df784612e07bedbb925a6763a3b6ff1c58cda604
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-17 23:43:16 +0700

    [cs] Fixes #2329 Add TooDeepNewClassNestingRule (#2375)
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

---
+++ packages/coding-standard/src/Rules/TooDeepNewClassNestingRule.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\New_;
+use PhpParser\NodeFinder;
+use PHPStan\Analyser\Scope;
+
+/**
+ * @see \Symplify\CodingStandard\Tests\Rules\TooDeepNewClassNestingRule\TooDeepNewClassNestingRuleTest
+ */
+final class TooDeepNewClassNestingRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'new <class> is limited to %d "new <class>(new <class>))" nesting to each other. You have %d nesting.';
+
+    /**
+     * @var NodeFinder
+     */
+    private $nodeFinder;
+
+    /**
+     * @var int
+     */
+    private $maxNewClassNesting;
+
+    public function __construct(NodeFinder $nodeFinder, int $maxNewClassNesting = 3)
+    {
+        $this->nodeFinder = $nodeFinder;
+        $this->maxNewClassNesting = $maxNewClassNesting;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [New_::class];
+    }
+
+    /**
+     * @param New_ $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        $countNew = count($this->nodeFinder->findInstanceOf($node, New_::class));
+
+        if ($this->maxNewClassNesting >= $countNew) {
+            return [];
+        }
+
+        return [sprintf(self::ERROR_MESSAGE, $this->maxNewClassNesting, $countNew)];
+    }
+}
