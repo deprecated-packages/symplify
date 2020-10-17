cc63f8bb405bff27963b5cb352e927c14a8bbbb1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-17 23:02:42 +0700

    rector

--- packages/coding-standard/tests/Rules/ForbidMethodOrFuncCallInForeachRule/ForbidMethodOrFuncCallInForeachRuleTest.php
+++ packages/coding-standard/tests/Rules/ForbidMethodOrFuncCallInForeachRule/ForbidMethodOrFuncCallInForeachRuleTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Symplify\CodingStandard\Tests\Rules\ForbidMethodCallInForeachRule;
+namespace Symplify\CodingStandard\Tests\Rules\ForbidMethodOrFuncCallInForeachRule;
 
 use Iterator;
 use PHPStan\Rules\Rule;
