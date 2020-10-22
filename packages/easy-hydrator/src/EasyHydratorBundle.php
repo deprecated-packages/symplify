a021753c2c948c6d88c5ac74870c0e199cfbdc20
Jakub Janata <jakubjanata@gmail.com>
2020-10-22 14:44:04 +0200

    Fixes #2393 easy-hydrator composer pass

--- packages/easy-hydrator/src/EasyHydratorBundle.php
+++ packages/easy-hydrator/src/EasyHydratorBundle.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Symplify\EasyHydrator;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
 use Symplify\EasyHydrator\DependencyInjection\Extension\EasyHydratorExtension;
 
 final class EasyHydratorBundle extends Bundle
 {
+    public function build(ContainerBuilder $containerBuilder): void
+    {
+        $containerBuilder->addCompilerPass(new AutowireArrayParameterCompilerPass());
+    }
+
     protected function createContainerExtension(): ?ExtensionInterface
     {
         return new EasyHydratorExtension();
