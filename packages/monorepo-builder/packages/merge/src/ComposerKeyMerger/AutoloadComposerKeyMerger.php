8ea7011eefd77e5dfd408867d64acc29a0e6046d
TomasVotruba <tomas.vot@gmail.com>
2020-10-23 16:32:47 +0200

    add path check on merge command

--- packages/monorepo-builder/packages/merge/src/ComposerKeyMerger/AutoloadComposerKeyMerger.php
+++ packages/monorepo-builder/packages/merge/src/ComposerKeyMerger/AutoloadComposerKeyMerger.php
@@ -6,14 +6,27 @@ namespace Symplify\MonorepoBuilder\Merge\ComposerKeyMerger;
 
 use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
 use Symplify\MonorepoBuilder\Merge\Contract\ComposerKeyMergerInterface;
+use Symplify\MonorepoBuilder\Merge\Validation\AutoloadPathValidator;
 
 final class AutoloadComposerKeyMerger extends AbstractComposerKeyMerger implements ComposerKeyMergerInterface
 {
+    /**
+     * @var AutoloadPathValidator
+     */
+    private $autoloadPathValidator;
+
+    public function __construct(AutoloadPathValidator $autoloadPathValidator)
+    {
+        $this->autoloadPathValidator = $autoloadPathValidator;
+    }
+
     public function merge(ComposerJson $mainComposerJson, ComposerJson $newComposerJson): void
     {
         if ($newComposerJson->getAutoload() === []) {
             return;
         }
+
+        $this->autoloadPathValidator->ensureAutoloadPathExists($newComposerJson);
 
         $autoload = $this->mergeRecursiveAndSort($mainComposerJson->getAutoload(), $newComposerJson->getAutoload());
         $mainComposerJson->setAutoload($autoload);
---
+++ packages/monorepo-builder/packages/merge/src/Validation/AutoloadPathValidator.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Merge\Validation;
+
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
+use Symplify\SmartFileSystem\FileSystemGuard;
+
+final class AutoloadPathValidator
+{
+    /**
+     * @var FileSystemGuard
+     */
+    private $fileSystemGuard;
+
+    public function __construct(FileSystemGuard $fileSystemGuard)
+    {
+        $this->fileSystemGuard = $fileSystemGuard;
+    }
+
+    public function ensureAutoloadPathExists(ComposerJson $composerJson): void
+    {
+        $composerJsonFileInfo = $composerJson->getFileInfo();
+        if ($composerJsonFileInfo === null) {
+            return;
+        }
+
+        $autoloadDirectories = $composerJson->getAbsoluteAutoloadDirectories();
+        foreach ($autoloadDirectories as $autoloadDirectory) {
+            $message = sprintf('In "%s"', $composerJsonFileInfo->getRelativeFilePathFromCwd());
+            $this->fileSystemGuard->ensureDirectoryExists($autoloadDirectory, $message);
+        }
+    }
+}
