00c2168908da946570f71a3aa603a440b5d54ee2
Tomas Votruba <tomas.vot@gmail.com>
2020-10-28 18:19:07 +0100

    try dev master

--- packages/monorepo-builder/packages/testing/src/ComposerJson/ComposerVersionManipulator.php
+++ packages/monorepo-builder/packages/testing/src/ComposerJson/ComposerVersionManipulator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\MonorepoBuilder\Testing\ComposerJson;
 
+use OndraM\CiDetector\CiDetector;
 use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 
 final class ComposerVersionManipulator
@@ -15,13 +16,23 @@ final class ComposerVersionManipulator
      */
     public function setAsteriskVersionForUsedPackages(array $packageComposerJson, array $usedPackageNames): array
     {
+        $ciDetector = new CiDetector();
+
         foreach ([ComposerJsonSection::REQUIRE, ComposerJsonSection::REQUIRE_DEV] as $section) {
             foreach ($usedPackageNames as $usedPackageName) {
                 if (! isset($packageComposerJson[$section][$usedPackageName])) {
                     continue;
                 }
 
-                $packageComposerJson[$section][$usedPackageName] = '*';
+                if ($ciDetector->isCiDetected()) {
+                    $ci = $ciDetector->detect();
+                    /** hotfix for Github Actions, @see https://github.com/composer/composer/issues/9368#issuecomment-718089230 */
+                    $version = 'dev-' . $ci->getGitCommit() . ' || dev-master';
+                } else {
+                    $version = 'dev-master';
+                }
+
+                $packageComposerJson[$section][$usedPackageName] = $version;
             }
         }
 
