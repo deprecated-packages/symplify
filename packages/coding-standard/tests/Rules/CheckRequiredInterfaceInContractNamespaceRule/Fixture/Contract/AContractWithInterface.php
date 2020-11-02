6b183542d69feb2ddbcb2b621d23795ceaf501cd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-02 13:01:00 +0700

    test contract namespace with interface

---
+++ packages/coding-standard/tests/Rules/CheckRequiredInterfaceInContractNamespaceRule/Fixture/Contract/AContractWithInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Rules\CheckRequiredInterfaceInContractNamespaceRule\Fixture;
+
+use Psr\Container\Containerinterface;
+
+interface AContractWithInterface
+{
+
+}
