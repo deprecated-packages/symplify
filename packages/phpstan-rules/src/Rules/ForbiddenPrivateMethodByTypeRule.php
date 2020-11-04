340227da2042ebfb374f0289d0132ac19bbee252
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-04 10:25:29 +0700

    [PHPStanRules] Fixes #2404 Add ForbiddenPrivateMethodByTypeRule

---
+++ packages/phpstan-rules/src/Rules/ForbiddenPrivateMethodByTypeRule.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\NodeFinder;
+use PHPStan\Analyser\Scope;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\ForbiddenPrivateMethodByTypeRule\ForbiddenPrivateMethodByTypeRuleTest
+ */
+final class ForbiddenPrivateMethodByTypeRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'private method "%s" in "%s" is not allowed.';
+
+    /**
+     * @var array<string, string[]>
+     */
+    private $forbiddenTypes = [];
+
+    /**
+     * @param array<string, string[]> $forbiddenTypes
+     */
+    public function __construct(NodeFinder $nodeFinder, array $forbiddenTypes = [])
+    {
+        $this->forbiddenTypes = $forbiddenTypes;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    /**
+     * @param ClassMethod $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        if (! $node->isPrivate()) {
+            return [];
+        }
+
+        $className = $this->resolveCurrentClassName($node);
+        if ($className === null) {
+            return [];
+        }
+
+        if (! in_array($className, $this->forbiddenTypes, true)) {
+            return [];
+        }
+
+        return [sprintf(self::ERROR_MESSAGE, (string) $node->name, $className)];
+    }
+}
