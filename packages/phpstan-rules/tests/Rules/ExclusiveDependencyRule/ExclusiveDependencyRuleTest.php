3f03b1c421625bb34e20beee94cd47acdd616aaf
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-11 16:38:12 +0700

    [PHPStanRules] Add ExclusiveDependencyRule (#2476)
    Co-authored-by: rector-bot <tomas@getrector.org>

---
+++ packages/phpstan-rules/tests/Rules/ExclusiveDependencyRule/ExclusiveDependencyRuleTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\ExclusiveDependencyRule;
+
+use Doctrine\ORM\EntityManager;
+use Iterator;
+use PHPStan\Rules\Rule;
+use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
+use Symplify\PHPStanRules\Rules\ExclusiveDependencyRule;
+
+final class ExclusiveDependencyRuleTest extends AbstractServiceAwareRuleTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function testRule(string $filePath, array $expectedErrorMessagesWithLines): void
+    {
+        $this->analyse([$filePath], $expectedErrorMessagesWithLines);
+    }
+
+    /**
+     * @return Iterator<mixed>
+     */
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/SkipNotSpecified.php', []];
+        yield [__DIR__ . '/Fixture/SomeRepository.php', []];
+        yield [__DIR__ . '/Fixture/SomeController.php', [
+            [sprintf(ExclusiveDependencyRule::ERROR_MESSAGE, '*Repository', EntityManager::class), 9],
+        ]];
+    }
+
+    protected function getRule(): Rule
+    {
+        return $this->getRuleFromConfig(ExclusiveDependencyRule::class, __DIR__ . '/config/configured_rule.neon');
+    }
+}
---
+++ packages/phpstan-rules/tests/Rules/ExclusiveDependencyRule/Fixture/SkipNotSpecified.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Symplify\PHPStanRules\Tests\Rules\ExclusiveDependencyRule\Fixture;
+
+class SkipNotSpecified
+{
+    public function __construct(string $a)
+    {
+
+    }
+}
