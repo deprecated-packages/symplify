4003270c4c229217d3de2799c93bb5b119d84e48
Tomas Votruba <tomas.vot@gmail.com>
2020-11-22 21:29:58 +0100

    [LatteToTwigConverter] Make name more explicit (#2532)

---
+++ packages/latte-to-twig-converter/src/CaseConverter/CommentCaseConverter.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\LatteToTwigConverter\CaseConverter;
+
+use Nette\Utils\Strings;
+use Symplify\LatteToTwigConverter\Contract\CaseConverter\CaseConverterInterface;
+
+final class CommentCaseConverter implements CaseConverterInterface
+{
+    /**
+     * @see https://regex101.com/r/LknXm2/1
+     * @var string
+     */
+    private const COMMENT_REGEX = '#{\*(.*?)\*}#s';
+
+    public function getPriority(): int
+    {
+        return 800;
+    }
+
+    public function convertContent(string $content): string
+    {
+        return Strings::replace($content, self::COMMENT_REGEX, '{#$1#}');
+    }
+}
