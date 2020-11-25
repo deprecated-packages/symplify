8efaafed4cadf98653536e3de679e8208f3c3e0f
Tomas Votruba <tomas.vot@gmail.com>
2020-11-25 22:05:43 +0000

    trigger ci

--- rector-ci.php
+++ rector-ci.php
@@ -83,6 +83,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     $parameters->set(Option::EXCLUDE_PATHS, [
+        __DIR__ . '/tests/Printer/SmartPhpConfigPrinter/Source',
         '*/scoper.inc.php',
         '/vendor/',
         '/init/',
