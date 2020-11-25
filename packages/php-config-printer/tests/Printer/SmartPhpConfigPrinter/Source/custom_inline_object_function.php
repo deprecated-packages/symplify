6d688a9d979b95ee0df7700cfe3867fda16d1f03
Tomas Votruba <tomas.vot@gmail.com>
2020-11-25 22:05:23 +0000

    trigger ci

--- packages/php-config-printer/tests/Printer/SmartPhpConfigPrinter/Source/custom_inline_object_function.php
+++ packages/php-config-printer/tests/Printer/SmartPhpConfigPrinter/Source/custom_inline_object_function.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Symplify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source;
 
+if (function_exists('Symplify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source\custom_inline_object_function')) {
+    return;
+}
+
 function custom_inline_object_function(): void
 {
 }
