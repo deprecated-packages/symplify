09322da4245cedb28b011e80d2599d1c78dcf598
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-27 13:02:32 +0700

    test correct command

--- packages/phpstan-rules/tests/Rules/CheckOptionArgumentCommandRule/CheckOptionArgumentCommandRuleTest.php
+++ packages/phpstan-rules/tests/Rules/CheckOptionArgumentCommandRule/CheckOptionArgumentCommandRuleTest.php
@@ -22,6 +22,7 @@ final class CheckOptionArgumentCommandRuleTest extends AbstractServiceAwareRuleT
     public function provideData(): Iterator
     {
         yield [__DIR__ . '/Fixture/NotCommand.php', []];
+        yield [__DIR__ . '/Fixture/CorrectCommand.php', []];
     }
 
     protected function getRule(): Rule
