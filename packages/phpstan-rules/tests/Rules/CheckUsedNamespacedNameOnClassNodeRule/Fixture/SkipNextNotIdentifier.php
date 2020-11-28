628d82d6b09d2a6f01542d08ade795f71364afb7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-29 03:07:40 +0700

    fixture next is not identifier

---
+++ packages/phpstan-rules/tests/Rules/CheckUsedNamespacedNameOnClassNodeRule/Fixture/SkipNextNotIdentifier.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\CheckUsedNamespacedNameOnClassNodeRule\Fixture;
+
+use PhpParser\Node\Stmt\Class_;
+use PHPStan\Analyser\Scope;
+
+final class SkipNextNotIdentifier
+{
+    public function process(Class_ $class, Scope $scope): array
+    {
+        $class->{'a'};
+    }
+}
