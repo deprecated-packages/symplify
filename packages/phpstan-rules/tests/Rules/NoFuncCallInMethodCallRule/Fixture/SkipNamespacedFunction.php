1a77b75032a0a91a9604c9dae3771f1df14689b0
Ronald Márföldi <ronald.marfoldi@petitpress.sk>
2020-11-30 20:45:11 +0100

    Use service function if exists in Symfony DependencyInjection

--- packages/phpstan-rules/tests/Rules/NoFuncCallInMethodCallRule/Fixture/SkipNamespacedFunction.php
+++ packages/phpstan-rules/tests/Rules/NoFuncCallInMethodCallRule/Fixture/SkipNamespacedFunction.php
@@ -4,16 +4,27 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\Tests\Rules\NoFuncCallInMethodCallRule\Fixture;
 
+use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 final class SkipNamespacedFunction
 {
     public function something()
     {
-        $this->process(ref('reference'));
+        $this->process($this->getReferenceConfigurator());
     }
 
     private function process(\Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator $ref)
     {
+    }
+
+    private function getReferenceConfigurator(): ReferenceConfigurator
+    {
+        if (function_exists('Symfony\Component\DependencyInjection\Loader\Configurator\service')) {
+            return service('reference');
+        }
+
+        return ref('reference');
     }
 }
