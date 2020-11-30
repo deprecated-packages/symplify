817ba7034c36e1d7b0dadcf43b13a70370707e40
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-30 15:18:57 +0700

    [PHPStanRules] [WIP] Fixes #2523 Add CheckClassNamespaceFollowPsr4Rule

---
+++ packages/phpstan-rules/src/Rules/CheckClassNamespaceFollowPsr4Rule.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use PHPStan\Analyser\Scope;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\CheckClassNamespaceFollowPsr4Rule\CheckClassNamespaceFollowPsr4RuleTest
+ */
+final class CheckClassNamespaceFollowPsr4Rule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Class namespace %s does not follow PSR-4 configuration in composer.json';
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Class_ $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        return [];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+// defined "Foo\\Bar" in composer.json
+namespace Foo;
+
+class Baz
+{
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+// defined "Foo\\Bar" in composer.json
+namespace Foo\Bar;
+
+class Baz
+{
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+}
