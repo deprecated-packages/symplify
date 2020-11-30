6ac42bc2b4043239a76e5b2b4bc3489b5854c101
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-01 06:51:15 +0700

    fix existing code base namespace

--- packages/vendor-patches/tests/Finder/VendorFilesFinderSource/Vendor/some/package/src/PackageClass.php
+++ packages/vendor-patches/tests/Finder/VendorFilesFinderSource/Vendor/some/package/src/PackageClass.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Symplify\VendorPatches\Tests\Finder\VendorFilesFinderSource;
+namespace Symplify\VendorPatches\Tests\Finder\VendorFilesFinderSource\Vendor\some\package;
 
 class PackageClass
 {
