826aecc268152d2269b13ab1fdd1b81a85881311
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-01 07:01:16 +0700

    phpstan

--- packages/vendor-patches/tests/Finder/VendorFilesFinderSource/Vendor/some/package/src/PackageClass.php
+++ packages/vendor-patches/tests/Finder/VendorFilesFinderSource/Vendor/some/package/src/PackageClass.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Symplify\VendorPatches\Tests\Finder\VendorFilesFinderSource\Vendor\some\package;
+namespace Symplify\VendorPatches\Tests\Finder\VendorFilesFinderSource\Vendor\some\package\src;
 
 class PackageClass
 {
