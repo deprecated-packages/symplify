aea15f358f8e24dcb0767322e048b2d0f489ef73
TomasVotruba <tomas.vot@gmail.com>
2020-12-01 22:10:40 +0100

    add missing privates accessor

--- packages/console-color-diff/config/config.php
+++ packages/console-color-diff/config/config.php
@@ -6,6 +6,7 @@ use SebastianBergmann\Diff\Differ;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
+use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -23,4 +24,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(SymfonyStyleFactory::class);
     $services->set(SymfonyStyle::class)
         ->factory([service(SymfonyStyleFactory::class), 'create']);
+
+    $services->set(PrivatesAccessor::class);
 };
