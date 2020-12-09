f15cce9a0e0a24cb144269bbdaca0c6d4233db4f
TomasVotruba <tomas.vot@gmail.com>
2020-12-09 15:15:13 +0100

    [ECS] Remove deprecated Option

--- packages/sniffer-fixer-to-ecs-converter/src/SymfonyConfigFormatFactory.php
+++ packages/sniffer-fixer-to-ecs-converter/src/SymfonyConfigFormatFactory.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\SnifferFixerToECSConverter;
 
-use Symplify\EasyCodingStandard\Configuration\Option;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 use Symplify\PhpConfigPrinter\ValueObject\YamlKey;
 
