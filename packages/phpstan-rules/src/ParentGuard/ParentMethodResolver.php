0bd0ebab98d9f58e10d3b4c809a71d9ff42b16ac
Tomas Votruba <tomas.vot@gmail.com>
2020-12-12 02:51:41 +0100

    [PHPStanRules] Improve NoSetterOnServiceRule, skip parent setter interface (#2636)

---
+++ packages/phpstan-rules/src/ParentGuard/ParentMethodResolver.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\ParentGuard;
+
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\Php\PhpMethodReflection;
+
+final class ParentMethodResolver
+{
+    public function resolve(Scope $scope, string $methodName): ?PhpMethodReflection
+    {
+        $classReflection = $scope->getClassReflection();
+        if (! $classReflection instanceof ClassReflection) {
+            return null;
+        }
+
+        /** @var ClassReflection[] $parentClassLikeReflections */
+        $parentClassLikeReflections = array_merge($classReflection->getParents(), $classReflection->getInterfaces());
+
+        foreach ($parentClassLikeReflections as $parentClassLikeReflection) {
+            if (! $parentClassLikeReflection->hasMethod($methodName)) {
+                continue;
+            }
+
+            $nativeMethodReflection = $parentClassLikeReflection->getNativeMethod($methodName);
+            if (! $nativeMethodReflection instanceof PhpMethodReflection) {
+                continue;
+            }
+
+            return $nativeMethodReflection;
+        }
+
+        return null;
+    }
+}
