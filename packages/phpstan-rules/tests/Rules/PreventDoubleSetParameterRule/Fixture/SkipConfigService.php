7ec17a2e76135085787ebad167c0553ddc2ae28e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-13 20:06:52 +0700

    skip config service fixture

---
+++ packages/phpstan-rules/tests/Rules/PreventDoubleSetParameterRule/Fixture/SkipConfigService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->public();
+};
