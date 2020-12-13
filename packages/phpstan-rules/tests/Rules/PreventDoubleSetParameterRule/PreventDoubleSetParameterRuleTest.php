46c36773718e4983fd8bf4ef6618beefb867bf41
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-13 20:06:48 +0700

    skip config service fixture

--- packages/phpstan-rules/tests/Rules/PreventDoubleSetParameterRule/PreventDoubleSetParameterRuleTest.php
+++ packages/phpstan-rules/tests/Rules/PreventDoubleSetParameterRule/PreventDoubleSetParameterRuleTest.php
@@ -21,6 +21,7 @@ final class PreventDoubleSetParameterRuleTest extends AbstractServiceAwareRuleTe
 
     public function provideData(): Iterator
     {
+        yield [__DIR__ . '/Fixture/SkipConfigService.php', []];
         yield [__DIR__ . '/Fixture/SkipOnce.php', []];
         yield [__DIR__ . '/Fixture/SkipNoDuplicateValue.php', []];
         yield [__DIR__ . '/Fixture/DuplicateValue.php', [[PreventDoubleSetParameterRule::ERROR_MESSAGE, 7]]];
