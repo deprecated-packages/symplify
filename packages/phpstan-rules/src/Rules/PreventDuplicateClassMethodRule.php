928a60afec667cb746f79cc5c14e243f5525836e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-18 20:55:15 +0700

    inject Standard printer

--- packages/phpstan-rules/src/Rules/PreventDuplicateClassMethodRule.php
+++ packages/phpstan-rules/src/Rules/PreventDuplicateClassMethodRule.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\Rules;
 
-use PhpParser\ConstExprEvaluator;
+use Nette\Utils\Strings;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\PrettyPrinter\Standard;
 use PHPStan\Analyser\Scope;
 use Symplify\PHPStanRules\Naming\SimpleNameResolver;
-use Symplify\PHPStanRules\NodeAnalyzer\SymfonyPhpConfigClosureAnalyzer;
+use Symplify\PHPStanRules\ValueObject\MethodName;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
-use Nette\Utils\Strings;
-use Symplify\PHPStanRules\ValueObject\MethodName;
 
 /**
  * @see \Symplify\PHPStanRules\Tests\Rules\PreventDuplicateClassMethodRule\PreventDuplicateClassMethodRuleTest
@@ -32,28 +31,19 @@ final class PreventDuplicateClassMethodRule extends AbstractSymplifyRule
     private $simpleNameResolver;
 
     /**
-     * @var SymfonyPhpConfigClosureAnalyzer
+     * @var Standard
      */
-    private $symfonyPhpConfigClosureAnalyzer;
+    private $printerStandard;
 
     /**
      * @var array<string, string[]>
      */
     private $contentMethodByNameFile = [];
 
-    /**
-     * @var ConstExprEvaluator
-     */
-    private $constExprEvaluator;
-
-    public function __construct(
-        SimpleNameResolver $simpleNameResolver,
-        SymfonyPhpConfigClosureAnalyzer $symfonyPhpConfigClosureAnalyzer,
-        ConstExprEvaluator $constExprEvaluator
-    ) {
+    public function __construct(SimpleNameResolver $simpleNameResolver, Standard $printerStandard)
+    {
         $this->simpleNameResolver = $simpleNameResolver;
-        $this->symfonyPhpConfigClosureAnalyzer = $symfonyPhpConfigClosureAnalyzer;
-        $this->constExprEvaluator = $constExprEvaluator;
+        $this->printerStandard = $printerStandard;
     }
 
     /**
@@ -75,26 +65,6 @@ final class PreventDuplicateClassMethodRule extends AbstractSymplifyRule
         }
 
         return [];
-    }
-
-    private function shouldSkip(ClassMethod $classMethod, Scope $scope): bool
-    {
-        if ($scope->getClassReflection() === null) {
-            return true;
-        }
-
-        if (! $this->simpleNameResolver->isName($classMethod->name, MethodName::CONSTRUCTOR)) {
-            return true;
-        }
-
-        /** @var Class_|null $class */
-        $class = $this->resolveCurrentClass($classMethod);
-
-        if ($class === null || Strings::endWith($class->toString(), 'Test')) {
-            return true;
-        }
-
-        return false;
     }
 
     public function getRuleDefinition(): RuleDefinition
@@ -138,5 +108,25 @@ class B
 CODE_SAMPLE
             ),
         ]);
+    }
+
+    private function shouldSkip(ClassMethod $classMethod, Scope $scope): bool
+    {
+        if ($scope->getClassReflection() === null) {
+            return true;
+        }
+
+        if (! $this->simpleNameResolver->isName($classMethod->name, MethodName::CONSTRUCTOR)) {
+            return true;
+        }
+
+        /** @var Class_|null $class */
+        $class = $this->resolveCurrentClass($classMethod);
+
+        if ($class === null || Strings::endWith($class->toString(), 'Test')) {
+            return true;
+        }
+
+        return false;
     }
 }
