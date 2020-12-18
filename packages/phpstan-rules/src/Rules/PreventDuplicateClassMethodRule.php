24524c770e4f48cf940ddfaa21866c9faed6df01
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-18 20:11:55 +0700

    [PHPStanRules] [WIP] Fixes #2647 Add PreventDuplicateClassMethodRule

---
+++ packages/phpstan-rules/src/Rules/PreventDuplicateClassMethodRule.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules;
+
+use PhpParser\ConstExprEvaluator;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Analyser\Scope;
+use Symplify\PHPStanRules\Naming\SimpleNameResolver;
+use Symplify\PHPStanRules\NodeAnalyzer\SymfonyPhpConfigClosureAnalyzer;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\PreventDuplicateClassMethodRule\PreventDuplicateClassMethodRuleTest
+ */
+final class PreventDuplicateClassMethodRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Content of method "%s" is duplicated, use unique content instead';
+
+    /**
+     * @var SimpleNameResolver
+     */
+    private $simpleNameResolver;
+
+    /**
+     * @var SymfonyPhpConfigClosureAnalyzer
+     */
+    private $symfonyPhpConfigClosureAnalyzer;
+
+    /**
+     * @var array<string, string[]>
+     */
+    private $contentMethodByNameFile = [];
+
+    /**
+     * @var ConstExprEvaluator
+     */
+    private $constExprEvaluator;
+
+    public function __construct(
+        SimpleNameResolver $simpleNameResolver,
+        SymfonyPhpConfigClosureAnalyzer $symfonyPhpConfigClosureAnalyzer,
+        ConstExprEvaluator $constExprEvaluator
+    ) {
+        $this->simpleNameResolver = $simpleNameResolver;
+        $this->symfonyPhpConfigClosureAnalyzer = $symfonyPhpConfigClosureAnalyzer;
+        $this->constExprEvaluator = $constExprEvaluator;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    /**
+     * @param ClassMethod $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        return [];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class A
+{
+    public function someMethod()
+    {
+        (new SmartFinder())->run('.php');
+    }
+}
+
+class B
+{
+    public function someMethod()
+    {
+        (new SmartFinder())->run('.php');
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+class A
+{
+    public function someMethod()
+    {
+        (new SmartFinder())->run('.php');
+    }
+}
+
+class B
+{
+    public function someMethod()
+    {
+        (new SmartFinder())->run('.js');
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+}
