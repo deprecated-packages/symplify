243ef5c11ecbe53e5af92af9800a6e7d9e93d5f1
TomasVotruba <tomas.vot@gmail.com>
2020-12-19 01:45:44 +0100

    [PHPStanRules] Skip callables in dynamic naming

---
+++ packages/phpstan-rules/src/Types/TypeUnwrapper.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Types;
+
+use PHPStan\Type\NullType;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+
+final class TypeUnwrapper
+{
+    public function unwrapNullableType(Type $type): Type
+    {
+        if (! $type instanceof UnionType) {
+            return $type;
+        }
+
+        $unionedTypes = $type->getTypes();
+        if (count($unionedTypes) !== 2) {
+            return $type;
+        }
+
+        $nullSuperTypeTrinaryLogic = $type->isSuperTypeOf(new NullType());
+        if (! $nullSuperTypeTrinaryLogic->yes()) {
+            return $type;
+        }
+
+        foreach ($unionedTypes as $unionedType) {
+            if ($unionedType instanceof NullType) {
+                continue;
+            }
+
+            return $unionedType;
+        }
+
+        return $type;
+    }
+}
--- packages/phpstan-rules/tests/Rules/NoDynamicNameRule/NoDynamicNameRuleTest.php
+++ packages/phpstan-rules/tests/Rules/NoDynamicNameRule/NoDynamicNameRuleTest.php
@@ -32,6 +32,7 @@ final class NoDynamicNameRuleTest extends AbstractServiceAwareRuleTestCase
         yield [__DIR__ . '/Fixture/SkipClosure.php', []];
         yield [__DIR__ . '/Fixture/SkipCallable.php', []];
         yield [__DIR__ . '/Fixture/SkipNullableClosure.php', []];
+        yield [__DIR__ . '/Fixture/SkipForeachVariable.php', []];
     }
 
     protected function getRule(): Rule
