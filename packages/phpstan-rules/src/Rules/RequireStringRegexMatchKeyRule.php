dbc968b38b4ed18bf304752efdeb2d250640a9ce
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-20 16:10:37 +0700

    functional

--- packages/phpstan-rules/src/Rules/RequireStringRegexMatchKeyRule.php
+++ packages/phpstan-rules/src/Rules/RequireStringRegexMatchKeyRule.php
@@ -4,23 +4,21 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\Rules;
 
+use Nette\Utils\Strings;
 use PhpParser\Node;
-use PhpParser\Node\Identifier;
 use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\LNumber;
-use PhpParser\Node\Scalar\String_;
-use PhpParser\Node\Stmt\Expression;
+use PhpParser\NodeFinder;
 use PHPStan\Analyser\Scope;
+use Symplify\PHPStanRules\Printer\NodeComparator;
+use Symplify\PHPStanRules\ValueObject\PHPStanAttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
-use Symplify\PHPStanRules\ValueObject\PHPStanAttributeKey;
-use Symplify\PHPStanRules\Printer\NodeComparator;
-use PhpParser\NodeFinder;
-use PhpParser\Node\Expr\StaticCall;
-use PhpParser\Node\Name\FullyQualified;
-use Nette\Utils\Strings;
-use PhpParser\Node\Expr\ClassConstFetch;
 
 /**
  * @see \Symplify\PHPStanRules\Tests\Rules\RequireStringRegexMatchKeyRule\RequireStringRegexMatchKeyRuleTest
@@ -101,27 +99,6 @@ final class RequireStringRegexMatchKeyRule extends AbstractSymplifyRule
         return null;
     }
 
-    private function isExprStringsMatch(?Assign $assign): bool
-    {
-        if (! $assign->expr instanceof StaticCall) {
-            return false;
-        }
-
-        if (! $assign->expr->class instanceof FullyQualified) {
-            return false;
-        }
-
-        if ($assign->expr->class->toString() !== Strings::class) {
-            return false;
-        }
-
-        if (! $assign->expr->name instanceof Identifier) {
-            return false;
-        }
-
-        return $assign->expr->name->toString() === 'match';
-    }
-
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(self::ERROR_MESSAGE, [
@@ -161,5 +138,26 @@ class SomeClass
 CODE_SAMPLE
             ),
         ]);
+    }
+
+    private function isExprStringsMatch(?Assign $assign): bool
+    {
+        if (! $assign->expr instanceof StaticCall) {
+            return false;
+        }
+
+        if (! $assign->expr->class instanceof FullyQualified) {
+            return false;
+        }
+
+        if ($assign->expr->class->toString() !== Strings::class) {
+            return false;
+        }
+
+        if (! $assign->expr->name instanceof Identifier) {
+            return false;
+        }
+
+        return $assign->expr->name->toString() === 'match';
     }
 }
