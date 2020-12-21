e03b9f1d2c771412a1d27829c0bf91f50ff291d1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-21 15:57:26 +0700

    more fixture

---
+++ packages/phpstan-rules/tests/Rules/RequireStringRegexMatchKeyRule/Fixture/SkipNotUsed.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\RequireQuoteStringValueSprintfRule\Fixture;
+
+use Nette\Utils\Strings;
+
+class SkipNotUsed
+{
+    private const REGEX = '#(a content)#';
+
+    public function run()
+    {
+        $matches = Strings::match('a content', self::REGEX);
+    }
+}
