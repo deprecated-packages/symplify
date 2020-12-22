96561891c2ae99eee3c4faae6c5b1e1735a9ca99
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-22 20:58:51 +0700

    [PHPStanRules] Fixes #2669 Add RequireThisCallOnLocalMethodRule

---
+++ packages/phpstan-rules/src/Rules/RequireThisCallOnLocalMethodRule.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Analyser\Scope;
+use Symplify\PHPStanRules\Naming\SimpleNameResolver;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\RequireThisCallOnLocalMethodRule\RequireThisCallOnLocalMethodRuleTest
+ */
+final class RequireThisCallOnLocalMethodRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Use "$this-><method>()" instead of "self::<method>()" to call local method';
+
+    /**
+     * @var SimpleNameResolver
+     */
+    private $simpleNameResolver;
+
+    public function __construct(SimpleNameResolver $simpleNameResolver)
+    {
+        $this->simpleNameResolver = $simpleNameResolver;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [StaticCall::class];
+    }
+
+    /**
+     * @param StaticCall $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        if (! $node->class instanceof Name) {
+            return [];
+        }
+
+        if (! $this->simpleNameResolver->isName($node->class, 'self')) {
+            return [];
+        }
+
+        $classMethod = $this->getClassMethodInCurrentClass($node);
+        if ($classMethod === null) {
+            return [];
+        }
+
+        if ($classMethod->isStatic()) {
+            return [];
+        }
+
+        return [self::ERROR_MESSAGE];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeParentClass
+{
+    public function run()
+    {
+    }
+}
+
+class SomeClass extends SomeParentClass
+{
+    public function go()
+    {
+        parent::run();
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+class SomeParentClass
+{
+    public function run()
+    {
+    }
+}
+
+class SomeClass extends SomeParentClass
+{
+    public function go()
+    {
+        $tihs->run();
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    private function getClassMethodInCurrentClass(StaticCall $staticCall): ?ClassMethod
+    {
+        $class = $this->resolveCurrentClass($staticCall);
+        if (! $class instanceof Class_) {
+            return null;
+        }
+
+        return $class->getMethod((string) $staticCall->name);
+    }
+}
