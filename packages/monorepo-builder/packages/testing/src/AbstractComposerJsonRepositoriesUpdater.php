a059590103a208ce2bd7ecf6eec63dbfb0c1eb91
Leonardo Losoviz <leo@getpop.org>
2020-12-23 23:15:34 +0800

    Created class hierarchy

---
+++ packages/monorepo-builder/packages/testing/src/AbstractComposerJsonRepositoriesUpdater.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Testing;
+
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symplify\ComposerJsonManipulator\FileSystem\JsonFileManager;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
+use Symplify\ConsoleColorDiff\Console\Output\ConsoleDiffer;
+use Symplify\MonorepoBuilder\Package\PackageNamesProvider;
+use Symplify\MonorepoBuilder\Testing\PackageDependency\UsedPackagesResolver;
+use Symplify\SmartFileSystem\SmartFileInfo;
+use Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
+
+abstract class AbstractComposerJsonRepositoriesUpdater
+{
+    /**
+     * @var PackageNamesProvider
+     */
+    private $packageNamesProvider;
+
+    /**
+     * @var JsonFileManager
+     */
+    private $jsonFileManager;
+
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
+
+    /**
+     * @var UsedPackagesResolver
+     */
+    private $usedPackagesResolver;
+
+    /**
+     * @var ConsoleDiffer
+     */
+    private $consoleDiffer;
+
+    public function __construct(
+        PackageNamesProvider $packageNamesProvider,
+        JsonFileManager $jsonFileManager,
+        SymfonyStyle $symfonyStyle,
+        UsedPackagesResolver $usedPackagesResolver,
+        ConsoleDiffer $consoleDiffer
+    ) {
+        $this->packageNamesProvider = $packageNamesProvider;
+        $this->jsonFileManager = $jsonFileManager;
+        $this->symfonyStyle = $symfonyStyle;
+        $this->usedPackagesResolver = $usedPackagesResolver;
+        $this->consoleDiffer = $consoleDiffer;
+    }
+
+    public function processPackage(SmartFileInfo $packageFileInfo, ComposerJson $rootComposerJson, ?bool $symlink): void
+    {
+        $packageComposerJson = $this->jsonFileManager->loadFromFileInfo($packageFileInfo);
+
+        $usedPackageNames = $this->usedPackagesResolver->resolveForPackage($packageComposerJson);
+        if ($usedPackageNames === []) {
+            $message = sprintf(
+                'Package "%s" does not use any mutual dependencies, so we skip it',
+                $packageFileInfo->getRelativeFilePathFromCwd()
+            );
+            $this->symfonyStyle->note($message);
+            return;
+        }
+
+        // possibly replace them all to cover recursive secondary dependencies
+        $packageNames = $this->packageNamesProvider->provide();
+
+        $oldComposerJsonContents = $packageFileInfo->getContents();
+
+        $rootComposerJsonFileInfo = $rootComposerJson->getFileInfo();
+        if ($rootComposerJsonFileInfo === null) {
+            throw new ShouldNotHappenException();
+        }
+
+        $packageComposerJson = $this->decoratePackageComposerJson(
+            $packageComposerJson,
+            $packageNames,
+            $rootComposerJsonFileInfo,
+            $symlink
+        );
+
+        $newComposerJsonContents = $this->jsonFileManager->printJsonToFileInfo($packageComposerJson, $packageFileInfo);
+
+        $message = sprintf('File "%s" was updated', $packageFileInfo->getRelativeFilePathFromCwd());
+        $this->symfonyStyle->title($message);
+
+        $this->consoleDiffer->diffAndPrint($oldComposerJsonContents, $newComposerJsonContents);
+        $this->symfonyStyle->newLine(2);
+    }
+
+    /**
+     * @param mixed[] $packageComposerJson
+     * @param string[] $packageNames
+     * @return mixed[]
+     */
+    abstract protected function decoratePackageComposerJson(array $packageComposerJson, array $packageNames, SmartFileInfo $rootComposerJsonFileInfo, ?bool $symlink): array;
+}
---
+++ packages/monorepo-builder/src/Command/RemoveSymlinkToLocalPackagesCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symplify\MonorepoBuilder\FileSystem\ComposerJsonProvider;
+use Symplify\MonorepoBuilder\Testing\ComposerJsonRepositoriesUpdater;
+use Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
+use Symplify\PackageBuilder\Console\ShellCode;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class RemoveSymlinkToLocalPackagesCommand extends AbstractSymplifyCommand
+{
+    /**
+     * @var ComposerJsonProvider
+     */
+    private $composerJsonProvider;
+
+    /**
+     * @var ComposerJsonRepositoriesUpdater
+     */
+    private $composerJsonRepositoriesUpdater;
+
+    public function __construct(
+        ComposerJsonProvider $composerJsonProvider,
+        ComposerJsonRepositoriesUpdater $composerJsonRepositoriesUpdater
+    ) {
+        $this->composerJsonProvider = $composerJsonProvider;
+        $this->composerJsonRepositoriesUpdater = $composerJsonRepositoriesUpdater;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Remove paths to local packages as repositories in every composer.json');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $rootComposerJson = $this->composerJsonProvider->getRootComposerJson();
+
+        $packagesFileInfos = $this->composerJsonProvider->getPackagesComposerFileInfos();
+        foreach ($packagesFileInfos as $packageFileInfo) {
+            // $symlink => `true`: when executing a package,
+            // any modification to another local package can be seen immediately
+            $this->composerJsonRepositoriesUpdater->processPackage(
+                $packageFileInfo,
+                $rootComposerJson,
+                true
+            );
+        }
+
+        $message = sprintf(
+            'The composer.json for the following packages has been updated, removing the path to all local packages as repositories: "%s"',
+            implode(', ', array_map(
+                function (SmartFileInfo $packageFileInfo): string {
+                    return $packageFileInfo->getRelativeFilePathFromCwd();
+                },
+                $packagesFileInfos
+            ))
+        );
+        $this->symfonyStyle->success($message);
+
+        return ShellCode::SUCCESS;
+    }
+}
