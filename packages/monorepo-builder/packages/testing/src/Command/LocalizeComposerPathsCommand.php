87d0b06b9d56b9b674b40fe9852c9e9cc16368a9
Leonardo Losoviz <leo@getpop.org>
2020-12-28 18:18:57 +0800

    Restore original

--- packages/monorepo-builder/packages/testing/src/Command/LocalizeComposerPathsCommand.php
+++ packages/monorepo-builder/packages/testing/src/Command/LocalizeComposerPathsCommand.php
@@ -6,7 +6,6 @@ namespace Symplify\MonorepoBuilder\Testing\Command;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symplify\MonorepoBuilder\FileSystem\ComposerJsonProvider;
 use Symplify\MonorepoBuilder\Testing\ComposerJsonRepositoriesUpdater;
@@ -48,23 +47,7 @@ final class LocalizeComposerPathsCommand extends AbstractSymplifyCommand
     protected function configure(): void
     {
         $this->setDescription('Set mutual package paths to local packages - use for pre-split package testing');
-        $this->addArgument(
-            Option::PACKAGE_COMPOSER_JSON,
-            InputArgument::REQUIRED,
-            'Path to package "composer.json"'
-        );
-        $this->addOption(
-            Option::SYMLINK,
-            null,
-            InputOption::VALUE_NONE,
-            'Localize composer paths with symlinks'
-        );
-        $this->addOption(
-            Option::SKIP_DEV_BRANCH_UPDATE,
-            null,
-            InputOption::VALUE_NONE,
-            'Do not update all "require" and "require-dev" entries to the "dev-master" branch of all local packages'
-        );
+        $this->addArgument(Option::PACKAGE_COMPOSER_JSON, InputArgument::REQUIRED, 'Path to package "composer.json"');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -76,26 +59,13 @@ final class LocalizeComposerPathsCommand extends AbstractSymplifyCommand
         $rootComposerJson = $this->composerJsonProvider->getRootComposerJson();
 
         // 1. update "require" to "*" for all local packages
-        $skipDevBranchUpdate = (bool) $input->getOption(Option::SKIP_DEV_BRANCH_UPDATE);
-        if (! $skipDevBranchUpdate) {
-            $packagesFileInfos = $this->composerJsonProvider->getPackagesComposerFileInfos();
-            foreach ($packagesFileInfos as $packageFileInfo) {
-                $this->composerJsonRequireUpdater->processPackage($packageFileInfo);
-            }
+        $packagesFileInfos = $this->composerJsonProvider->getPackagesComposerFileInfos();
+        foreach ($packagesFileInfos as $packageFileInfo) {
+            $this->composerJsonRequireUpdater->processPackage($packageFileInfo);
         }
 
         // 2. update "repository" to "*" for current composer.json
-        // $symlink => `false` is needed for testing on GitHub Actions:
-        // we need hard copy of files,
-        // as in normal composer install of standalone package
-        // $symlink => `true` is needed to point to local packages
-        // during development, avoiding Packagist
-        $symlink = (bool) $input->getOption(Option::SYMLINK);
-        $this->composerJsonRepositoriesUpdater->processPackage(
-            $packageComposerJsonFileInfo,
-            $rootComposerJson,
-            $symlink
-        );
+        $this->composerJsonRepositoriesUpdater->processPackage($packageComposerJsonFileInfo, $rootComposerJson);
 
         $message = sprintf(
             'Package paths in "%s" have been updated',
--- packages/monorepo-builder/packages/testing/src/ValueObject/Option.php
+++ packages/monorepo-builder/packages/testing/src/ValueObject/Option.php
@@ -10,12 +10,4 @@ final class Option
      * @var string
      */
     public const PACKAGE_COMPOSER_JSON = 'package_composer_json';
-    /**
-     * @var string
-     */
-    public const SYMLINK = 'symlink';
-    /**
-     * @var string
-     */
-    public const SKIP_DEV_BRANCH_UPDATE = 'skip-dev-branch-update';
 }
