5ecadb271acd15994c40ffcca47c244e08f69bb1
Leonardo Losoviz <leo@getpop.org>
2020-12-29 14:52:56 +0800

    Fixed getting name from origin

--- packages/monorepo-builder/src/Github/GithubRepositoryResolver.php
+++ packages/monorepo-builder/src/Github/GithubRepositoryResolver.php
@@ -6,12 +6,18 @@ namespace Symplify\MonorepoBuilder\Github;
 
 use Nette\Utils\Strings;
 use Symfony\Component\Process\Process;
+use Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
 
 /**
  * @see \Symplify\MonorepoBuilder\Tests\Github\GithubRepositoryResolverTest
  */
 final class GithubRepositoryResolver
 {
+    /**
+     * @var string
+     */
+    private const GITHUB_URL = 'https://github.com/';
+
     /**
      * @var GithubRepositoryFromRemoteResolver
      */
@@ -28,14 +34,23 @@ final class GithubRepositoryResolver
         $process = new Process(['git', 'config', '--get', 'remote.origin.url']);
         $process->run();
         $repositoryOriginUrl = trim($process->getOutput());
-        $repositoryUrl = $this->githubRepositoryFromRemoteResolver->resolveFromUrl($repositoryOriginUrl);
-        return $this->resolveGitHubRepositoryName($repositoryUrl);
+        return $this->resolveGitHubRepositoryName($repositoryOriginUrl);
     }
 
-    public function resolveGitHubRepositoryName(string $repositoryUrl): string
+    public function resolveGitHubRepositoryName(string $repositoryOriginUrl): string
     {
+        $repositoryUrl = $this->githubRepositoryFromRemoteResolver->resolveFromUrl($repositoryOriginUrl);
+        if (! Strings::startsWith($repositoryUrl, self::GITHUB_URL)) {
+            throw new ShouldNotHappenException(
+                sprintf(
+                    'Remote URL "%s" is not hosted on GitHub (should start with "%s")',
+                    $repositoryUrl,
+                    self::GITHUB_URL
+                )
+            );
+        }
         // Extract the account name: everything after "github.com/", and before the next "/"
-        $repository = Strings::substring($repositoryUrl, Strings::length('https://github.com/'));
+        $repository = Strings::substring($repositoryUrl, Strings::length(self::GITHUB_URL));
         return Strings::before($repository, '/');
     }
 }
