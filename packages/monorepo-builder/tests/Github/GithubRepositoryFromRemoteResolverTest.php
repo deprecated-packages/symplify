2ae9c790c8d686f34c3543499d336292c91949d8
Leonardo Losoviz <leo@getpop.org>
2020-12-29 15:20:00 +0800

    Copied class from ChangelogLinker

---
+++ packages/monorepo-builder/tests/Github/GithubRepositoryFromRemoteResolverTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Tests\Github;
+
+use Iterator;
+use PHPUnit\Framework\TestCase;
+use Symplify\MonorepoBuilder\Exception\Git\InvalidGitRemoteException;
+use Symplify\MonorepoBuilder\Github\GithubRepositoryFromRemoteResolver;
+
+final class GithubRepositoryFromRemoteResolverTest extends TestCase
+{
+    /**
+     * @var GithubRepositoryFromRemoteResolver
+     */
+    private $githubRepositoryFromRemoteResolver;
+
+    protected function setUp(): void
+    {
+        $this->githubRepositoryFromRemoteResolver = new GithubRepositoryFromRemoteResolver();
+    }
+
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $commitHash, string $expectedHttpsUrl): void
+    {
+        $this->assertSame($expectedHttpsUrl, $this->githubRepositoryFromRemoteResolver->resolveFromUrl($commitHash));
+    }
+
+    public function provideData(): Iterator
+    {
+        yield ['git@github.com:symplify/symplify.git', 'https://github.com/symplify/symplify'];
+        yield ['https://github.com/symplify/symplify.git', 'https://github.com/symplify/symplify'];
+        yield ['https://UserName@github.com/symplify/symplify.git', 'https://github.com/symplify/symplify'];
+        yield [
+            'https://UserName:PassWord@github.com:443/symplify/symplify.git',
+            'https://github.com/symplify/symplify',
+        ];
+        yield ['https://www.my-company.com/symplify/symplify.git', 'https://www.my-company.com/symplify/symplify'];
+        yield ['https://gitlab.com/my-group/my-user/my-repo.git', 'https://gitlab.com/my-group/my-user/my-repo'];
+        yield ['https://git/user/project.git', 'https://git/user/project'];
+        yield ['git@github.com:space/low-orbit.git', 'https://github.com/space/low-orbit'];
+    }
+
+    public function testInvalid(): void
+    {
+        $this->expectException(InvalidGitRemoteException::class);
+        $this->githubRepositoryFromRemoteResolver->resolveFromUrl('http://url.git');
+    }
+}
