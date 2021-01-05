1ad6484ddf6e14c90d60dcda3f14b82455668847
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-06 02:57:47 +0700

    [CodingStandard] Remove reference on @param for ParamReturnAndVarTagMalformsFixer (#2741)
    * Fixture [CodingStandard] Fixes #2716 Remove reference on @param for ParamReturnAndVarTagMalformsFixer
    
    * implemented
    
    * cs fix
    
    * clean up
    
    * regex update link

---
+++ packages/coding-standard/src/TokenRunner/DocBlock/MalformWorker/ParamNameReferenceMalformWorker.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\TokenRunner\DocBlock\MalformWorker;
+
+use Nette\Utils\Strings;
+use PhpCsFixer\Tokenizer\Tokens;
+
+final class ParamNameReferenceMalformWorker extends AbstractMalformWorker
+{
+    /**
+     * @var string
+     * @see https://regex101.com/r/B4rWNk/3
+     */
+    private const PARAM_NAME_REGEX = '#(?<param>@param(.*?))&(?<paramName>\$\w+)#';
+
+    public function work(string $docContent, Tokens $tokens, int $position): string
+    {
+        return Strings::replace($docContent, self::PARAM_NAME_REGEX, function ($match) {
+            return $match['param'] . $match['paramName'];
+        });
+    }
+}
