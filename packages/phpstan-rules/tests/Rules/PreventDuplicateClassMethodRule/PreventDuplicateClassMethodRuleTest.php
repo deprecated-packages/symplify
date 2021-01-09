d30f174f8e65d0fc1aa51fa29c19ee8974b70407
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-10 04:38:50 +0700

    works 🎉

--- packages/phpstan-rules/tests/Rules/PreventDuplicateClassMethodRule/PreventDuplicateClassMethodRuleTest.php
+++ packages/phpstan-rules/tests/Rules/PreventDuplicateClassMethodRule/PreventDuplicateClassMethodRuleTest.php
@@ -9,6 +9,7 @@ use PHPStan\Rules\Rule;
 use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
 use Symplify\PHPStanRules\Rules\PreventDuplicateClassMethodRule;
 use Symplify\PHPStanRules\Tests\Rules\PreventDuplicateClassMethodRule\Fixture\FirstClass;
+use Symplify\PHPStanRules\Tests\Rules\PreventDuplicateClassMethodRule\Fixture\FirstClassWithParameter;
 
 final class PreventDuplicateClassMethodRuleTest extends AbstractServiceAwareRuleTestCase
 {
@@ -39,6 +40,12 @@ final class PreventDuplicateClassMethodRuleTest extends AbstractServiceAwareRule
             __DIR__ . '/Fixture/FirstClass.php',
             __DIR__ . '/Fixture/SecondClassDuplicateFirstClassMethod.php',
         ], [[$errorMessage, 15]]];
+
+        $errorMessage = sprintf(PreventDuplicateClassMethodRule::ERROR_MESSAGE, 'method', FirstClassWithParameter::class);
+        yield [[
+            __DIR__ . '/Fixture/FirstClassWithParameter.php',
+            __DIR__ . '/Fixture/SecondClassDuplicateFirstClassWithParameterMethod.php',
+        ], [[$errorMessage, 12]]];
     }
 
     protected function getRule(): Rule
