53aa08f780a4ff5a537c94afa0742abef95dfbc5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-10 04:32:12 +0700

    separated test due phpunit serialization error process

--- packages/phpstan-rules/tests/Rules/PreventDuplicateClassMethodRule/MaskedContentTest.php
+++ packages/phpstan-rules/tests/Rules/PreventDuplicateClassMethodRule/MaskedContentTest.php
@@ -9,8 +9,9 @@ use PHPStan\Rules\Rule;
 use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
 use Symplify\PHPStanRules\Rules\PreventDuplicateClassMethodRule;
 use Symplify\PHPStanRules\Tests\Rules\PreventDuplicateClassMethodRule\Fixture\FirstClass;
+use Symplify\PHPStanRules\Tests\Rules\PreventDuplicateClassMethodRule\Fixture\FirstClassWithParameter;
 
-final class PreventDuplicateClassMethodRuleTest extends AbstractServiceAwareRuleTestCase
+final class MaskedContent extends AbstractServiceAwareRuleTestCase
 {
     /**
      * @param string[] $filePaths
@@ -23,22 +24,11 @@ final class PreventDuplicateClassMethodRuleTest extends AbstractServiceAwareRule
 
     public function provideData(): Iterator
     {
-        yield [[__DIR__ . '/Fixture/ValueObject/SkipChair.php', __DIR__ . '/Fixture/ValueObject/SkipTable.php'], []];
-        yield [[__DIR__ . '/Fixture/Entity/SkipApple.php', __DIR__ . '/Fixture/Entity/SkipCar.php'], []];
-
-        yield [[__DIR__ . '/Fixture/SkipInterface.php'], []];
-
+        $errorMessage = sprintf(PreventDuplicateClassMethodRule::ERROR_MESSAGE, 'method', FirstClassWithParameter::class);
         yield [[
-            __DIR__ . '/Fixture/SkipClassWithTrait.php',
-            __DIR__ . '/Fixture/SkipTraitUsingTrait.php',
-            __DIR__ . '/Fixture/SkipSomeTrait.php',
-        ], []];
-
-        $errorMessage = sprintf(PreventDuplicateClassMethodRule::ERROR_MESSAGE, 'someMethod', FirstClass::class);
-        yield [[
-            __DIR__ . '/Fixture/FirstClass.php',
-            __DIR__ . '/Fixture/SecondClassDuplicateFirstClassMethod.php',
-        ], [[$errorMessage, 15]]];
+            __DIR__ . '/Fixture/FirstClassWithParameter.php',
+            __DIR__ . '/Fixture/SecondClassDuplicateFirstClassWithParameterMethod.php',
+        ], [[$errorMessage, 12]]];
     }
 
     protected function getRule(): Rule
