f71b93116e6d96e362bf1dd5a2c79492b57a9221
TomasVotruba <tomas.vot@gmail.com>
2021-01-10 09:22:11 +0100

    [Astral] Add NodeValueResolverTest, add TypeChecker

---
+++ packages/package-builder/src/Php/TypeChecker.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Php;
+
+final class TypeChecker
+{
+    /**
+     * @param string[] $types
+     */
+    public function isInstanceOf($object, array $types): bool
+    {
+        foreach ($types as $type) {
+            if (is_a($object, $type, true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
