ae2fa2ca1a0ed430cb48f28f212d31fe861350de
TomasVotruba <tomas.vot@gmail.com>
2021-01-12 18:56:02 +0100

    use string class, now that its missing

--- packages/coding-standard/config/symplify.php
+++ packages/coding-standard/config/symplify.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ClassNotation\FinalInternalClassFixer;
 use PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer;
-use SlevomatCodingStandard\Sniffs\Namespaces\ReferenceUsedNamesOnlySniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -21,12 +20,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->load('Symplify\CodingStandard\Fixer\\', __DIR__ . '/../src/Fixer')
         ->exclude([__DIR__ . '/../src/Fixer/Annotation']);
-
-    $services->set(ReferenceUsedNamesOnlySniff::class)
-        ->property('searchAnnotations', true)
-        ->property('allowFallbackGlobalFunctions', true)
-        ->property('allowFallbackGlobalConstants', true)
-        ->property('allowPartialUses', false);
 
     $services->set(PhpdocLineSpanFixer::class);
 };
