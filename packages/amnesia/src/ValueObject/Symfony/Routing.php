03baeb9e72d831275eb0fe37fe18a5bc5e48e614
TomasVotruba <tomas.vot@gmail.com>
2021-01-17 21:36:36 +0100

    add routing

---
+++ packages/amnesia/src/ValueObject/Symfony/Routing.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\Amnesia\ValueObject\Symfony;
+
+/**
+ * @api
+ * @see https://symfony.com/doc/current/routing.html
+ */
+final class Routing
+{
+    /**
+     * @var string
+     * @see https://symfony.com/doc/current/routing.html#creating-routes-as-attributes-or-annotations
+     */
+    public const TYPE_ANNOTATION = 'annotation';
+}
