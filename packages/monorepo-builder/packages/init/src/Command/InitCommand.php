cd2e432268cc8300196f80d6cc060c1ce95d3b58
TomasVotruba <tomas.vot@gmail.com>
2021-01-18 17:12:26 +0100

    drop placeholder, not needed anymore

--- packages/monorepo-builder/packages/init/src/Command/InitCommand.php
+++ packages/monorepo-builder/packages/init/src/Command/InitCommand.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace Symplify\MonorepoBuilder\Init\Command;
 
-use PharIo\Version\Version;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symplify\MonorepoBuilder\Init\Composer\PackageNameVersionProvider;
-use Symplify\MonorepoBuilder\ValueObject\File;
 use Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
 use Symplify\PackageBuilder\Console\ShellCode;
 
@@ -19,18 +16,6 @@ final class InitCommand extends AbstractSymplifyCommand
      * @var string
      */
     private const OUTPUT = 'output';
-
-    /**
-     * @var PackageNameVersionProvider
-     */
-    private $packageNameVersionProvider;
-
-    public function __construct(PackageNameVersionProvider $monorepoBuilderVersionExtractor)
-    {
-        $this->packageNameVersionProvider = $monorepoBuilderVersionExtractor;
-
-        parent::__construct();
-    }
 
     protected function configure(): void
     {
@@ -44,18 +29,6 @@ final class InitCommand extends AbstractSymplifyCommand
         $output = $input->getArgument(self::OUTPUT);
 
         $this->smartFileSystem->mirror(__DIR__ . '/../../templates/monorepo', $output);
-
-        // Replace MonorepoBuilder version in monorepo-builder.php
-        $filename = sprintf('%s/%s', $output, File::CONFIG);
-
-        $fileContent = $this->smartFileSystem->readFile($filename);
-        $content = str_replace(
-            '<version>',
-            $this->packageNameVersionProvider->provide('symplify/monorepo-builder'),
-            $fileContent
-        );
-
-        $this->smartFileSystem->dumpFile($filename, $content);
 
         $this->symfonyStyle->success('Congrats! Your first monorepo is here.');
         $message = sprintf(
