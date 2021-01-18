101691d0f26ff92ca813af9c10583cc6c6b10d2e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-18 18:46:43 +0700

    class name

--- packages/phpstan-rules/tests/Rules/ForbiddenAssignInLoopRule/Fixture/SkipReUsedAssignExprVarInForeach.php
+++ packages/phpstan-rules/tests/Rules/ForbiddenAssignInLoopRule/Fixture/SkipReUsedAssignExprVarInForeach.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\Tests\Rules\ForbiddenAssignInLoopRule\Fixture;
 
-final class SkipAssignExprUseForeachVar
+final class SkipReUsedAssignExprVarInForeach
 {
     public function run()
     {
