73a1bc7798cbe2e34cbacd737738cb94882e0abd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-23 20:27:17 +0700

    final touch

--- packages/phpstan-rules/tests/Rules/ForbiddenAssignInLoopRule/Fixture/SkipAssignExprUseForeachVar.php
+++ packages/phpstan-rules/tests/Rules/ForbiddenAssignInLoopRule/Fixture/SkipAssignExprUseForeachVar.php
@@ -8,7 +8,13 @@ final class SkipAssignExprUseForeachVar
 {
     public function run()
     {
+        foreach ($data as $d) {
+            $value = new SmartFileInfo($d);
+        }
 
+        foreach ($data as $d) {
+            $value = new SmartFileInfo($data);
+        }
 
         foreach ($data as $key => $d) {
             $value = new SmartFileInfo($key);
