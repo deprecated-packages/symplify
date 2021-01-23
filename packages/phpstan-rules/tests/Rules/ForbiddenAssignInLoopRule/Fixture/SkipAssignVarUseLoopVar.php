d1cfc845063af423b21e6632004ad19d5de7d88e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-23 15:45:03 +0700

    multi tests

--- packages/phpstan-rules/tests/Rules/ForbiddenAssignInLoopRule/Fixture/SkipAssignVarUseLoopVar.php
+++ packages/phpstan-rules/tests/Rules/ForbiddenAssignInLoopRule/Fixture/SkipAssignVarUseLoopVar.php
@@ -19,5 +19,62 @@ final class SkipAssignVarUseLoopVar
                 return;
             }
         }
+
+        foreach ($funcCall->args as $position => $arg) {
+            if (! $arg->value instanceof Array_) {
+                continue;
+            }
+
+            foreach ($arg->value->items as $arrayKey => $item) {
+                if (! $item instanceof ArrayItem) {
+                    continue;
+                }
+
+                $value = $this->getValue($item->value);
+                if ($scope->hasVariableType($value)->yes()) {
+                    continue;
+                }
+
+                unset($arg->value->items[$arrayKey]);
+            }
+
+            if ($arg->value->items === []) {
+                // Drops empty array from `compact()` arguments.
+                unset($funcCall->args[$position]);
+            }
+        }
+
+        foreach ($usedTraits as $usedTrait) {
+            foreach ($this->traitsToRemove as $traitToRemove) {
+                if ($this->isName($usedTrait, $traitToRemove)) {
+                    $this->removeNode($usedTrait);
+                    $this->classHasChanged = true;
+                    continue 2;
+                }
+            }
+        }
+
+        foreach ($class->getMethods() as $classMethod) {
+            foreach ($removedPropertyNames as $removedPropertyName) {
+                // remove methods
+                $setMethodName = 'set' . ucfirst($removedPropertyName);
+                $getMethodName = 'get' . ucfirst($removedPropertyName);
+
+                if ($this->isNames($classMethod, [$setMethodName, $getMethodName])) {
+                    continue;
+                }
+
+                $this->removeNode($classMethod);
+            }
+        }
+
+        foreach ($parentNestingBreakTypes as $parentNestingBreakType) {
+            if (! is_a($node, $parentNestingBreakType, true)) {
+                continue;
+            }
+
+            $this->isBreakingNodeFoundFirst = true;
+            return true;
+        }
     }
 }
