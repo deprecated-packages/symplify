936e71ddd232feaa35c9d18844f4ce8df42743a9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-27 20:12:26 +0700

    [PHPStanRules] Add NoDynamicPropertyOnStaticCallRule (#2869)
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

---
+++ packages/phpstan-rules/src/Rules/NoDynamicPropertyOnStaticCallRule.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Name;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\StaticCall;
+use PHPStan\Analyser\Scope;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\NoDynamicPropertyOnStaticCallRule\NoDynamicPropertyOnStaticCallRuleTest
+ */
+final class NoDynamicPropertyOnStaticCallRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Use non-dynamic property on static call';
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [StaticCall::class, ClassConstFetch::class];
+    }
+
+    /**
+     * @param StaticCall|ClassConstFetch $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        if ($node->class instanceof Name) {
+            return [];
+        }
+
+        return [self::ERROR_MESSAGE];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        return $this->connection::literal;
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        return Connection::literal;
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+}
---
+++ packages/phpstan-rules/tests/Rules/NoDynamicPropertyOnStaticCallRule/NoDynamicPropertyOnStaticCallRuleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\NoDynamicPropertyOnStaticCallRule;
+
+use Iterator;
+use PHPStan\Rules\Rule;
+use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
+use Symplify\PHPStanRules\Rules\NoDynamicPropertyOnStaticCallRule;
+
+final class NoDynamicPropertyOnStaticCallRuleTest extends AbstractServiceAwareRuleTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function testRule(string $filePath, array $expectedErrorMessagesWithLines): void
+    {
+        $this->analyse([$filePath], $expectedErrorMessagesWithLines);
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/DynamicMethodCall.php', [[NoDynamicPropertyOnStaticCallRule::ERROR_MESSAGE, 11]]];
+        yield [__DIR__ . '/Fixture/DynamicPropertyCall.php', [[NoDynamicPropertyOnStaticCallRule::ERROR_MESSAGE, 11]]];
+        yield [__DIR__ . '/Fixture/SkipNonDynamicPropertyCall.php', []];
+        yield [__DIR__ . '/Fixture/SkipNonDynamicMethodCall.php', []];
+        yield [__DIR__ . '/Fixture/SkipSelfStatic.php', []];
+    }
+
+    protected function getRule(): Rule
+    {
+        return $this->getRuleFromConfig(
+            NoDynamicPropertyOnStaticCallRule::class,
+            __DIR__ . '/../../../config/symplify-rules.neon'
+        );
+    }
+}
