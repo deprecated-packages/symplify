b93c8bf9f985d1cc4c48398426f3a786d555b95c
TomasVotruba <tomas.vot@gmail.com>
2021-01-28 12:16:14 +0100

    [MonorepoBuilder] Remove AbstractComposerKeyMerger to make PHPStan happy

--- packages/monorepo-builder/packages/merge/src/ComposerKeyMerger/MinimalStabilityKeyMerger.php
+++ packages/monorepo-builder/packages/merge/src/ComposerKeyMerger/MinimalStabilityKeyMerger.php
@@ -7,7 +7,7 @@ namespace Symplify\MonorepoBuilder\Merge\ComposerKeyMerger;
 use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
 use Symplify\MonorepoBuilder\Merge\Contract\ComposerKeyMergerInterface;
 
-final class MinimalStabilityKeyMerger extends AbstractComposerKeyMerger implements ComposerKeyMergerInterface
+final class MinimalStabilityKeyMerger implements ComposerKeyMergerInterface
 {
     public function merge(ComposerJson $mainComposerJson, ComposerJson $newComposerJson): void
     {
--- packages/monorepo-builder/packages/merge/src/ComposerKeyMerger/RequireDevComposerKeyMerger.php
+++ packages/monorepo-builder/packages/merge/src/ComposerKeyMerger/RequireDevComposerKeyMerger.php
@@ -5,17 +5,31 @@ declare(strict_types=1);
 namespace Symplify\MonorepoBuilder\Merge\ComposerKeyMerger;
 
 use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
+use Symplify\MonorepoBuilder\Merge\Arrays\SortedParameterMerger;
 use Symplify\MonorepoBuilder\Merge\Contract\ComposerKeyMergerInterface;
 
-final class RequireDevComposerKeyMerger extends AbstractComposerKeyMerger implements ComposerKeyMergerInterface
+final class RequireDevComposerKeyMerger implements ComposerKeyMergerInterface
 {
+    /**
+     * @var SortedParameterMerger
+     */
+    private $sortedParameterMerger;
+
+    public function __construct(SortedParameterMerger $sortedParameterMerger)
+    {
+        $this->sortedParameterMerger = $sortedParameterMerger;
+    }
+
     public function merge(ComposerJson $mainComposerJson, ComposerJson $newComposerJson): void
     {
         if ($newComposerJson->getRequireDev() === []) {
             return;
         }
 
-        $requireDev = $this->mergeAndSort($newComposerJson->getRequireDev(), $mainComposerJson->getRequireDev());
+        $requireDev = $this->sortedParameterMerger->mergeAndSort(
+            $newComposerJson->getRequireDev(),
+            $mainComposerJson->getRequireDev()
+        );
         $mainComposerJson->setRequireDev($requireDev);
     }
 }
