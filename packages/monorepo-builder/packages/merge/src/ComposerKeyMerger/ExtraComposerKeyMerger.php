32c7349c840919157c3ad848e6cdb36a1c2de441
TomasVotruba <tomas.vot@gmail.com>
2021-01-28 12:37:05 +0100

    fallback to last working phpstan

--- packages/monorepo-builder/packages/merge/src/ComposerKeyMerger/ExtraComposerKeyMerger.php
+++ packages/monorepo-builder/packages/merge/src/ComposerKeyMerger/ExtraComposerKeyMerger.php
@@ -6,6 +6,7 @@ namespace Symplify\MonorepoBuilder\Merge\ComposerKeyMerger;
 
 use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
 use Symplify\MonorepoBuilder\Merge\Contract\ComposerKeyMergerInterface;
+use Symplify\PackageBuilder\Yaml\ParametersMerger;
 
 final class ExtraComposerKeyMerger implements ComposerKeyMergerInterface
 {
@@ -13,6 +14,16 @@ final class ExtraComposerKeyMerger implements ComposerKeyMergerInterface
      * @var string
      */
     private const PHPSTAN = 'phpstan';
+
+    /**
+     * @var ParametersMerger
+     */
+    private $parametersMerger;
+
+    public function __construct(ParametersMerger $parametersMerger)
+    {
+        $this->parametersMerger = $parametersMerger;
+    }
 
     public function merge(ComposerJson $mainComposerJson, ComposerJson $newComposerJson): void
     {
