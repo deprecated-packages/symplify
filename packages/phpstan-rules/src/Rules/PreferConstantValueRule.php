d5c15251113a978a6fd54e10a2a111f42defd25b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-30 19:31:46 +0700

    [PHPStanRules] [WIP] Fixes #2751 Add PreferConstantValueRule

---
+++ packages/phpstan-rules/src/Rules/PreferConstantValueRule.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Scalar\String_;
+use PHPStan\Analyser\Scope;
+use Symplify\Astral\Naming\SimpleNameResolver;
+use Symplify\RuleDocGenerator\Contract\ConfigurableRuleInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\PreferConstantValueRule\PreferConstantValueRuleTest
+ */
+final class PreferConstantValueRule extends AbstractSymplifyRule implements ConfigurableRuleInterface
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Use defined constant %s::%s over string %s';
+
+    /**
+     * @var array<string, array<int, string>>
+     */
+    private $constantHoldingObjects = [];
+
+    /**
+     * @var SimpleNameResolver
+     */
+    private $simpleNameResolver;
+
+    /**
+     * @param array<string, array<int, string>> $constantHoldingObjects
+     */
+    public function __construct(SimpleNameResolver $simpleNameResolver, array $constantHoldingObjects = [])
+    {
+        $this->simpleNameResolver = $simpleNameResolver;
+        $this->constantHoldingObjects = $constantHoldingObjects;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [String_::class];
+    }
+
+    /**
+     * @param String_ $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        return [self::ERROR_MESSAGE];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new ConfiguredCodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        return 'require';
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        return \ComposerJsonSectoin::REQUIRE;
+    }
+}
+CODE_SAMPLE
+                ,
+                [
+                    'constantHoldingObjects' => ['ComposerJsonSection'],
+                ]
+            ),
+        ]);
+    }
+}
