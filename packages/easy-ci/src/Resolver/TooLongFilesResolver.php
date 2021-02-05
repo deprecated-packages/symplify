77b80fd13e6322ab4ddd881a659aa79f834f7f05
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-05 23:59:58 +0700

    [EasyCI] Move ValidateFileLengthCommand to easy-ci command

---
+++ packages/easy-ci/src/Resolver/TooLongFilesResolver.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCI\Resolver;
+
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class TooLongFilesResolver
+{
+    /**
+     * In windows the max-path length is 260 chars. we give a bit room for the path up to the rector project
+     * @var int
+     */
+    public const MAX_FILE_LENGTH = 200;
+
+    /**
+     * @param SmartFileInfo[] $fileInfos
+     * @return SmartFileInfo[]
+     */
+    public function resolve(array $fileInfos): array
+    {
+        return array_filter($fileInfos, function (SmartFileInfo $fileInfo): bool {
+            $filePathLength = strlen($fileInfo->getRealPath());
+            return $filePathLength > self::MAX_FILE_LENGTH;
+        });
+    }
+}
