136ff5413f81b6c0b40e981b224d98c1ac0f54da
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-05 19:48:39 +0700

    [PHPStanRules] [WIP] Fixes #2889 Add MaxFileLengthRule

---
+++ packages/phpstan-rules/src/Rules/MaxFileLengthRule.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Param;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\FileNode;
+use Symplify\RuleDocGenerator\Contract\ConfigurableRuleInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\MaxFileLengthRule\MaxFileLengthRuleTest
+ */
+final class MaxFileLengthRule extends AbstractSymplifyRule implements ConfigurableRuleInterface
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Paths for file "%s" has %d chars, but must be shorter than %d.';
+
+    /**
+     * @var int
+     */
+    private $maxLength;
+
+    public function __construct(int $maxLength)
+    {
+        $this->maxLength = $maxLength;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [FileNode::class];
+    }
+
+    /**
+     * @param FileNode $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        return [self::ERROR_MESSAGE];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new ConfiguredCodeSample(
+                <<<'CODE_SAMPLE'
+# file path
+/app/foo/bar/baz.php
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+# file path
+/app/foo/bar/baz.php
+CODE_SAMPLE
+                ,
+                [
+                    'maxLength' => $this->maxLength,
+                ]
+            ),
+        ]);
+    }
+}
