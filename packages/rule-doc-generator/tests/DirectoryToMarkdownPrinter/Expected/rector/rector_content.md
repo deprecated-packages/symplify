eb83e2d8b65bc5ee6787cabe9b5554d684bf82c5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-18 16:12:04 +0700

    add whitespace test

--- packages/rule-doc-generator/tests/DirectoryToMarkdownPrinter/Expected/rector/rector_content.md
+++ packages/rule-doc-generator/tests/DirectoryToMarkdownPrinter/Expected/rector/rector_content.md
@@ -1,10 +1,23 @@
-# 1 Rules Overview
+# 2 Rules Overview
 
 ## SomeRector
 
 Some change
 
 - class: [`Symplify\RuleDocGenerator\Tests\DirectoryToMarkdownPrinter\Fixture\Rector\Standard\SomeRector`](Fixture/Rector/Standard/SomeRector.php)
+
+```diff
+-before
++after
+```
+
+<br>
+
+## SomeWhiteSpaceCodeSampleRector
+
+Some change
+
+- class: [`Symplify\RuleDocGenerator\Tests\DirectoryToMarkdownPrinter\Fixture\Rector\Standard\SomeWhiteSpaceCodeSampleRector`](Fixture/Rector/Standard/SomeWhiteSpaceCodeSampleRector.php)
 
 ```diff
 -before
---
+++ packages/rule-doc-generator/tests/DirectoryToMarkdownPrinter/Fixture/Rector/Standard/SomeWhiteSpaceCodeSampleRector.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Symplify\RuleDocGenerator\Tests\DirectoryToMarkdownPrinter\Fixture\Rector\Standard;
+
+use Rector\Core\Contract\Rector\RectorInterface;
+use Rector\Core\RectorDefinition\RectorDefinition;
+use Symplify\RuleDocGenerator\Contract\DocumentedRuleInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class SomeWhiteSpaceCodeSampleRector implements RectorInterface, DocumentedRuleInterface
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Some change', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+before
+
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+after
+
+CODE_SAMPLE
+            )
+        ]);
+    }
+}
