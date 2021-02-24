6e754a98d648431f9bec6b11a7f1cf01fae755c9
Paul DelRe <pdelre@gmail.com>
2021-02-24 15:18:24 -0500

    fix typo

--- packages/easy-coding-standard/scoper.inc.php
+++ packages/easy-coding-standard/scoper.inc.php
@@ -20,7 +20,7 @@ $symfonyPolyfillAllowlist = \array_map(
 
 return [
     'files-whitelist' => [
-        // do not prefix "trigger_deprecatoin" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
+        // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
         'vendor/symfony/deprecation-contracts/function.php',
     ] + $symfonyPolyfillAllowlist,
