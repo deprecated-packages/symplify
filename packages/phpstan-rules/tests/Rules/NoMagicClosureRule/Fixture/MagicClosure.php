f9c002e8940f099f33ae50b74412b65b957ffce3
Tomas Votruba <tomas.vot@gmail.com>
2021-03-09 19:48:07 +0100

    [PHPStanRules] Add NoMagicClosureRule (#3037)

---
+++ packages/phpstan-rules/tests/Rules/NoMagicClosureRule/Fixture/MagicClosure.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\NoMagicClosureRule\Fixture;
+
+(static function () {
+    // ...
+});
---
+++ packages/phpstan-rules/tests/Rules/NoMagicClosureRule/NoMagicClosureRuleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\NoMagicClosureRule;
+
+use Iterator;
+use PHPStan\Rules\Rule;
+use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
+use Symplify\PHPStanRules\Rules\NoMagicClosureRule;
+
+final class NoMagicClosureRuleTest extends AbstractServiceAwareRuleTestCase
+{
+    /**
+     * @dataProvider provideData()
+     * @param array<string|int> $expectedErrorMessagesWithLines
+     */
+    public function testRule(string $filePath, array $expectedErrorMessagesWithLines): void
+    {
+        $this->analyse([$filePath], $expectedErrorMessagesWithLines);
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/MagicClosure.php', [[NoMagicClosureRule::ERROR_MESSAGE, 7]]];
+        yield [__DIR__ . '/Fixture/SkipClosureAssign.php', []];
+    }
+
+    protected function getRule(): Rule
+    {
+        return $this->getRuleFromConfig(
+            NoMagicClosureRule::class,
+            __DIR__ . '/../../../config/symplify-rules.neon'
+        );
+    }
+}
