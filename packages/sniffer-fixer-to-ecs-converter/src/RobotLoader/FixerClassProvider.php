ce32c494a743cd6c612d1e46dc0ef5b42ff6c7fb
Joyce Babu <joyce@ennexa.com>
2021-04-05 20:16:40 +0530

    [SnifferFixerToECSConverter] improve PHP-CS-Fixer path detection

--- packages/sniffer-fixer-to-ecs-converter/src/RobotLoader/FixerClassProvider.php
+++ packages/sniffer-fixer-to-ecs-converter/src/RobotLoader/FixerClassProvider.php
@@ -23,7 +23,20 @@ final class FixerClassProvider
         }
 
         $robotLoader = new RobotLoader();
-        $robotLoader->addDirectory(__DIR__ . '/../../../../vendor/friendsofphp/php-cs-fixer/src');
+
+        $possiblePhpCsFixerPaths = [
+            // after split package
+            __DIR__ . '/../../../../friendsofphp/php-cs-fixer/src',
+            // monorepo
+            __DIR__ . '/../../../../vendor/friendsofphp/php-cs-fixer/src',
+        ];
+
+        foreach ($possiblePhpCsFixerPaths as $possiblePhpCsFixerPath) {
+            if (file_exists($possiblePhpCsFixerPath)) {
+                $robotLoader->addDirectory($possiblePhpCsFixerPath);
+                break;
+            }
+        }
 
         $robotLoader->acceptFiles = ['*Fixer.php'];
         $robotLoader->rebuild();
