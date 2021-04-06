56a64e65eac73e1a6eae19733594d5cd3b8be72e
Joyce Babu <joyce@ennexa.com>
2021-04-06 12:42:02 +0530

    Revert "[SnifferFixerToECSConverter] fix path error detected by static analysis"
    This reverts commit e0f157cf1a4bb3d3755133ebac153a946a7df808.

--- packages/sniffer-fixer-to-ecs-converter/src/RobotLoader/FixerClassProvider.php
+++ packages/sniffer-fixer-to-ecs-converter/src/RobotLoader/FixerClassProvider.php
@@ -24,10 +24,18 @@ final class FixerClassProvider
 
         $robotLoader = new RobotLoader();
 
-        if (defined('SYMPLIFY_MONOREPO')) {
-            $robotLoader->addDirectory(__DIR__ . '/../../../../vendor/friendsofphp/php-cs-fixer/src');
-        } else {
-            $robotLoader->addDirectory(__DIR__ . '/../../../../friendsofphp/php-cs-fixer/src');
+        $possiblePhpCsFixerPaths = [
+            // after split package
+            __DIR__ . '/../../../../friendsofphp/php-cs-fixer/src',
+            // monorepo
+            __DIR__ . '/../../../../vendor/friendsofphp/php-cs-fixer/src',
+        ];
+
+        foreach ($possiblePhpCsFixerPaths as $possiblePhpCsFixerPath) {
+            if (file_exists($possiblePhpCsFixerPath)) {
+                $robotLoader->addDirectory($possiblePhpCsFixerPath);
+                break;
+            }
         }
 
         $robotLoader->acceptFiles = ['*Fixer.php'];
