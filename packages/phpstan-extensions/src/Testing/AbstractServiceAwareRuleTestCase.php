9a7e2f8f4191d05220cac7d809589e997ffd95a3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-19 15:34:47 +0700

    Update packages/phpstan-extensions/src/Testing/AbstractServiceAwareRuleTestCase.php

--- packages/phpstan-extensions/src/Testing/AbstractServiceAwareRuleTestCase.php
+++ packages/phpstan-extensions/src/Testing/AbstractServiceAwareRuleTestCase.php
@@ -24,7 +24,7 @@ abstract class AbstractServiceAwareRuleTestCase extends RuleTestCase
         try {
             parent::analyse($filePaths, $expectedErrorsWithLines);
         } catch (ExpectationFailedException $throwable) {
-            if ($this->isMatchTokenEmulationExceptoin($throwable)) {
+            if ($this->isMatchTokenEmulationException($throwable)) {
                 return;
             }
 
