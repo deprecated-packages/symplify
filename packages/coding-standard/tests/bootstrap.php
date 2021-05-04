0aea820bd8bc86980b90c3fb25e8cb6833cb550a
TomasVotruba <tomas.vot@gmail.com>
2021-05-04 12:14:44 +0200

    add T_MATCH token compact

--- packages/coding-standard/tests/bootstrap.php
+++ packages/coding-standard/tests/bootstrap.php
@@ -7,6 +7,11 @@ use PHP_CodeSniffer\Util\Tokens;
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
 
+// initalize token with INT type, otherwise php-cs-fixer and php-parser breaks
+if (defined('T_MATCH') === false) {
+    define('T_MATCH', 5000);
+}
+
 // required for PHP_CodeSniffer in packages/EasyCodingStandard/tests/*
 if (! defined('PHP_CODESNIFFER_VERBOSITY')) {
     define('PHP_CODESNIFFER_VERBOSITY', 0);
