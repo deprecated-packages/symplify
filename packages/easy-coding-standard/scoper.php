04484296307c90fdf223692d50910846d89f5bb7
TomasVotruba <tomas.vot@gmail.com>
2021-06-05 14:02:20 +0200

    unprefix

--- packages/easy-coding-standard/scoper.php
+++ packages/easy-coding-standard/scoper.php
@@ -25,9 +25,6 @@ $timestamp = (new DateTime('now'))->format('Ymd');
 return [
     'prefix' => 'ECSPrefix' . $timestamp,
     'files-whitelist' => [
-        // part of public interface of configs.php
-        'Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator',
-
         // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
         'vendor/symfony/deprecation-contracts/function.php',
@@ -40,8 +37,13 @@ return [
         'Symplify\CodingStandard\*',
         'PhpCsFixer\*',
         'PHP_CodeSniffer\*',
+<<<<<<< HEAD
         'Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator',
         'Symfony\Component\DependencyInjection\Extension\ExtensionInterface',
+=======
+        // part of public interface of configs.php
+        'Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator',
+>>>>>>> 8192d9df8 (unprefix)
     ],
     'patchers' => [
         // unprefix polyfill functions
