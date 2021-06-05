f9039ec5c18b41b8003d4edec41e6cb1e051386a
TomasVotruba <tomas.vot@gmail.com>
2021-06-05 14:20:25 +0200

    preload container configurator

--- packages/easy-coding-standard/build/build-preload.php
+++ packages/easy-coding-standard/build/build-preload.php
@@ -46,6 +46,9 @@ CODE_SAMPLE;
     $fileInfos[] = new SplFileInfo(
         $vendorDir . '/symplify/rule-doc-generator-contracts/src/Contract/DocumentedRuleInterface.php'
     );
+    $fileInfos[] = new SplFileInfo(
+        $vendorDir . '/symfony/dependency-injection/Loader/Configurator/ContainerConfigurator.php'
+    );
 
     foreach ($fileInfos as $fileInfo) {
         $realPath = $fileInfo->getRealPath();
