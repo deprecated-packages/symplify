504cbbd2764f7cb3007a88240afdb8a85d9ed424
Jáchym Toušek <enumag@gmail.com>
2021-06-08 15:23:20 +0200

    Add testcase

---
+++ packages/easy-coding-standard/build/target-repository/tests/FixerTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+use Symplify\EasyCodingStandard\Testing\PHPUnit\AbstractCheckerTestCase;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class FixerTest extends AbstractCheckerTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    /**
+     * @return iterable<array<SmartFileInfo>>
+     */
+    public function provideData(): iterable
+    {
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixtures', '*.txt');
+    }
+
+    public function provideConfig(): string
+    {
+        return __DIR__ . '/config.php';
+    }
+}
