ddca14da0ee50089d5513688cdf43f4ca9a43c12
Markus Staab <m.staab@complex-it.de>
2021-06-23 15:22:50 +0200

    Build phpstan-rules Prefixed

---
+++ packages/phpstan-rules/build/build-preload.php
@@ -0,0 +1,57 @@
+<?php
+
+// inspired at https://github.com/phpstan/phpstan-src/commit/87897c2a4980d68efa1c46049ac2eefe767ec946#diff-e897e523125a694bd8ea69bf83374c206803c98720c46d7401b7a7cf53915a26
+// and https://github.com/rectorphp/rector-src/blob/main/build/build-preload.php
+
+declare(strict_types=1);
+
+use Nette\Utils\Strings;
+use Symfony\Component\Finder\Finder;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$buildDirectory = $argv[1];
+
+buildPreloadScript($buildDirectory);
+
+function buildPreloadScript(string $buildDirectory): void
+{
+    $vendorDir = $buildDirectory . '/vendor';
+
+    $preloadFileContent = <<<'CODE_SAMPLE'
+<?php
+
+declare(strict_types = 1);
+
+
+CODE_SAMPLE;
+
+    $finder = (new Finder())
+        ->files()
+        ->name('*.php')
+        ->notPath('#\/tests\/#')
+        ->notPath('#\/Tests\/#')
+        ->notPath('#\/config\/#')
+        ->notPath('#\/set\/#')
+        // order matters, as lower files depend on higher ones
+        ->in($vendorDir . '/psr/container');
+
+    /** @var \Symfony\Component\Finder\SplFileInfo[] $fileInfos */
+    $fileInfos = iterator_to_array($finder->getIterator());
+
+    // must be in specific order, as included class needs the classes it contains
+    $fileInfos[] = new SplFileInfo(
+        $vendorDir . '/symplify/rule-doc-generator-contracts/src/ValueObject/RuleDefinition.php'
+    );
+    $fileInfos[] = new SplFileInfo(
+        $vendorDir . '/symplify/rule-doc-generator-contracts/src/Contract/DocumentedRuleInterface.php'
+    );
+
+    foreach ($fileInfos as $fileInfo) {
+        $realPath = $fileInfo->getRealPath();
+        $filePath = '/vendor/' . Strings::after($realPath, 'vendor/');
+        $preloadFileContent .= "require_once __DIR__ . '" . $filePath . "';" . PHP_EOL;
+    }
+
+    file_put_contents($buildDirectory . '/preload.php', $preloadFileContent);
+}
---
+++ packages/phpstan-rules/build/downgrade.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# inspired by https://raw.githubusercontent.com/rectorphp/rector/main/build/downgrade-rector.sh
+
+# see https://stackoverflow.com/questions/66644233/how-to-propagate-colors-from-bash-script-to-github-action?noredirect=1#comment117811853_66644233
+export TERM=xterm-color
+
+# show errors
+set -e
+
+# script fails if trying to access to an undefined variable
+set -u
+
+
+# functions
+note()
+{
+    MESSAGE=$1;
+    printf "\n";
+    echo "\033[0;33m[NOTE] $MESSAGE\033[0m";
+}
+
+
+# configure - 1st argument, use like
+# sh build/downgrade-ecs.sh <directory-to-downgrade>
+BUILD_DIRECTORY=$1
+
+#---------------------------------------------
+
+# 1. downgrade it
+note "Running downgrade in '$BUILD_DIRECTORY' directory\n"
+
+# 2. provide directories to downgrade, joined by spaces to run at once
+directories="vendor/symfony vendor/psr;vendor/symplify config bin src packages;vendor/composer;vendor/doctrine vendor/friendsofphp vendor/nette;vendor/php-cs-fixer vendor/sebastian vendor/squizlabs"
+
+# split array see https://stackoverflow.com/a/1407098/1348344
+export IFS=";"
+
+# 4. downgrade the directories
+for directory in $directories; do
+    note "Downgrading '$directory' directory\n"
+
+    # --working-dir is needed, so "SKIP" parameter is applied in absolute path of nested directory
+    php -d memory_limit=-1 vendor/bin/rector process $directory --config packages/easy-coding-standard/build/config/config-downgrade.php --working-dir $BUILD_DIRECTORY --ansi
+done
+
+
+# CONFIRMED: give time to print all the files, before the next process takes over newly printed content
+# avoids bugs like these half of files done, next half waiting https://github.com/rectorphp/rector-src/runs/2565478682
+sleep 20
