ea8d020e5ac373cd59fbebfe548d8193d0cb4c6d
Tomas Votruba <tomas.vot@gmail.com>
2021-06-24 16:22:34 +0200

    [PHPStanRules] Add RequireExceptionNamespaceRule

---
+++ packages/phpstan-rules/src/Rules/Domain/RequireExceptionNamespaceRule.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules\Domain;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassNode;
+use Symplify\PHPStanRules\Rules\AbstractSymplifyRule;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\Domain\RequireExceptionNamespaceRule\RequireExceptionNamespaceRuleTest
+ */
+final class RequireExceptionNamespaceRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Exception must be located in "Exception" namespace';
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+// app/Controller/SomeException.php
+final class SomeException extends Exception
+{
+
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+// app/Exception/SomeException.php
+final class SomeException extends Exception
+{
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<\PhpParser\Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [InClassNode::class];
+    }
+
+    /**
+     * @param InClassNode $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        $classReflection = $scope->getClassReflection();
+        if ($classReflection === null) {
+            return [];
+        }
+
+        if (! $classReflection->isClass()) {
+            return [];
+        }
+
+        if (! $classReflection->isSubclassOf('Exception')) {
+            return [];
+        }
+
+        // is class in "Exception" namespace?
+        $className = $classReflection->getName();
+        if (str_contains($className, '\\Exception\\')) {
+            return [];
+        }
+
+        return [self::ERROR_MESSAGE];
+    }
+}
---
+++ packages/phpstan-rules/tests/Rules/Domain/RequireExceptionNamespaceRule/Fixture/MisslocatedException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\Domain\RequireExceptionNamespaceRule\Fixture;
+
+final class MisslocatedException extends \Exception
+{
+}
