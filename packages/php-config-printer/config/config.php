a6a55af1e102680638292680416701fd29c02091
Ruud Kamphuis <ruudk@users.noreply.github.com>
2021-06-28 09:56:07 +0200

    [PhpConfigPrinter] Properly import function calls
    I noticed that the `service` function would not be imported in my setup.
    
    It would be imported as:
    ```php
    use Symfony\Component\DependencyInjection\Loader\Configurator\service;
    ```
    instead of
    ```php
    use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
    ```
    
    I checked the code, and it turns out that it's a bit naive. It has a special rule
    for when the function is called `ref` (the old `service` function). In other cases
    it imports it as function when the function exists.
    
    Instead of doing this, why not use the nodes to know the context of the import?
    
    The `ImportFullyQualifiedNamesNodeVisitor` keeps track of the context in where it finds a
    `FullyQualified` node. When the `FullyQualified` node is used inside a `FuncCall` expression
    it will import the FQCN as function.

--- packages/php-config-printer/config/config.php
+++ packages/php-config-printer/config/config.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use PhpParser\BuilderFactory;
 use PhpParser\NodeFinder;
+use PhpParser\NodeVisitor\ParentConnectingVisitor;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\Yaml\Parser;
@@ -20,11 +21,17 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autoconfigure();
 
     $services->load('Symplify\PhpConfigPrinter\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/HttpKernel', __DIR__ . '/../src/Dummy', __DIR__ . '/../src/Bundle']);
+        ->exclude([
+            __DIR__ . '/../src/HttpKernel',
+            __DIR__ . '/../src/Dummy',
+            __DIR__ . '/../src/Bundle',
+            __DIR__ . '/../src/ValueObject/FullyQualifiedImport.php',
+        ]);
 
     $services->set(NodeFinder::class);
     $services->set(Parser::class);
     $services->set(BuilderFactory::class);
+    $services->set(ParentConnectingVisitor::class);
 
     $services->set(ParameterProvider::class)
         ->args([service(ContainerInterface::class)]);
