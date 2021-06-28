c30fce657e999ef3340eefcd3ac746d6f0630e2e
Tomas Votruba <tomas.vot@gmail.com>
2021-06-28 14:54:08 +0200

    [ESC] Fix missing require downgrade - ref https://github.com/symplify/symplify/issues/3355#issuecomment-869640777

--- packages/easy-coding-standard/build/config/config-downgrade.php
+++ packages/easy-coding-standard/build/config/config-downgrade.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\DowngradePhp80\Rector\Class_\DowngradeAttributeToAnnotationRector;
+use Rector\DowngradePhp80\ValueObject\DowngradeAttributeToAnnotation;
 use Rector\Set\ValueObject\DowngradeSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(DowngradeSetList::PHP_80);
@@ -14,6 +17,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(DowngradeSetList::PHP_71);
     // currently breaks - https://github.com/symplify/easy-coding-standard/runs/2603926642
     // $containerConfigurator->import(DowngradeSetList::PHP_70);
+
+    $services = $containerConfigurator->services();
+    $services->set(DowngradeAttributeToAnnotationRector::class)
+        ->call('configure', [[
+            DowngradeAttributeToAnnotationRector::ATTRIBUTE_TO_ANNOTATION => ValueObjectInliner::inline([
+                new DowngradeAttributeToAnnotation('Symfony\Contracts\Service\Attribute\Required', 'required'),
+            ]),
+        ]]);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::SKIP, ['*/Tests/*', '*/tests/*', __DIR__ . '/../../tests']);
