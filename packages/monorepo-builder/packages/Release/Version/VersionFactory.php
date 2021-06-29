cde1547595aa8103238bc51bc05d5d625540ea7f
Tomas Votruba <tomas.vot@gmail.com>
2021-06-29 19:56:10 +0200

    [MonorepoBuilder] Remove the v prefix from version to work with numbers

--- packages/monorepo-builder/packages/Release/Version/VersionFactory.php
+++ packages/monorepo-builder/packages/Release/Version/VersionFactory.php
@@ -66,6 +66,6 @@ final class VersionFactory
             ++$currentPatchVersion;
         }
 
-        return new Version(sprintf('v%d.%d.%d', $value, $currentMinorVersion, $currentPatchVersion));
+        return new Version(sprintf('%d.%d.%d', $value, $currentMinorVersion, $currentPatchVersion));
     }
 }
