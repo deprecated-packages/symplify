b87e8305ab87e17a0a5d01e04b0c7f53743f1bb8
Geraint Hywel <g.s.j.hywel@swansea.ac.uk>
2021-07-01 19:42:51 +0100

    Merge PSR-1 and PSR-12 files
    Skip option array cannot be merged with existing values, so must be done
    in a single hit.

--- packages/easy-coding-standard/config/set/php_codesniffer/php-codesniffer-psr12.php
+++ packages/easy-coding-standard/config/set/php_codesniffer/php-codesniffer-psr12.php
@@ -7,38 +7,83 @@ use PHP_CodeSniffer\Standards\PEAR\Sniffs as PEAR;
 use PHP_CodeSniffer\Standards\PSR2\Sniffs as PSR2;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs as Squiz;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
+    $skip = [];
 
-    $containerConfigurator->import(__DIR__ . '/php-codesniffer-psr1.php');
+    ////////////////////////////////////////////////////////////////////////////////
+    // PSR-1
+    ////////////////////////////////////////////////////////////////////////////////
+    // 2. Files
+
+    // 2.1 PHP Tags
+    $services->set(Generic\PHP\DisallowAlternativePHPTagsSniff::class);
+    $services->set(Generic\PHP\DisallowShortOpenTagSniff::class);
+    $skip[Generic\PHP\DisallowShortOpenTagSniff::class . '.EchoFound'] = null;
+
+    // 2.2 Character Encoding
+    $services->set(Generic\Files\ByteOrderMarkSniff::class);
+
+    // 3. Namespace and Class Names
+    $services->set(Squiz\Classes\ValidClassNameSniff::class);
+
+    // 4. Class Constants, Properties, and Methods
+
+    // 4.1. Constants
+    $services->set(Generic\NamingConventions\UpperCaseConstantNameSniff::class);
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // PSR-12
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // 2. General
 
     // 2.2 Files
-    $services->set(Generic\Files\LineEndingsSniff::class)->property('eolChar', '\\n');
+
+    $services->set(Generic\Files\LineEndingsSniff::class)
+             ->property('eolChar', '\\n');
+
     $services->set(PSR2\Files\EndFileNewlineSniff::class);
+
+    /* The closing ?> tag MUST be omitted from files containing only PHP. */
     $services->set(PSR2\Files\ClosingTagSniff::class);
 
     // 2.3 Lines
-    $services->set(Generic\Files\LineLengthSniff::class)->property('lineLimit', 120)->property('absoluteLineLimit', 0);
+
+    $services->set(Generic\Files\LineLengthSniff::class)
+             ->property('lineLimit', 120)
+             ->property('absoluteLineLimit', 0);
+
     $services->set(Squiz\WhiteSpace\SuperfluousWhitespaceSniff::class);
-    $services->set(Squiz\WhiteSpace\SuperfluousWhitespace\StartFileSniff::class);
-    $services->set(Squiz\WhiteSpace\SuperfluousWhitespace\EndFileSniff::class);
-    $services->set(Squiz\WhiteSpace\SuperfluousWhitespace\EmptyLinesSniff::class);
+    $skip[Squiz\WhiteSpace\SuperfluousWhitespaceSniff::class . '.StartFile'] = null;
+    $skip[Squiz\WhiteSpace\SuperfluousWhitespaceSniff::class . '.EndFile'] = null;
+    $skip[Squiz\WhiteSpace\SuperfluousWhitespaceSniff::class . '.EmptyLines'] = null;
+
     $services->set(Generic\Formatting\DisallowMultipleStatementsSniff::class);
 
     // 2.4 Indenting
-    $services->set(Generic\WhiteSpace\ScopeIndentSniff::class)->property('ignoreIndentationTokens', ['T_COMMENT', 'T_DOC_COMMENT_OPEN_TAG']);
+
+    $services->set(Generic\WhiteSpace\ScopeIndentSniff::class)
+             ->property('ignoreIndentationTokens', [
+                 'T_COMMENT',
+                 'T_DOC_COMMENT_OPEN_TAG',
+             ]);
     $services->set(Generic\WhiteSpace\DisallowTabIndentSniff::class);
 
     // 2.5 Keywords and Types
+
     $services->set(Generic\PHP\LowerCaseKeywordSniff::class);
     $services->set(Generic\PHP\LowerCaseConstantSniff::class);
     $services->set(Generic\PHP\LowerCaseTypeSniff::class);
 
+    // 4. Classes, Properties, and Methods
+
     // 4.1 Extends and Implements
     $services->set(PSR2\Classes\ClassDeclarationSniff::class);
 
-    // Properties and Constants
+    // 4.3 Properties and Constants
     $services->set(PSR2\Classes\PropertyDeclarationSniff::class);
 
     // 4.4 Methods and Functions
@@ -50,21 +95,33 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(Squiz\Functions\LowercaseFunctionKeywordsSniff::class);
 
     // 4.5 Method and Function Arguments
-    $services->set(Squiz\Functions\FunctionDeclarationArgumentSpacingSniff::class)->property('equalsSpacing', 1);
+    $services->set(Squiz\Functions\FunctionDeclarationArgumentSpacingSniff::class)
+             ->property('equalsSpacing', 1);
     $services->set(PEAR\Functions\ValidDefaultValueSniff::class);
     $services->set(Squiz\Functions\MultiLineFunctionDeclarationSniff::class);
 
     // 4.7 Method and Function Calls
     $services->set(Generic\Functions\FunctionCallArgumentSpacingSniff::class);
     $services->set(PSR2\Methods\FunctionCallSignatureSniff::class);
+    $skip[PSR2\Methods\FunctionCallSignatureSniff::class . '.SpaceAfterCloseBracket'] = null;
+    $skip[PSR2\Methods\FunctionCallSignatureSniff::class . '.OpeningIndent'] = null;
 
     // 5. Control Structures
+
     $services->set(Squiz\ControlStructures\ControlSignatureSniff::class);
+    #$services->set(Squiz\WhiteSpace\ControlStructureSpacing::class . '.SpacingAfterOpen');
+    #$services->set(Squiz\WhiteSpace\ControlStructureSpacing::class . '.SpacingBeforeClose');
     $services->set(Squiz\WhiteSpace\ScopeClosingBraceSniff::class);
     $services->set(Squiz\ControlStructures\ForEachLoopDeclarationSniff::class);
-    $services->set(Squiz\ControlStructures\ForLoopDeclarationSniff::class)->property('ignoreNewlines', \true);
+
+    $services->set(Squiz\ControlStructures\ForLoopDeclarationSniff::class)
+             ->property('ignoreNewlines', \true);
+    $skip[Squiz\ControlStructures\ForLoopDeclarationSniff::class . '.SpacingAfterOpen'] = null;
+    $skip[Squiz\ControlStructures\ForLoopDeclarationSniff::class . '.SpacingBeforeClose'] = null;
+
     $services->set(Squiz\ControlStructures\LowercaseDeclarationSniff::class);
     $services->set(Generic\ControlStructures\InlineControlStructureSniff::class);
+    $skip[Squiz\ControlStructures\ForEachLoopDeclarationSniff::class . '.AsNotLower'] = null;
 
     // 5.1 if, elseif, else
     $services->set(PSR2\ControlStructures\ElseIfDeclarationSniff::class);
@@ -72,21 +129,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // 5.2 switch, case
     $services->set(PSR2\ControlStructures\SwitchDeclarationSniff::class);
 
+    // 5.5 foreach
+    $skip[Squiz\ControlStructures\ForEachLoopDeclaration::class . '.SpaceAfterOpen'] = null;
+    $skip[Squiz\ControlStructures\ForEachLoopDeclaration::class . '.SpaceBeforeClose'] = null;
+
+    // 6. Operators
+
     // 6.1. Unary operators
     $services->set(Generic\WhiteSpace\IncrementDecrementSpacingSniff::class);
     $services->set(Squiz\WhiteSpace\CastSpacingSniff::class);
 
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(\Symplify\EasyCodingStandard\ValueObject\Option::SKIP, [
-        Squiz\WhiteSpace\SuperfluousWhitespaceSniff::class . 'StartFile' => null,
-        Squiz\WhiteSpace\SuperfluousWhitespaceSniff::class . 'EndFile' => null,
-        Squiz\WhiteSpace\SuperfluousWhitespaceSniff::class . 'EmptyLines' => null,
-        PSR2\Methods\FunctionCallSignatureSniff::class . 'SpaceAfterCloseBracket' => null,
-        PSR2\Methods\FunctionCallSignatureSniff::class . '.OpeningIndent' => null,
-        Squiz\ControlStructures\ForLoopDeclarationSniff::class . 'SpacingAfterOpen' => null,
-        Squiz\ControlStructures\ForLoopDeclarationSniff::class . 'SpacingBeforeClose' => null,
-        Squiz\ControlStructures\ForEachLoopDeclarationSniff::class . 'AsNotLower' => null,
-        Squiz\ControlStructures\ForEachLoopDeclarationSniff::class . 'SpaceAfterOpen' => null,
-        Squiz\ControlStructures\ForEachLoopDeclarationSniff::class . 'SpaceBeforeClose' => null,
-    ]);
+    $parameters->set(Option::SKIP, $skip);
 };
