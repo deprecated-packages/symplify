8e046c1153883a9edf2559c1df793ce7f6ea2e4e
Tomas Votruba <tomas.vot@gmail.com>
2021-07-10 11:23:27 +0200

    [MonorepoBuilder] Update config downgrade

--- packages/monorepo-builder/build/config/config-downgrade.php
+++ packages/monorepo-builder/build/config/config-downgrade.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector;
 use Rector\Set\ValueObject\DowngradeSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -11,6 +12,27 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(DowngradeSetList::PHP_74);
     $containerConfigurator->import(DowngradeSetList::PHP_73);
     $containerConfigurator->import(DowngradeSetList::PHP_72);
+
+    $services = $containerConfigurator->services();
+    $services->set(DowngradeParameterTypeWideningRector::class)
+        ->call('configure', [[
+            DowngradeParameterTypeWideningRector::SAFE_TYPES => [
+                'PHP_CodeSniffer\Sniffs\Sniff',
+                \PhpCsFixer\Fixer\FixerInterface::class,
+                \Symfony\Component\Console\Output\OutputInterface::class,
+                \Symfony\Component\Console\Style\StyleInterface::class,
+                // phpstan
+                \PhpParser\Parser::class,
+                \PhpParser\PrettyPrinterAbstract::class,
+            ],
+            DowngradeParameterTypeWideningRector::SAFE_TYPES_TO_METHODS => [
+                \Symfony\Component\DependencyInjection\ContainerInterface::class => [
+                    'setParameter',
+                    'getParameter',
+                    'hasParameter',
+                ],
+            ],
+        ]]);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::SKIP, ['*/Tests/*', '*/tests/*', __DIR__ . '/../../tests']);
