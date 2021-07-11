279e93e2a62b135aa1f72326c668b74baf2aa4ff
Tomas Votruba <tomas.vot@gmail.com>
2021-07-12 00:29:36 +0200

    Scoping fixes for ContainerConfigurator (#3405)

--- packages/monorepo-builder/scoper.php
+++ packages/monorepo-builder/scoper.php
@@ -119,5 +119,20 @@ return [
                 'load(\'' . 'Symplify\\MonorepoBuilder',
             );
         },
+
+        // unprefixed ContainerConfigurator
+        function (string $filePath, string $prefix, string $content): string {
+            // keep vendor prefixed the prefixed file loading; not part of public API
+            // except @see https://github.com/symfony/symfony/commit/460b46f7302ec7319b8334a43809523363bfef39#diff-1cd56b329433fc34d950d6eeab9600752aa84a76cbe0693d3fab57fed0f547d3R110
+            if (str_contains($filePath, 'vendor/symfony') && ! str_ends_with($filePath, 'vendor/symfony/dependency-injection/Loader/PhpFileLoader.php')) {
+                return $content;
+            }
+
+            return Strings::replace(
+                $content,
+                '#' . $prefix . '\\\\Symfony\\\\Component\\\\DependencyInjection\\\\Loader\\\\Configurator\\\\ContainerConfigurator#',
+                'Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator'
+            );
+        },
     ],
 ];
