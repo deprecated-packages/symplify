7b6668143952e958b4e002c47f4c4c5f105b77aa
GitHub Action <action@github.com>
2021-07-13 09:28:48 +0000

    [ci-review] Rector Rectify

--- packages/php-config-printer/src/ServiceOptionConverter/ParentServiceOptionKeyYamlToPhpFactory.php
+++ packages/php-config-printer/src/ServiceOptionConverter/ParentServiceOptionKeyYamlToPhpFactory.php
@@ -8,18 +8,12 @@ use PhpParser\BuilderHelpers;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\MethodCall;
 use Symplify\PhpConfigPrinter\Contract\Converter\ServiceOptionsKeyYamlToPhpFactoryInterface;
-use Symplify\PhpConfigPrinter\NodeFactory\ArgsNodeFactory;
-use Symplify\PhpConfigPrinter\NodeFactory\CommonNodeFactory;
-use Symplify\PhpConfigPrinter\ServiceOptionAnalyzer\ServiceOptionAnalyzer;
 use Symplify\PhpConfigPrinter\ValueObject\YamlKey;
 
 final class ParentServiceOptionKeyYamlToPhpFactory implements ServiceOptionsKeyYamlToPhpFactoryInterface
 {
-    public function __construct(
-        private CommonNodeFactory $commonNodeFactory,
-        private ArgsNodeFactory $argsNodeFactory,
-        private ServiceOptionAnalyzer $serviceOptionAnalyzer
-    ) {
+    public function __construct()
+    {
     }
 
     public function decorateServiceMethodCall($key, $yaml, $values, MethodCall $methodCall): MethodCall
