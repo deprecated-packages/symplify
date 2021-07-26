2962db8fc05ec6d30e5dbf6397064ccc3967de4e
Markus Staab <47448731+clxmstaab@users.noreply.github.com>
2021-07-26 14:23:34 +0200

    simplify

--- packages/phpstan-rules/tests/Rules/NoDynamicNameRule/Fixture/SkipImmediatelyInvokedFunctionExpression.php
+++ packages/phpstan-rules/tests/Rules/NoDynamicNameRule/Fixture/SkipImmediatelyInvokedFunctionExpression.php
@@ -6,26 +6,10 @@ namespace Symplify\PHPStanRules\Tests\Rules\NoDynamicNameRule\Fixture;
 
 final class SkipImmediatelyInvokedFunctionExpression
 {
-    /**
-     * method copied from phpstan-src
-     */
     public function load(string $key, string $variableKey)
     {
         return (function (string $key, string $variableKey) {
-            [,, $filePath] = $this->getFilePaths($key);
-            if (!is_file($filePath)) {
-                return null;
-            }
-
-            $cacheItem = require $filePath;
-            if (!$cacheItem instanceof CacheItem) {
-                return null;
-            }
-            if (!$cacheItem->isVariableKeyValid($variableKey)) {
-                return null;
-            }
-
-            return $cacheItem->getData();
+            return "hello";
         })($key, $variableKey);
     }
 }
