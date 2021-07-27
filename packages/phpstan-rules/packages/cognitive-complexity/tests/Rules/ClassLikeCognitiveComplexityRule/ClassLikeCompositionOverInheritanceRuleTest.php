8b2c68f850a378935274403766a23429bf4ce98e
Markus Staab <m.staab@complex-it.de>
2021-07-27 11:47:47 +0200

    separated scoreCompositionOverInheritance:true tests

--- packages/phpstan-rules/packages/cognitive-complexity/tests/Rules/ClassLikeCognitiveComplexityRule/ClassLikeCompositionOverInheritanceRuleTest.php
+++ packages/phpstan-rules/packages/cognitive-complexity/tests/Rules/ClassLikeCognitiveComplexityRule/ClassLikeCompositionOverInheritanceRuleTest.php
@@ -12,7 +12,7 @@ use Symplify\PHPStanRules\CognitiveComplexity\Rules\ClassLikeCognitiveComplexity
 /**
  * @extends AbstractServiceAwareRuleTestCase<ClassLikeCognitiveComplexityRule>
  */
-final class ClassLikeCognitiveComplexityRuleTest extends AbstractServiceAwareRuleTestCase
+final class ClassLikeCompositionOverInheritanceRuleTest extends AbstractServiceAwareRuleTestCase
 {
     /**
      * @dataProvider provideDataForTest()
@@ -28,15 +28,18 @@ final class ClassLikeCognitiveComplexityRuleTest extends AbstractServiceAwareRul
         $errorMessage = sprintf(ClassLikeCognitiveComplexityRule::ERROR_MESSAGE, 'Class', 54, 50);
         yield [__DIR__ . '/Fixture/ClassWithManyComplexMethods.php', [[$errorMessage, 7]]];
 
-        $errorMessage = sprintf(ClassLikeCognitiveComplexityRule::ERROR_MESSAGE, 'Class', 9, 5);
+        $errorMessage = sprintf(ClassLikeCognitiveComplexityRule::ERROR_MESSAGE, 'Class', 34, 5);
         yield [__DIR__ . '/Fixture/SimpleCommand.php', [[$errorMessage, 9]]];
+
+        $errorMessage = sprintf(ClassLikeCognitiveComplexityRule::ERROR_MESSAGE, 'Class', 44, 5);
+        yield [__DIR__ . '/Fixture/NonFinalClass.php', [[$errorMessage, 9]]];
     }
 
     protected function getRule(): Rule
     {
         return $this->getRuleFromConfig(
             ClassLikeCognitiveComplexityRule::class,
-            __DIR__ . '/config/configured_rule.neon'
+            __DIR__ . '/config/configured_composition_rule.neon'
         );
     }
 }
