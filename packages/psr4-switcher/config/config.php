5d9fe314c89951e02b48612e7d821aa18089e7b4
Tomas Votruba <tomas.vot@gmail.com>
2021-08-04 16:41:46 +0200

    [PSR-4 Switcher] Add missing console app (#3466)

--- packages/psr4-switcher/config/config.php
+++ packages/psr4-switcher/config/config.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\PackageBuilder\Console\Command\CommandNaming;
+use Symplify\Psr4Switcher\Console\PSR4SwitcherConsoleApplication;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
@@ -14,4 +17,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->load('Symplify\Psr4Switcher\\', __DIR__ . '/../src')
         ->exclude([__DIR__ . '/../src/HttpKernel', __DIR__ . '/../src/ValueObject']);
+
+    $services->alias(Application::class, PSR4SwitcherConsoleApplication::class);
+    $services->set(CommandNaming::class);
 };
