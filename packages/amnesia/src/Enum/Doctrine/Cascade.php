7633da71497862a3d065f9116b545c819c5ddedc
Tomas Votruba <tomas.vot@gmail.com>
2021-08-18 19:47:55 +0200

    [Amnesia] Add Cascade and Strategy enums

---
+++ packages/amnesia/src/Enum/Doctrine/Cascade.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\Amnesia\Enum\Doctrine;
+
+/**
+ * @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.9/reference/working-with-associations.html#transitive-persistence-cascade-operations
+ * @enum
+ */
+final class Cascade
+{
+    /**
+     * @var string
+     */
+    public const PERSIST = 'persist';
+
+    /**
+     * @var string
+     */
+    public const REMOVE = 'remove';
+
+    /**
+     * @var string
+     */
+    public const MERGE = 'merge';
+
+    /**
+     * @var string
+     */
+    public const DETACH = 'detach';
+
+    /**
+     * @var string
+     */
+    public const REFRESH = 'refresh';
+
+    /**
+     * @var string
+     */
+    public const ALL = 'all';
+}
