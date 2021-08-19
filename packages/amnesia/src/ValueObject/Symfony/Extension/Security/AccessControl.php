be134461e5af6e7ddbfc63c310e8a280cca0dfe0
Tomas Votruba <tomas.vot@gmail.com>
2021-08-19 08:52:01 +0200

    [Amnesia] Add Cascade and Strategy enums (#3501)

---
+++ packages/amnesia/src/ValueObject/Symfony/Extension/Security/AccessControl.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\Amnesia\ValueObject\Symfony\Extension\Security;
+
+final class AccessControl
+{
+    /**
+     * @var string
+     */
+    public const PATH = 'path';
+
+    /**
+     * @var string
+     */
+    public const ROLES = 'roles';
+}
