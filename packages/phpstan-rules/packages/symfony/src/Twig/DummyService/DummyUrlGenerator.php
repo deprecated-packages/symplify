61eb913a9398398830104485b0824f68ccc04692
Tomas Votruba <tomas.vot@gmail.com>
2021-08-23 17:23:30 +0200

    [PHPStanRules] Add twig + path etension for easier parsing

--- packages/phpstan-rules/packages/symfony/src/Twig/DummyService/DummyUrlGenerator.php
+++ packages/phpstan-rules/packages/symfony/src/Twig/DummyService/DummyUrlGenerator.php
@@ -9,19 +9,34 @@ use Symfony\Component\Routing\RequestContext;
 
 final class DummyUrlGenerator implements UrlGeneratorInterface
 {
+<<<<<<< HEAD
     private RequestContext $requestContext;
 
     public function setContext(RequestContext $requestContext): void
     {
         $this->requestContext = $requestContext;
+=======
+    private RequestContext $context;
+
+    public function setContext(RequestContext $context): void
+    {
+        $this->context = $context;
+>>>>>>> 45db8b8e4 ([PHPStanRules] Add twig + path etension for easier parsing)
     }
 
     public function getContext(): RequestContext
     {
+<<<<<<< HEAD
         return $this->requestContext;
     }
 
     public function generate(string $name, array $parameters = [], int $referenceType = self::ABSOLUTE_PATH): string
+=======
+        return $this->context;
+    }
+
+    public function generate(string $name, array $parameters = [], int $referenceType = self::ABSOLUTE_PATH)
+>>>>>>> 45db8b8e4 ([PHPStanRules] Add twig + path etension for easier parsing)
     {
         return 'some_dummy_url';
     }
