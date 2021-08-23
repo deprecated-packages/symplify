0022f3261bd0cf6b12a23c6d019bdc57c8ed11ce
Tomas Votruba <tomas.vot@gmail.com>
2021-08-23 14:17:17 +0200

    [GitWrapper] Add GitWorkingCopyFactory

---
+++ packages/git-wrapper/src/ValueObjectFactory/GitWorkingCopyFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\GitWrapper\ValueObjectFactory;
+
+use Symplify\GitWrapper\GitWorkingCopy;
+use Symplify\GitWrapper\GitWrapper;
+
+final class GitWorkingCopyFactory
+{
+    public function __construct(
+        private GitWrapper $gitWrapper
+    ) {
+    }
+
+    public function createWorkingCopy(string $directory): GitWorkingCopy
+    {
+        return new GitWorkingCopy($this->gitWrapper, $directory);
+    }
+}
