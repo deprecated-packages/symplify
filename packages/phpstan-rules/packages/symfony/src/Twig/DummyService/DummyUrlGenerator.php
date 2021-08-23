e464b1a77b96b4dd9a60bf52b22ef164ab931fd5
Tomas Votruba <tomas.vot@gmail.com>
2021-08-23 17:30:25 +0200

    add app deuflat variable

--- packages/phpstan-rules/packages/symfony/src/Twig/DummyService/DummyUrlGenerator.php
+++ packages/phpstan-rules/packages/symfony/src/Twig/DummyService/DummyUrlGenerator.php
@@ -10,6 +10,7 @@ use Symfony\Component\Routing\RequestContext;
 final class DummyUrlGenerator implements UrlGeneratorInterface
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
     private RequestContext $requestContext;
 
     public function setContext(RequestContext $requestContext): void
@@ -17,15 +18,23 @@ final class DummyUrlGenerator implements UrlGeneratorInterface
         $this->requestContext = $requestContext;
 =======
     private RequestContext $context;
+=======
+    private RequestContext $requestContext;
+>>>>>>> e6b358d0c (add app deuflat variable)
 
-    public function setContext(RequestContext $context): void
+    public function setContext(RequestContext $requestContext): void
     {
+<<<<<<< HEAD
         $this->context = $context;
 >>>>>>> 45db8b8e4 ([PHPStanRules] Add twig + path etension for easier parsing)
+=======
+        $this->requestContext = $requestContext;
+>>>>>>> e6b358d0c (add app deuflat variable)
     }
 
     public function getContext(): RequestContext
     {
+<<<<<<< HEAD
 <<<<<<< HEAD
         return $this->requestContext;
     }
@@ -37,6 +46,12 @@ final class DummyUrlGenerator implements UrlGeneratorInterface
 
     public function generate(string $name, array $parameters = [], int $referenceType = self::ABSOLUTE_PATH)
 >>>>>>> 45db8b8e4 ([PHPStanRules] Add twig + path etension for easier parsing)
+=======
+        return $this->requestContext;
+    }
+
+    public function generate(string $name, array $parameters = [], int $referenceType = self::ABSOLUTE_PATH): string
+>>>>>>> e6b358d0c (add app deuflat variable)
     {
         return 'some_dummy_url';
     }
