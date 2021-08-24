fcd6989ed6e32de589b8e3ca666d499d51738ed4
Tomas Votruba <tomas.vot@gmail.com>
2021-08-24 12:59:27 +0200

    skip functions too

--- packages/phpstan-rules/packages/symfony/src/Twig/TolerantTwigEnvironment.php
+++ packages/phpstan-rules/packages/symfony/src/Twig/TolerantTwigEnvironment.php
@@ -6,6 +6,7 @@ namespace Symplify\PHPStanRules\Symfony\Twig;
 
 use Twig\Environment;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 /**
  * Allows any function and filter
@@ -15,5 +16,10 @@ final class TolerantTwigEnvironment extends Environment
     public function getFilter(string $name): ?TwigFilter
     {
         return new TwigFilter($name);
+    }
+
+    public function getFunction(string $name): ?TwigFunction
+    {
+        return new TwigFunction($name);
     }
 }
