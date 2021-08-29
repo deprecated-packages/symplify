ef04e8df73cae516f28a5e2e169bd99f46df362a
Tomas Votruba <tomas.vot@gmail.com>
2021-08-29 13:38:00 +0200

    [PHPStanRules] Add EnumSpotterRule

---
+++ packages/phpstan-rules/tests/Rules/Domain/EnumSpotterRule/EnumSpotterRuleTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\Domain\EnumSpotterRule;
+
+use Iterator;
+use PHPStan\Rules\Rule;
+use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
+use Symplify\PHPStanRules\Rules\Domain\EnumSpotterRule;
+
+/**
+ * @extends AbstractServiceAwareRuleTestCase<EnumSpotterRule>
+ */
+final class EnumSpotterRuleTest extends AbstractServiceAwareRuleTestCase
+{
+    /**
+     * @dataProvider provideData()
+     * @param string[] $filePaths
+     * @param array<string|int> $expectedErrorMessagesWithLines
+     */
+    public function testRule(array $filePaths, array $expectedErrorMessagesWithLines): void
+    {
+        $this->analyse($filePaths, $expectedErrorMessagesWithLines);
+    }
+
+    public function provideData(): Iterator
+    {
+        $errorMessage = sprintf(EnumSpotterRule::ERROR_MESSAGE, 'info', 2);
+
+        yield [[__DIR__ . '/Fixture/FirstUse.php', __DIR__ . '/Fixture/SecondUse.php'], [[$errorMessage, 13]]];
+
+        yield [[__DIR__ . '/Fixture/SkipFirstUseInTest.php', __DIR__ . '/Fixture/SkipSecondUseInTest.php'], []];
+        yield [[__DIR__ . '/Fixture/SkipNotRepeatedUse.php'], []];
+    }
+
+    protected function getRule(): Rule
+    {
+        return $this->getRuleFromConfig(EnumSpotterRule::class, __DIR__ . '/config/configured_rule.neon');
+    }
+}
---
+++ packages/phpstan-rules/tests/Rules/Domain/EnumSpotterRule/Fixture/SkipSecondUseInTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\Domain\EnumSpotterRule\Fixture;
+
+use PHPStan\Testing\TestCase;
+use Symplify\PHPStanRules\Tests\Rules\Domain\EnumSpotterRule\Source\AnotherFlashAdder;
+
+final class SkipSecondUseInTest extends TestCase
+{
+    public function apply(AnotherFlashAdder $flashAdder)
+    {
+        $flashAdder->addFlash('some message', 'info');
+    }
+}
