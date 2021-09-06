d47984ec3363b71e0aad7fda564c49aa94ef41a5
Tomas Votruba <tomas.vot@gmail.com>
2021-09-07 00:41:13 +0200

    [EasyCI] Skip setup (#3559)

--- packages/easy-ci/packages/Neon/Application/NeonFilesProcessor.php
+++ packages/easy-ci/packages/Neon/Application/NeonFilesProcessor.php
@@ -8,6 +8,7 @@ use Nette\Neon\Entity;
 use Nette\Neon\Neon;
 use Nette\Utils\Arrays;
 use Symplify\EasyCI\Contract\Application\FileProcessorInterface;
+use Symplify\EasyCI\Contract\ValueObject\FileErrorInterface;
 use Symplify\EasyCI\ValueObject\FileError;
 use Symplify\SmartFileSystem\SmartFileInfo;
 use Symplify\SmartFileSystem\SmartFileSystem;
@@ -22,6 +23,11 @@ final class NeonFilesProcessor implements FileProcessorInterface
      */
     private const SERVICES_KEY = 'services';
 
+    /**
+     * @var string
+     */
+    private const SETUP_KEY = 'setup';
+
     public function __construct(
         private SmartFileSystem $smartFileSystem
     ) {
@@ -29,15 +35,14 @@ final class NeonFilesProcessor implements FileProcessorInterface
 
     /**
      * @param SmartFileInfo[] $fileInfos
-     * @return FileError[]
+     * @return FileErrorInterface[]
      */
     public function processFileInfos(array $fileInfos): array
     {
         $fileErrors = [];
 
         foreach ($fileInfos as $fileInfo) {
-            $fileContent = $this->smartFileSystem->readFile($fileInfo->getRealPath());
-            $neon = Neon::decode($fileContent);
+            $neon = $this->readNeonFile($fileInfo);
 
             // 1. we only take care about services
             $servicesNeon = $neon[self::SERVICES_KEY] ?? null;
@@ -45,25 +50,76 @@ final class NeonFilesProcessor implements FileProcessorInterface
                 continue;
             }
 
+            $currentFileErrors = $this->processServicesSection($servicesNeon, $fileInfo);
+            $fileErrors = array_merge($fileErrors, $currentFileErrors);
+        }
+
+        return $fileErrors;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    private function readNeonFile(SmartFileInfo $fileInfo): array
+    {
+        $fileContent = $this->smartFileSystem->readFile($fileInfo->getRealPath());
+        return Neon::decode($fileContent);
+    }
+
+    private function createErrorMessageFromNeonEntity(Entity $neonEntity): string
+    {
+        $neonEntityContent = Neon::encode($neonEntity);
+
+        return sprintf(
+            'Complex entity found "%s"%s   Change it to explicit syntax with named keys, that is easier to read.',
+            $neonEntityContent,
+            PHP_EOL,
+        );
+    }
+
+    /**
+     * @param mixed[] $servicesNeon
+     * @return FileErrorInterface[]
+     */
+    private function processServicesSection(array $servicesNeon, SmartFileInfo $fileInfo): array
+    {
+        $fileErrors = [];
+
+        foreach ($servicesNeon as $singleService) {
+            if ($singleService instanceof Entity) {
+                $errorMessage = $this->createErrorMessageFromNeonEntity($singleService);
+                $fileErrors[] = new FileError($errorMessage, $fileInfo);
+                continue;
+            }
+
+            // 1. the "setup" has allowed entities
+            $singleService = $this->removeSetupKey($singleService);
+
             // 2. detect complex neon entities
-            $flatNeon = Arrays::flatten($servicesNeon);
+            $flatNeon = Arrays::flatten($singleService, true);
             foreach ($flatNeon as $itemNeon) {
                 if (! $itemNeon instanceof Entity) {
                     continue;
                 }
 
-                $neonEntityContent = Neon::encode($itemNeon);
-
-                $errorMessage = sprintf(
-                    'Complex entity found "%s"%s   Change it to explicit syntax with named keys, that is easier to read.',
-                    $neonEntityContent,
-                    PHP_EOL,
-                );
-
+                $errorMessage = $this->createErrorMessageFromNeonEntity($itemNeon);
                 $fileErrors[] = new FileError($errorMessage, $fileInfo);
             }
         }
 
         return $fileErrors;
+    }
+
+    /**
+     * @param array<int|string, mixed> $singleService
+     * @return array<int|string, mixed>
+     */
+    private function removeSetupKey(array $singleService): array
+    {
+        if (isset($singleService[self::SETUP_KEY])) {
+            unset($singleService[self::SETUP_KEY]);
+        }
+
+        return $singleService;
     }
 }
