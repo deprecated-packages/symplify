7b47cb6fe4f307fa1974b89ed59b99a4dfa752b5
Carlos C Soto <eclipxe13@gmail.com>
2021-09-10 13:04:12 -0500

    Fix CI PHPStan issue on WarnOnPrintFakeSniff
    WarnOnPrintFakeSniff is missing @see annotation with test case class reference

--- packages/easy-coding-standard/tests/Error/ErrorCollector/SniffRunnerSource/WarnOnPrintFakeSniff.php
+++ packages/easy-coding-standard/tests/Error/ErrorCollector/SniffRunnerSource/WarnOnPrintFakeSniff.php
@@ -7,6 +7,9 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 
 /**
  * This class has been created specifically to produce a warning on any print token
+ * Special crafted file that trigger this sniff is located at "./warn-on-print-code.inc"
+ *
+ * @see \Symplify\EasyCodingStandard\Tests\Error\ErrorCollector\SniffFileProcessorReportWarningTest
  */
 final class WarnOnPrintFakeSniff implements Sniff
 {
