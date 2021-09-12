5433ab778c5d106a96c7b8c9c83bae441341dd4a
Tomas Votruba <tomas.vot@gmail.com>
2021-09-12 17:52:46 +0200

    add magic filter to explicit node visitor

--- packages/phpstan-rules/packages/latte-phpstan-printer/src/Latte/Tokens/PhpToLatteLineNumbersResolver.php
+++ packages/phpstan-rules/packages/latte-phpstan-printer/src/Latte/Tokens/PhpToLatteLineNumbersResolver.php
@@ -4,62 +4,36 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\LattePHPStanPrinter\Latte\Tokens;
 
-use Nette\Utils\Strings;
+use PhpParser\NodeTraverser;
+use PhpParser\Parser;
+use Symplify\PHPStanRules\LattePHPStanPrinter\PhpParser\NodeVisitor\LatteLineNumberNodeVisitor;
 
 final class PhpToLatteLineNumbersResolver
 {
-    /**
-     * @var string
-     * @see https://regex101.com/r/Qb4cuo/1
-     */
-    private const COMMENTED_LINE_NUMBER_REGEX = '#^\/\* line (?<' . self::NUMBER_KEY . '>\d+) \*\/$#';
-
-    /**
-     * @var string
-     */
-    private const NUMBER_KEY = 'number';
-
-    /**
-     * @param array<int, mixed> $tokens
-     * @return array<int, int>
-     */
-    public function resolve(array $tokens, int $variablesAndTypesCount): array
-    {
-        $phpLinesToLatteLines = [];
-
-        foreach ($tokens as $position => $token) {
-            // 388
-            if ($token[0] !== T_COMMENT) {
-                continue;
-            }
-
-            $lineMatch = Strings::match($token[1], self::COMMENTED_LINE_NUMBER_REGEX);
-            if (! isset($lineMatch[self::NUMBER_KEY])) {
-                continue;
-            }
-
-            $phpLineNumber = $this->resolveLineNumberFromTokensOnPosition($tokens, $position);
-            // correct the line number by number of added var types
-            $phpLineNumber += $variablesAndTypesCount - 1;
-            $latteLineNumber = (int) $lineMatch[self::NUMBER_KEY];
-
-            $phpLinesToLatteLines[$phpLineNumber] = $latteLineNumber;
-        }
-
-        return $phpLinesToLatteLines;
+    public function __construct(
+        private LatteLineNumberNodeVisitor $latteLineNumberNodeVisitor,
+        private Parser $parser
+    ) {
     }
 
-    private function resolveLineNumberFromTokensOnPosition(array $tokens, int $desiredPosition): int
+    /**
+     * Here we have to use file content and parse it again, so we have updated start line positions
+     *
+     * @return array<int, int>
+     */
+    public function resolve(string $phpFileContent): array
     {
-        $lineNumber = 0;
+        $phpNodes = $this->parser->parse($phpFileContent);
 
-        foreach ($tokens as $position => $token) {
-            $lineNumber += substr_count($token[1], "\n");
-            if ($position === $desiredPosition) {
-                break;
-            }
+        // nothign to resolve
+        if ($phpNodes === null) {
+            return [];
         }
 
-        return $lineNumber;
+        $nodeTraverser = new NodeTraverser();
+        $nodeTraverser->addVisitor($this->latteLineNumberNodeVisitor);
+        $nodeTraverser->traverse($phpNodes);
+
+        return $this->latteLineNumberNodeVisitor->getPhpLinesToLatteLines();
     }
 }
