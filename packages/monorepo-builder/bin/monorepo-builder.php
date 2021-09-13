59c7972ce8c42c0e075ebb2557e82b3f7634dab4
Marco Lipparini <1532277+liarco@users.noreply.github.com>
2021-09-13 13:18:22 +0200

    Changing order in $possibleAutoloadPaths (fixes #3570) (#3573)
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

--- packages/monorepo-builder/bin/monorepo-builder.php
+++ packages/monorepo-builder/bin/monorepo-builder.php
@@ -13,12 +13,12 @@ use Symplify\SymplifyKernel\ValueObject\KernelBootAndApplicationRun;
 
 # 1. autoload
 $possibleAutoloadPaths = [
+    // monorepo
+    __DIR__ . '/../../../vendor/autoload.php',
     // after split package
     __DIR__ . '/../vendor/autoload.php',
     // dependency
     __DIR__ . '/../../../autoload.php',
-    // monorepo
-    __DIR__ . '/../../../vendor/autoload.php',
 ];
 
 foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
