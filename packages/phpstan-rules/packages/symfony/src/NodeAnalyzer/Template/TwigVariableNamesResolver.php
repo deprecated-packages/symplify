d158ef9fb834ed39896ce96a14ce70e79ff29236
Tomas Votruba <tomas.vot@gmail.com>
2021-09-25 16:14:07 +0200

    re-use parent aware parser

--- packages/phpstan-rules/packages/symfony/src/NodeAnalyzer/Template/TwigVariableNamesResolver.php
+++ packages/phpstan-rules/packages/symfony/src/NodeAnalyzer/Template/TwigVariableNamesResolver.php
@@ -4,22 +4,20 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\Symfony\NodeAnalyzer\Template;
 
-use PhpParser\Node\Stmt;
 use PhpParser\NodeFinder;
 use PhpParser\NodeTraverser;
-use PhpParser\NodeVisitor\NodeConnectingVisitor;
-use PhpParser\Parser;
 use Symplify\Astral\Naming\SimpleNameResolver;
 use Symplify\PHPStanRules\Nette\PhpParser\NodeVisitor\TemplateVariableCollectingNodeVisitor;
+use Symplify\PHPStanRules\Nette\PhpParser\ParentNodeAwarePhpParser;
 use Symplify\PHPStanRules\TwigPHPStanPrinter\TwigToPhpCompiler;
 
 final class TwigVariableNamesResolver
 {
     public function __construct(
         private TwigToPhpCompiler $twigToPhpCompiler,
-        private Parser $parser,
         private SimpleNameResolver $simpleNameResolver,
-        private NodeFinder $nodeFinder
+        private NodeFinder $nodeFinder,
+        private ParentNodeAwarePhpParser $parentNodeAwarePhpParser
     ) {
     }
 
@@ -30,20 +28,11 @@ final class TwigVariableNamesResolver
     {
         $variableNames = [];
         foreach ($filePaths as $filePath) {
-            $variableNames = array_merge($variableNames, $this->resolveFromFilePath($filePath));
+            $currentVariableNames = $this->resolveFromFilePath($filePath);
+            $variableNames = array_merge($variableNames, $currentVariableNames);
         }
 
         return $variableNames;
-    }
-
-    /**
-     * @param Stmt[] $stmts
-     */
-    private function decorateParentAttribute(array $stmts): void
-    {
-        $nodeTraverser = new NodeTraverser();
-        $nodeTraverser->addVisitor(new NodeConnectingVisitor());
-        $nodeTraverser->traverse($stmts);
     }
 
     /**
@@ -52,23 +41,17 @@ final class TwigVariableNamesResolver
     private function resolveFromFilePath(string $filePath): array
     {
         $phpFileContent = $this->twigToPhpCompiler->compileContent($filePath, []);
+        $stmts = $this->parentNodeAwarePhpParser->parsePhpContent($phpFileContent);
 
-        $stmts = $this->parser->parse($phpFileContent);
-        if ($stmts === null) {
-            return [];
-        }
-
-        $this->decorateParentAttribute($stmts);
-
-        $nodeTraverser = new NodeTraverser();
         $templateVariableCollectingNodeVisitor = new TemplateVariableCollectingNodeVisitor(
             ['context', 'macros', 'this', '_parent', 'loop', 'tmp'],
             ['doDisplay'],
             $this->simpleNameResolver,
             $this->nodeFinder
         );
-        $nodeTraverser->addVisitor($templateVariableCollectingNodeVisitor);
 
+        $nodeTraverser = new NodeTraverser();
+        $nodeTraverser->addVisitor($templateVariableCollectingNodeVisitor);
         $nodeTraverser->traverse($stmts);
 
         return $templateVariableCollectingNodeVisitor->getUsedVariableNames();
