ea7a69597c2165aad911430ff541473b226c2cf1
Tomas Votruba <tomas.vot@gmail.com>
2021-09-29 02:51:42 +0300

    keep reference to autor

--- packages/console-color-diff/src/Console/Formatter/ColorConsoleDiffFormatter.php
+++ packages/console-color-diff/src/Console/Formatter/ColorConsoleDiffFormatter.php
@@ -8,8 +8,8 @@ use Nette\Utils\Strings;
 use Symfony\Component\Console\Formatter\OutputFormatter;
 
 /**
- * Most is copy-pasted from https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/src/Differ/DiffConsoleFormatter.php
- * to be used as standalone class, without need to require whole package.
+ * Inspired by @see https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/src/Differ/DiffConsoleFormatter.php
+ * to be used as standalone class, without need to require whole package by Dariusz Rumiński <dariusz.ruminski@gmail.com>
  *
  * @see \Symplify\ConsoleColorDiff\Tests\Console\Formatter\ColorConsoleDiffFormatterTest
  */
