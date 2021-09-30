934abe8c38de4ee259df964c8aba48c7d7c735ad
Tomas Votruba <tomas.vot@gmail.com>
2021-10-01 00:30:03 +0200

    [TwigPHPStanCompiler] Add existing filter/Function if possible

--- packages/twig-phpstan-compiler/src/Twig/TolerantTwigEnvironment.php
+++ packages/twig-phpstan-compiler/src/Twig/TolerantTwigEnvironment.php
@@ -15,12 +15,22 @@ final class TolerantTwigEnvironment extends Environment
 {
     public function getFilter(string $name): ?TwigFilter
     {
+        $twigFilter = parent::getFilter($name);
+        if ($twigFilter instanceof TwigFilter) {
+            return $twigFilter;
+        }
+
         // 2nd argument is dummy function, so the function name is not empty and compilation twig to PHP passes
         return new TwigFilter($name, 'strlen');
     }
 
     public function getFunction(string $name): ?TwigFunction
     {
+        $twigFunction = parent::getFunction($name);
+        if ($twigFunction instanceof TwigFunction) {
+            return $twigFunction;
+        }
+
         // 2nd argument is dummy function, so the function name is not empty and compilation twig to PHP passes
         return new TwigFunction($name, 'strlen');
     }
