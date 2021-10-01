70b5787ac669ce6657b6bc8fc2d953b0f4bb5741
Tomas Votruba <tomas.vot@gmail.com>
2021-10-01 10:52:53 +0200

    [TwigPHPStanCompiler] Collect variable from var doc types

--- packages/twig-phpstan-compiler/src/PhpParser/NodeVisitor/ReplaceEchoWithVarDocTypeNodeVisitor.php
+++ packages/twig-phpstan-compiler/src/PhpParser/NodeVisitor/ReplaceEchoWithVarDocTypeNodeVisitor.php
@@ -11,12 +11,26 @@ use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\Echo_;
 use PhpParser\Node\Stmt\Nop;
 use PhpParser\NodeVisitorAbstract;
+use PHPStan\Type\ObjectType;
 use Symplify\Astral\ValueObject\AttributeKey;
+use Symplify\TemplatePHPStanCompiler\ValueObject\VariableAndType;
 use Symplify\TwigPHPStanCompiler\TwigToPhpCompiler;
 use Symplify\TwigPHPStanCompiler\ValueObject\VarTypeDoc;
 
 final class ReplaceEchoWithVarDocTypeNodeVisitor extends NodeVisitorAbstract
 {
+    /**
+     * @var VariableAndType[]
+     */
+    private array $collectedVariablesAndTypes = [];
+
+    public function beforeTraverse(array $nodes)
+    {
+        $this->collectedVariablesAndTypes = [];
+
+        return parent::beforeTraverse($nodes);
+    }
+
     public function enterNode(Node $node)
     {
         if (! $node instanceof Echo_) {
@@ -48,6 +62,19 @@ final class ReplaceEchoWithVarDocTypeNodeVisitor extends NodeVisitorAbstract
         $nop = new Nop();
         $nop->setAttribute(AttributeKey::COMMENTS, $comments);
 
+        // @todo assumption that type is an object - resolve in some strict/doc parser clean way
+        $this->collectedVariablesAndTypes[] = new VariableAndType($varTypeDoc->getVariableName(), new ObjectType(
+            $varTypeDoc->getType()
+        ));
+
         return $nop;
+    }
+
+    /**
+     * @return VariableAndType[]
+     */
+    public function getCollectedVariablesAndTypes(): array
+    {
+        return $this->collectedVariablesAndTypes;
     }
 }
