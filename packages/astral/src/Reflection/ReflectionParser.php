9d2c4afa210fd18c8b7c78c56e748f9c5ba90440
Tomas Votruba <tomas.vot@gmail.com>
2021-10-24 13:52:45 +0200

    [Astral] Add ReflectionParser

--- packages/astral/src/Reflection/ReflectionParser.php
+++ packages/astral/src/Reflection/ReflectionParser.php
@@ -2,23 +2,38 @@
 
 declare(strict_types=1);
 
-namespace Symplify\PHPStanRules\Reflection\Parser;
+namespace Symplify\Astral\Reflection;
 
+use PhpParser\Lexer\Emulative;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\NodeFinder;
+use PhpParser\NodeVisitor\NameResolver;
+use PhpParser\ParserFactory;
+use PHPStan\Parser\CachedParser;
 use PHPStan\Parser\Parser;
+use PHPStan\Parser\SimpleParser;
 use ReflectionMethod;
 use ReflectionProperty;
 use Throwable;
 
 final class ReflectionParser
 {
+    private Parser $parser;
+
     public function __construct(
-        private Parser $parser,
+        ParserFactory $parserFactory,
         private NodeFinder $nodeFinder
     ) {
+        // @todo extract to DI factory and require by interface to allow re-use injection
+        $lexerEmulative = new Emulative();
+        $nativeParser = $parserFactory->create(ParserFactory::PREFER_PHP7, $lexerEmulative);
+
+        $nameResolver = new NameResolver();
+        $simpleParser = new SimpleParser($nativeParser, $nameResolver);
+
+        $this->parser = new CachedParser($simpleParser, 1024);
     }
 
     public function parseMethodReflectionToClassMethod(ReflectionMethod $reflectionMethod): ?ClassMethod
