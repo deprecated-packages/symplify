863f90c3e9b6dc6524672661543bea87433ff896
Tomas Votruba <tomas.vot@gmail.com>
2021-10-24 14:17:01 +0200

    add SmartPhpParser for easy autowiring

--- packages/astral/src/Reflection/ReflectionParser.php
+++ packages/astral/src/Reflection/ReflectionParser.php
@@ -4,36 +4,24 @@ declare(strict_types=1);
 
 namespace Symplify\Astral\Reflection;
 
-use PhpParser\Lexer\Emulative;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\NodeFinder;
-use PhpParser\NodeVisitor\NameResolver;
-use PhpParser\ParserFactory;
-use PHPStan\Parser\CachedParser;
-use PHPStan\Parser\Parser;
-use PHPStan\Parser\SimpleParser;
 use ReflectionMethod;
 use ReflectionProperty;
+use Symplify\Astral\PhpParser\SmartPhpParser;
 use Throwable;
 
+/**
+ * @api
+ */
 final class ReflectionParser
 {
-    private Parser $parser;
-
     public function __construct(
-        ParserFactory $parserFactory,
+        private SmartPhpParser $smartPhpParser,
         private NodeFinder $nodeFinder
     ) {
-        // @todo extract to DI factory and require by interface to allow re-use injection
-        $lexerEmulative = new Emulative();
-        $nativeParser = $parserFactory->create(ParserFactory::PREFER_PHP7, $lexerEmulative);
-
-        $nameResolver = new NameResolver();
-        $simpleParser = new SimpleParser($nativeParser, $nameResolver);
-
-        $this->parser = new CachedParser($simpleParser, 1024);
     }
 
     public function parseMethodReflectionToClassMethod(ReflectionMethod $reflectionMethod): ?ClassMethod
@@ -66,13 +54,13 @@ final class ReflectionParser
         }
 
         try {
-            $nodes = $this->parser->parseFile($fileName);
+            $stmts = $this->smartPhpParser->parseFile($fileName);
         } catch (Throwable) {
             // not reachable
             return null;
         }
 
-        $class = $this->nodeFinder->findFirstInstanceOf($nodes, Class_::class);
+        $class = $this->nodeFinder->findFirstInstanceOf($stmts, Class_::class);
         if (! $class instanceof Class_) {
             return null;
         }
