5577be43dba8bcb90563729f00f7497186ce9f93
Tomas Votruba <tomas.vot@gmail.com>
2021-10-25 15:44:19 +0200

    [Astral] Remove autowired array

--- packages/astral/src/Bundle/AstralBundle.php
+++ packages/astral/src/Bundle/AstralBundle.php
@@ -4,18 +4,11 @@ declare(strict_types=1);
 
 namespace Symplify\Astral\Bundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symplify\Astral\DependencyInjection\Extension\AstralExtension;
-use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
 
 final class AstralBundle extends Bundle
 {
-    public function build(ContainerBuilder $containerBuilder): void
-    {
-        $containerBuilder->addCompilerPass(new AutowireArrayParameterCompilerPass());
-    }
-
     protected function createContainerExtension(): AstralExtension
     {
         return new AstralExtension();
