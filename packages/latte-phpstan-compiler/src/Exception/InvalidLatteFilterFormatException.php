f36fcd4ac70a9910f9da2c348d6e7b96bbc1009f
GitHub Action <action@github.com>
2021-10-29 18:10:45 +0000

    [ci-review] Rector Rectify

--- packages/latte-phpstan-compiler/src/Exception/InvalidLatteFilterFormatException.php
+++ packages/latte-phpstan-compiler/src/Exception/InvalidLatteFilterFormatException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\LattePHPStanCompiler\Exception;
 
-final class InvalidLatteFilterFormatException extends \Exception
+use Exception;
+final class InvalidLatteFilterFormatException extends Exception
 {
 }
