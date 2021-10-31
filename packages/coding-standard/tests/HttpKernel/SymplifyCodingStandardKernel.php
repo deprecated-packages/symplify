a9eb16da52d9c4198a8f165822300be407356129
Tomas Votruba <tomas.vot@gmail.com>
2021-10-31 21:07:25 +0100

    [CodingStandard] Remove dependency on http-kernel + fix absolute paths (#3729)

--- packages/coding-standard/tests/HttpKernel/SymplifyCodingStandardKernel.php
+++ packages/coding-standard/tests/HttpKernel/SymplifyCodingStandardKernel.php
@@ -4,33 +4,46 @@ declare(strict_types=1);
 
 namespace Symplify\CodingStandard\Tests\HttpKernel;
 
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Kernel;
-use Symplify\CodingStandard\Bundle\SymplifyCodingStandardBundle;
-use Symplify\ConsoleColorDiff\Bundle\ConsoleColorDiffBundle;
-use Symplify\EasyCodingStandard\Bundle\EasyCodingStandardBundle;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\Container;
+use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
+use Symplify\CodingStandard\DependencyInjection\Extension\SymplifyCodingStandardExtension;
+use Symplify\ConfigTransformer\Exception\ShouldNotHappenException;
+use Symplify\ConsoleColorDiff\DependencyInjection\Extension\ConsoleColorDiffExtension;
+use Symplify\EasyCodingStandard\DependencyInjection\Extension\EasyCodingStandardExtension;
+use Symplify\SymfonyContainerBuilder\ContainerBuilderFactory;
+use Symplify\SymplifyKernel\Contract\LightKernelInterface;
 
-final class SymplifyCodingStandardKernel extends Kernel
+final class SymplifyCodingStandardKernel implements LightKernelInterface
 {
-    public function registerContainerConfiguration(LoaderInterface $loader): void
-    {
-    }
-
-    public function getCacheDir(): string
-    {
-        return sys_get_temp_dir() . '/symplify_coding_standard';
-    }
-
-    public function getLogDir(): string
-    {
-        return sys_get_temp_dir() . '/symplify_coding_standard_log';
-    }
+    private Container|null $container = null;
 
     /**
-     * @return SymplifyCodingStandardBundle[]|ConsoleColorDiffBundle[]|EasyCodingStandardBundle[]
+     * @param string[] $configFiles
      */
-    public function registerBundles(): iterable
+    public function createFromConfigs(array $configFiles): ContainerInterface
     {
-        return [new SymplifyCodingStandardBundle(), new EasyCodingStandardBundle(), new ConsoleColorDiffBundle()];
+        $containerBuilderFactory = new ContainerBuilderFactory();
+
+        $extensions = [
+            new SymplifyCodingStandardExtension(), new EasyCodingStandardExtension(), new ConsoleColorDiffExtension(),
+        ];
+        $compilerPasses = [new AutowireArrayParameterCompilerPass()];
+
+        $containerBuilder = $containerBuilderFactory->create($extensions, $compilerPasses, $configFiles);
+        $containerBuilder->compile();
+
+        $this->container = $containerBuilder;
+
+        return $containerBuilder;
+    }
+
+    public function getContainer(): ContainerInterface
+    {
+        if (! $this->container instanceof Container) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $this->container;
     }
 }
--- packages/easy-coding-standard/config/services.php
+++ packages/easy-coding-standard/config/services.php
@@ -33,7 +33,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->load('Symplify\EasyCodingStandard\\', __DIR__ . '/../src')
         ->exclude([
             // only for "bin/ecs" file, where container does not exist yet
-            __DIR__ . '/../src/Bundle',
             __DIR__ . '/../src/DependencyInjection',
             __DIR__ . '/../src/HttpKernel',
             __DIR__ . '/../src/Exception',
