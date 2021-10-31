6d2395ab45e2e5d6736886e94ac4a1fc76c830fb
Tomas Votruba <tomas.vot@gmail.com>
2021-10-31 21:21:46 +0100

    [PSR4Switcher] [EasyCI] Update kernel to LightKernelInterface

--- packages/easy-ci/src/HttpKernel/EasyCIKernel.php
+++ packages/easy-ci/src/HttpKernel/EasyCIKernel.php
@@ -4,28 +4,47 @@ declare(strict_types=1);
 
 namespace Symplify\EasyCI\HttpKernel;
 
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
-use Symplify\Astral\Bundle\AstralBundle;
-use Symplify\ComposerJsonManipulator\Bundle\ComposerJsonManipulatorBundle;
-use Symplify\PackageBuilder\Contract\HttpKernel\ExtraConfigAwareKernelInterface;
-use Symplify\SymplifyKernel\Bundle\SymplifyKernelBundle;
-use Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\Container;
+use Symplify\Astral\DependencyInjection\Extension\AstralExtension;
+use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonManipulatorConfig;
+use Symplify\ConfigTransformer\Exception\ShouldNotHappenException;
+use Symplify\SymfonyContainerBuilder\ContainerBuilderFactory;
+use Symplify\SymplifyKernel\Contract\LightKernelInterface;
+use Symplify\SymplifyKernel\DependencyInjection\Extension\SymplifyKernelExtension;
 
-final class EasyCIKernel extends AbstractSymplifyKernel implements ExtraConfigAwareKernelInterface
+final class EasyCIKernel implements LightKernelInterface
 {
-    public function registerContainerConfiguration(LoaderInterface $loader): void
-    {
-        $loader->load(__DIR__ . '/../../config/config.php');
-
-        parent::registerContainerConfiguration($loader);
-    }
+    private Container|null $container = null;
 
     /**
-     * @return iterable<BundleInterface>
+     * @param string[] $configFiles
      */
-    public function registerBundles(): iterable
+    public function createFromConfigs(array $configFiles): ContainerInterface
     {
-        return [new ComposerJsonManipulatorBundle(), new SymplifyKernelBundle(), new AstralBundle()];
+        $containerBuilderFactory = new ContainerBuilderFactory();
+
+        $extensions = [new SymplifyKernelExtension(), new AstralExtension()];
+        $compilerPasses = [new AutowireArrayParameterCompilerPass()];
+
+        $configFiles[] = __DIR__ . '/../../config/config.php';
+        $configFiles[] = ComposerJsonManipulatorConfig::FILE_PATH;
+
+        $containerBuilder = $containerBuilderFactory->create($extensions, $compilerPasses, $configFiles);
+        $containerBuilder->compile();
+
+        $this->container = $containerBuilder;
+
+        return $containerBuilder;
+    }
+
+    public function getContainer(): ContainerInterface
+    {
+        if (! $this->container instanceof Container) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $this->container;
     }
 }
--- packages/psr4-switcher/src/HttpKernel/Psr4SwitcherKernel.php
+++ packages/psr4-switcher/src/HttpKernel/Psr4SwitcherKernel.php
@@ -4,24 +4,46 @@ declare(strict_types=1);
 
 namespace Symplify\Psr4Switcher\HttpKernel;
 
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
-use Symplify\ComposerJsonManipulator\Bundle\ComposerJsonManipulatorBundle;
-use Symplify\SymplifyKernel\Bundle\SymplifyKernelBundle;
-use Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\Container;
+use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonManipulatorConfig;
+use Symplify\ConfigTransformer\Exception\ShouldNotHappenException;
+use Symplify\SymfonyContainerBuilder\ContainerBuilderFactory;
+use Symplify\SymplifyKernel\Contract\LightKernelInterface;
+use Symplify\SymplifyKernel\DependencyInjection\Extension\SymplifyKernelExtension;
 
-final class Psr4SwitcherKernel extends AbstractSymplifyKernel
+final class Psr4SwitcherKernel implements LightKernelInterface
 {
-    public function registerContainerConfiguration(LoaderInterface $loader): void
-    {
-        $loader->load(__DIR__ . '/../../config/config.php');
-    }
+    private Container|null $container = null;
 
     /**
-     * @return BundleInterface[]
+     * @param string[] $configFiles
      */
-    public function registerBundles(): array
+    public function createFromConfigs(array $configFiles): ContainerInterface
     {
-        return [new ComposerJsonManipulatorBundle(), new SymplifyKernelBundle()];
+        $containerBuilderFactory = new ContainerBuilderFactory();
+
+        $extensions = [new SymplifyKernelExtension()];
+        $compilerPasses = [new AutowireArrayParameterCompilerPass()];
+
+        $configFiles[] = __DIR__ . '/../../config/config.php';
+        $configFiles[] = ComposerJsonManipulatorConfig::FILE_PATH;
+
+        $containerBuilder = $containerBuilderFactory->create($extensions, $compilerPasses, $configFiles);
+        $containerBuilder->compile();
+
+        $this->container = $containerBuilder;
+
+        return $containerBuilder;
+    }
+
+    public function getContainer(): ContainerInterface
+    {
+        if (! $this->container instanceof Container) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $this->container;
     }
 }
