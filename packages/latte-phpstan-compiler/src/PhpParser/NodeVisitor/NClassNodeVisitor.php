7f0d18ed1de85a97a1ef0bbd6022b1626e7058ee
Michal Lulco <lulco@efabrica.sk>
2021-11-19 12:05:22 +0100

    Changed to more simple output

--- packages/latte-phpstan-compiler/src/PhpParser/NodeVisitor/NClassNodeVisitor.php
+++ packages/latte-phpstan-compiler/src/PhpParser/NodeVisitor/NClassNodeVisitor.php
@@ -5,61 +5,65 @@ declare(strict_types=1);
 namespace Symplify\LattePHPStanCompiler\PhpParser\NodeVisitor;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\BinaryOp\Concat;
+use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\Ternary;
+use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\String_;
-use PhpParser\Node\Stmt\Echo_;
-use PhpParser\Node\Stmt\Expression;
 use PhpParser\NodeVisitorAbstract;
 
 /**
  * from: <code> echo ($ʟ_tmp = \array_filter(['class1', $var ? 'class2' : \null])) ? ' class="' .
  * \Latte\Runtime\Filters::escapeHtmlAttr(\implode(" ", \array_unique($ʟ_tmp))) . '"' : ""; </code>
  *
- * to: <code> $ʟ_tmp = \array_filter(['class1', $var ? 'class2' : \null]); echo ' class="' .
- * \Latte\Runtime\Filters::escapeHtmlAttr(\implode(" ", \array_unique($ʟ_tmp))) . '"'; </code>
+ * to: <code> echo ' class="' . \implode(" ", ['class1', $var ? 'class2' : \null]) . '"'; </code>
  */
 final class NClassNodeVisitor extends NodeVisitorAbstract
 {
-    /**
-     * @return Node[]|null
-     */
-    public function leaveNode(Node $node): array|null
+    public function enterNode(Node $node): Node|null
     {
-        if (! $node instanceof Echo_) {
+        if (! $node instanceof Ternary) {
             return null;
         }
-
-        if (! isset($node->exprs[0])) {
-            return null;
-        }
-
-        if (! $node->exprs[0] instanceof Ternary) {
-            return null;
-        }
-
-        $ternary = $node->exprs[0];
 
         // looking for `class="' . \Latte\Runtime\Filters::escapeHtmlAttr()`
-        if (! $ternary->if instanceof Concat) {
+        if (! $node->if instanceof Concat) {
             return null;
         }
 
-        if (! $ternary->if->left instanceof Concat) {
+        if (! $node->if->left instanceof Concat) {
             return null;
         }
 
-        if (! $ternary->if->left->left instanceof String_) {
+        if (! $node->if->left->left instanceof String_) {
             return null;
         }
-        $left = $ternary->if->left->left;
+        $left = $node->if->left->left;
         if ($left->value !== ' class="') {
             return null;
         }
 
-        return [
-            new Expression($ternary->cond, $node->getAttributes()),
-            new Echo_([new Concat(new Concat($left, $ternary->if->left->right), $ternary->if->right)]),
-        ];
+        if (! $node->cond instanceof Assign) {
+            return null;
+        }
+
+        if (! $node->cond->expr instanceof FuncCall) {
+            return null;
+        }
+
+        /** @var FuncCall $funcCall */
+        $funcCall = $node->cond->expr;
+
+        if (! isset($funcCall->args[0])) {
+            return null;
+        }
+
+        $implodeSeparator = new Arg(new String_(' '));
+        $classes = $funcCall->args[0];
+        $implode = new FuncCall(new FullyQualified('implode'), [$implodeSeparator, $classes]);
+
+        return new Concat(new Concat($left, $implode), $node->if->right);
     }
 }
