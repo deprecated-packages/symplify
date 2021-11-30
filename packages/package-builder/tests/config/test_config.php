fc70882333f19a6800f93e583615c0a1c2bc8737
Tomas Votruba <tomas.vot@gmail.com>
2021-11-30 13:37:58 +0300

    require Symfony 5.4 and 6.0

--- packages/package-builder/tests/config/test_config.php
+++ packages/package-builder/tests/config/test_config.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->set(ParameterProvider::class)
-        ->autowire()
-        ->public();
+        ->args([service(ContainerInterface::class)]);
 };
