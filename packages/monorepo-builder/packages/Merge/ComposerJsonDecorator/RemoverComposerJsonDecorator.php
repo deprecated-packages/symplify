b708f00d674ee4219f8b3ca04811c53ade369a6d
GitHub Action <action@github.com>
2021-12-02 22:35:35 +0000

    [ci-review] Rector Rectify

--- packages/monorepo-builder/packages/Merge/ComposerJsonDecorator/RemoverComposerJsonDecorator.php
+++ packages/monorepo-builder/packages/Merge/ComposerJsonDecorator/RemoverComposerJsonDecorator.php
@@ -120,12 +120,15 @@ final class RemoverComposerJsonDecorator implements ComposerJsonDecoratorInterfa
         if ($removingComposerJson->getPreferStable()) {
             $composerJson->removePreferStable();
         }
+
         if (count($removingComposerJson->getRepositories()) !== 1) {
             return;
         }
+
         if ($removingComposerJson->getRepositories()[0] !== Option::REMOVE_COMPLETELY) {
             return;
         }
+
         $composerJson->setRepositories([]);
     }
 }
