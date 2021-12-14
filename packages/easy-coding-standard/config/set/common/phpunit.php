b5cc7aa8fe5d07f63f6987a71dd5f4349c243ce5
Tomas Votruba <tomas.vot@gmail.com>
2021-12-14 11:17:04 +0100

    [ECS] Remove  strict phpunit fixer breaking rule, let Rector handle it (#3844)

--- packages/easy-coding-standard/config/set/common/phpunit.php
+++ packages/easy-coding-standard/config/set/common/phpunit.php
@@ -3,16 +3,12 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitSetUpTearDownVisibilityFixer;
-use PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer;
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestAnnotationFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
-    $services->set(PhpUnitStrictFixer::class);
-
     $services->set(PhpUnitTestAnnotationFixer::class);
-
     $services->set(PhpUnitSetUpTearDownVisibilityFixer::class);
 };
