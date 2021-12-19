5d436bd446588bb990c0dc462b403c66718d03e5
Tomas Votruba <tomas.vot@gmail.com>
2021-12-19 12:26:10 +0100

    [Amnesia] Add attribute mapping

--- packages/amnesia/src/ValueObject/Symfony/Extension/Doctrine/Mapping.php
+++ packages/amnesia/src/ValueObject/Symfony/Extension/Doctrine/Mapping.php
@@ -30,6 +30,12 @@ final class Mapping
     public const TYPE_ANNOTATION = 'annotation';
 
     /**
+     * @see https://github.com/doctrine/DoctrineBundle/pull/1322/files#diff-6c0dba5076ce8a2e9ff3e1e07f6661e095a18f80e32ad9766945f55c0414f8a2R805
+     * @var string
+     */
+    public const TYPE_ATTRIBUTE = 'attribute';
+
+    /**
      * @var string
      */
     public const DIR = 'dir';
