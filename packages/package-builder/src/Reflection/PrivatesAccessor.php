e8cd20a909af2feeb0cc37614c081363c270184e
Markus Staab <markus.staab@redaxo.de>
2021-12-30 11:04:32 +0100

    [PackageBuilder] Added type-safe `PrivatesAccessor`-methods (#3860)
    Co-authored-by: Abdul Malik Ikhsan <samsonasik@gmail.com>
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

--- packages/package-builder/src/Reflection/PrivatesAccessor.php
+++ packages/package-builder/src/Reflection/PrivatesAccessor.php
@@ -14,6 +14,24 @@ use Symplify\PHPStanRules\Exception\ShouldNotHappenException;
 final class PrivatesAccessor
 {
     /**
+     * @template T
+     *
+     * @param class-string<T> $valueClassName
+     *
+     * @return T
+     */
+    public function getPrivatePropertyOfClass(object $object, string $propertyName, string $valueClassName)
+    {
+        $value = $this->getPrivateProperty($object, $propertyName);
+
+        if ($value instanceof $valueClassName) {
+            return $value;
+        }
+
+        throw new ShouldNotHappenException();
+    }
+
+    /**
      * @return mixed
      */
     public function getPrivateProperty(object $object, string $propertyName)
@@ -22,6 +40,21 @@ final class PrivatesAccessor
         $propertyReflection->setAccessible(true);
 
         return $propertyReflection->getValue($object);
+    }
+
+    /**
+     * @template T
+     *
+     * @param class-string<T> $valueClassName
+     * @param T $value
+     */
+    public function setPrivatePropertyOfClass(object $object, string $propertyName, mixed $value, string $valueClassName): void
+    {
+        if (! $value instanceof $valueClassName) {
+            throw new ShouldNotHappenException();
+        }
+
+        $this->setPrivateProperty($object, $propertyName, $value);
     }
 
     public function setPrivateProperty(object $object, string $propertyName, mixed $value): void
--- packages/package-builder/tests/Reflection/PrivatesAccessorTest.php
+++ packages/package-builder/tests/Reflection/PrivatesAccessorTest.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Symplify\PackageBuilder\Tests\Reflection;
 
 use PHPUnit\Framework\TestCase;
+use stdClass;
 use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
 use Symplify\PackageBuilder\Tests\Reflection\Source\SomeClassWithPrivateProperty;
 
 final class PrivatesAccessorTest extends TestCase
 {
-    public function test(): void
+    public function testGetterSetter(): void
     {
         $privatesAccessor = new PrivatesAccessor();
         $someClassWithPrivateProperty = new SomeClassWithPrivateProperty();
@@ -20,5 +21,22 @@ final class PrivatesAccessorTest extends TestCase
 
         $fetchedParentValue = $privatesAccessor->getPrivateProperty($someClassWithPrivateProperty, 'parentValue');
         $this->assertSame($someClassWithPrivateProperty->getParentValue(), $fetchedParentValue);
+
+        $privatesAccessor->setPrivateProperty($someClassWithPrivateProperty, 'value', 25);
+        $this->assertSame(25, $someClassWithPrivateProperty->getValue());
+    }
+
+    public function testGetterSetterTypesafe(): void
+    {
+        $privatesAccessor = new PrivatesAccessor();
+        $someClassWithPrivateProperty = new SomeClassWithPrivateProperty();
+
+        $newObject = new stdClass();
+        $this->assertNotSame($newObject, $someClassWithPrivateProperty->getObject());
+        $privatesAccessor->setPrivatePropertyOfClass($someClassWithPrivateProperty, 'object', $newObject, stdClass::class);
+        $this->assertSame($newObject, $someClassWithPrivateProperty->getObject());
+
+        $fetchedValue = $privatesAccessor->getPrivatePropertyOfClass($someClassWithPrivateProperty, 'object', stdClass::class);
+        $this->assertSame($someClassWithPrivateProperty->getObject(), $fetchedValue);
     }
 }
