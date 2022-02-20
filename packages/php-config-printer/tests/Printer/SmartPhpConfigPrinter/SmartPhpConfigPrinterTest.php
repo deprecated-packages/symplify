0b64f9be69b7fd7d530b82dd5517d2010321648c
Tomas Votruba <tomas.vot@gmail.com>
2022-02-20 02:09:46 +0000

    [SymfonyPHPConfig] Deprecate, as just 1 helper class (#3941)

--- packages/php-config-printer/tests/Printer/SmartPhpConfigPrinter/SmartPhpConfigPrinterTest.php
+++ packages/php-config-printer/tests/Printer/SmartPhpConfigPrinter/SmartPhpConfigPrinterTest.php
@@ -5,17 +5,12 @@ declare(strict_types=1);
 namespace Symplify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter;
 
 use Iterator;
-use PHPStan\Type\IntegerType;
-use PHPStan\Type\StringType;
-use PHPStan\Type\UnionType;
 use Symplify\PackageBuilder\Testing\AbstractKernelTestCase;
 use Symplify\PhpConfigPrinter\Printer\SmartPhpConfigPrinter;
 use Symplify\PhpConfigPrinter\Tests\HttpKernel\PhpConfigPrinterKernel;
 use Symplify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source\ClassWithConstants;
-use Symplify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source\ClassWithType;
 use Symplify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source\FirstClass;
 use Symplify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source\SecondClass;
-use Symplify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source\ValueObject\Simple;
 
 final class SmartPhpConfigPrinterTest extends AbstractKernelTestCase
 {
@@ -52,40 +47,5 @@ final class SmartPhpConfigPrinterTest extends AbstractKernelTestCase
                 ClassWithConstants::NUMERIC_CONFIG_KEY => 'a lot of numbers',
             ],
         ], __DIR__ . '/Fixture/expected_constant_file.php.inc'];
-
-        yield [[
-            SecondClass::class => [
-                'some_key' => new Simple('Steve'),
-            ],
-        ], __DIR__ . '/Fixture/expected_value_object_file.php.inc'];
-
-        yield [[
-            SecondClass::class => [
-                'some_key' => [new Simple('Paul')],
-            ],
-        ], __DIR__ . '/Fixture/expected_value_objects_file.php.inc'];
-
-        yield [[
-            SecondClass::class => [
-                'some_key' => [
-                    'inside_key' => new Simple('Paul'),
-                    'inside_key2' => new Simple('Paul 2'),
-                ],
-            ],
-        ], __DIR__ . '/Fixture/expected_value_objects_file2.php.inc'];
-
-        yield [[
-            SecondClass::class => [
-                'some_key' => [new ClassWithType(new StringType())],
-            ],
-        ], __DIR__ . '/Fixture/expected_value_nested_objects.php.inc'];
-
-        $unionType = new UnionType([new StringType(), new IntegerType()]);
-
-        yield [[
-            SecondClass::class => [
-                'some_key' => [new ClassWithType($unionType)],
-            ],
-        ], __DIR__ . '/Fixture/expected_value_nested_union_objects.php.inc'];
     }
 }
