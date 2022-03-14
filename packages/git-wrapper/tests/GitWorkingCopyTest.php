2759b008e031ae442a9c156094d0a1bd3a6d2d91
Richard van Baarsen <baarsen@pm.me>
2022-03-14 12:40:39 +0100

    Move PHPStan ignore statement

--- packages/git-wrapper/tests/GitWorkingCopyTest.php
+++ packages/git-wrapper/tests/GitWorkingCopyTest.php
@@ -470,7 +470,6 @@ CODE_SAMPLE;
         $gitWrapper->streamOutput(true);
         $git->status();
 
-        /* @phpstan-ignore-next-line */
         $invader = (fn() => $this->outputEventSubscriber);
 
         $this->assertInstanceOf(StreamOutputEventSubscriber::class, $invader->call($this->gitWrapper));
