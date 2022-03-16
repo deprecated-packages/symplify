108c7ec73c376852efcb51c9d4b01879c5211fe0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-03-16 18:29:23 +0700

    [ECS] Skip StandaloneLineConstructorParamFixer

--- ecs.php
+++ ecs.php
@@ -7,6 +7,7 @@ use SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\Annotation\DoctrineAnnotationNestedBracketsFixer;
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+use Symplify\CodingStandard\Fixer\Spacing\StandaloneLineConstructorParamFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
@@ -65,5 +66,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             // breaks interface contract
             __DIR__ . '/packages/config-transformer/src/DependencyInjection/Loader/IdAwareXmlFileLoader.php',
         ],
+        StandaloneLineConstructorParamFixer::class,
     ]);
 };
