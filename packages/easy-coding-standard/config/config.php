b173b21f0d8c3730ea676ead29d55b229c187332
Tomas Votruba <tomas.vot@gmail.com>
2022-04-14 19:03:31 +0200

    [ECS] Use single config file for context list of every used service

--- packages/easy-coding-standard/config/config.php
+++ packages/easy-coding-standard/config/config.php
@@ -2,14 +2,31 @@
 
 declare(strict_types=1);
 
+use PHP_CodeSniffer\Fixer;
+use PhpCsFixer\Differ\DifferInterface;
+use PhpCsFixer\Differ\UnifiedDiffer;
+use PhpCsFixer\WhitespacesFixerConfig;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Terminal;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\Application\Version\StaticVersionResolver;
+use Symplify\EasyCodingStandard\Caching\Cache;
+use Symplify\EasyCodingStandard\Caching\CacheFactory;
+use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
+use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyleFactory;
+use Symplify\EasyCodingStandard\FixerRunner\Application\FixerFileProcessor;
+use Symplify\EasyCodingStandard\FixerRunner\WhitespacesFixerConfigFactory;
 use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
+use Symplify\PackageBuilder\Yaml\ParametersMerger;
+use Symplify\SmartFileSystem\FileSystemFilter;
+use Symplify\SmartFileSystem\FileSystemGuard;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use Symplify\SmartFileSystem\Finder\SmartFinder;
+use Symplify\SmartFileSystem\SmartFileSystem;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/services.php');
-    $containerConfigurator->import(__DIR__ . '/packages.php');
-
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::INDENTATION, Option::INDENTATION_SPACES);
     $parameters->set(Option::LINE_ENDING, PHP_EOL);
@@ -36,4 +53,56 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::FILE_EXTENSIONS, ['php']);
 
     $parameters->set('env(TEST_SUFFIX)', '');
+
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Symplify\EasyCodingStandard\\', __DIR__ . '/../src')
+        ->exclude([
+            // only for "bin/ecs" file, where container does not exist yet
+            __DIR__ . '/../src/Config/ECSConfig.php',
+            __DIR__ . '/../src/DependencyInjection',
+            __DIR__ . '/../src/Kernel',
+            __DIR__ . '/../src/Exception',
+            __DIR__ . '/../src/ValueObject',
+            // for 3rd party tests
+            __DIR__ . '/../src/Testing',
+        ]);
+
+    $services->load('Symplify\EasyCodingStandard\\', __DIR__ . '/../packages')
+        ->exclude([__DIR__ . '/../packages/*/ValueObject/*']);
+
+    $services->set(Cache::class)
+        ->factory([service(CacheFactory::class), 'create']);
+
+    $services->set(Terminal::class);
+
+    $services->set(FileSystemGuard::class);
+    $services->set(FinderSanitizer::class);
+    $services->set(SmartFileSystem::class);
+    $services->set(SmartFinder::class);
+    $services->set(FileSystemFilter::class);
+
+    $services->set(SymfonyStyleFactory::class);
+    $services->set(SymfonyStyle::class)
+        ->factory([service(SymfonyStyleFactory::class), 'create']);
+
+    $services->set(ParametersMerger::class);
+
+    $services->set(EasyCodingStandardStyle::class)
+        ->factory([service(EasyCodingStandardStyleFactory::class), 'create']);
+
+    $services->set(WhitespacesFixerConfig::class)
+        ->factory([service(WhitespacesFixerConfigFactory::class), 'create']);
+
+    // code sniffer
+    $services->set(Fixer::class);
+
+    // fixer
+    $services->set(UnifiedDiffer::class);
+    $services->alias(DifferInterface::class, UnifiedDiffer::class);
+    $services->set(FixerFileProcessor::class);
 };
