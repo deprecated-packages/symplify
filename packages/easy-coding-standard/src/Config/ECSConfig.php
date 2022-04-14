5e7db16500edc20270dda8a47cdeb9c1c1c35d09
Tomas Votruba <tomas.vot@gmail.com>
2022-04-14 19:10:13 +0200

    add ECSConfig patch

--- packages/easy-coding-standard/src/Config/ECSConfig.php
+++ packages/easy-coding-standard/src/Config/ECSConfig.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Symplify\EasyCodingStandard\Config;
 
-final class ECSConfig
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+final class ECSConfig extends ContainerConfigurator
 {
 }
