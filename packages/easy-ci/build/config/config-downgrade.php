05442fdd9544e97a36f4fe2f2cc3d65e45049171
Tomas Votruba <tomas.vot@gmail.com>
2022-05-12 17:42:50 +0200

    fix cs

--- packages/easy-ci/build/config/config-downgrade.php
+++ packages/easy-ci/build/config/config-downgrade.php
@@ -8,9 +8,5 @@ use Rector\Set\ValueObject\DowngradeLevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
 
-    $rectorConfig->skip([
-        '*/Tests/*',
-        '*/tests/*',
-        __DIR__ . '/../../tests',
-    ]);
+    $rectorConfig->skip(['*/Tests/*', '*/tests/*', __DIR__ . '/../../tests']);
 };
