7c1f66f355a47af4cc0ed8d999a8ed157d54a081
Tomas Votruba <tomas.vot@gmail.com>
2022-05-12 17:15:27 +0200

    cleanup easy-ci dowgnrade config

--- packages/easy-ci/build/config/config-downgrade.php
+++ packages/easy-ci/build/config/config-downgrade.php
@@ -12,16 +12,5 @@ return static function (RectorConfig $rectorConfig): void {
         '*/Tests/*',
         '*/tests/*',
         __DIR__ . '/../../tests',
-        # missing "optional" dependency and never used here
-        '*/symfony/framework-bundle/KernelBrowser.php',
-        '*/symfony/http-kernel/HttpKernelBrowser.php',
-        '*/symfony/cache/*',
-        // fails on DOMCaster
-        '*/symfony/var-dumper/*',
-        '*/symfony/var-exporter/*',
-        '*/symfony/error-handler/*',
-        '*/symfony/event-dispatcher/*',
-        '*/symfony/event-dispatcher-contracts/*',
-        '*/symfony/http-foundation/*',
     ]);
 };
