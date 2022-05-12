d5a30b5dfeb526d317fa34588862f65c6eaa33c5
Tomas Votruba <tomas.vot@gmail.com>
2022-05-12 14:13:37 +0200

    bump ECS to 7.2

--- packages/easy-coding-standard/build/config/config-downgrade.php
+++ packages/easy-coding-standard/build/config/config-downgrade.php
@@ -3,21 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector;
 use Rector\DowngradePhp80\Rector\Class_\DowngradeAttributeToAnnotationRector;
 use Rector\DowngradePhp80\ValueObject\DowngradeAttributeToAnnotation;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
-use Symfony\Component\Config\Loader\Loader;
-use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Contracts\Service\Attribute\Required;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_71]);
-
-    $rectorConfig->ruleWithConfiguration(DowngradeParameterTypeWideningRector::class, [
-        LoaderInterface::class => ['load'],
-        Loader::class => ['import'],
-    ]);
+    $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
 
     $rectorConfig->ruleWithConfiguration(DowngradeAttributeToAnnotationRector::class, [
         new DowngradeAttributeToAnnotation(Required::class, 'required'),
