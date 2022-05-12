20388c2e870d94b94aa92e3c2826af61ff064410
Tomas Votruba <tomas.vot@gmail.com>
2022-05-12 17:54:40 +0200

    try skipping the array spread

--- packages/easy-ci/build/config/config-downgrade.php
+++ packages/easy-ci/build/config/config-downgrade.php
@@ -3,10 +3,17 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DowngradePhp81\Rector\Array_\DowngradeArraySpreadStringKeyRector;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
 
-    $rectorConfig->skip(['*/Tests/*', '*/tests/*', __DIR__ . '/../../tests']);
+    $rectorConfig->skip([
+        '*/Tests/*',
+        '*/tests/*',
+        __DIR__ . '/../../tests',
+        // only one should run, in case of PHP 7.2 the stronger one
+        DowngradeArraySpreadStringKeyRector::class,
+    ]);
 };
