19af540858177aa9e2a1c507a2442aed5c86a6b0
Tomas Votruba <tomas.vot@gmail.com>
2022-05-25 01:07:42 +0200

    mb config

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualConflictsReleaseWorker;
@@ -13,31 +13,29 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorke
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
+return static function (MBConfig $mbConfig): void {
+    $mbConfig->packageDirectories([__DIR__ . '/packages']);
 
-    $parameters->set(Option::PACKAGE_DIRECTORIES, [__DIR__ . '/packages']);
-
-    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
-    $parameters->set(Option::DATA_TO_REMOVE, [
-        'require' => [
-            # remove these to merge of packages' composer.json
-            'tracy/tracy' => '*',
-            'phpunit/phpunit' => '*',
-        ],
-        'minimum-stability' => 'dev',
-        'prefer-stable' => true,
-    ]);
-
-    $services = $containerConfigurator->services();
-
-    # release workers - in order to execute
-    $services->set(UpdateReplaceReleaseWorker::class);
-    $services->set(SetCurrentMutualConflictsReleaseWorker::class);
-    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-    $services->set(TagVersionReleaseWorker::class);
-    $services->set(PushTagReleaseWorker::class);
-    $services->set(SetNextMutualDependenciesReleaseWorker::class);
-    $services->set(UpdateBranchAliasReleaseWorker::class);
-    $services->set(PushNextDevReleaseWorker::class);
+//    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
+//    $parameters->set(Option::DATA_TO_REMOVE, [
+//        'require' => [
+//            # remove these to merge of packages' composer.json
+//            'tracy/tracy' => '*',
+//            'phpunit/phpunit' => '*',
+//        ],
+//        'minimum-stability' => 'dev',
+//        'prefer-stable' => true,
+//    ]);
+//
+//    $services = $mbConfig->services();
+//
+//    # release workers - in order to execute
+//    $services->set(UpdateReplaceReleaseWorker::class);
+//    $services->set(SetCurrentMutualConflictsReleaseWorker::class);
+//    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
+//    $services->set(TagVersionReleaseWorker::class);
+//    $services->set(PushTagReleaseWorker::class);
+//    $services->set(SetNextMutualDependenciesReleaseWorker::class);
+//    $services->set(UpdateBranchAliasReleaseWorker::class);
+//    $services->set(PushNextDevReleaseWorker::class);
 };
--- packages/monorepo-builder/src/Config/MBConfig.php
+++ packages/monorepo-builder/src/Config/MBConfig.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace Symplify\MonorepoBuilder\Config;
 
-final class MBConfig
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+use Webmozart\Assert\Assert;
+
+final class MBConfig extends ContainerConfigurator
 {
+    /**
+     * @param string[] $packageDirectories
+     */
+    public function packageDirectories(array $packageDirectories): void
+    {
+        Assert::allString($packageDirectories);
+        Assert::allFileExists($packageDirectories);
+
+        $parameters = $this->parameters();
+        $parameters->set(Option::PACKAGE_DIRECTORIES, $packageDirectories);
+    }
 }
