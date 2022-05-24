9f5c3d79edc2d3d169a8ccd777f5e76c529f0307
Tomas Votruba <tomas.vot@gmail.com>
2022-05-25 01:22:42 +0200

    make use of most monorepo-builder methods

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -11,31 +11,30 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesRele
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
-use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (MBConfig $mbConfig): void {
     $mbConfig->packageDirectories([__DIR__ . '/packages']);
+    $mbConfig->defaultBranch('main');
 
-//    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
-//    $parameters->set(Option::DATA_TO_REMOVE, [
-//        'require' => [
-//            # remove these to merge of packages' composer.json
-//            'tracy/tracy' => '*',
-//            'phpunit/phpunit' => '*',
-//        ],
-//        'minimum-stability' => 'dev',
-//        'prefer-stable' => true,
-//    ]);
-//
-//    $services = $mbConfig->services();
-//
-//    # release workers - in order to execute
-//    $services->set(UpdateReplaceReleaseWorker::class);
-//    $services->set(SetCurrentMutualConflictsReleaseWorker::class);
-//    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-//    $services->set(TagVersionReleaseWorker::class);
-//    $services->set(PushTagReleaseWorker::class);
-//    $services->set(SetNextMutualDependenciesReleaseWorker::class);
-//    $services->set(UpdateBranchAliasReleaseWorker::class);
-//    $services->set(PushNextDevReleaseWorker::class);
+    $mbConfig->dataToRemove([
+        'require' => [
+            # remove these to merge of packages' composer.json
+            'tracy/tracy' => '*',
+            'phpunit/phpunit' => '*',
+        ],
+        'minimum-stability' => 'dev',
+        'prefer-stable' => true,
+    ]);
+
+    $mbConfig->workers([
+        // release workers - in order to execute
+        UpdateReplaceReleaseWorker::class,
+        SetCurrentMutualConflictsReleaseWorker::class,
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
+        SetNextMutualDependenciesReleaseWorker::class,
+        UpdateBranchAliasReleaseWorker::class,
+        PushNextDevReleaseWorker::class,
+    ]);
 };
--- packages/monorepo-builder/src/Config/MBConfig.php
+++ packages/monorepo-builder/src/Config/MBConfig.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symplify\MonorepoBuilder\Config;
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 use Webmozart\Assert\Assert;
 
@@ -20,5 +21,37 @@ final class MBConfig extends ContainerConfigurator
 
         $parameters = $this->parameters();
         $parameters->set(Option::PACKAGE_DIRECTORIES, $packageDirectories);
+    }
+
+    public function defaultBranch(string $defaultBranch): void
+    {
+        $parameters = $this->parameters();
+        $parameters->set(Option::DEFAULT_BRANCH_NAME, $defaultBranch);
+    }
+
+    /**
+     * @param array<string, mixed> $dataToRemove
+     */
+    public function dataToRemove(array $dataToRemove): void
+    {
+        Assert::allString(array_keys($dataToRemove));
+
+        $parameters = $this->parameters();
+        $parameters->set(Option::DATA_TO_REMOVE, $dataToRemove);
+    }
+
+    /**
+     * @param array<class-string<ReleaseWorkerInterface>> $workerClasses
+     */
+    public function workers(array $workerClasses): void
+    {
+        Assert::allString($workerClasses);
+        Assert::allIsAOf($workerClasses, ReleaseWorkerInterface::class);
+
+        $services = $this->services();
+
+        foreach ($workerClasses as $workerClass) {
+            $services->set($workerClass);
+        }
     }
 }
