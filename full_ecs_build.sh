b32e06f94192108c8c19e3001222987aa69b7a0f
Tomas Votruba <tomas.vot@gmail.com>
2022-05-29 19:10:13 +0200

    no limit

--- full_ecs_build.sh
+++ full_ecs_build.sh
@@ -11,7 +11,7 @@ cp -r packages/easy-coding-standard/. ecs-build
 rm -rf ecs-build/tests ecs-build/packages-tests
 
 # downgrade
-vendor/bin/rector process ecs-build --config packages/easy-coding-standard/build/config/config-downgrade.php -a ecs-build/vendor/autoload.php --ansi --memory-limit -1
+vendor/bin/rector process ecs-build --config packages/easy-coding-standard/build/config/config-downgrade.php -a ecs-build/vendor/autoload.php --ansi
 
 # prefix
 sh packages/easy-coding-standard/build/build-ecs-scoped.sh ecs-build ecs-prefixed-downgraded
