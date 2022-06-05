aaefeaf6f85bcef891f8a714fa8580a751043434
Tomas Votruba <tomas.vot@gmail.com>
2022-06-05 19:21:16 +0200

    [PHPStanRules] Add ValueObjectDestructRule

---
+++ packages/phpstan-rules/tests/Rules/Complexity/ValueObjectDestructRule/Source/PossiblyService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\Complexity\ValueObjectDestructRule\Source;
+
+final class PossiblyService
+{
+    public function run(): int
+    {
+        return 1000;
+    }
+}
