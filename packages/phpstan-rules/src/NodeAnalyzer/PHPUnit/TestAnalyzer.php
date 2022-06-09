68f8fb042f3195399914089de17d6b0d8a7d8bd4
Tomas Votruba <tomas.vot@gmail.com>
2022-06-09 10:13:37 +0200

    [PHPStanRules] Remove ForbiddenSameNamedAssignRule as very unreliable due to various scoping conflicts (#4141)

--- packages/phpstan-rules/src/NodeAnalyzer/PHPUnit/TestAnalyzer.php
+++ packages/phpstan-rules/src/NodeAnalyzer/PHPUnit/TestAnalyzer.php
@@ -4,21 +4,14 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\NodeAnalyzer\PHPUnit;
 
-use PhpParser\Node;
 use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Stmt\ClassMethod;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\ClassReflection;
 use PHPUnit\Framework\TestCase;
-use Symplify\Astral\NodeFinder\SimpleNodeFinder;
 
 final class TestAnalyzer
 {
-    public function __construct(
-        private SimpleNodeFinder $simpleNodeFinder
-    ) {
-    }
-
     public function isTestClassMethod(Scope $scope, FunctionLike $functionLike): bool
     {
         $classReflection = $scope->getClassReflection();
@@ -27,26 +20,6 @@ final class TestAnalyzer
         }
 
         return $this->isPublicClassMethod($functionLike, $classReflection);
-    }
-
-    public function isInTestClassMethod(Scope $scope, Node $node): bool
-    {
-        $classMethod = $this->simpleNodeFinder->findFirstParentByType($node, ClassMethod::class);
-        if (! $classMethod instanceof ClassMethod) {
-            return false;
-        }
-
-        return $this->isTestClassMethod($scope, $classMethod);
-    }
-
-    public function isInTest(Scope $scope): bool
-    {
-        $classReflection = $scope->getClassReflection();
-        if (! $classReflection instanceof ClassReflection) {
-            return false;
-        }
-
-        return $classReflection->isSubclassOf('PHPUnit\Framework\TestCase');
     }
 
     private function isPublicClassMethod(FunctionLike $functionLike, ClassReflection $classReflection): bool
