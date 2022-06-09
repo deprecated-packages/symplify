0571cfdc23305f996b51cb72ecbfa8847c28a434
Tomas Votruba <tomas.vot@gmail.com>
2022-06-09 09:13:22 +0200

    [PHPStanRules] Remove parent from ForbiddenProtectedPropertyRule

--- packages/phpstan-rules/src/NodeAnalyzer/ProtectedAnalyzer.php
+++ packages/phpstan-rules/src/NodeAnalyzer/ProtectedAnalyzer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\NodeAnalyzer;
 
+use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Property;
 
 final class ProtectedAnalyzer
@@ -14,13 +15,13 @@ final class ProtectedAnalyzer
     ) {
     }
 
-    public function isProtectedPropertyOrClassConstAllowed(Property $property): bool
+    public function isProtectedPropertyOrClassConstAllowed(Property $property, Class_ $class): bool
     {
-        if ($this->dependencyNodeAnalyzer->isInsideAbstractClassAndPassedAsDependency($property)) {
+        if ($this->dependencyNodeAnalyzer->isInsideAbstractClassAndPassedAsDependency($property, $class)) {
             return true;
         }
 
-        if ($this->dependencyNodeAnalyzer->isInsideClassAndAutowiredMethod($property)) {
+        if ($this->dependencyNodeAnalyzer->isInsideClassAndAutowiredMethod($property, $class)) {
             return true;
         }
 
