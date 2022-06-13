b7e7f0923bccd7ddc57c87d21edfdf4b3e52abe9
Markus Staab <m.staab@complex-it.de>
2022-06-13 18:53:13 +0200

    use plain "parent"

--- packages/easy-ci/packages/ActiveClass/NodeVisitor/UsedClassNodeVisitor.php
+++ packages/easy-ci/packages/ActiveClass/NodeVisitor/UsedClassNodeVisitor.php
@@ -60,7 +60,7 @@ final class UsedClassNodeVisitor extends NodeVisitorAbstract
     private function isNonNameNode(Name $name): bool
     {
         // skip nodes that are not part of class names
-        $parent = $name->getAttribute(AttributeKey::PARENT);
+        $parent = $name->getAttribute("parent");
         if ($parent instanceof Namespace_) {
             return true;
         }
