dff88cb86edaae0b0d3b7df2b12cac871f767ad6
Tomas Votruba <tomas.vot@gmail.com>
2022-06-13 14:36:59 +0200

    fix T_READONLY, T_ENUM token emulation type

--- packages/coding-standard/tests/bootstrap.php
+++ packages/coding-standard/tests/bootstrap.php
@@ -8,9 +8,19 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
 
 // initalize token with INT type, otherwise php-cs-fixer and php-parser breaks
+// initalize token with INT type, otherwise php-cs-fixer and php-parser breaks
 if (! defined('T_MATCH')) {
-    define('T_MATCH', 5000);
+    define('T_MATCH', 5005);
 }
+
+if (! defined('T_READONLY')) {
+    define('T_READONLY', 5010);
+}
+
+if (! defined('T_ENUM')) {
+    define('T_ENUM', 5015);
+}
+
 
 // required for PHP_CodeSniffer in packages/EasyCodingStandard/tests/*
 if (! defined('PHP_CODESNIFFER_VERBOSITY')) {
