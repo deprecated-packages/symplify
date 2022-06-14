0ffd944b6c55ac98e89449bb01f868351467969a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-14 19:51:35 +0700

    implemented :tada:

--- packages/php-config-printer/src/NodeFactory/ContainerConfiguratorReturnClosureFactory.php
+++ packages/php-config-printer/src/NodeFactory/ContainerConfiguratorReturnClosureFactory.php
@@ -6,6 +6,7 @@ namespace Symplify\PhpConfigPrinter\NodeFactory;
 
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\BinaryOp\Identical;
 use PhpParser\Node\Expr\MethodCall;
@@ -15,13 +16,12 @@ use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\Return_;
+use Symplify\Astral\Exception\ShouldNotHappenException;
 use Symplify\PhpConfigPrinter\Contract\CaseConverterInterface;
 use Symplify\PhpConfigPrinter\PhpParser\NodeFactory\ConfiguratorClosureNodeFactory;
 use Symplify\PhpConfigPrinter\ValueObject\VariableMethodName;
 use Symplify\PhpConfigPrinter\ValueObject\VariableName;
 use Symplify\PhpConfigPrinter\ValueObject\YamlKey;
-
-use function RectorPrefix20220609\dump_node;
 
 final class ContainerConfiguratorReturnClosureFactory
 {
@@ -94,36 +94,51 @@ final class ContainerConfiguratorReturnClosureFactory
                     continue;
                 }
 
-                $explodeAt = explode('@', $key);
-                if (str_starts_with($key, 'when@') && count($explodeAt) === 2) {
-                    $containerConfigurator = new Variable(VariableName::CONTAINER_CONFIGURATOR);
-                    $identical = new Identical(
-                        new String_($explodeAt[1]),
-                        new MethodCall(
-                            $containerConfigurator,
-                            'env'
-                        )
-                    );
-
-                    $args = $expression->expr->getArgs();
-                    $newExpression = new Expression(
-                        new MethodCall(
-                            $containerConfigurator,
-                            'extension',
-                            [
-                                new String_('framework')
-                            ]
-                        )
-                    );
-                    $expression = new If_($identical);
-                    $expression->stmts = [$newExpression];
-                }
-
-                $nodes[] = $expression;
+                $nodes[] = $this->resolveExpressionWhenAtEnv($expression, $key);
             }
         }
 
         return $nodes;
+    }
+
+    private function resolveExpressionWhenAtEnv(Expression $expression, string $key): Expression|If_
+    {
+        $explodeAt = explode('@', $key);
+        if (str_starts_with($key, 'when@') && count($explodeAt) === 2) {
+            $containerConfigurator = new Variable(VariableName::CONTAINER_CONFIGURATOR);
+            $identical = new Identical(
+                new String_($explodeAt[1]),
+                new MethodCall($containerConfigurator, 'env')
+            );
+
+            $expr = $expression->expr;
+            if (! $expr instanceof MethodCall) {
+                throw new ShouldNotHappenException();
+            }
+
+            $args = $expr->getArgs();
+
+            if (! isset($args[1]) || ! $args[1]->value instanceof Array_ || ! isset($args[1]->value->items[0]) || ! $args[1]->value->items[0] instanceof ArrayItem) {
+                throw new ShouldNotHappenException();
+            }
+
+            $newExpression = new Expression(
+                new MethodCall(
+                    $containerConfigurator,
+                    'extension',
+                    [
+                        new Arg(new String_('framework')),
+                        new Arg($args[1]->value->items[0]->value)
+                    ]
+                )
+            );
+            $if = new If_($identical);
+            $if->stmts = [$newExpression];
+
+            return $if;
+        }
+
+        return $expression;
     }
 
     /**
@@ -140,8 +155,8 @@ final class ContainerConfiguratorReturnClosureFactory
     }
 
     /**
-     * @param Expression[] $nodes
-     * @return Expression[]
+     * @param Expression[]|If_[] $nodes
+     * @return Expression[]|If_[]
      */
     private function createInitializeNode(string $key, array $nodes): array
     {
