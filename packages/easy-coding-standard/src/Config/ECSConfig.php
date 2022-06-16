0002e6ef030e6a888966f06f960e6d8c207edc07
Aleksey Polyvanyi <aleksey.polyvanyi@eonx.com>
2022-06-16 21:01:40 +0200

    -deprecate consts -add all methods to ECSConfig

--- packages/easy-coding-standard/src/Config/ECSConfig.php
+++ packages/easy-coding-standard/src/Config/ECSConfig.php
@@ -21,36 +21,42 @@ final class ECSConfig extends ContainerConfigurator
     /**
      * @param string[] $paths
      */
-    public function paths(array $paths): void
+    public function paths(array $paths): self
     {
         Assert::allString($paths);
 
         $parameters = $this->parameters();
         $parameters->set(Option::PATHS, $paths);
+
+        return $this;
     }
 
     /**
      * @param mixed[] $skips
      */
-    public function skip(array $skips): void
+    public function skip(array $skips): self
     {
         $parameters = $this->parameters();
         $parameters->set(Option::SKIP, $skips);
+
+        return $this;
     }
 
     /**
      * @param mixed[] $onlys
      */
-    public function only(array $onlys): void
+    public function only(array $onlys): self
     {
         $parameters = $this->parameters();
         $parameters->set(Option::ONLY, $onlys);
+
+        return $this;
     }
 
     /**
      * @param string[] $sets
      */
-    public function sets(array $sets): void
+    public function sets(array $sets): self
     {
         Assert::allString($sets);
         Assert::allFileExists($sets);
@@ -58,37 +64,43 @@ final class ECSConfig extends ContainerConfigurator
         foreach ($sets as $set) {
             $this->import($set);
         }
+
+        return $this;
     }
 
     /**
      * @param class-string<Sniff|FixerInterface> $checkerClass
      */
-    public function rule(string $checkerClass): void
+    public function rule(string $checkerClass): self
     {
         $this->isCheckerClass($checkerClass);
 
         $services = $this->services();
         $services->set($checkerClass)
             ->public();
+
+        return $this;
     }
 
     /**
      * @param array<class-string<Sniff|FixerInterface>> $checkerClasses
      */
-    public function rules(array $checkerClasses): void
+    public function rules(array $checkerClasses): self
     {
         $this->ensureCheckerClassesAreUnique($checkerClasses);
 
         foreach ($checkerClasses as $checkerClass) {
             $this->rule($checkerClass);
         }
+
+        return $this;
     }
 
     /**
      * @param class-string $checkerClass
      * @param mixed[] $configuration
      */
-    public function ruleWithConfiguration(string $checkerClass, array $configuration): void
+    public function ruleWithConfiguration(string $checkerClass, array $configuration): self
     {
         $this->isCheckerClass($checkerClass);
 
@@ -108,27 +120,104 @@ final class ECSConfig extends ContainerConfigurator
                 $service->property($propertyName, $value);
             }
         }
+
+        return $this;
     }
 
     /**
      * @param Option::INDENTATION_* $indentation
      */
-    public function indentation(string $indentation): void
+    public function indentation(string $indentation): self
     {
         $parameters = $this->parameters();
         $parameters->set(Option::INDENTATION, $indentation);
+
+        return $this;
     }
 
-    public function lineEnding(string $lineEnding): void
+    public function lineEnding(string $lineEnding): self
     {
         $parameters = $this->parameters();
         $parameters->set(Option::LINE_ENDING, $lineEnding);
+
+        return $this;
     }
 
-    public function parallel(): void
+    public function cacheDirectory(string $cacheDirectory): self
+    {
+        $parameters = $this->parameters();
+        $parameters->set(Option::CACHE_DIRECTORY, $cacheDirectory);
+
+        return $this;
+    }
+
+    public function cacheNamespace(string $cacheNamespace): self
+    {
+        $parameters = $this->parameters();
+        $parameters->set(Option::CACHE_NAMESPACE, $cacheNamespace);
+
+        return $this;
+    }
+
+    /**
+     * @param string[] $fileExtensions
+     */
+    public function fileExtensions(array $fileExtensions): self
+    {
+        Assert::allString($fileExtensions);
+
+        $parameters = $this->parameters();
+        $parameters->set(Option::FILE_EXTENSIONS, $fileExtensions);
+
+        return $this;
+    }
+
+    public function parallel(): self
     {
         $parameters = $this->parameters();
         $parameters->set(Option::PARALLEL, true);
+
+        return $this;
+    }
+
+    public function parallelJobSize(int $parallelJobSize): self
+    {
+        $parameters = $this->parameters();
+        $parameters->set(Option::PARALLEL_JOB_SIZE, $parallelJobSize);
+
+        return $this;
+    }
+
+    public function parallelPort(string $parallelPort): self
+    {
+        $parameters = $this->parameters();
+        $parameters->set(Option::PARALLEL_PORT, $parallelPort);
+
+        return $this;
+    }
+
+    public function parallelIdentifier(string $parallelIdentifier): self
+    {
+        $parameters = $this->parameters();
+        $parameters->set(Option::PARALLEL_IDENTIFIER, $parallelIdentifier);
+
+        return $this;
+    }
+
+    public function parallelMaxNumberOfProcesses(int $parallelMaxNumberOfProcesses): self
+    {
+        $parameters = $this->parameters();
+        $parameters->set(Option::PARALLEL_MAX_NUMBER_OF_PROCESSES, $parallelMaxNumberOfProcesses);
+
+        return $this;
+    }
+
+    public function parallelTimeoutInSeconds(int $parallelTimeoutInSeconds): self
+    {
+        $parameters = $this->parameters();
+        $parameters->set(Option::PARALLEL_TIMEOUT_IN_SECONDS, $parallelTimeoutInSeconds);
+
+        return $this;
     }
 
     /**
--- packages/easy-coding-standard/src/ValueObject/Option.php
+++ packages/easy-coding-standard/src/ValueObject/Option.php
@@ -33,13 +33,16 @@ final class Option
 
     /**
      * @api
+     * @deprecated Use @see \Symplify\EasyCodingStandard\Config\ECSConfig::skip()
      * @var string
      */
     public const SKIP = 'skip';
 
     /**
-     * @api
      * Alias for SOURCE
+     *
+     * @api
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::paths()
      * @var string
      */
     public const PATHS = self::SOURCE;
@@ -51,31 +54,37 @@ final class Option
 
     /**
      * @api
+     * @deprecated Use @see \Symplify\EasyCodingStandard\Config\ECSConfig::only()
      * @var string
      */
     public const ONLY = 'only';
 
     /**
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::cacheDirectory()
      * @var string
      */
     public const CACHE_DIRECTORY = 'cache_directory';
 
     /**
+     * @deprecated Use @see \Symplify\EasyCodingStandard\Config\ECSConfig::lineEnding()
      * @var string
      */
     public const LINE_ENDING = 'line_ending';
 
     /**
+     * @deprecated Use @see \Symplify\EasyCodingStandard\Config\ECSConfig::indentation()
      * @var string
      */
     public const INDENTATION = 'indentation';
 
     /**
+     * @deprecated Use @see \Symplify\EasyCodingStandard\Config\ECSConfig::cacheNamespace()
      * @var string
      */
     public const CACHE_NAMESPACE = 'cache_namespace';
 
     /**
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::fileExtensions()
      * @var string
      */
     public const FILE_EXTENSIONS = 'file_extensions';
@@ -102,6 +111,7 @@ final class Option
     public const DEBUG = 'debug';
 
     /**
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::parallel()
      * @var string
      */
     public const PARALLEL = 'parallel';
@@ -109,29 +119,28 @@ final class Option
     /**
      * @var string
      */
-    public const SYSTEM_ERROR_COUNT_LIMIT = 'system-error-count-limit';
-
-    /**
-     * @var string
-     */
     public const CONFIG = 'config';
 
     /**
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::parallelJobSize()
      * @var string
      */
     public const PARALLEL_JOB_SIZE = 'parallel_job_size';
 
     /**
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::parallelPort()
      * @var string
      */
     public const PARALLEL_PORT = 'port';
 
     /**
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::parallelIdentifier()
      * @var string
      */
     public const PARALLEL_IDENTIFIER = 'identifier';
 
     /**
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::parallelMaxNumberOfProcesses()
      * @var string
      */
     public const PARALLEL_MAX_NUMBER_OF_PROCESSES = 'max-number-of-processes';
@@ -142,6 +151,7 @@ final class Option
     public const MEMORY_LIMIT = 'memory-limit';
 
     /**
+     * @see \Symplify\EasyCodingStandard\Config\ECSConfig::parallelTimeoutInSeconds()
      * @var string
      */
     public const PARALLEL_TIMEOUT_IN_SECONDS = 'parallel-timeout-in-seconds';
