485b7bac97e0d8fe7e9d0a572f3b01e23c7f6f97
Tomas Votruba <tomas.vot@gmail.com>
2022-06-30 15:18:41 +0200

    [PHPStanRules] Skip class api doc

---
+++ packages/phpstan-rules/tests/Rules/DeadCode/UnusedPublicClassConstRule/Fixture/SkipApiClassPublicConstant.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\DeadCode\UnusedPublicClassConstRule\Fixture;
+
+/**
+ * @api
+ */
+final class SkipApiClassPublicConstant
+{
+    public const PATHS = 'paths';
+}
