78556a7862b7c5d9637cb2b725e20a71376f9b54
Tomas Votruba <tomas.vot@gmail.com>
2022-07-04 14:27:16 +0200

    [PHPStanRules] Remove ClassReferencePhpDocNodeVisitor and ClassReferencePhpDocNodeTraverser (#4196)

--- packages/phpstan-rules/src/PhpDoc/ClassReferencePhpDocNodeVisitor.php
+++ packages/phpstan-rules/src/PhpDoc/ClassReferencePhpDocNodeVisitor.php
@@ -5,35 +5,14 @@ declare(strict_types=1);
 namespace Symplify\PHPStanRules\PhpDoc;
 
 use Nette\Utils\Reflection;
-use Nette\Utils\Strings;
 use PHPStan\PhpDocParser\Ast\Node;
-use PHPStan\PhpDocParser\Ast\PhpDoc\GenericTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
 use PHPStan\Reflection\ReflectionProvider;
 use Symplify\Astral\PhpDocParser\PhpDocNodeVisitor\AbstractPhpDocNodeVisitor;
-use Symplify\Astral\ValueObject\AttributeKey;
 use Symplify\PHPStanRules\Exception\ShouldNotHappenException;
-use Symplify\PHPStanRules\ValueObject\ClassConstantReference;
-use Symplify\PHPStanRules\ValueObject\MethodCallReference;
 
 final class ClassReferencePhpDocNodeVisitor extends AbstractPhpDocNodeVisitor
 {
-    /**
-     * @var string
-     */
-    private const CLASS_SNIPPET_PART = 'class_snippet';
-
-    /**
-     * @var string
-     */
-    private const REFERENCE_PART = 'reference';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/jPevTN/1
-     */
-    private const PARTIAL_CLASS_REFERENCE_REGEX = '#(?<' . self::CLASS_SNIPPET_PART . '>[A-Za-z_\\\\0-9]+)::(?<' . self::REFERENCE_PART . '>class|[A-Za-z_0-9]+(\((.*?)?\))?)#';
-
     private ?string $className = null;
 
     public function __construct(
@@ -50,10 +29,6 @@ final class ClassReferencePhpDocNodeVisitor extends AbstractPhpDocNodeVisitor
     {
         if ($node instanceof PhpDocTagNode) {
             $this->processPhpDocTagNode($node);
-        }
-
-        if ($node instanceof GenericTagValueNode) {
-            $this->processGenericTagValueNode($node);
         }
 
         return $node;
@@ -87,52 +62,5 @@ final class ClassReferencePhpDocNodeVisitor extends AbstractPhpDocNodeVisitor
         if ($resolvedFullyQualifiedName) {
             $phpDocTagNode->name = $resolvedFullyQualifiedName;
         }
-    }
-
-    private function processGenericTagValueNode(GenericTagValueNode $genericTagValueNode): void
-    {
-        if ($this->className === null) {
-            throw new ShouldNotHappenException();
-        }
-
-        $matches = Strings::matchAll($genericTagValueNode->value, self::PARTIAL_CLASS_REFERENCE_REGEX);
-
-        $resolveFullyQualifiedNames = [];
-        $referencedClassConstants = [];
-        $referencedMethodCalls = [];
-
-        foreach ($matches as $match) {
-            $resolveFullyQualifiedName = $this->resolveShortNamesToFullyQualified(
-                $match[self::CLASS_SNIPPET_PART],
-                $this->className
-            );
-
-            if ($resolveFullyQualifiedName === null) {
-                continue;
-            }
-
-            $referencePart = $match[self::REFERENCE_PART];
-            if ($referencePart === 'class') {
-                $resolveFullyQualifiedNames[] = $resolveFullyQualifiedName;
-                continue;
-            }
-
-            // method call
-            if (\str_contains($referencePart, '(')) {
-                $referencePart = trim($referencePart, '()');
-                $referencedMethodCalls[] = new MethodCallReference($resolveFullyQualifiedName, $referencePart);
-                continue;
-            }
-
-            // constant reference
-            $referencedClassConstants[] = new ClassConstantReference(
-                $resolveFullyQualifiedName,
-                $match[self::REFERENCE_PART]
-            );
-        }
-
-        $genericTagValueNode->setAttribute(AttributeKey::REFERENCED_CLASSES, $resolveFullyQualifiedNames);
-        $genericTagValueNode->setAttribute(AttributeKey::REFERENCED_CLASS_CONSTANTS, $referencedClassConstants);
-        $genericTagValueNode->setAttribute(AttributeKey::REFERENCED_METHOD_CALLS, $referencedMethodCalls);
     }
 }
