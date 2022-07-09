b70eb2e1cdc9f2fa3f7a7f44f773aaa6fd604448
Jan-Marten de Boer <janmarten@elgentos.nl>
2022-07-09 13:03:33 +0200

    Implement test fixtures

--- packages/monorepo-builder/packages-tests/Merge/ComposerKeyMerger/MinimalStabilityKeyMergerTest.php
+++ packages/monorepo-builder/packages-tests/Merge/ComposerKeyMerger/MinimalStabilityKeyMergerTest.php
@@ -4,73 +4,41 @@ declare(strict_types=1);
 
 namespace Symplify\MonorepoBuilder\Tests\Merge\ComposerKeyMerger;
 
-use PHPUnit\Framework\TestCase;
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
+use Iterator;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
 use Symplify\MonorepoBuilder\Merge\ComposerKeyMerger\MinimalStabilityKeyMerger;
+use Symplify\MonorepoBuilder\Tests\Merge\ComposerJsonDecorator\AbstractComposerJsonDecoratorTest;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 /**
  * @coversDefaultClass \Symplify\MonorepoBuilder\Merge\ComposerKeyMerger\MinimalStabilityKeyMerger
  */
-final class MinimalStabilityKeyMergerTest extends TestCase
+final class MinimalStabilityKeyMergerTest extends AbstractComposerJsonDecoratorTest
 {
     /**
-     * @dataProvider mergeProvider
+     * @dataProvider provideData
      * @covers ::merge
      */
-    public function testMerge(
-        ComposerJson $mainComposerJson,
-        ComposerJson $newComposerJson,
-        ?string $expected
-    ): void {
-        $subject = new MinimalStabilityKeyMerger();
-        $subject->merge($mainComposerJson, $newComposerJson);
-        self::assertEquals(
-            $expected,
-            $mainComposerJson->getMinimumStability(),
-            'Minimum stability of main Composer JSON does not match expected stability.'
-        );
-    }
-
-    private static function createPackage(
-        ?string $minimumStability
-    ): ComposerJson {
-        $package = new ComposerJson();
-
-        if ($minimumStability !== null) {
-            $package->setMinimumStability($minimumStability);
-        }
-
-        return $package;
-    }
-
-    public function mergeProvider(): array
+    public function testFixture(SmartFileInfo $fixtureFileInfo): void
     {
-        return [
-            'Neither has minimum stability' => [
-                'mainComposerJson' => self::createPackage(null),
-                'newComposerJson' => self::createPackage(null),
-                'expected' => null
-            ],
-            'Only new package has minimum stability' => [
-                'mainComposerJson' => self::createPackage(null),
-                'newComposerJson' => self::createPackage('rc'),
-                'expected' => 'rc'
-            ],
-            'Only main package has minimum stability' => [
-                'mainComposerJson' => self::createPackage('rc'),
-                'newComposerJson' => self::createPackage(null),
-                'expected' => 'rc'
-            ],
-            'Wider stability' => [
-                'mainComposerJson' => self::createPackage('alpha'),
-                'newComposerJson' => self::createPackage('beta'),
-                'expected' => 'alpha'
-            ],
-            'Narrower stability' => [
-                'mainComposerJson' => self::createPackage('beta'),
-                'newComposerJson' => self::createPackage('alpha'),
-                'expected' => 'alpha'
-            ]
-        ];
+        $trioContent = $this->trioFixtureSplitter->splitFileInfo($fixtureFileInfo);
+        $mainComposerJson = $this->createComposerJson($trioContent->getFirstValue());
+        $packageComposerJson = $this->createComposerJson($trioContent->getSecondValue());
+
+        $subject = new MinimalStabilityKeyMerger();
+        $subject->merge($mainComposerJson, $packageComposerJson);
+
+        $this->assertComposerJsonEquals($trioContent->getExpectedResult(), $mainComposerJson);
+    }
+
+    /**
+     * @return Iterator<mixed, SmartFileInfo>
+     */
+    public function provideData(): Iterator
+    {
+        return StaticFixtureFinder::yieldDirectoryExclusively(
+            __DIR__ . '/Fixture/MinimalStability',
+            '*.json'
+        );
     }
 }
