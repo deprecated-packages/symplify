e3d1d4e2f5d7fdf397b08e8fa5f62158925b321e
Tomas Votruba <tomas.vot@gmail.com>
2022-07-18 17:42:45 +0200

    [PHPStanRules] Add NoMissingAssingNoVoidMethodCallRule

---
+++ packages/phpstan-rules/src/Rules/Explicit/NoMissingAssingNoVoidMethodCallRule.php
@@ -0,0 +1,140 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules\Explicit;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\NodeTraverser;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Rules\Rule;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PHPStan\Type\VoidType;
+use Symplify\PHPStanRules\NodeAnalyzer\MethodCall\AllowedChainCallSkipper;
+use Symplify\RuleDocGenerator\Contract\DocumentedRuleInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Symplify\PHPStanRules\Tests\Rules\Explicit\NoMissingAssingNoVoidMethodCallRule\NoMissingAssingNoVoidMethodCallRuleTest
+ *
+ * @implements Rule<Expression>
+ */
+final class NoMissingAssingNoVoidMethodCallRule implements Rule, DocumentedRuleInterface
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Method call return value that should be used, but is not';
+
+    /**
+     * @var string[]
+     */
+    private const SKIPPED_TYPES = [
+        NodeTraverser::class,
+        \Symfony\Component\Finder\Finder::class,
+        'Symfony\Component\DependencyInjection\Loader\Configurator\AbstractConfigurator',
+    ];
+
+    public function __construct(
+        private AllowedChainCallSkipper $allowedChainCallSkipper,
+    ) {
+    }
+
+    /**
+     * @return class-string<Node>
+     */
+    public function getNodeType(): string
+    {
+        return Expression::class;
+    }
+
+    /**
+     * @param Expression $node
+     * @return mixed[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (! $node->expr instanceof MethodCall) {
+            return [];
+        }
+
+        $methodCall = $node->expr;
+        $methodCallReturnType = $scope->getType($methodCall);
+
+        if ($methodCallReturnType instanceof VoidType) {
+            return [];
+        }
+
+        if ($this->isFluentMethodCall($methodCallReturnType, $scope, $methodCall)) {
+            return [];
+        }
+
+        if ($methodCallReturnType instanceof MixedType) {
+            return [];
+        }
+
+        return [self::ERROR_MESSAGE];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $this->getResult();
+    }
+
+    private function getResult()
+    {
+        return [];
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+final class SomeClass
+{
+    public function run()
+    {
+        return $this->getResult();
+    }
+
+    private function getResult()
+    {
+        return [];
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    private function isFluentMethodCall(Type $methodCallReturnType, Scope $scope, MethodCall $methodCall): bool
+    {
+        if ($this->allowedChainCallSkipper->isAllowedFluentMethodCall($scope, $methodCall, self::SKIPPED_TYPES)) {
+            return true;
+        }
+
+        // 2. filter skipped call return types
+        if ($methodCallReturnType instanceof ObjectType) {
+
+            // 3. skip self static call
+            $currentClassReflection = $scope->getClassReflection();
+            if ($currentClassReflection instanceof ClassReflection) {
+                return $currentClassReflection->getName() === $methodCallReturnType->getClassName();
+            }
+        }
+
+        return false;
+    }
+}
---
+++ packages/phpstan-rules/tests/Rules/Explicit/NoMissingAssingNoVoidMethodCallRule/NoMissingAssingNoVoidMethodCallRuleTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\Explicit\NoMissingAssingNoVoidMethodCallRule;
+
+use Iterator;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use Symplify\PHPStanRules\Rules\Explicit\NoMissingAssingNoVoidMethodCallRule;
+
+/**
+ * @extends RuleTestCase<NoMissingAssingNoVoidMethodCallRule>
+ */
+final class NoMissingAssingNoVoidMethodCallRuleTest extends RuleTestCase
+{
+    /**
+     * @dataProvider provideData()
+     * @param mixed[] $expectedErrorsWithLines
+     */
+    public function testRule(string $filePath, array $expectedErrorsWithLines): void
+    {
+        $this->analyse([$filePath], $expectedErrorsWithLines);
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/ReturnedNoVoid.php', [[NoMissingAssingNoVoidMethodCallRule::ERROR_MESSAGE, 11]]];
+
+        yield [__DIR__ . '/Fixture/SkipReturnedNoVoid.php', []];
+        yield [__DIR__ . '/Fixture/SkipFluentOutsideOnPurpose.php', []];
+        yield [__DIR__ . '/Fixture/SkipSymfonyContainerConfigurator.php', []];
+        yield [__DIR__ . '/Fixture/SkipDefaultSymfonyAutowire.php', []];
+        yield [__DIR__ . '/Fixture/SkipNodeTraverser.php', []];
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [__DIR__ . '/config/configured_rule.neon'];
+    }
+
+    protected function getRule(): Rule
+    {
+        return self::getContainer()->getByType(NoMissingAssingNoVoidMethodCallRule::class);
+    }
+}
