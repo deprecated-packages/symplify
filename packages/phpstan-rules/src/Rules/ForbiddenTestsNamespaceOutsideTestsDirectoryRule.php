c39bf7206c7ce5a6e33279be994171505775daf6
Tomas Votruba <tomas.vot@gmail.com>
2022-08-08 00:44:38 +0200

    [PHPStanRules] Remove NoNetteTemplateVariableReadRule, way too specific

--- packages/phpstan-rules/src/Rules/ForbiddenTestsNamespaceOutsideTestsDirectoryRule.php
+++ packages/phpstan-rules/src/Rules/ForbiddenTestsNamespaceOutsideTestsDirectoryRule.php
@@ -55,11 +55,7 @@ final class ForbiddenTestsNamespaceOutsideTestsDirectoryRule implements Rule, Do
         }
 
         $matches = Strings::match($node->name->toString(), Regex::TESTS_PART_REGEX);
-<<<<<<< HEAD
         if ($matches === null) {
-=======
-        if ($matches !== null) {
->>>>>>> move NetteTypeAnalyzer
             return [];
         }
 
