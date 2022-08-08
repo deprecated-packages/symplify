966686cf462b8b56b5f4c529cc4d683e68b5c950
Tomas Votruba <tomas.vot@gmail.com>
2022-08-08 23:00:55 +0200

    misc

--- packages/php-config-printer/src/NodeFinder/TypeAwareNodeFinder.php
+++ packages/php-config-printer/src/NodeFinder/TypeAwareNodeFinder.php
@@ -9,10 +9,7 @@ use PhpParser\NodeFinder;
 
 /**
  * @api
-<<<<<<< HEAD
  * @todo remove after https://github.com/nikic/PHP-Parser/pull/869 is released
-=======
->>>>>>> [PhpConfigPrinter] Remove astral dependency
  */
 final class TypeAwareNodeFinder
 {
