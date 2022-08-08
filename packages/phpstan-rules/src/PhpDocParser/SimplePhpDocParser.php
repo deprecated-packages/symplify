98611d34162ea3f4e6186a567fad7035fb2698f2
Tomas Votruba <tomas.vot@gmail.com>
2022-08-08 16:23:02 +0200

    use PhpDocNode, not needed

--- packages/phpstan-rules/src/PhpDocParser/SimplePhpDocParser.php
+++ packages/phpstan-rules/src/PhpDocParser/SimplePhpDocParser.php
@@ -6,10 +6,10 @@ namespace Symplify\PHPStanRules\PhpDocParser;
 
 use PhpParser\Comment\Doc;
 use PhpParser\Node;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
 use PHPStan\PhpDocParser\Lexer\Lexer;
 use PHPStan\PhpDocParser\Parser\PhpDocParser;
 use PHPStan\PhpDocParser\Parser\TokenIterator;
-use Symplify\Astral\PhpDocParser\ValueObject\Ast\PhpDoc\SimplePhpDocNode;
 
 /**
  * @see \Symplify\Astral\Tests\PhpDocParser\SimplePhpDocParser\SimplePhpDocParserTest
@@ -22,7 +22,7 @@ final class SimplePhpDocParser
     ) {
     }
 
-    public function parseNode(Node $node): ?SimplePhpDocNode
+    public function parseNode(Node $node): ?PhpDocNode
     {
         $docComment = $node->getDocComment();
         if (! $docComment instanceof Doc) {
@@ -32,12 +32,12 @@ final class SimplePhpDocParser
         return $this->parseDocBlock($docComment->getText());
     }
 
-    public function parseDocBlock(string $docBlock): SimplePhpDocNode
+    public function parseDocBlock(string $docBlock): PhpDocNode
     {
         $tokens = $this->lexer->tokenize($docBlock);
         $tokenIterator = new TokenIterator($tokens);
 
         $phpDocNode = $this->phpDocParser->parse($tokenIterator);
-        return new SimplePhpDocNode($phpDocNode->children);
+        return new PhpDocNode($phpDocNode->children);
     }
 }
