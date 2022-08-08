4a87e7191966f957e13a6bc9dc41c44a70cb4c75
Tomas Votruba <tomas.vot@gmail.com>
2022-08-08 21:34:56 +0200

    fix test stan

--- packages/phpstan-rules/build/target-repository/src-for-tests/SomeClass.php
+++ packages/phpstan-rules/build/target-repository/src-for-tests/SomeClass.php
@@ -4,5 +4,10 @@ declare(strict_types=1);
 
 final class SomeClass
 {
+    private string $name = 'not empty class';
 
+    public function getName(): string
+    {
+        return $this->name;
+    }
 }
