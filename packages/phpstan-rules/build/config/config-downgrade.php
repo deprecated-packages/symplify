a70e8a4b9c936d6deea221863bba934b42223350
Tomas Votruba <tomas.vot@gmail.com>
2022-08-08 20:19:23 +0200

    Remove documented rule config

--- packages/phpstan-rules/build/config/config-downgrade.php
+++ packages/phpstan-rules/build/config/config-downgrade.php
@@ -3,12 +3,17 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Removing\Rector\Class_\RemoveInterfacesRector;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->parallel();
 
     $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
+
+    $rectorConfig->ruleWithConfiguration(RemoveInterfacesRector::class, [
+        'Symplify\RuleDocGenerator\Contract\DocumentedRuleInterface',
+    ]);
 
     $rectorConfig->skip(['*/Tests/*', '*/tests/*', __DIR__ . '/../../tests']);
 };
