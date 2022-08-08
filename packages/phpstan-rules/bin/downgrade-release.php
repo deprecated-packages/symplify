aba517cdd7ccf57f0aedc552451f187722ed2863
Tomas Votruba <tomas.vot@gmail.com>
2022-08-08 19:31:46 +0200

    keep tests, do not push vendor

--- packages/phpstan-rules/bin/downgrade-release.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require __DIR__ . '/../vendor/autoload.php';
-
-// 1. @todo release repository with PHP 7.2 in composer.json
-// 2. downgrade the code to 7.2 B-)
