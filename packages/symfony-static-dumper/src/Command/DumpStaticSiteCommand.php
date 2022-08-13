17145949f076d2be398179448b65cc939f820570
Tomas Votruba <tomas.vot@gmail.com>
2022-08-13 16:00:40 +0200

    [SymfonyStaticDumper] Add --public-directory and --output-directory to allow customize the io directories

--- packages/symfony-static-dumper/src/Command/DumpStaticSiteCommand.php
+++ packages/symfony-static-dumper/src/Command/DumpStaticSiteCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symplify\SymfonyStaticDumper\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
@@ -12,32 +13,47 @@ use Symplify\SymfonyStaticDumper\Application\SymfonyStaticDumperApplication;
 
 final class DumpStaticSiteCommand extends AbstractSymplifyCommand
 {
-    private string $publicDirectory;
-
-    private string $outputDirectory;
+    private string $projectDir;
 
     public function __construct(
         private SymfonyStaticDumperApplication $symfonyStaticDumperApplication,
         ParameterBagInterface $parameterBag
     ) {
+        $this->projectDir = (string) $parameterBag->get('kernel.project_dir');
+
         parent::__construct();
-
-        $projectDir = (string) $parameterBag->get('kernel.project_dir');
-
-        $this->publicDirectory = $projectDir . '/public';
-        $this->outputDirectory = getcwd() . '/output';
     }
 
     protected function configure(): void
     {
         $this->setName('dump-static-site');
         $this->setDescription('Dump website to static HTML and CSS in the output directory');
+
+        // Adding arguments options for the main command
+        $this->addOption(
+            'public-directory',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Define the input public directory',
+            $this->projectDir . '/public'
+        );
+
+        $this->addOption(
+            'output-directory',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Define the output directory for generated static content',
+            getcwd() . '/output'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->symfonyStyle->section('Dumping static website');
-        $this->symfonyStaticDumperApplication->run($this->publicDirectory, $this->outputDirectory);
+
+        $publicDirectory = $input->getOption('public-directory');
+        $outputDirectory = $input->getOption('output-directory');
+        $this->symfonyStaticDumperApplication->run($publicDirectory, $outputDirectory);
 
         $this->symfonyStyle->note('Run local server to see the output: "php -S localhost:8001 -t output"');
 
