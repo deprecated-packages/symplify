79d43c047549c3685a2580507c04968e1d7e84f4
Tomas Votruba <tomas.vot@gmail.com>
2022-08-14 11:42:12 +0200

    [ConfigTransformer] Split controller + method shortcut (#4342)

---
+++ packages/php-config-printer/src/ValueObject/Routing/RouteDefaults.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PhpConfigPrinter\ValueObject\Routing;
+
+final class RouteDefaults
+{
+    /**
+     * @var string
+     */
+    public const CONTROLLER = '_controller';
+}
