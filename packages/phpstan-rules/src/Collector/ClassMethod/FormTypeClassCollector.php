ecabc0b702ae5c857caf56acdd345d03b68fb906
Tomas Votruba <tomas.vot@gmail.com>
2022-08-16 21:35:20 +0200

    [PHPStanRules] Include the setDeault() in NoConstructorSymfonyFormObjectRule

--- packages/phpstan-rules/src/Collector/ClassMethod/FormTypeClassCollector.php
+++ packages/phpstan-rules/src/Collector/ClassMethod/FormTypeClassCollector.php
@@ -6,6 +6,8 @@ namespace Symplify\PHPStanRules\Collector\ClassMethod;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
 use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\NodeFinder;
@@ -34,17 +36,44 @@ final class FormTypeClassCollector implements Collector
      */
     public function processNode(Node $node, Scope $scope): ?array
     {
-        if ($node->isMagic()) {
-            return null;
-        }
-
         $methodName = $node->name->toString();
         if ($methodName !== 'configureOptions') {
             return null;
         }
 
+        $stmts = $node->stmts;
+        if (! is_array($stmts)) {
+            return null;
+        }
+
+        /** @var MethodCall[] $methodCalls */
+        $methodCalls = $this->nodeFinder->findInstanceOf($stmts, MethodCall::class);
+
+        foreach ($methodCalls as $methodCall) {
+            if (! $methodCall->name instanceof Identifier) {
+                continue;
+            }
+
+            $methodCallName = $methodCall->name->toString();
+            if ($methodCallName === 'setDefaults') {
+                return $this->resolveDataClassFromSetDefaults($methodCall, $scope);
+            }
+
+            if ($methodCallName === 'setDefault') {
+                return $this->resolveDataClassFromSetDefault($methodCall, $scope);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @return string[]|null
+     */
+    private function resolveDataClassFromSetDefaults(MethodCall $methodCall, Scope $scope): ?array
+    {
         /** @var ArrayItem[] $arrayItems */
-        $arrayItems = $this->nodeFinder->findInstanceOf((array) $node->stmts, ArrayItem::class);
+        $arrayItems = $this->nodeFinder->findInstanceOf($methodCall->args, ArrayItem::class);
 
         foreach ($arrayItems as $arrayItem) {
             if (! $arrayItem->key instanceof String_) {
@@ -65,5 +94,33 @@ final class FormTypeClassCollector implements Collector
         }
 
         return null;
+    }
+
+    /**
+     * @return string[]|null
+     */
+    private function resolveDataClassFromSetDefault(MethodCall $methodCall, Scope $scope): ?array
+    {
+        $args = $methodCall->getArgs();
+        if (count($args) !== 2) {
+            return null;
+        }
+
+        $firstArgValue = $args[0]->value;
+        if (! $firstArgValue instanceof String_) {
+            return null;
+        }
+
+        if ($firstArgValue->value !== 'data_class') {
+            return null;
+        }
+
+        $secondArgValue = $args[1]->value;
+        $secondArgType = $scope->getType($secondArgValue);
+        if (! $secondArgType instanceof ConstantStringType) {
+            return null;
+        }
+
+        return [$secondArgType->getValue()];
     }
 }
