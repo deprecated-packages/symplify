0292d9b3c31d396aaad6b243ba1d2259b8cb5d93
Tomas Votruba <tomas.vot@gmail.com>
2022-08-18 11:48:25 +0200

    [EasyCI] Remove check-latte-template, not much used and outsource the maintenance to community

--- packages/easy-ci/src/Command/CheckTwigTemplateCommand.php
+++ packages/easy-ci/src/Command/CheckTwigTemplateCommand.php
@@ -30,7 +30,7 @@ final class CheckTwigTemplateCommand extends AbstractSymplifyCommand
             InputArgument::REQUIRED | InputArgument::IS_ARRAY,
             'One or more paths with templates'
         );
-        $this->setDescription('Analyze missing classes, constant and static calls in Latte templates');
+        $this->setDescription('Analyze missing classes, constant and static calls in Twig templates');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
