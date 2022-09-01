8047321d8686d7f8fac291726e2d2054d3af2f52
Tomas Votruba <tomas.vot@gmail.com>
2022-09-01 17:43:50 +0200

    remove direct symplify/smart-file-system, still transitional

--- packages/package-builder/tests/config/test_config.php
+++ packages/package-builder/tests/config/test_config.php
@@ -8,6 +8,8 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
+
     $services->set(ParameterProvider::class)
-        ->args([service('service_container')]);
+        ->args([service('service_container')])
+        ->public();
 };
