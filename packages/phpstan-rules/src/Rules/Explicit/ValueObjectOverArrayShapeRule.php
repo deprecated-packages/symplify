20cc59997eb8d6736217e65731a573686729fc74
Tomas Votruba <tomas.vot@gmail.com>
2022-09-07 16:34:22 +0200

    [PHPStanRules] Skip ValueObjectOverArrayShapeRule for Iterator (#4394)

--- packages/phpstan-rules/src/Rules/Explicit/ValueObjectOverArrayShapeRule.php
+++ packages/phpstan-rules/src/Rules/Explicit/ValueObjectOverArrayShapeRule.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\Rules\Explicit;
 
+use Iterator;
 use JsonSerializable;
 use Nette\Utils\Strings;
 use PhpParser\Comment\Doc;
 use PhpParser\Node;
 use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
 use PHPStan\Analyser\Scope;
@@ -96,6 +98,10 @@ CODE_SAMPLE
             return [];
         }
 
+        if ($this->isIteratorReturnType($node)) {
+            return [];
+        }
+
         $match = Strings::match($docComment->getText(), self::ARRAY_SHAPE_REGEX);
         if ($match === null) {
             return [];
@@ -125,5 +131,16 @@ CODE_SAMPLE
         }
 
         return $classReflection->implementsInterface(JsonSerializable::class);
+    }
+
+    private function isIteratorReturnType(ClassMethod|Function_ $node): bool
+    {
+        $returnType = $node->returnType;
+        if (! $returnType instanceof FullyQualified) {
+            return false;
+        }
+
+        $returnTypeClass = $returnType->toString();
+        return $returnTypeClass === Iterator::class;
     }
 }
