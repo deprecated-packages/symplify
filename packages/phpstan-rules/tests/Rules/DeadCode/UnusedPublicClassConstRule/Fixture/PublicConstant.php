fb8ce4c0098687ea70f5515a8e0ce39bdf4f3e7d
Markus Staab <maggus.staab@googlemail.com>
2022-09-25 16:38:04 +0200

    fix tests

--- packages/phpstan-rules/tests/Rules/DeadCode/UnusedPublicClassConstRule/Fixture/PublicConstant.php
+++ packages/phpstan-rules/tests/Rules/DeadCode/UnusedPublicClassConstRule/Fixture/PublicConstant.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\Tests\Rules\DeadCode\UnusedPublicClassConstRule\Fixture;
 
-final class SkipUsedConstantInSubclass extends PublicConstant
+class PublicConstant
 {
+    public const USED_FROM_SUBCLASS = 'yes, please';
 }
