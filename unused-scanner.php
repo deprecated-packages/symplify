07c809a867eb7353c2598f4a05e9b97347f4801c
Tomas Votruba <tomas.vot@gmail.com>
2022-10-03 19:06:54 +0200

    cs

--- unused-scanner.php
+++ unused-scanner.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 // @see https://cylab.be/blog/53/detect-unused-composer-dependencies?accept-cookies=1
+// @see https://github.com/Insolita/unused-scanner/blob/master/scanner_config.example.php
 
 return [
     'composerJsonPath' => __DIR__ . '/composer.json',
