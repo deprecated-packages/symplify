89c6759961cde7d24e23076aa3f0d2253216fe4a
Markus Staab <maggus.staab@googlemail.com>
2022-10-03 19:27:33 +0200

    Update ConstantUser.php

--- packages/phpstan-rules/tests/Rules/DeadCode/UnusedPublicClassConstRule/Source/ConstantUser.php
+++ packages/phpstan-rules/tests/Rules/DeadCode/UnusedPublicClassConstRule/Source/ConstantUser.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Symplify\PHPStanRules\Tests\Rules\DeadCode\UnusedPublicClassConstRule\Source;
 
 use Symplify\PHPStanRules\Tests\Rules\DeadCode\UnusedPublicClassConstRule\Fixture\SkipUsedPublicConstant;
-use Symplify\PHPStanRules\Tests\Rules\DeadCode\UnusedPublicClassConstRule\Fixture\SkipUsedPublicConstantInSubclass;
 
 final class ConstantUser
 {
