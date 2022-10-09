0ae952903f7b7b633709d6fe7b9aa94cc3f5d1f6
Tomas Votruba <tomas.vot@gmail.com>
2022-10-09 10:24:36 +0200

    bump phpstan versions

--- ecs.php
+++ ecs.php
@@ -28,7 +28,7 @@ return static function (ECSConfig $ecsConfig): void {
         __DIR__ . '/ecs.php',
         __DIR__ . '/monorepo-builder.php',
         __DIR__ . '/rector.php',
-        __DIR__ . '/unused-scanner.php',
+        __DIR__ . '/composer-unused.php',
     ]);
 
     $ecsConfig->skip([
