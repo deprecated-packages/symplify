79c1ca716b283fb89f92edef0368f2758498b897
Tomas Votruba <tomas.vot@gmail.com>
2022-12-13 17:43:37 +0100

    [PHPStanRules] Add UnusedPublicStaticPropertyRule (#4502)
    * [PHPStanRules] Add nusedPublicStaticPropertyRule
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ packages/phpstan-rules/tests/Rules/DeadCode/UnusedPublicStaticPropertyRule/Fixture/SkipNonStaticProperty.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\DeadCode\UnusedPublicStaticPropertyRule\Fixture;
+
+final class SkipNonStaticProperty
+{
+    public $somePublicStaticProperty;
+}
