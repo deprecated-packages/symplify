315d24ab109fb7d4c8897ccab29bd4779364a8cc
GitHub Action <action@github.com>
2022-12-23 13:10:28 +0000

    [ci-review] Rector Rectify

--- packages/phpstan-rules/src/Exception/DeprecatedException.php
+++ packages/phpstan-rules/src/Exception/DeprecatedException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\PHPStanRules\Exception;
 
-class DeprecatedException extends \Exception
+use Exception;
+final class DeprecatedException extends Exception
 {
 }
