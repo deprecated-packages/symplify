43e5adfa99aaa019ca12e0487d31d0ab03511dcf
Tomas Votruba <tomas.vot@gmail.com>
2022-12-23 02:26:45 +0100

    setup unused public

--- packages/phpstan-rules/src/NodeFinder/TypeAwareNodeFinder.php
+++ packages/phpstan-rules/src/NodeFinder/TypeAwareNodeFinder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symplify\PHPStanRules\NodeFinder;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\NodeFinder;
 
 /**
@@ -32,12 +33,11 @@ final class TypeAwareNodeFinder
     /**
      * @template TNode as Node
      *
-     * @param Node[]|Node $nodes
      * @param class-string<TNode> $type
      * @return TNode[]
      */
-    public function findInstanceOf(array|Node $nodes, string $type): array
+    public function findInstanceOf(Expr $expr, string $type): array
     {
-        return $this->nodeFinder->findInstanceOf($nodes, $type);
+        return $this->nodeFinder->findInstanceOf($expr, $type);
     }
 }
