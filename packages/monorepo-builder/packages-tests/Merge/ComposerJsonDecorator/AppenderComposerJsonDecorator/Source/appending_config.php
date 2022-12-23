b3a3105fd6955c318da2cff5823f48d46140a3c2
Tomas Votruba <tomas.vot@gmail.com>
2022-12-23 13:58:35 +0100

    [MonorepoBuilder] Fix old config leftovers [closes #4405] (#4514)

--- packages/monorepo-builder/packages-tests/Merge/ComposerJsonDecorator/AppenderComposerJsonDecorator/Source/appending_config.php
+++ packages/monorepo-builder/packages-tests/Merge/ComposerJsonDecorator/AppenderComposerJsonDecorator/Source/appending_config.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::DATA_TO_APPEND, [
+return static function (MBConfig $mbConfig): void {
+    $mbConfig->dataToAppend([
         ComposerJsonSection::REQUIRE_DEV => [
             'phpstan/phpstan' => '^0.9',
             'tracy/tracy' => '^2.4',
