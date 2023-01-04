f9d2f8e3cc59d859cd74c23fe0a4eaa9c0e18a4b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-04 19:22:53 +0700

    Remove monorepo-builder from packages/ as moving

--- packages/monorepo-builder/templates/monorepo/monorepo-builder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Symplify\MonorepoBuilder\Config\MBConfig;
-
-return static function (MBConfig $mbConfig): void {
-    $mbConfig->packageDirectories([__DIR__ . '/packages']);
-};
