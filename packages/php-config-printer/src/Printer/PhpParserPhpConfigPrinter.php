543381e96b4da88d087c83551474a12fa5b8ac3a
Tomas Votruba <tomas.vot@gmail.com>
2023-01-30 16:54:41 +0000

    [PhpDocPrinter] Prepare automated array indent (#4549)

--- packages/php-config-printer/src/Printer/PhpParserPhpConfigPrinter.php
+++ packages/php-config-printer/src/Printer/PhpParserPhpConfigPrinter.php
@@ -96,6 +96,22 @@ final class PhpParserPhpConfigPrinter extends Standard
     }
 
     /**
+     * Inspired by https://github.com/rectorphp/rector-src/pull/3271/files
+     *
+     * Always print array items with newlines to make look nice out of the oven.
+     */
+    protected function pExpr_Array(Array_ $array): string
+    {
+        // short always :)
+        $array->setAttribute(self::KIND, Array_::KIND_SHORT);
+
+        $printedArray = '[';
+        $printedArray .= $this->pCommaSeparatedMultiline($array->items, true);
+
+        return $printedArray . ($this->nl . ']');
+    }
+
+    /**
      * Do not preslash all slashes (parent behavior), but only those:
      * - followed by "\"
      * - by "'" - or the end of the string
@@ -105,13 +121,6 @@ final class PhpParserPhpConfigPrinter extends Standard
     protected function pSingleQuotedString(string $string): string
     {
         return "'" . Strings::replace($string, self::QUOTE_SLASH_REGEX, '\\\\$0') . "'";
-    }
-
-    protected function pExpr_Array(Array_ $array): string
-    {
-        $array->setAttribute(self::KIND, Array_::KIND_SHORT);
-
-        return parent::pExpr_Array($array);
     }
 
     protected function pExpr_MethodCall(MethodCall $methodCall): string
