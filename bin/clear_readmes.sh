00643f51f310bea56a5d7d081e61681b666d3d13
Tomas Votruba <tomas.vot@gmail.com>
2023-02-08 15:55:55 +0000

    split

--- bin/clear_readmes.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# trailing whitespaces
-sed -i -E 's/\s+$//g' packages/*/README.md *.md
-
-# remove extra lines
-sed -i -E ':a;$!N;s/^\s*\n\s*$//;ta;P;D' packages/*/README.md *.md
